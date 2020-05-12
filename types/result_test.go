b561a986b0226a20ffaed0235928bd52e58f008f
Yongwoo Lee <whylee@linecorp.com>
2020-05-12 10:06:25 +0900

    feat: add codespace to broadcast response

--- types/result_test.go
+++ types/result_test.go
@@ -1,9 +1,11 @@
 package types
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 )
@@ -26,4 +28,26 @@ func TestABCIMessageLog(t *testing.T) {
 	bz, err := codec.Cdc.MarshalJSON(msgLogs)
 	require.NoError(t, err)
 	require.Equal(t, string(bz), msgLogs.String())
+}
+
+func TestNewResponseFormatBroadcastTx(t *testing.T) {
+	hash, err := hex.DecodeString("00000000000000000000000000000000")
+	require.NoError(t, err)
+	result := ctypes.ResultBroadcastTx{
+		Code:      1,
+		Data:      []byte("some data"),
+		Log:       `[{"log":"","msg_index":1,"success":true}]`,
+		Codespace: "codespace",
+		Hash:      hash,
+	}
+
+	txResponse := NewResponseFormatBroadcastTx(&result)
+
+	require.NoError(t, err)
+	require.Equal(t, result.Code, txResponse.Code)
+	require.Equal(t, result.Data.String(), txResponse.Data)
+	require.NotEmpty(t, txResponse.Logs)
+	require.Equal(t, result.Log, txResponse.RawLog)
+	require.Equal(t, result.Codespace, txResponse.Codespace)
+	require.Equal(t, result.Hash.String(), txResponse.TxHash)
 }
