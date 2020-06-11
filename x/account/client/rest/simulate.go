754ba3e6f551a84a3f391750abaacddb8d63947b
seungyeon-hwang <seungyeon.hwang@linecorp.com>
2020-06-11 13:53:26 +0900

    feat: add tx simulate API (#899)

---
+++ x/account/client/rest/simulate.go
@@ -0,0 +1,65 @@
+package rest
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
+
+	"github.com/line/link/client"
+)
+
+// SimulateReq defines a tx simulating request.
+type SimulateReq struct {
+	Tx            types.StdTx `json:"tx" yaml:"tx"`
+	GasAdjustment string      `json:"gas_adjustment"`
+}
+
+// SimulateTxRequest implements a tx simulating handler that is responsible
+// for simulating a valid and signed tx.
+func SimulateTxRequest(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req SimulateReq
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		err = cliCtx.Codec.UnmarshalJSON(body, &req)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		txBytes, err := cliCtx.Codec.MarshalBinaryLengthPrefixed(req.Tx)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		gasAdj, ok := rest.ParseFloat64OrReturnBadRequest(w, req.GasAdjustment, client.DefaultGasAdjustment)
+		if !ok {
+			// ParseFloat64OrReturnBadRequest has already written error response.
+			return
+		}
+
+		if gasAdj < 0 {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, fmt.Sprintf("invalid gas adjustment: %g", gasAdj))
+			return
+		}
+
+		_, adjusted, err := utils.CalculateGas(cliCtx.QueryWithData, cliCtx.Codec, txBytes, gasAdj)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		rest.WriteSimulationResponse(w, cliCtx.Codec, adjusted)
+	}
+}
