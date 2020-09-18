a36c79115adc31912565e5dc53bb6fcbebae7757
Kim, JinSan <jinsan@linecorp.com>
2020-09-18 13:36:23 +0900

    chore: use `127.0.0.1` instead of `localhost`

--- server/test_helpers.go
+++ server/test_helpers.go
@@ -16,7 +16,7 @@ import (
 // Get a free address for a test tendermint server
 // protocol is either tcp, http, etc
 func FreeTCPAddr() (addr, port string, err error) {
-	l, err := net.Listen("tcp", "localhost:0")
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		return "", "", err
 	}
