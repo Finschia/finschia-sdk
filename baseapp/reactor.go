aaeda6722d62d8e85d44bb68f65a8d88c41a29e9
KIm, JinSan <jinsan@linecorp.com>
2021-01-26 17:47:27 +0900

    feat: concurrent recheckTx (#52)
    * chore: simply implement abci.`CheckTxSync()` and abci.`CheckTxAsync()`
    
    * chore: move `accountLock` from `anteTx()` to `checkTxWithLock()`
    
    * feat: impl `abci.CheckTxAsync()` with reactor
    
    # Conflicts:
    #	baseapp/baseapp.go
    
    * feat: impl `accountwgs`
    
    * feat: impl `accountwgs_test`
    
    * chore: revise code after cherry-pick
    
    * chore: bump-up tendermint & iavl
    
    * chore: rename func from `startCheckTxAsyncReactor()` to `checkTxAsyncReactor()`
    
    * fix: imports for lint
    
    * fix: imports for lint
    
    * chore: rename `Waits()` to `Wait()`
    # Conflicts:
    #	baseapp/abci.go
    #	baseapp/accountlock.go
    #	baseapp/accountwgs_test.go
    #	baseapp/baseapp.go
    #	baseapp/baseapp_test.go
    #	go.mod
    #	go.sum

---
+++ baseapp/reactor.go
@@ -0,0 +1,59 @@
+package baseapp
+
+import (
+	"sync"
+
+	abci "github.com/line/ostracon/abci/types"
+
+	sdk "github.com/line/lbm-sdk/v2/types"
+	sdkerrors "github.com/line/lbm-sdk/v2/types/errors"
+)
+
+func (app *BaseApp) startReactors() {
+	go app.checkTxAsyncReactor()
+}
+
+type RequestCheckTxAsync struct {
+	txBytes  []byte
+	recheck  bool
+	callback abci.CheckTxCallback
+	prepare  *sync.WaitGroup
+	tx       sdk.Tx
+	err      error
+}
+
+func (app *BaseApp) checkTxAsyncReactor() {
+	for req := range app.chCheckTx {
+		req.prepare.Wait()
+		if req.err != nil {
+			req.callback(sdkerrors.ResponseCheckTx(req.err, 0, 0, app.trace))
+			continue
+		}
+
+		waits, signals := app.checkAccountWGs.Register(req.tx)
+
+		go app.checkTxAsync(req, waits, signals)
+	}
+}
+
+func (app *BaseApp) prepareCheckTx(req *RequestCheckTxAsync) {
+	defer req.prepare.Done()
+	req.tx, req.err = app.preCheckTx(req.txBytes)
+}
+
+func (app *BaseApp) checkTxAsync(req *RequestCheckTxAsync, waits []*sync.WaitGroup, signals []*AccountWG) {
+	app.checkAccountWGs.Wait(waits)
+	defer app.checkAccountWGs.Done(signals)
+
+	gInfo, err := app.checkTx(req.txBytes, req.tx, req.recheck)
+
+	if err != nil {
+		req.callback(sdkerrors.ResponseCheckTx(err, gInfo.GasWanted, gInfo.GasUsed, app.trace))
+		return
+	}
+
+	req.callback(abci.ResponseCheckTx{
+		GasWanted: int64(gInfo.GasWanted), // TODO: Should type accept unsigned ints?
+		GasUsed:   int64(gInfo.GasUsed),   // TODO: Should type accept unsigned ints?
+	})
+}
