e682f34182c8f8297d03aec442a51871c2e78b6a
kokeshiM0chi <52264064+kokeshiM0chi@users.noreply.github.com>
2021-02-15 13:54:54 +0900

    test: add test whether pubkey is generated with composite type or not (#75)

--- x/genutil/client/cli/init_test.go
+++ x/genutil/client/cli/init_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io"
 	"os"
+	"reflect"
 	"testing"
 	"time"
 
@@ -11,6 +12,8 @@ import (
 	"github.com/stretchr/testify/require"
 	abciServer "github.com/tendermint/tendermint/abci/server"
 	tcmd "github.com/tendermint/tendermint/cmd/tendermint/commands"
+	"github.com/tendermint/tendermint/crypto/composite"
+	"github.com/tendermint/tendermint/crypto/ed25519"
 	"github.com/tendermint/tendermint/libs/cli"
 	"github.com/tendermint/tendermint/libs/log"
 
@@ -132,7 +135,8 @@ func TestInitNodeValidatorFiles(t *testing.T) {
 	nodeID, valPubKey, err := genutil.InitializeNodeValidatorFiles(cfg)
 	require.Nil(t, err)
 	require.NotEqual(t, "", nodeID)
-	require.NotEqual(t, 0, len(valPubKey.Bytes()))
+	require.Equal(t, 37, len(valPubKey.Bytes()))
+	require.EqualValues(t, reflect.TypeOf(ed25519.PubKeyEd25519{}), reflect.TypeOf(valPubKey))
 }
 
 func TestInitNodeValidatorFilesWithComposite(t *testing.T) {
@@ -147,7 +151,8 @@ func TestInitNodeValidatorFilesWithComposite(t *testing.T) {
 	nodeID, valPubKey, err := genutil.InitializeNodeValidatorFiles(cfg)
 	require.Nil(t, err)
 	require.NotEqual(t, "", nodeID)
-	require.NotEqual(t, 0, len(valPubKey.Bytes()))
+	require.Equal(t, 90, len(valPubKey.Bytes()))
+	require.EqualValues(t, reflect.TypeOf(composite.PubKeyComposite{}), reflect.TypeOf(valPubKey))
 }
 
 // custom tx codec
