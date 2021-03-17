48d7061aac1f9c90f317e32b46517c802b66c7d5
KIm, JinSan <jinsan@linecorp.com>
2021-03-17 13:35:58 +0900

    chore: cherry-pick cosmos-sdk#8857 (#92)
    * Fix SendToModuleAccountTest
    * https://github.com/cosmos/cosmos-sdk/pull/8857
    * https://github.com/cosmos/cosmos-sdk/commit/280ee4f15e46117a5061ab3a374d9b695ed082bf
    
    Co-authored-by: Jonathan Gimeno <jgimeno@gmail.com>

--- x/bank/handler_test.go
+++ x/bank/handler_test.go
@@ -1,4 +1,4 @@
-package bank
+package bank_test
 
 import (
 	"strings"
@@ -7,13 +7,20 @@ import (
 	"github.com/stretchr/testify/require"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
+	"github.com/line/lbm-sdk/v2/crypto/keys/secp256k1"
+	"github.com/line/lbm-sdk/v2/simapp"
 	"github.com/line/lbm-sdk/v2/testutil/testdata"
 	sdk "github.com/line/lbm-sdk/v2/types"
 	sdkerrors "github.com/line/lbm-sdk/v2/types/errors"
+	authtypes "github.com/line/lbm-sdk/v2/x/auth/types"
+	"github.com/line/lbm-sdk/v2/x/bank"
+	bankkeeper "github.com/line/lbm-sdk/v2/x/bank/keeper"
+	"github.com/line/lbm-sdk/v2/x/bank/types"
+	stakingtypes "github.com/line/lbm-sdk/v2/x/staking/types"
 )
 
 func TestInvalidMsg(t *testing.T) {
-	h := NewHandler(nil)
+	h := bank.NewHandler(nil)
 
 	res, err := h(sdk.NewContext(nil, tmproto.Header{}, false, nil), testdata.NewTestMsg())
 	require.Error(t, err)
@@ -21,4 +28,61 @@ func TestInvalidMsg(t *testing.T) {
 
 	_, _, log := sdkerrors.ABCIInfo(err, false)
 	require.True(t, strings.Contains(log, "unrecognized bank message type"))
+}
+
+// A module account cannot be the recipient of bank sends unless it has been marked as such
+func TestSendToModuleAccount(t *testing.T) {
+	priv1 := secp256k1.GenPrivKey()
+	addr1 := sdk.AccAddress(priv1.PubKey().Address())
+	moduleAccAddr := authtypes.NewModuleAddress(stakingtypes.BondedPoolName)
+	coins := sdk.Coins{sdk.NewInt64Coin("foocoin", 10)}
+
+	tests := []struct {
+		name          string
+		expectedError error
+		msg           *types.MsgSend
+	}{
+		{
+			name:          "not allowed module account",
+			msg:           types.NewMsgSend(addr1, moduleAccAddr, coins),
+			expectedError: sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "%s is not allowed to receive funds", moduleAccAddr),
+		},
+		{
+			name:          "allowed module account",
+			msg:           types.NewMsgSend(addr1, authtypes.NewModuleAddress(stakingtypes.ModuleName), coins),
+			expectedError: nil,
+		},
+	}
+
+	acc1 := &authtypes.BaseAccount{
+		Address: addr1.String(),
+	}
+	accs := authtypes.GenesisAccounts{acc1}
+	balances := []types.Balance{
+		{
+			Address: addr1.String(),
+			Coins:   coins,
+		},
+	}
+
+	app := simapp.SetupWithGenesisAccounts(accs, balances...)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	app.BankKeeper = bankkeeper.NewBaseKeeper(
+		app.AppCodec(), app.GetKey(types.StoreKey), app.AccountKeeper, app.GetSubspace(types.ModuleName), map[string]bool{
+			moduleAccAddr.String(): true,
+		},
+	)
+	handler := bank.NewHandler(app.BankKeeper)
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := handler(ctx, tc.msg)
+			if tc.expectedError != nil {
+				require.EqualError(t, err, tc.expectedError.Error())
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
 }
