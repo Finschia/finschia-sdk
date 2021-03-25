d127e0ab6740058c2ecbef7de7ec77d9a7041aa6
Jonathan Gimeno <jgimeno@gmail.com>
2021-03-25 11:03:13 +0100

    Improve set supply (#8950)
    * temp commit
    
    * remove set supply
    
    * fix supply
    
    * remove keys
    
    * improve supply set
    
    * update changelog
    
    * improve linter
    
    * update setSupply to get only one coin
    
    * update genesis
    
    * remove dirt
    
    * save only supply
    
    * go fmt
    
    * update rosetta test bootstrap
    
    Co-authored-by: Alessio Treglia <alessio@tendermint.com>
    Co-authored-by: Frojdi Dymylja <33157909+fdymylja@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- x/bank/keeper/genesis.go
+++ x/bank/keeper/genesis.go
@@ -26,7 +26,9 @@ func (k BaseKeeper) InitGenesis(ctx sdk.Context, genState *types.GenesisState) {
 		panic(fmt.Errorf("genesis supply is incorrect, expected %v, got %v", genState.Supply, totalSupply))
 	}
 
-	k.setSupply(ctx, totalSupply)
+	for _, supply := range totalSupply {
+		k.setSupply(ctx, supply)
+	}
 
 	for _, meta := range genState.DenomMetadata {
 		k.SetDenomMetaData(ctx, meta)
