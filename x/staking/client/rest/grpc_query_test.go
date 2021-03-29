0c4444ed97a55a3113ce1d54f8db6f522f0605fe
MD Aleem <72057206+aleem1314@users.noreply.github.com>
2021-03-29 19:12:43 +0530

    fix: grpc-gateway error codes (#9015)
    * update grpc query handler
    
    * try fix http response error codes
    
    * add test
    
    * add changelog
    
    Co-authored-by: Alessio Treglia <alessio@tendermint.com>
    Co-authored-by: Amaury <1293565+amaurym@users.noreply.github.com>

--- x/staking/client/rest/grpc_query_test.go
+++ x/staking/client/rest/grpc_query_test.go
@@ -9,11 +9,10 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/line/lbm-sdk/client/flags"
-
 	"github.com/gogo/protobuf/proto"
 	"github.com/stretchr/testify/suite"
-
+	
+	"github.com/line/lbm-sdk/client/flags"
 	"github.com/line/lbm-sdk/crypto/hd"
 	"github.com/line/lbm-sdk/crypto/keyring"
 	"github.com/line/lbm-sdk/testutil"
@@ -422,10 +421,17 @@ func (s *IntegrationTestSuite) TestQueryDelegationsResponseCode() {
 	// Create new account in the keyring.
 	info, _, err := val.ClientCtx.Keyring.NewMnemonic("test", keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
 	s.Require().NoError(err)
+<<<<<<< HEAD
 	newAddr := sdk.BytesToAccAddress(info.GetPubKey().Address())
 
 	s.T().Log("expect 404 error for address without delegations")
 	res, statusCode, err := getRequest(fmt.Sprintf("%s/lbm/staking/v1/delegations/%s", val.APIAddress, newAddr.String()))
+=======
+	newAddr := sdk.AccAddress(info.GetPubKey().Address())
+
+	s.T().Log("expect 404 error for address without delegations")
+	res, statusCode, err := getRequest(fmt.Sprintf("%s/cosmos/staking/v1beta1/delegations/%s", val.APIAddress, newAddr.String()))
+>>>>>>> 636e6598b (fix: grpc-gateway error codes (#9015))
 	s.Require().NoError(err)
 	s.Require().Contains(string(res), "\"code\": 5")
 	s.Require().Equal(404, statusCode)
