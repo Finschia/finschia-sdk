f6912b40f0145ddb58157d58985fd2a7878bdace
Jiyong Ha <jiyong.ha@linecorp.com>
2021-04-26 13:41:23 +0900

    test(wasm): adjust gas cost in test

--- x/wasm/internal/keeper/keeper_test.go
+++ x/wasm/internal/keeper/keeper_test.go
@@ -284,7 +284,7 @@ func TestInstantiate(t *testing.T) {
 
 	gasAfter := ctx.GasMeter().GasConsumed()
 	if types.EnableGasVerification {
-		require.Equal(t, uint64(0x11c4e), gasAfter-gasBefore)
+		require.Equal(t, uint64(0x11b54), gasAfter-gasBefore)
 	}
 
 	// ensure it is stored properly
@@ -517,7 +517,7 @@ func TestExecute(t *testing.T) {
 	// make sure gas is properly deducted from ctx
 	gasAfter := ctx.GasMeter().GasConsumed()
 	if types.EnableGasVerification {
-		require.Equal(t, uint64(0x128ee), gasAfter-gasBefore)
+		require.Equal(t, uint64(0x1288a), gasAfter-gasBefore)
 	}
 	// ensure bob now exists and got both payments released
 	bobAcct = accKeeper.GetAccount(ctx, bob)
