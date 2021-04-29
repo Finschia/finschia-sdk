3cec30a858e3928bd03dd4652a6ac4ecf06ea727
Woosang Son <woosang.son@linecorp.com>
2021-04-29 17:49:43 +0900

    chore: remove link-modules

--- x/account/internal/types/errors.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-)
-
-var (
-	ErrAccountAlreadyExist = sdkerrors.Register(ModuleName, 1, "Target account already exists")
-)
