20463781724324d37e01dc2237ff06e5dcb3a9f8
loloicci <loloicci@linecorp.com>
2021-04-30 18:08:04 +0900

    feat: add message for storing and Instantiating (#154)
    * feat: add the message MsgStoreCodeAndInstantiateContract
    
    * feat: Add handler for MsgStoreCodeAndInstantiateContract
    
    * feat: add cli command
    
    * feat: add rest endpoint
    
    * chore: go fmt
    
    * chore: execute go mod tidy
    
    * feat: respond review

--- x/wasm/client/rest/tx.go
+++ x/wasm/client/rest/tx.go
@@ -16,6 +16,7 @@ import (
 func registerTxRoutes(cliCtx client.Context, r *mux.Router) {
 	r.HandleFunc("/wasm/code", storeCodeHandlerFn(cliCtx)).Methods("POST")
 	r.HandleFunc("/wasm/code/{codeId}", instantiateContractHandlerFn(cliCtx)).Methods("POST")
+	r.HandleFunc("/wasm/codeinit", storeCodeAndInstantiateContractHandlerFn(cliCtx)).Methods("POST")
 	r.HandleFunc("/wasm/contract/{contractAddr}", executeContractHandlerFn(cliCtx)).Methods("POST")
 }
 
@@ -30,6 +31,15 @@ type instantiateContractReq struct {
 	Deposit sdk.Coins    `json:"deposit" yaml:"deposit"`
 	Admin   string       `json:"admin,omitempty" yaml:"admin"`
 	InitMsg []byte       `json:"init_msg" yaml:"init_msg"`
+}
+
+type storeCodeAndInstantiateContractReq struct {
+	BaseReq   rest.BaseReq `json:"base_req" yaml:"base_req"`
+	WasmBytes []byte       `json:"wasm_bytes"`
+	Label     string       `json:"label" yaml:"label"`
+	Deposit   sdk.Coins    `json:"deposit" yaml:"deposit"`
+	Admin     string       `json:"admin,omitempty" yaml:"admin"`
+	InitMsg   []byte       `json:"init_msg" yaml:"init_msg"`
 }
 
 type executeContractReq struct {
@@ -107,6 +117,52 @@ func instantiateContractHandlerFn(cliCtx client.Context) http.HandlerFunc {
 			Funds:   req.Deposit,
 			InitMsg: req.InitMsg,
 			Admin:   req.Admin,
+		}
+
+		if err := msg.ValidateBasic(); err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		tx.WriteGeneratedTxResponse(cliCtx, w, req.BaseReq, &msg)
+	}
+}
+
+func storeCodeAndInstantiateContractHandlerFn(cliCtx client.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req storeCodeAndInstantiateContractReq
+		if !rest.ReadRESTReq(w, r, cliCtx.LegacyAmino, &req) {
+			return
+		}
+
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+
+		var err error
+		wasm := req.WasmBytes
+
+		// gzip the wasm file
+		if wasmUtils.IsWasm(wasm) {
+			wasm, err = wasmUtils.GzipIt(wasm)
+			if err != nil {
+				rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+				return
+			}
+		} else if !wasmUtils.IsGzip(wasm) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "Invalid input file, use wasm binary or zip")
+			return
+		}
+
+		// build and sign the transaction, then broadcast to Tendermint
+		msg := types.MsgStoreCodeAndInstantiateContract{
+			Sender:       req.BaseReq.From,
+			WASMByteCode: wasm,
+			Label:        req.Label,
+			Funds:        req.Deposit,
+			InitMsg:      req.InitMsg,
+			Admin:        req.Admin,
 		}
 
 		if err := msg.ValidateBasic(); err != nil {
