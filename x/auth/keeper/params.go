95adf6479143c59ebf0053dd38c39e446c226403
Woosang Son <woosang.son@linecorp.com>
2021-05-03 10:44:43 +0900

    chore: param caching

--- x/auth/keeper/params.go
+++ x/auth/keeper/params.go
@@ -5,13 +5,23 @@ import (
 	"github.com/line/lbm-sdk/v2/x/auth/types"
 )
 
+var param types.Params
+var paramCached bool = false
+
 // SetParams sets the auth module's parameters.
 func (ak AccountKeeper) SetParams(ctx sdk.Context, params types.Params) {
+	param = params
 	ak.paramSubspace.SetParamSet(ctx, &params)
+	paramCached = true
 }
 
 // GetParams gets the auth module's parameters.
 func (ak AccountKeeper) GetParams(ctx sdk.Context) (params types.Params) {
-	ak.paramSubspace.GetParamSet(ctx, &params)
+	if !paramCached {
+		ak.paramSubspace.GetParamSet(ctx, &params)
+		param = params
+		paramCached = true
+	}
+	params = param
 	return
 }
