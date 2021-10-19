066a747e51eaf324a89eb17255d36e6603cfa3fc
Steve H. Jung <iproudhon@gmail.com>
2021-10-19 09:24:42 +0900

    feat: iavl, db & disk stats logging when IAVL_DEV env. var. is set

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -1,17 +1,23 @@
 package baseapp
 
 import (
+	"bufio"
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"io"
 	"os"
+	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
 
 	"github.com/gogo/protobuf/proto"
+	iavltree "github.com/line/iavl/v2"
 	abci "github.com/line/ostracon/abci/types"
+	"github.com/line/ostracon/libs/log"
 	ocproto "github.com/line/ostracon/proto/ostracon/types"
 	"google.golang.org/grpc/codes"
 	grpcstatus "google.golang.org/grpc/status"
@@ -326,6 +332,9 @@ func (app *BaseApp) Commit() (res abci.ResponseCommit) {
 	app.deliverState.ms.Write()
 	commitID := app.cms.Commit()
 	app.logger.Info("commit synced", "commit", fmt.Sprintf("%X", commitID))
+
+	// iavl, db & disk stats
+	logIoStats(app.logger)
 
 	// empty/reset the deliver state
 	app.deliverState = nil
@@ -900,4 +909,101 @@ func splitPath(requestPath string) (path []string) {
 	}
 
 	return path
+}
+
+// log iavl, db & disk stats if IAVL_DEV env. var. is set
+var (
+	logIoDev                                                     string
+	tStats                                                       time.Time
+	iavlSetCount, iavlSetBytes, iavlDelCount                     uint64
+	diskReadCount, diskReadBytes, diskWriteCount, diskWriteBytes uint64
+)
+
+// read disk io stats from /proc/diskstats in linux
+func getDeviceIoStats(device string) (readCount, readBytes, writeCount, writeBytes uint64, err error) {
+	inf, err := os.Open("/proc/diskstats")
+	if err != nil {
+		return
+	}
+	defer inf.Close()
+
+	in := bufio.NewReader(inf)
+	r := regexp.MustCompile(`\s+`)
+	for {
+		line, err2 := in.ReadString('\n')
+		if err2 != nil {
+			if err2 == io.EOF {
+				break
+			}
+			err = err2
+			return
+		}
+		parts := r.Split(strings.TrimSpace(line), -1)
+		if len(parts) < 10 {
+			continue
+		}
+		if len(device) > 0 && device != parts[2] {
+			continue
+		} else if len(device) == 0 && parts[1] != "0" {
+			continue
+		}
+
+		rCount, e1 := strconv.ParseUint(parts[3], 10, 64)
+		rSectors, e2 := strconv.ParseUint(parts[5], 10, 64)
+		wCount, e3 := strconv.ParseUint(parts[7], 10, 64)
+		wSectors, e4 := strconv.ParseUint(parts[9], 10, 64)
+		if e1 != nil || e2 != nil || e3 != nil || e4 != nil {
+			continue
+		}
+
+		readCount += rCount
+		readBytes += rSectors * 512
+		writeCount += wCount
+		writeBytes += wSectors * 512
+	}
+	return readCount, readBytes, writeCount, writeBytes, err
+}
+
+func logIoStats(logger log.Logger) {
+	if len(logIoDev) == 0 {
+		// not set yet
+		if dev := os.Getenv("IAVL_DEV"); len(dev) >= 0 {
+			logIoDev = dev
+		} else {
+			logIoDev = " nop"
+			return
+		}
+	} else if logIoDev == " nop" {
+		return
+	}
+
+	now := time.Now()
+	dt := now.Sub(tStats)
+	tStats = now
+
+	newSetCount, newSetBytes, newDelCount := iavltree.GetDBStats()
+	newReadCount, newReadBytes, newWriteCount, newWriteBytes, _ := getDeviceIoStats(logIoDev)
+	diffSetCount, diffSetBytes, diffDelCount := newSetCount-iavlSetCount, newSetBytes-iavlSetBytes, newDelCount-iavlDelCount
+	_, diffReadBytes, _, diffWriteBytes := newReadCount-diskReadCount, newReadBytes-diskReadBytes, newWriteCount-diskWriteCount, newWriteBytes-diskWriteBytes
+	iavlSetCount, iavlSetBytes, iavlDelCount = newSetCount, newSetBytes, newDelCount
+	diskReadCount, diskReadBytes, diskWriteCount, diskWriteBytes = newReadCount, newReadBytes, newWriteCount, newWriteBytes
+
+	// read & write bytes per second
+	rps, wps := int64(0), int64(0)
+	if dt.Milliseconds() > 0 && dt.Seconds() < 1000 {
+		rps = int64(diffReadBytes) * 1000 / dt.Milliseconds()
+		wps = int64(diffWriteBytes) * 1000 / dt.Milliseconds()
+	}
+
+	// write amplification factors
+	wampf := float64(0)
+	if diffSetBytes > 0 {
+		wampf = float64(diffWriteBytes*1000/diffSetBytes) / 1000.0
+	}
+
+	logger.Info("io_stats", "set_count", diffSetCount,
+		"set_bytes", diffSetBytes, "del_count", diffDelCount,
+		"read-bytes", diffReadBytes, "rps", rps,
+		"write_bytes", diffWriteBytes, "wps", wps,
+		"write_amp_f", wampf, "ellapsed", float64(dt.Milliseconds())/1000.0)
 }
