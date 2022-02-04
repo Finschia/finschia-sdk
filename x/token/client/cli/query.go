e0c27720fc9517f935746b5750bd8f21e50e9381
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2022-02-04 05:03:49 +0000

    feat: implement x/token query client

--- x/token/client/cli/query.go
+++ x/token/client/cli/query.go
@@ -1,16 +1,13 @@
 package cli
 
 import (
-	// "fmt"
-	// "strings"
+	"fmt"
 
 	"github.com/spf13/cobra"
 
 	"github.com/line/lbm-sdk/client"
-	// "github.com/line/lbm-sdk/client/flags"
-	// sdk "github.com/line/lbm-sdk/types"
-	// "github.com/line/lbm-sdk/types/errors"
-	// "github.com/line/lbm-sdk/version"
+	"github.com/line/lbm-sdk/client/flags"
+	"github.com/line/lbm-sdk/version"
 	"github.com/line/lbm-sdk/x/token"
 )
 
@@ -31,7 +28,122 @@ func NewQueryCmd() *cobra.Command {
 	}
 
 	nftQueryCmd.AddCommand(
+		NewQueryCmdBalance(),
+		NewQueryCmdSupply(),
+		NewQueryCmdToken(),
+		NewQueryCmdTokens(),
 	)
-	panic("Not implemented")
+
 	return nftQueryCmd
+}
+
+func NewQueryCmdBalance() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "balance [class-id] [address]",
+		Args:    cobra.ExactArgs(2),
+		Short:   "query for token balances by a given address",
+		Example: fmt.Sprintf(`$ %s query %s balance <class-id> <address>`, version.AppName, token.ModuleName),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := token.NewQueryClient(clientCtx)
+			res, err := queryClient.Balance(cmd.Context(), &token.QueryBalanceRequest{
+				ClassId: args[0],
+				Address: args[1],
+			})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewQueryCmdSupply() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "supply [class-id] [type]",
+		Args:    cobra.ExactArgs(2),
+		Short:   "query the supply of tokens of the class",
+		Example: fmt.Sprintf(`$ %s query %s supply <class-id> <type>`, version.AppName, token.ModuleName),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := token.NewQueryClient(clientCtx)
+			res, err := queryClient.Supply(cmd.Context(), &token.QuerySupplyRequest{
+				ClassId: args[0],
+				Type: args[1],
+			})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewQueryCmdToken() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "token [class-id]",
+		Args:    cobra.ExactArgs(1),
+		Short:   "query token metadata based on its id",
+		Example: fmt.Sprintf(`$ %s query %s token <class-id>`, version.AppName, token.ModuleName),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := token.NewQueryClient(clientCtx)
+			res, err := queryClient.Token(cmd.Context(), &token.QueryTokenRequest{
+				ClassId: args[0],
+			})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewQueryCmdTokens() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "tokens",
+		Args:    cobra.NoArgs,
+		Short:   "query all token metadata",
+		Example: fmt.Sprintf(`$ %s query %s tokens`, version.AppName, token.ModuleName),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := token.NewQueryClient(clientCtx)
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+			res, err := queryClient.Tokens(cmd.Context(), &token.QueryTokensRequest{
+				Pagination: pageReq,
+			})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	flags.AddPaginationFlagsToCmd(cmd, "tokens")
+	return cmd
 }
