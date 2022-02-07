24823540210c8bab154f9e47e166e0a2265d6857
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2022-02-07 01:15:39 +0000

    feat: implement x/token tx client

--- x/token/client/cli/tx.go
+++ x/token/client/cli/tx.go
@@ -1,16 +1,29 @@
 package cli
 
 import (
-	// "fmt"
-	// "strings"
+	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/line/lbm-sdk/client"
-	// "github.com/line/lbm-sdk/client/tx"
-	// sdk "github.com/line/lbm-sdk/types"
-	// "github.com/line/lbm-sdk/version"
+	"github.com/line/lbm-sdk/client/flags"
+	"github.com/line/lbm-sdk/client/tx"
+	sdk "github.com/line/lbm-sdk/types"
+	sdkerrors "github.com/line/lbm-sdk/types/errors"
+	"github.com/line/lbm-sdk/version"
 	"github.com/line/lbm-sdk/x/token"
+)
+
+const (
+	flagSupply = "supply"
+	flagDecimals = "decimals"
+	flagMintable = "mintable"
+	flagMeta = "meta"
+	flagImageUri = "image-uri"
+
+	DefaultDecimals = "8"
+	DefaultSupply = "1"
 )
 
 // NewTxCmd returns the transaction commands for this module
@@ -24,7 +37,362 @@ func NewTxCmd() *cobra.Command {
 	}
 
 	nftTxCmd.AddCommand(
+		NewTxCmdTransfer(),
+		NewTxCmdTransferFrom(),
+		NewTxCmdApprove(),
+		NewTxCmdIssue(),
+		NewTxCmdGrant(),
+		NewTxCmdRevoke(),
+		NewTxCmdMint(),
+		NewTxCmdBurn(),
+		NewTxCmdBurnFrom(),
+		NewTxCmdModify(),
 	)
-	panic("Not implemented")
+
 	return nftTxCmd
+}
+
+func NewTxCmdTransfer() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "transfer [class-id] [from] [to] [amount]",
+		Args:  cobra.ExactArgs(4),
+		Short: "transfer tokens",
+		Long: strings.TrimSpace(fmt.Sprintf(`
+			$ %s tx %s transfer <class-id> <from> <to> <amount>`, version.AppName, token.ModuleName),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			amount, ok := sdk.NewIntFromString(args[3])
+			if !ok {
+				return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "failed to set amount: %s")
+			}
+			msg := token.MsgTransfer{
+				ClassId:  args[0],
+				From: args[1],
+				To: args[2],
+				Amount: amount,
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewTxCmdTransferFrom() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "transfer-from [class-id] [proxy] [from] [to] [amount]",
+		Args:  cobra.ExactArgs(5),
+		Short: "transfer tokens by proxy",
+		Long: strings.TrimSpace(fmt.Sprintf(`
+			$ %s tx %s transfer-from <class-id> <proxy> <from> <to> <amount>`, version.AppName, token.ModuleName),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			amount, ok := sdk.NewIntFromString(args[4])
+			if !ok {
+				return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "failed to set amount: %s")
+			}
+			msg := token.MsgTransferFrom{
+				ClassId:  args[0],
+				Proxy: args[1],
+				From: args[2],
+				To: args[3],
+				Amount: amount,
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewTxCmdApprove() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "approve [class-id] [approver] [proxy]",
+		Args:  cobra.ExactArgs(3),
+		Short: "approve transfer of tokens to a given proxy",
+		Long: strings.TrimSpace(fmt.Sprintf(`
+			$ %s tx %s approve <class-id> <approver> <proxy>`, version.AppName, token.ModuleName),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := token.MsgApprove{
+				ClassId:  args[0],
+				Approver: args[1],
+				Proxy: args[2],
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewTxCmdIssue() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "issue [owner] [to] [name] [symbol]",
+		Args:  cobra.ExactArgs(4),
+		Short: "issue token",
+		Long: strings.TrimSpace(fmt.Sprintf(`
+			$ %s tx %s issue <owner> <to> <name> <symbol>`, version.AppName, token.ModuleName),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			imageUri, err := cmd.Flags().GetString(flagImageUri)
+			if err != nil {
+				return err
+			}
+
+			meta, err := cmd.Flags().GetString(flagMeta)
+			if err != nil {
+				return err
+			}
+
+			supplyStr, err := cmd.Flags().GetString(flagSupply)
+			if err != nil {
+				return err
+			}
+			supply, ok := sdk.NewIntFromString(supplyStr)
+			if !ok {
+				return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "failed to set supply: %s")
+			}
+
+			mintable, err := cmd.Flags().GetBool(flagMintable)
+			if err != nil {
+				return err
+			}
+
+			decimalsStr, err := cmd.Flags().GetString(flagDecimals)
+			if err != nil {
+				return err
+			}
+			decimals, ok := sdk.NewIntFromString(decimalsStr)
+			if !ok {
+				return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "failed to set decimals: %s")
+			}
+
+			msg := token.MsgIssue{
+				Owner: args[0],
+				To: args[1],
+				Name: args[2],
+				Symbol: args[3],
+				ImageUri: imageUri,
+				Meta: meta,
+				Amount: supply,
+				Mintable: mintable,
+				Decimals: decimals,
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	cmd.Flags().String(flagImageUri, "", "set image-uri")
+	cmd.Flags().String(flagMeta, "", "set meta")
+	cmd.Flags().String(flagSupply, DefaultSupply, "initial supply")
+	cmd.Flags().Bool(flagMintable, false, "set mintable")
+	cmd.Flags().String(flagDecimals, DefaultDecimals, "set decimals")
+
+	return cmd
+}
+
+func NewTxCmdGrant() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "grant [class-id] [granter] [grantee] [action]",
+		Args:  cobra.ExactArgs(4),
+		Short: "grant an action for mint, burn and modify",
+		Long: strings.TrimSpace(fmt.Sprintf(`
+			$ %s tx %s grant <class-id> <granter> <grantee> <action>`, version.AppName, token.ModuleName),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := token.MsgGrant{
+				ClassId:  args[0],
+				Granter: args[1],
+				Grantee: args[2],
+				Action: args[3],
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewTxCmdRevoke() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "revoke [class-id] [grantee] [action]",
+		Args:  cobra.ExactArgs(3),
+		Short: "revoke an action by a given grantee",
+		Long: strings.TrimSpace(fmt.Sprintf(`
+			$ %s tx %s revoke <class-id> <grantee> <action>`, version.AppName, token.ModuleName),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := token.MsgRevoke{
+				ClassId:  args[0],
+				Grantee: args[1],
+				Action: args[2],
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewTxCmdMint() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "mint [class-id] [grantee] [to] [amount]",
+		Args:  cobra.ExactArgs(4),
+		Short: "mint tokens",
+		Long: strings.TrimSpace(fmt.Sprintf(`
+			$ %s tx %s mint <class-id> <grantee> <to> <amount>`, version.AppName, token.ModuleName),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			amount, ok := sdk.NewIntFromString(args[3])
+			if !ok {
+				return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "failed to set amount: %s")
+			}
+
+			msg := token.MsgMint{
+				ClassId:  args[0],
+				Grantee: args[1],
+				To: args[2],
+				Amount: amount,
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewTxCmdBurn() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "burn [class-id] [from] [amount]",
+		Args:  cobra.ExactArgs(3),
+		Short: "burn tokens",
+		Long: strings.TrimSpace(fmt.Sprintf(`
+			$ %s tx %s burn <class-id> <from> <amount>`, version.AppName, token.ModuleName),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			amount, ok := sdk.NewIntFromString(args[2])
+			if !ok {
+				return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "failed to set amount: %s")
+			}
+
+			msg := token.MsgBurn{
+				ClassId:  args[0],
+				From: args[1],
+				Amount: amount,
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewTxCmdBurnFrom() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "burn-from [class-id] [grantee] [from] [amount]",
+		Args:  cobra.ExactArgs(4),
+		Short: "burn tokens by a given grantee",
+		Long: strings.TrimSpace(fmt.Sprintf(`
+			$ %s tx %s burn-from <class-id> <grantee> <from> <amount>`, version.AppName, token.ModuleName),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			amount, ok := sdk.NewIntFromString(args[3])
+			if !ok {
+				return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "failed to set amount: %s")
+			}
+
+			msg := token.MsgBurnFrom{
+				ClassId:  args[0],
+				Grantee: args[1],
+				From: args[2],
+				Amount: amount,
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewTxCmdModify() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "modify [class-id] [grantee] [key] [value]",
+		Args:  cobra.ExactArgs(4),
+		Short: "modify token metadata",
+		Long: strings.TrimSpace(fmt.Sprintf(`
+			$ %s tx %s modify <class-id> <grantee> <key> <value>`, version.AppName, token.ModuleName),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			change := token.Pair{Key: args[2], Value: args[3]}
+			msg := token.MsgModify{
+				ClassId:  args[0],
+				Grantee: args[1],
+				Changes: []token.Pair{change},
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
 }
---
+++ x/token/msgs.go
@@ -0,0 +1,490 @@
+package token
+
+import (
+	"unicode/utf8"
+	"regexp"
+
+	"github.com/line/lbm-sdk/codec/legacy"
+	sdk "github.com/line/lbm-sdk/types"
+	sdkerrors "github.com/line/lbm-sdk/types/errors"
+)
+
+const (
+	TypeMsgTransfer = "transfer"
+	TypeMsgTransferFrom = "transfer-from"
+	TypeMsgApprove = "approve"
+	TypeMsgIssue = "issue"
+	TypeMsgGrant = "grant"
+	TypeMsgRevoke = "revoke"
+	TypeMsgMint = "mint"
+	TypeMsgBurn = "burn"
+	TypeMsgBurnFrom = "burn-from"
+	TypeMsgModify = "modify"
+
+	ActionMint = "mint"
+	ActionBurn = "burn"
+	ActionModify = "modify"
+
+	AttributeKeyName = "name"
+	AttributeKeyImageUri = "image_uri"
+	AttributeKeyMeta = "meta"
+
+	maxName = 20
+	maxImageUri = 1000
+	maxMeta = 1000
+)
+
+func validateAmount(amount sdk.Int) error {
+	if !amount.IsPositive() {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Amount must be positive: %s", amount)
+	}
+	return nil
+}
+var _ sdk.Msg = (*MsgTransfer)(nil)
+
+// Route implements Msg.
+func (m MsgTransfer) Route() string { return RouterKey }
+
+// Type implements Msg.
+func (m MsgTransfer) Type() string { return sdk.MsgTypeURL(&m) }
+
+// ValidateBasic implements Msg.
+func (m MsgTransfer) ValidateBasic() error {
+	if err := sdk.ValidateAccAddress(m.From); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid from address: %s", m.From)
+	}
+
+	if err := sdk.ValidateAccAddress(m.To); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid to address: %s", m.To)
+	}
+
+	if err := validateAmount(m.Amount); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetSignBytes implements Msg.
+func (m MsgTransfer) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&m))
+}
+
+// GetSigners implements Msg
+func (m MsgTransfer) GetSigners() []sdk.AccAddress {
+	signer := sdk.AccAddress(m.From)
+	return []sdk.AccAddress{signer}
+}
+
+var _ sdk.Msg = (*MsgTransferFrom)(nil)
+
+// Route implements Msg.
+func (m MsgTransferFrom) Route() string { return RouterKey }
+
+// Type implements Msg.
+func (m MsgTransferFrom) Type() string { return sdk.MsgTypeURL(&m) }
+
+// ValidateBasic implements Msg.
+func (m MsgTransferFrom) ValidateBasic() error {
+	if err := sdk.ValidateAccAddress(m.Proxy); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid proxy address: %s", m.Proxy)
+	}
+
+	if err := sdk.ValidateAccAddress(m.From); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid from address: %s", m.From)
+	}
+
+	if err := sdk.ValidateAccAddress(m.To); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid to address: %s", m.To)
+	}
+
+	if err := validateAmount(m.Amount); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetSignBytes implements Msg.
+func (m MsgTransferFrom) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&m))
+}
+
+// GetSigners implements Msg
+func (m MsgTransferFrom) GetSigners() []sdk.AccAddress {
+	signer := sdk.AccAddress(m.Proxy)
+	return []sdk.AccAddress{signer}
+}
+
+var _ sdk.Msg = (*MsgApprove)(nil)
+
+// Route implements Msg.
+func (m MsgApprove) Route() string { return RouterKey }
+
+// Type implements Msg.
+func (m MsgApprove) Type() string { return sdk.MsgTypeURL(&m) }
+
+// ValidateBasic implements Msg.
+func (m MsgApprove) ValidateBasic() error {
+	if err := sdk.ValidateAccAddress(m.Approver); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid approver address: %s", m.Approver)
+	}
+
+	if err := sdk.ValidateAccAddress(m.Proxy); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid proxy address: %s", m.Proxy)
+	}
+
+	return nil
+}
+
+// GetSignBytes implements Msg.
+func (m MsgApprove) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&m))
+}
+
+// GetSigners implements Msg.
+func (m MsgApprove) GetSigners() []sdk.AccAddress {
+	signer := sdk.AccAddress(m.Approver)
+	return []sdk.AccAddress{signer}
+}
+
+func stringInSize(str string, size int) bool {
+	return utf8.RuneCountInString(str) <= size
+}
+
+func validateName(name string) error {
+	if len(name) == 0 {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Name cannot be empty")
+	} else if !stringInSize(name, maxName) {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "Name cannot be longer than %d", maxName)
+	}
+	return nil
+}
+
+func validateSymbol(symbol string) error {
+	reg := regexp.MustCompile(`[A-Z][A-Z0-9]{1,4}`)
+	if !reg.MatchString(symbol) {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "Invalid symbol: %s, valid expression is: %s", symbol, reg.String())
+	}
+	return nil
+}
+
+func validateImageUri(uri string) error {
+	if !stringInSize(uri, maxImageUri) {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "ImageUri cannot be longer than %d", maxImageUri)
+	}
+	return nil
+}
+
+func validateMeta(meta string) error {
+	if !stringInSize(meta, maxMeta) {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "Meta cannot be longer than %d", maxMeta)
+	}
+	return nil
+}
+
+func validateDecimals(decimals sdk.Int) error {
+	if decimals.GT(sdk.NewInt(18)) || decimals.IsNegative() {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "Invalid decimals: %s", decimals.String())
+	}
+	return nil
+}
+
+var _ sdk.Msg = (*MsgIssue)(nil)
+
+// Route implements Msg.
+func (m MsgIssue) Route() string { return RouterKey }
+
+// Type implements Msg.
+func (m MsgIssue) Type() string { return sdk.MsgTypeURL(&m) }
+
+// ValidateBasic implements Msg.
+func (m MsgIssue) ValidateBasic() error {
+	if err := sdk.ValidateAccAddress(m.Owner); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid owner address: %s", m.Owner)
+	}
+
+	if err := sdk.ValidateAccAddress(m.To); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid to address: %s", m.To)
+	}
+
+	if err := validateName(m.Name); err != nil {
+		return err
+	}
+
+	if err := validateSymbol(m.Symbol); err != nil {
+		return err
+	}
+
+	if err := validateImageUri(m.ImageUri); err != nil {
+		return err
+	}
+
+	if err := validateMeta(m.Meta); err != nil {
+		return err
+	}
+
+	if err := validateDecimals(m.Decimals); err != nil {
+		return err
+	}
+
+	if err := validateAmount(m.Amount); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetSignBytes implements Msg.
+func (m MsgIssue) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&m))
+}
+
+// GetSigners implements Msg.
+func (m MsgIssue) GetSigners() []sdk.AccAddress {
+	signer := sdk.AccAddress(m.Owner)
+	return []sdk.AccAddress{signer}
+}
+
+func validateAction(action string) error {
+	actions := []string{
+		ActionMint,
+		ActionBurn,
+		ActionModify,
+	}
+	for _, a := range actions {
+		if action == a {
+			return nil
+		}
+	}
+	return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "Invalid action: %s", action)
+}
+
+var _ sdk.Msg = (*MsgGrant)(nil)
+
+// Route implements Msg.
+func (m MsgGrant) Route() string { return RouterKey }
+
+// Type implements Msg.
+func (m MsgGrant) Type() string { return sdk.MsgTypeURL(&m) }
+
+// ValidateBasic implements Msg.
+func (m MsgGrant) ValidateBasic() error {
+	if err := sdk.ValidateAccAddress(m.Granter); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid granter address: %s", m.Granter)
+	}
+
+	if err := sdk.ValidateAccAddress(m.Grantee); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid grantee address: %s", m.Grantee)
+	}
+
+	if err := validateAction(m.Action); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetSignBytes implements Msg.
+func (m MsgGrant) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&m))
+}
+
+// GetSigners implements Msg
+func (m MsgGrant) GetSigners() []sdk.AccAddress {
+	signer := sdk.AccAddress(m.Granter)
+	return []sdk.AccAddress{signer}
+}
+
+var _ sdk.Msg = (*MsgRevoke)(nil)
+
+// Route implements Msg.
+func (m MsgRevoke) Route() string { return RouterKey }
+
+// Type implements Msg.
+func (m MsgRevoke) Type() string { return sdk.MsgTypeURL(&m) }
+
+// ValidateBasic implements Msg.
+func (m MsgRevoke) ValidateBasic() error {
+	if err := sdk.ValidateAccAddress(m.Grantee); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid grantee address: %s", m.Grantee)
+	}
+
+	if err := validateAction(m.Action); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetSignBytes implements Msg.
+func (m MsgRevoke) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&m))
+}
+
+// GetSigners implements Msg
+func (m MsgRevoke) GetSigners() []sdk.AccAddress {
+	signer := sdk.AccAddress(m.Grantee)
+	return []sdk.AccAddress{signer}
+}
+
+var _ sdk.Msg = (*MsgMint)(nil)
+
+// Route implements Msg.
+func (m MsgMint) Route() string { return RouterKey }
+
+// Type implements Msg.
+func (m MsgMint) Type() string { return sdk.MsgTypeURL(&m) }
+
+// ValidateBasic implements Msg.
+func (m MsgMint) ValidateBasic() error {
+	if err := sdk.ValidateAccAddress(m.Grantee); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid grantee address: %s", m.Grantee)
+	}
+
+	if err := sdk.ValidateAccAddress(m.To); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid to address: %s", m.To)
+	}
+
+	if err := validateAmount(m.Amount); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetSignBytes implements Msg.
+func (m MsgMint) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&m))
+}
+
+// GetSigners implements Msg
+func (m MsgMint) GetSigners() []sdk.AccAddress {
+	signer := sdk.AccAddress(m.Grantee)
+	return []sdk.AccAddress{signer}
+}
+
+var _ sdk.Msg = (*MsgBurn)(nil)
+
+// Route implements Msg.
+func (m MsgBurn) Route() string { return RouterKey }
+
+// Type implements Msg.
+func (m MsgBurn) Type() string { return sdk.MsgTypeURL(&m) }
+
+// ValidateBasic implements Msg.
+func (m MsgBurn) ValidateBasic() error {
+	if err := sdk.ValidateAccAddress(m.From); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid from address: %s", m.From)
+	}
+
+	if err := validateAmount(m.Amount); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetSignBytes implements Msg.
+func (m MsgBurn) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&m))
+}
+
+// GetSigners implements Msg
+func (m MsgBurn) GetSigners() []sdk.AccAddress {
+	signer := sdk.AccAddress(m.From)
+	return []sdk.AccAddress{signer}
+}
+
+var _ sdk.Msg = (*MsgBurnFrom)(nil)
+
+// Route implements Msg.
+func (m MsgBurnFrom) Route() string { return RouterKey }
+
+// Type implements Msg.
+func (m MsgBurnFrom) Type() string { return sdk.MsgTypeURL(&m) }
+
+// ValidateBasic implements Msg.
+func (m MsgBurnFrom) ValidateBasic() error {
+	if err := sdk.ValidateAccAddress(m.Grantee); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid grantee address: %s", m.Grantee)
+	}
+
+	if err := sdk.ValidateAccAddress(m.From); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid from address: %s", m.From)
+	}
+
+	if err := validateAmount(m.Amount); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetSignBytes implements Msg.
+func (m MsgBurnFrom) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&m))
+}
+
+// GetSigners implements Msg
+func (m MsgBurnFrom) GetSigners() []sdk.AccAddress {
+	signer := sdk.AccAddress(m.Grantee)
+	return []sdk.AccAddress{signer}
+}
+
+func validateChange(change Pair) error {
+	validators := map[string]func(string)error{
+		AttributeKeyName: validateName,
+		AttributeKeyImageUri: validateImageUri,
+		AttributeKeyMeta: validateMeta,
+	}
+	validator, ok := validators[change.Key]
+	if !ok {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "Invalid field: %s", change.Key)
+	}
+	return validator(change.Value)
+}
+	
+var _ sdk.Msg = (*MsgModify)(nil)
+
+// Route implements Msg.
+func (m MsgModify) Route() string { return RouterKey }
+
+// Type implements Msg.
+func (m MsgModify) Type() string { return sdk.MsgTypeURL(&m) }
+
+// ValidateBasic implements Msg.
+func (m MsgModify) ValidateBasic() error {
+	if err := sdk.ValidateAccAddress(m.Grantee); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid grantee address: %s", m.Grantee)
+	}
+
+	var checkedFields map[string]bool
+	for _, change := range m.Changes {
+		if checkedFields[change.Key] {
+			return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "Duplicated field: %s", change.Key)
+		}
+		checkedFields[change.Key] = true
+
+		if err := validateChange(change); err != nil {
+			return err
+		}
+	}
+	if len(checkedFields) == 0 {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "No field provided")
+	}
+
+	return nil
+}
+
+// GetSignBytes implements Msg.
+func (m MsgModify) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&m))
+}
+
+// GetSigners implements Msg
+func (m MsgModify) GetSigners() []sdk.AccAddress {
+	signer := sdk.AccAddress(m.Grantee)
+	return []sdk.AccAddress{signer}
+}
+
