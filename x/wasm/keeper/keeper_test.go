CosmWasm/wasmd

Update gas price

x/wasm/keeper/keeper_test.go
@@ -517,7 +517,7 @@ func TestExecute(t *testing.T) {
 	// make sure gas is properly deducted from ctx
 	gasAfter := ctx.GasMeter().GasConsumed()
 	if types.EnableGasVerification {
-		require.Equal(t, uint64(0x12aef), gasAfter-gasBefore)
+		require.Equal(t, uint64(0x12af1), gasAfter-gasBefore)
 	}
 	// ensure bob now exists and got both payments released
 	bobAcct = accKeeper.GetAccount(ctx, bob)

Finschia/finschia-sdk

x/wasm/keeper/keeper_test.go
@@ -522,7 +522,7 @@ func TestExecute(t *testing.T) {
 	// make sure gas is properly deducted from ctx
 	gasAfter := ctx.GasMeter().GasConsumed()
 	if types.EnableGasVerification {
-		require.Equal(t, uint64(0x1098d), gasAfter-gasBefore)
+		require.Equal(t, uint64(0x1098f), gasAfter-gasBefore)
 	}
 	// ensure bob now exists and got both payments released
 	bobAcct = accKeeper.GetAccount(ctx, bob)