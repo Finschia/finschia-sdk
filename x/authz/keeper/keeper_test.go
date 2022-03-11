cosmos/cosmos-sdk

snapshots: document use of ioutil.TempDir() (#8508)
Document with inline comments the changes introduced
in https://github.com/cosmos/cosmos-sdk/pull/8475.

Run make format.

x/authz/keeper/keeper_test.go
@@ -4,17 +4,16 @@ import (
 	"testing"
 	"time"
 
-	"github.com/cosmos/cosmos-sdk/baseapp"
 	proto "github.com/gogo/protobuf/proto"
-
+	"github.com/stretchr/testify/suite"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmtime "github.com/tendermint/tendermint/types/time"
 
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/authz/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	"github.com/stretchr/testify/suite"
 )
 
 type TestSuite struct {

x/feegrant/types/basic_fee_test.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 	"time"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/feegrant/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/feegrant/types"
 )
 
 func TestBasicFeeValidAllow(t *testing.T) {

Finschia/finschia-sdk

x/authz/keeper/keeper_test.go
@@ -5,16 +5,16 @@ import (
 	"time"
 
 	proto "github.com/gogo/protobuf/proto"
-	"github.com/line/lbm-sdk/baseapp"
+	"github.com/stretchr/testify/suite"
 
 	ocproto "github.com/line/ostracon/proto/ostracon/types"
-	tmtime "github.com/line/ostracon/types/time"
+	octime "github.com/line/ostracon/types/time"
 
+	"github.com/line/lbm-sdk/baseapp"
 	"github.com/line/lbm-sdk/simapp"
 	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/x/authz/types"
 	banktypes "github.com/line/lbm-sdk/x/bank/types"
-	"github.com/stretchr/testify/suite"
 )
 
 type TestSuite struct {
@@ -29,7 +29,7 @@ type TestSuite struct {
 func (s *TestSuite) SetupTest() {
 	app := simapp.Setup(false)
 	ctx := app.BaseApp.NewContext(false, ocproto.Header{})
-	now := tmtime.Now()
+	now := octime.Now()
 	ctx = ctx.WithBlockHeader(ocproto.Header{Time: now})
 	queryHelper := baseapp.NewQueryServerTestHelper(ctx, app.InterfaceRegistry())
 	types.RegisterQueryServer(queryHelper, app.AuthzKeeper)

x/feegrant/types/basic_fee_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/line/lbm-sdk/simapp"
 	sdk "github.com/line/lbm-sdk/types"
+	"github.com/line/lbm-sdk/x/feegrant/types"
 )
 
 func TestBasicFeeValidAllow(t *testing.T) {