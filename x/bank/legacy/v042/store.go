cosmos/cosmos-sdk

x{stake,slash,gov,distrib} In-place Store Migrations (#8504)
* Add 1st version of migrate

* Put migration logic into Configurator

* add test to bank store migration

* add test for configurator

* Error if no migration found

* Remove RunMigrations from Configurator interface

* Update spec

* Rename folders

* copy-paste from keys.go

* Fix nil map

* rename function

* Update simapp/app.go

Co-authored-by: Robert Zaremba <robert@zaremba.ch>

* Update simapp/app_test.go

Co-authored-by: Robert Zaremba <robert@zaremba.ch>

* Adderss reviews

* Fix tests

* Update testutil/context.go

Co-authored-by: Robert Zaremba <robert@zaremba.ch>

* Update docs for ConsensusVersion

* Rename to forVersion

* Fix tests

* Check error early

* Return 1 for intiial version

* Use MigrationKeeper

* Fix test

* Revert adding marshaler to Configurator

* Godoc updates

* Update docs

* Add distrib legacy folder

* Add tests for distrib migration

* Add gov migrations

* Copy paste whole keys file

* Add gov migrations

* Add staking

* Fix staking tests

* Update spec and module.go

* Update to latest changes

* Update migration scripts

* capability to 1

* Fix tests

* Add package godoc

* Remove whitespace

* Remove global

* Use Migrator

* Remove 042 keys files

* Fix build

* Unlambda

* Rename to Migrate1to2

Co-authored-by: Robert Zaremba <robert@zaremba.ch>

x/bank/legacy/v042/store.go
@@ -5,6 +5,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v040auth "github.com/cosmos/cosmos-sdk/x/auth/legacy/v040"
 	v040bank "github.com/cosmos/cosmos-sdk/x/bank/legacy/v040"
+	"github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
 // MigrateStore performs in-place store migrations from v0.40 to v0.42. The
@@ -27,7 +28,7 @@ func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
 	for ; oldStoreIter.Valid(); oldStoreIter.Next() {
 		addr := v040bank.AddressFromBalancesStore(oldStoreIter.Key())
 		denom := oldStoreIter.Key()[v040auth.AddrLen:]
-		newStoreKey := append(CreateAccountBalancesPrefix(addr), denom...)
+		newStoreKey := append(types.CreateAccountBalancesPrefix(addr), denom...)
 
 		// Set new key on store. Values don't change.
 		store.Set(newStoreKey, oldStoreIter.Value())

x/gov/legacy/v042/store.go
@@ -1,0 +1,43 @@
+package v042
+
+import (
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+	v040gov "github.com/cosmos/cosmos-sdk/x/gov/legacy/v040"
+)
+
+const proposalIDLen = 8
+
+// migratePrefixProposalAddress is a helper function that migrates all keys of format:
+// <prefix_bytes><proposal_id (8 bytes)><address_bytes>
+// into format:
+// <prefix_bytes><proposal_id (8 bytes)><address_len (1 byte)><address_bytes>
+func migratePrefixProposalAddress(store sdk.KVStore, prefixBz []byte) {
+	oldStore := prefix.NewStore(store, prefixBz)
+
+	oldStoreIter := oldStore.Iterator(nil, nil)
+	defer oldStoreIter.Close()
+
+	for ; oldStoreIter.Valid(); oldStoreIter.Next() {
+		proposalID := oldStoreIter.Key()[:proposalIDLen]
+		addr := oldStoreIter.Key()[proposalIDLen:]
+		newStoreKey := append(append(prefixBz, proposalID...), address.MustLengthPrefix(addr)...)
+
+		// Set new key on store. Values don't change.
+		store.Set(newStoreKey, oldStoreIter.Value())
+		oldStore.Delete(oldStoreIter.Key())
+	}
+}
+
+// MigrateStore performs in-place store migrations from v0.40 to v0.42. The
+// migration includes:
+//
+// - Change addresses to be length-prefixed.
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
+	store := ctx.KVStore(storeKey)
+	migratePrefixProposalAddress(store, v040gov.DepositsKeyPrefix)
+	migratePrefixProposalAddress(store, v040gov.VotesKeyPrefix)
+
+	return nil
+}

x/slashing/legacy/v042/store.go
@@ -1,0 +1,20 @@
+package v042
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	v042distribution "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v042"
+	v040slashing "github.com/cosmos/cosmos-sdk/x/slashing/legacy/v040"
+)
+
+// MigrateStore performs in-place store migrations from v0.40 to v0.42. The
+// migration includes:
+//
+// - Change addresses to be length-prefixed.
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
+	store := ctx.KVStore(storeKey)
+	v042distribution.MigratePrefixAddress(store, v040slashing.ValidatorSigningInfoKeyPrefix)
+	v042distribution.MigratePrefixAddressBytes(store, v040slashing.ValidatorMissedBlockBitArrayKeyPrefix)
+	v042distribution.MigratePrefixAddress(store, v040slashing.AddrPubkeyRelationKeyPrefix)
+
+	return nil
+}

x/slashing/legacy/v042/store_test.go
@@ -1,0 +1,68 @@
+package v042_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/cosmos-sdk/testutil"
+	"github.com/cosmos/cosmos-sdk/testutil/testdata"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	v040slashing "github.com/cosmos/cosmos-sdk/x/slashing/legacy/v040"
+	v042slashing "github.com/cosmos/cosmos-sdk/x/slashing/legacy/v042"
+	"github.com/cosmos/cosmos-sdk/x/slashing/types"
+)
+
+func TestStoreMigration(t *testing.T) {
+	slashingKey := sdk.NewKVStoreKey("slashing")
+	ctx := testutil.DefaultContext(slashingKey, sdk.NewTransientStoreKey("transient_test"))
+	store := ctx.KVStore(slashingKey)
+
+	_, _, addr1 := testdata.KeyTestPubAddr()
+	consAddr := sdk.ConsAddress(addr1)
+	// Use dummy value for all keys.
+	value := []byte("foo")
+
+	testCases := []struct {
+		name   string
+		oldKey []byte
+		newKey []byte
+	}{
+		{
+			"ValidatorSigningInfoKey",
+			v040slashing.ValidatorSigningInfoKey(consAddr),
+			types.ValidatorSigningInfoKey(consAddr),
+		},
+		{
+			"ValidatorMissedBlockBitArrayKey",
+			v040slashing.ValidatorMissedBlockBitArrayKey(consAddr, 2),
+			types.ValidatorMissedBlockBitArrayKey(consAddr, 2),
+		},
+		{
+			"AddrPubkeyRelationKey",
+			v040slashing.AddrPubkeyRelationKey(consAddr),
+			types.AddrPubkeyRelationKey(consAddr),
+		},
+	}
+
+	// Set all the old keys to the store
+	for _, tc := range testCases {
+		store.Set(tc.oldKey, value)
+	}
+
+	// Run migrations.
+	err := v042slashing.MigrateStore(ctx, slashingKey)
+	require.NoError(t, err)
+
+	// Make sure the new keys are set and old keys are deleted.
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			if bytes.Compare(tc.oldKey, tc.newKey) != 0 {
+				require.Nil(t, store.Get(tc.oldKey))
+			}
+			require.Equal(t, value, store.Get(tc.newKey))
+		})
+	}
+}

x/staking/legacy/v042/store.go
@@ -1,0 +1,78 @@
+package v042
+
+import (
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+	v040auth "github.com/cosmos/cosmos-sdk/x/auth/legacy/v040"
+	v042distribution "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v042"
+	v040staking "github.com/cosmos/cosmos-sdk/x/staking/legacy/v040"
+	"github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+// migratePrefixAddressAddressAddress is a helper function that migrates all keys of format:
+// prefix_bytes | address_1_bytes | address_2_bytes | address_3_bytes
+// into format:
+// prefix_bytes | address_1_len (1 byte) | address_1_bytes | address_2_len (1 byte) | address_2_bytes | address_3_len (1 byte) | address_3_bytes
+func migratePrefixAddressAddressAddress(store sdk.KVStore, prefixBz []byte) {
+	oldStore := prefix.NewStore(store, prefixBz)
+
+	oldStoreIter := oldStore.Iterator(nil, nil)
+	defer oldStoreIter.Close()
+
+	for ; oldStoreIter.Valid(); oldStoreIter.Next() {
+		addr1 := oldStoreIter.Key()[:v040auth.AddrLen]
+		addr2 := oldStoreIter.Key()[v040auth.AddrLen : 2*v040auth.AddrLen]
+		addr3 := oldStoreIter.Key()[2*v040auth.AddrLen:]
+		newStoreKey := append(append(append(
+			prefixBz,
+			address.MustLengthPrefix(addr1)...), address.MustLengthPrefix(addr2)...), address.MustLengthPrefix(addr3)...,
+		)
+
+		// Set new key on store. Values don't change.
+		store.Set(newStoreKey, oldStoreIter.Value())
+		oldStore.Delete(oldStoreIter.Key())
+	}
+}
+
+const powerBytesLen = 8
+
+func migrateValidatorsByPowerIndexKey(store sdk.KVStore) {
+	oldStore := prefix.NewStore(store, v040staking.ValidatorsByPowerIndexKey)
+
+	oldStoreIter := oldStore.Iterator(nil, nil)
+	defer oldStoreIter.Close()
+
+	for ; oldStoreIter.Valid(); oldStoreIter.Next() {
+		powerBytes := oldStoreIter.Key()[:powerBytesLen]
+		valAddr := oldStoreIter.Key()[powerBytesLen:]
+		newStoreKey := append(append(types.ValidatorsByPowerIndexKey, powerBytes...), address.MustLengthPrefix(valAddr)...)
+
+		// Set new key on store. Values don't change.
+		store.Set(newStoreKey, oldStoreIter.Value())
+		oldStore.Delete(oldStoreIter.Key())
+	}
+}
+
+// MigrateStore performs in-place store migrations from v0.40 to v0.42. The
+// migration includes:
+//
+// - Change addresses to be length-prefixed.
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
+	store := ctx.KVStore(storeKey)
+
+	v042distribution.MigratePrefixAddress(store, v040staking.LastValidatorPowerKey)
+
+	v042distribution.MigratePrefixAddress(store, v040staking.ValidatorsKey)
+	v042distribution.MigratePrefixAddress(store, v040staking.ValidatorsByConsAddrKey)
+	migrateValidatorsByPowerIndexKey(store)
+
+	v042distribution.MigratePrefixAddressAddress(store, v040staking.DelegationKey)
+	v042distribution.MigratePrefixAddressAddress(store, v040staking.UnbondingDelegationKey)
+	v042distribution.MigratePrefixAddressAddress(store, v040staking.UnbondingDelegationByValIndexKey)
+	migratePrefixAddressAddressAddress(store, v040staking.RedelegationKey)
+	migratePrefixAddressAddressAddress(store, v040staking.RedelegationByValSrcIndexKey)
+	migratePrefixAddressAddressAddress(store, v040staking.RedelegationByValDstIndexKey)
+
+	return nil
+}

x/staking/legacy/v042/store_test.go
@@ -1,0 +1,137 @@
+package v042_test
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/cosmos-sdk/testutil"
+	"github.com/cosmos/cosmos-sdk/testutil/testdata"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	v040staking "github.com/cosmos/cosmos-sdk/x/staking/legacy/v040"
+	v042staking "github.com/cosmos/cosmos-sdk/x/staking/legacy/v042"
+	"github.com/cosmos/cosmos-sdk/x/staking/teststaking"
+	"github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+func TestStoreMigration(t *testing.T) {
+	stakingKey := sdk.NewKVStoreKey("staking")
+	ctx := testutil.DefaultContext(stakingKey, sdk.NewTransientStoreKey("transient_test"))
+	store := ctx.KVStore(stakingKey)
+
+	_, pk1, addr1 := testdata.KeyTestPubAddr()
+	valAddr1 := sdk.ValAddress(addr1)
+	val := teststaking.NewValidator(t, valAddr1, pk1)
+	_, pk1, addr2 := testdata.KeyTestPubAddr()
+	valAddr2 := sdk.ValAddress(addr2)
+	_, _, addr3 := testdata.KeyTestPubAddr()
+	consAddr := sdk.ConsAddress(addr3.String())
+	_, _, addr4 := testdata.KeyTestPubAddr()
+	now := time.Now()
+	// Use dummy value for all keys.
+	value := []byte("foo")
+
+	testCases := []struct {
+		name   string
+		oldKey []byte
+		newKey []byte
+	}{
+		{
+			"LastValidatorPowerKey",
+			v040staking.GetLastValidatorPowerKey(valAddr1),
+			types.GetLastValidatorPowerKey(valAddr1),
+		},
+		{
+			"LastTotalPowerKey",
+			v040staking.LastTotalPowerKey,
+			types.LastTotalPowerKey,
+		},
+		{
+			"ValidatorsKey",
+			v040staking.GetValidatorKey(valAddr1),
+			types.GetValidatorKey(valAddr1),
+		},
+		{
+			"ValidatorsByConsAddrKey",
+			v040staking.GetValidatorByConsAddrKey(consAddr),
+			types.GetValidatorByConsAddrKey(consAddr),
+		},
+		{
+			"ValidatorsByPowerIndexKey",
+			v040staking.GetValidatorsByPowerIndexKey(val),
+			types.GetValidatorsByPowerIndexKey(val),
+		},
+		{
+			"DelegationKey",
+			v040staking.GetDelegationKey(addr4, valAddr1),
+			types.GetDelegationKey(addr4, valAddr1),
+		},
+		{
+			"UnbondingDelegationKey",
+			v040staking.GetUBDKey(addr4, valAddr1),
+			types.GetUBDKey(addr4, valAddr1),
+		},
+		{
+			"UnbondingDelegationByValIndexKey",
+			v040staking.GetUBDByValIndexKey(addr4, valAddr1),
+			types.GetUBDByValIndexKey(addr4, valAddr1),
+		},
+		{
+			"RedelegationKey",
+			v040staking.GetREDKey(addr4, valAddr1, valAddr2),
+			types.GetREDKey(addr4, valAddr1, valAddr2),
+		},
+		{
+			"RedelegationByValSrcIndexKey",
+			v040staking.GetREDByValSrcIndexKey(addr4, valAddr1, valAddr2),
+			types.GetREDByValSrcIndexKey(addr4, valAddr1, valAddr2),
+		},
+		{
+			"RedelegationByValDstIndexKey",
+			v040staking.GetREDByValDstIndexKey(addr4, valAddr1, valAddr2),
+			types.GetREDByValDstIndexKey(addr4, valAddr1, valAddr2),
+		},
+		{
+			"UnbondingQueueKey",
+			v040staking.GetUnbondingDelegationTimeKey(now),
+			types.GetUnbondingDelegationTimeKey(now),
+		},
+		{
+			"RedelegationQueueKey",
+			v040staking.GetRedelegationTimeKey(now),
+			types.GetRedelegationTimeKey(now),
+		},
+		{
+			"ValidatorQueueKey",
+			v040staking.GetValidatorQueueKey(now, 4),
+			types.GetValidatorQueueKey(now, 4),
+		},
+		{
+			"HistoricalInfoKey",
+			v040staking.GetHistoricalInfoKey(4),
+			types.GetHistoricalInfoKey(4),
+		},
+	}
+
+	// Set all the old keys to the store
+	for _, tc := range testCases {
+		store.Set(tc.oldKey, value)
+	}
+
+	// Run migrations.
+	err := v042staking.MigrateStore(ctx, stakingKey)
+	require.NoError(t, err)
+
+	// Make sure the new keys are set and old keys are deleted.
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			if bytes.Compare(tc.oldKey, tc.newKey) != 0 {
+				require.Nil(t, store.Get(tc.oldKey))
+			}
+			require.Equal(t, value, store.Get(tc.newKey))
+		})
+	}
+}

Finschia/finschia-sdk

x/bank/legacy/v042/store.go
@@ -5,6 +5,7 @@ import (
 	sdk "github.com/line/lbm-sdk/types"
 	v040auth "github.com/line/lbm-sdk/x/auth/legacy/v040"
 	v040bank "github.com/line/lbm-sdk/x/bank/legacy/v040"
+	"github.com/line/lbm-sdk/x/bank/types"
 )
 
 // MigrateStore performs in-place store migrations from v0.40 to v0.42. The
@@ -27,7 +28,7 @@ func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
 	for ; oldStoreIter.Valid(); oldStoreIter.Next() {
 		addr := v040bank.AddressFromBalancesStore(oldStoreIter.Key())
 		denom := oldStoreIter.Key()[v040auth.AddrLen:]
-		newStoreKey := append(CreateAccountBalancesPrefix(addr.Bytes()), denom...)
+		newStoreKey := append(types.CreateAccountBalancesPrefix(addr.Bytes()), denom...)
 
 		// Set new key on store. Values don't change.
 		store.Set(newStoreKey, oldStoreIter.Value())

x/gov/legacy/v042/store.go
@@ -1,0 +1,43 @@
+package v042
+
+import (
+	"github.com/line/lbm-sdk/store/prefix"
+	sdk "github.com/line/lbm-sdk/types"
+	"github.com/line/lbm-sdk/types/address"
+	v040gov "github.com/line/lbm-sdk/x/gov/legacy/v040"
+)
+
+const proposalIDLen = 8
+
+// migratePrefixProposalAddress is a helper function that migrates all keys of format:
+// <prefix_bytes><proposal_id (8 bytes)><address_bytes>
+// into format:
+// <prefix_bytes><proposal_id (8 bytes)><address_len (1 byte)><address_bytes>
+func migratePrefixProposalAddress(store sdk.KVStore, prefixBz []byte) {
+	oldStore := prefix.NewStore(store, prefixBz)
+
+	oldStoreIter := oldStore.Iterator(nil, nil)
+	defer oldStoreIter.Close()
+
+	for ; oldStoreIter.Valid(); oldStoreIter.Next() {
+		proposalID := oldStoreIter.Key()[:proposalIDLen]
+		addr := oldStoreIter.Key()[proposalIDLen:]
+		newStoreKey := append(append(prefixBz, proposalID...), address.MustLengthPrefix(addr)...)
+
+		// Set new key on store. Values don't change.
+		store.Set(newStoreKey, oldStoreIter.Value())
+		oldStore.Delete(oldStoreIter.Key())
+	}
+}
+
+// MigrateStore performs in-place store migrations from v0.40 to v0.42. The
+// migration includes:
+//
+// - Change addresses to be length-prefixed.
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
+	store := ctx.KVStore(storeKey)
+	migratePrefixProposalAddress(store, v040gov.DepositsKeyPrefix)
+	migratePrefixProposalAddress(store, v040gov.VotesKeyPrefix)
+
+	return nil
+}

x/slashing/legacy/v042/store.go
@@ -1,0 +1,20 @@
+package v042
+
+import (
+	sdk "github.com/line/lbm-sdk/types"
+	v042distribution "github.com/line/lbm-sdk/x/distribution/legacy/v042"
+	v040slashing "github.com/line/lbm-sdk/x/slashing/legacy/v040"
+)
+
+// MigrateStore performs in-place store migrations from v0.40 to v0.42. The
+// migration includes:
+//
+// - Change addresses to be length-prefixed.
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
+	store := ctx.KVStore(storeKey)
+	v042distribution.MigratePrefixAddress(store, v040slashing.ValidatorSigningInfoKeyPrefix)
+	v042distribution.MigratePrefixAddressBytes(store, v040slashing.ValidatorMissedBlockBitArrayKeyPrefix)
+	v042distribution.MigratePrefixAddress(store, v040slashing.AddrPubkeyRelationKeyPrefix)
+
+	return nil
+}

x/slashing/legacy/v042/store_test.go
@@ -1,0 +1,68 @@
+package v042_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/line/lbm-sdk/testutil"
+	"github.com/line/lbm-sdk/testutil/testdata"
+	sdk "github.com/line/lbm-sdk/types"
+	v040slashing "github.com/line/lbm-sdk/x/slashing/legacy/v040"
+	v042slashing "github.com/line/lbm-sdk/x/slashing/legacy/v042"
+	"github.com/line/lbm-sdk/x/slashing/types"
+)
+
+func TestStoreMigration(t *testing.T) {
+	slashingKey := sdk.NewKVStoreKey("slashing")
+	ctx := testutil.DefaultContext(slashingKey, sdk.NewTransientStoreKey("transient_test"))
+	store := ctx.KVStore(slashingKey)
+
+	_, _, addr1 := testdata.KeyTestPubAddr()
+	consAddr := sdk.ConsAddress(addr1)
+	// Use dummy value for all keys.
+	value := []byte("foo")
+
+	testCases := []struct {
+		name   string
+		oldKey []byte
+		newKey []byte
+	}{
+		{
+			"ValidatorSigningInfoKey",
+			v040slashing.ValidatorSigningInfoKey(consAddr),
+			types.ValidatorSigningInfoKey(consAddr),
+		},
+		{
+			"ValidatorMissedBlockBitArrayKey",
+			v040slashing.ValidatorMissedBlockBitArrayKey(consAddr, 2),
+			types.ValidatorMissedBlockBitArrayKey(consAddr, 2),
+		},
+		{
+			"AddrPubkeyRelationKey",
+			v040slashing.AddrPubkeyRelationKey(consAddr),
+			types.AddrPubkeyRelationKey(consAddr),
+		},
+	}
+
+	// Set all the old keys to the store
+	for _, tc := range testCases {
+		store.Set(tc.oldKey, value)
+	}
+
+	// Run migrations.
+	err := v042slashing.MigrateStore(ctx, slashingKey)
+	require.NoError(t, err)
+
+	// Make sure the new keys are set and old keys are deleted.
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			if bytes.Compare(tc.oldKey, tc.newKey) != 0 {
+				require.Nil(t, store.Get(tc.oldKey))
+			}
+			require.Equal(t, value, store.Get(tc.newKey))
+		})
+	}
+}

x/staking/legacy/v042/store.go
@@ -1,0 +1,78 @@
+package v042
+
+import (
+	"github.com/line/lbm-sdk/store/prefix"
+	sdk "github.com/line/lbm-sdk/types"
+	"github.com/line/lbm-sdk/types/address"
+	v040auth "github.com/line/lbm-sdk/x/auth/legacy/v040"
+	v042distribution "github.com/line/lbm-sdk/x/distribution/legacy/v042"
+	v040staking "github.com/line/lbm-sdk/x/staking/legacy/v040"
+	"github.com/line/lbm-sdk/x/staking/types"
+)
+
+// migratePrefixAddressAddressAddress is a helper function that migrates all keys of format:
+// prefix_bytes | address_1_bytes | address_2_bytes | address_3_bytes
+// into format:
+// prefix_bytes | address_1_len (1 byte) | address_1_bytes | address_2_len (1 byte) | address_2_bytes | address_3_len (1 byte) | address_3_bytes
+func migratePrefixAddressAddressAddress(store sdk.KVStore, prefixBz []byte) {
+	oldStore := prefix.NewStore(store, prefixBz)
+
+	oldStoreIter := oldStore.Iterator(nil, nil)
+	defer oldStoreIter.Close()
+
+	for ; oldStoreIter.Valid(); oldStoreIter.Next() {
+		addr1 := oldStoreIter.Key()[:v040auth.AddrLen]
+		addr2 := oldStoreIter.Key()[v040auth.AddrLen : 2*v040auth.AddrLen]
+		addr3 := oldStoreIter.Key()[2*v040auth.AddrLen:]
+		newStoreKey := append(append(append(
+			prefixBz,
+			address.MustLengthPrefix(addr1)...), address.MustLengthPrefix(addr2)...), address.MustLengthPrefix(addr3)...,
+		)
+
+		// Set new key on store. Values don't change.
+		store.Set(newStoreKey, oldStoreIter.Value())
+		oldStore.Delete(oldStoreIter.Key())
+	}
+}
+
+const powerBytesLen = 8
+
+func migrateValidatorsByPowerIndexKey(store sdk.KVStore) {
+	oldStore := prefix.NewStore(store, v040staking.ValidatorsByPowerIndexKey)
+
+	oldStoreIter := oldStore.Iterator(nil, nil)
+	defer oldStoreIter.Close()
+
+	for ; oldStoreIter.Valid(); oldStoreIter.Next() {
+		powerBytes := oldStoreIter.Key()[:powerBytesLen]
+		valAddr := oldStoreIter.Key()[powerBytesLen:]
+		newStoreKey := append(append(types.ValidatorsByPowerIndexKey, powerBytes...), address.MustLengthPrefix(valAddr)...)
+
+		// Set new key on store. Values don't change.
+		store.Set(newStoreKey, oldStoreIter.Value())
+		oldStore.Delete(oldStoreIter.Key())
+	}
+}
+
+// MigrateStore performs in-place store migrations from v0.40 to v0.42. The
+// migration includes:
+//
+// - Change addresses to be length-prefixed.
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
+	store := ctx.KVStore(storeKey)
+
+	v042distribution.MigratePrefixAddress(store, v040staking.LastValidatorPowerKey)
+
+	v042distribution.MigratePrefixAddress(store, v040staking.ValidatorsKey)
+	v042distribution.MigratePrefixAddress(store, v040staking.ValidatorsByConsAddrKey)
+	migrateValidatorsByPowerIndexKey(store)
+
+	v042distribution.MigratePrefixAddressAddress(store, v040staking.DelegationKey)
+	v042distribution.MigratePrefixAddressAddress(store, v040staking.UnbondingDelegationKey)
+	v042distribution.MigratePrefixAddressAddress(store, v040staking.UnbondingDelegationByValIndexKey)
+	migratePrefixAddressAddressAddress(store, v040staking.RedelegationKey)
+	migratePrefixAddressAddressAddress(store, v040staking.RedelegationByValSrcIndexKey)
+	migratePrefixAddressAddressAddress(store, v040staking.RedelegationByValDstIndexKey)
+
+	return nil
+}

x/staking/legacy/v042/store_test.go
@@ -1,0 +1,137 @@
+package v042_test
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/line/lbm-sdk/testutil"
+	"github.com/line/lbm-sdk/testutil/testdata"
+	sdk "github.com/line/lbm-sdk/types"
+	v040staking "github.com/line/lbm-sdk/x/staking/legacy/v040"
+	v042staking "github.com/line/lbm-sdk/x/staking/legacy/v042"
+	"github.com/line/lbm-sdk/x/staking/teststaking"
+	"github.com/line/lbm-sdk/x/staking/types"
+)
+
+func TestStoreMigration(t *testing.T) {
+	stakingKey := sdk.NewKVStoreKey("staking")
+	ctx := testutil.DefaultContext(stakingKey, sdk.NewTransientStoreKey("transient_test"))
+	store := ctx.KVStore(stakingKey)
+
+	_, pk1, addr1 := testdata.KeyTestPubAddr()
+	valAddr1 := sdk.ValAddress(addr1)
+	val := teststaking.NewValidator(t, valAddr1, pk1)
+	_, pk1, addr2 := testdata.KeyTestPubAddr()
+	valAddr2 := sdk.ValAddress(addr2)
+	_, _, addr3 := testdata.KeyTestPubAddr()
+	consAddr := sdk.ConsAddress(addr3.String())
+	_, _, addr4 := testdata.KeyTestPubAddr()
+	now := time.Now()
+	// Use dummy value for all keys.
+	value := []byte("foo")
+
+	testCases := []struct {
+		name   string
+		oldKey []byte
+		newKey []byte
+	}{
+		{
+			"LastValidatorPowerKey",
+			v040staking.GetLastValidatorPowerKey(valAddr1),
+			types.GetLastValidatorPowerKey(valAddr1),
+		},
+		{
+			"LastTotalPowerKey",
+			v040staking.LastTotalPowerKey,
+			types.LastTotalPowerKey,
+		},
+		{
+			"ValidatorsKey",
+			v040staking.GetValidatorKey(valAddr1),
+			types.GetValidatorKey(valAddr1),
+		},
+		{
+			"ValidatorsByConsAddrKey",
+			v040staking.GetValidatorByConsAddrKey(consAddr),
+			types.GetValidatorByConsAddrKey(consAddr),
+		},
+		{
+			"ValidatorsByPowerIndexKey",
+			v040staking.GetValidatorsByPowerIndexKey(val),
+			types.GetValidatorsByPowerIndexKey(val),
+		},
+		{
+			"DelegationKey",
+			v040staking.GetDelegationKey(addr4, valAddr1),
+			types.GetDelegationKey(addr4, valAddr1),
+		},
+		{
+			"UnbondingDelegationKey",
+			v040staking.GetUBDKey(addr4, valAddr1),
+			types.GetUBDKey(addr4, valAddr1),
+		},
+		{
+			"UnbondingDelegationByValIndexKey",
+			v040staking.GetUBDByValIndexKey(addr4, valAddr1),
+			types.GetUBDByValIndexKey(addr4, valAddr1),
+		},
+		{
+			"RedelegationKey",
+			v040staking.GetREDKey(addr4, valAddr1, valAddr2),
+			types.GetREDKey(addr4, valAddr1, valAddr2),
+		},
+		{
+			"RedelegationByValSrcIndexKey",
+			v040staking.GetREDByValSrcIndexKey(addr4, valAddr1, valAddr2),
+			types.GetREDByValSrcIndexKey(addr4, valAddr1, valAddr2),
+		},
+		{
+			"RedelegationByValDstIndexKey",
+			v040staking.GetREDByValDstIndexKey(addr4, valAddr1, valAddr2),
+			types.GetREDByValDstIndexKey(addr4, valAddr1, valAddr2),
+		},
+		{
+			"UnbondingQueueKey",
+			v040staking.GetUnbondingDelegationTimeKey(now),
+			types.GetUnbondingDelegationTimeKey(now),
+		},
+		{
+			"RedelegationQueueKey",
+			v040staking.GetRedelegationTimeKey(now),
+			types.GetRedelegationTimeKey(now),
+		},
+		{
+			"ValidatorQueueKey",
+			v040staking.GetValidatorQueueKey(now, 4),
+			types.GetValidatorQueueKey(now, 4),
+		},
+		{
+			"HistoricalInfoKey",
+			v040staking.GetHistoricalInfoKey(4),
+			types.GetHistoricalInfoKey(4),
+		},
+	}
+
+	// Set all the old keys to the store
+	for _, tc := range testCases {
+		store.Set(tc.oldKey, value)
+	}
+
+	// Run migrations.
+	err := v042staking.MigrateStore(ctx, stakingKey)
+	require.NoError(t, err)
+
+	// Make sure the new keys are set and old keys are deleted.
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			if bytes.Compare(tc.oldKey, tc.newKey) != 0 {
+				require.Nil(t, store.Get(tc.oldKey))
+			}
+			require.Equal(t, value, store.Get(tc.newKey))
+		})
+	}
+}