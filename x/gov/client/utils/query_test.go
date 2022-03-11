cosmos/cosmos-sdk

Use service Msgs in CLI tx commands (#8512)
* Use service Msgs in CLI tx commands

* Update comment

* Gracefully support amino signing

* CLI to use svc msg

* Fix lint

* Use fq method name in events

* Update tests

* Fix quering events

* Add docs

* Fix test build

* Fix events

* Fix search for events

* Handle old andd new event quering

* Use merge events

* Better encCfg

* Add page in search

* Fix tests

* Update test and comments

* Update x/auth/legacy/legacytx/stdsign.go

Co-authored-by: atheeshp <59333759+atheeshp@users.noreply.github.com>

* Fix conflict for weighted vote

* Make CopyTx actually run

* Fix CopyTx

* Fix test

* Add changelog entry

* Remove useless code

* Add msgs tests

* Remove testing proto Msg via CLI

* Fix lint

* Fix test

* Implement GetSignBytes on ServiceMsg

* Fix %T

Co-authored-by: atheeshp <59333759+atheeshp@users.noreply.github.com>
Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

x/gov/client/utils/query_test.go
@@ -2,6 +2,7 @@ package utils_test
 
 import (
 	"context"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,15 +11,14 @@ import (
 	tmtypes "github.com/tendermint/tendermint/types"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/cosmos/cosmos-sdk/x/gov/client/utils"
 	"github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
 type TxSearchMock struct {
+	txConfig client.TxConfig
 	mock.Client
 	txs []tmtypes.Tx
 }
@@ -32,12 +32,35 @@ func (mock TxSearchMock) TxSearch(ctx context.Context, query string, prove bool,
 		*perPage = 0
 	}
 
+	// Get the `message.action` value from the query.
+	messageAction := regexp.MustCompile(`message\.action='(.*)' .*$`)
+	msgType := messageAction.FindStringSubmatch(query)[1]
+
+	// Filter only the txs that match the query
+	matchingTxs := make([]tmtypes.Tx, 0)
+	for _, tx := range mock.txs {
+		sdkTx, err := mock.txConfig.TxDecoder()(tx)
+		if err != nil {
+			return nil, err
+		}
+		for _, msg := range sdkTx.GetMsgs() {
+			if msg.Type() == msgType {
+				matchingTxs = append(matchingTxs, tx)
+				break
+			}
+		}
+	}
+
 	start, end := client.Paginate(len(mock.txs), *page, *perPage, 100)
 	if start < 0 || end < 0 {
 		// nil result with nil error crashes utils.QueryTxsByEvents
 		return &ctypes.ResultTxSearch{}, nil
 	}
-	txs := mock.txs[start:end]
+	if len(matchingTxs) < end {
+		return &ctypes.ResultTxSearch{}, nil
+	}
+
+	txs := matchingTxs[start:end]
 	rst := &ctypes.ResultTxSearch{Txs: make([]*ctypes.ResultTx, len(txs)), TotalCount: len(txs)}
 	for i := range txs {
 		rst.Txs[i] = &ctypes.ResultTx{Tx: txs[i]}
@@ -50,15 +73,9 @@ func (mock TxSearchMock) Block(ctx context.Context, height *int64) (*ctypes.Resu
 	return &ctypes.ResultBlock{Block: &tmtypes.Block{}}, nil
 }
 
-func newTestCodec() *codec.LegacyAmino {
-	cdc := codec.NewLegacyAmino()
-	sdk.RegisterLegacyAminoCodec(cdc)
-	types.RegisterLegacyAminoCodec(cdc)
-	authtypes.RegisterLegacyAminoCodec(cdc)
-	return cdc
-}
-
 func TestGetPaginatedVotes(t *testing.T) {
+	encCfg := simapp.MakeTestEncodingConfig()
+
 	type testCase struct {
 		description string
 		page, limit int
@@ -144,17 +161,12 @@ func TestGetPaginatedVotes(t *testing.T) {
 		tc := tc
 
 		t.Run(tc.description, func(t *testing.T) {
-			var (
-				marshalled = make([]tmtypes.Tx, len(tc.msgs))
-				cdc        = newTestCodec()
-			)
-
-			encodingConfig := simapp.MakeTestEncodingConfig()
-			cli := TxSearchMock{txs: marshalled}
+			var marshalled = make([]tmtypes.Tx, len(tc.msgs))
+			cli := TxSearchMock{txs: marshalled, txConfig: encCfg.TxConfig}
 			clientCtx := client.Context{}.
-				WithLegacyAmino(cdc).
+				WithLegacyAmino(encCfg.Amino).
 				WithClient(cli).
-				WithTxConfig(encodingConfig.TxConfig)
+				WithTxConfig(encCfg.TxConfig)
 
 			for i := range tc.msgs {
 				txBuilder := clientCtx.TxConfig.NewTxBuilder()

x/ibc/applications/transfer/client/cli/tx.go
@@ -10,6 +10,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
 	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/types"
 	clienttypes "github.com/cosmos/cosmos-sdk/x/ibc/core/02-client/types"
@@ -96,11 +97,14 @@ to the counterparty channel. Any timeout set to 0 is disabled.`),
 			msg := types.NewMsgTransfer(
 				srcPort, srcChannel, coin, sender, receiver, timeoutHeight, timeoutTimestamp,
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.Transfer(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 

x/ibc/core/04-channel/client/cli/tx.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
 	ibctransfertypes "github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/types"
 	clienttypes "github.com/cosmos/cosmos-sdk/x/ibc/core/02-client/types"
 	connectionutils "github.com/cosmos/cosmos-sdk/x/ibc/core/03-connection/client/utils"
@@ -42,11 +43,14 @@ func NewChannelOpenInitCmd() *cobra.Command {
 				portID, version, order, hops,
 				counterpartyPortID, clientCtx.GetFromAddress(),
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelOpenInit(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 
@@ -93,11 +97,14 @@ func NewChannelOpenTryCmd() *cobra.Command {
 				counterpartyPortID, counterpartyChannelID, version,
 				proofInit, proofHeight, clientCtx.GetFromAddress(),
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelOpenTry(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 
@@ -139,11 +146,14 @@ func NewChannelOpenAckCmd() *cobra.Command {
 			msg := types.NewMsgChannelOpenAck(
 				portID, channelID, counterpartyChannelID, version, proofTry, proofHeight, clientCtx.GetFromAddress(),
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelOpenAck(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 	cmd.Flags().String(FlagIBCVersion, ibctransfertypes.Version, "IBC application version")
@@ -179,11 +189,14 @@ func NewChannelOpenConfirmCmd() *cobra.Command {
 			msg := types.NewMsgChannelOpenConfirm(
 				portID, channelID, proofAck, proofHeight, clientCtx.GetFromAddress(),
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelOpenConfirm(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 
@@ -207,11 +220,14 @@ func NewChannelCloseInitCmd() *cobra.Command {
 			channelID := args[1]
 
 			msg := types.NewMsgChannelCloseInit(portID, channelID, clientCtx.GetFromAddress())
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelCloseInit(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 
@@ -247,11 +263,14 @@ func NewChannelCloseConfirmCmd() *cobra.Command {
 			msg := types.NewMsgChannelCloseConfirm(
 				portID, channelID, proofInit, proofHeight, clientCtx.GetFromAddress(),
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelCloseConfirm(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 

Finschia/finschia-sdk

x/gov/client/utils/query_test.go
@@ -11,11 +11,8 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/line/lbm-sdk/client"
-	"github.com/line/lbm-sdk/codec"
 	"github.com/line/lbm-sdk/simapp"
 	sdk "github.com/line/lbm-sdk/types"
-	"github.com/line/lbm-sdk/x/auth/legacy/legacytx"
-	authtypes "github.com/line/lbm-sdk/x/auth/types"
 	"github.com/line/lbm-sdk/x/gov/client/utils"
 	"github.com/line/lbm-sdk/x/gov/types"
 )
@@ -47,7 +44,7 @@ func (mock TxSearchMock) TxSearch(ctx context.Context, query string, prove bool,
 			return nil, err
 		}
 		for _, msg := range sdkTx.GetMsgs() {
-			if msg.(legacytx.LegacyMsg).Type() == msgType {
+			if msg.Type() == msgType {
 				matchingTxs = append(matchingTxs, tx)
 				break
 			}
@@ -63,7 +60,7 @@ func (mock TxSearchMock) TxSearch(ctx context.Context, query string, prove bool,
 		return &ctypes.ResultTxSearch{}, nil
 	}
 
-	txs := mock.txs[start:end]
+	txs := matchingTxs[start:end]
 	rst := &ctypes.ResultTxSearch{Txs: make([]*ctypes.ResultTx, len(txs)), TotalCount: len(txs)}
 	for i := range txs {
 		rst.Txs[i] = &ctypes.ResultTx{Tx: txs[i]}
@@ -76,15 +73,9 @@ func (mock TxSearchMock) Block(ctx context.Context, height *int64) (*ctypes.Resu
 	return &ctypes.ResultBlock{Block: &octypes.Block{}}, nil
 }
 
-func newTestCodec() *codec.LegacyAmino {
-	cdc := codec.NewLegacyAmino()
-	sdk.RegisterLegacyAminoCodec(cdc)
-	types.RegisterLegacyAminoCodec(cdc)
-	authtypes.RegisterLegacyAminoCodec(cdc)
-	return cdc
-}
-
 func TestGetPaginatedVotes(t *testing.T) {
+	encCfg := simapp.MakeTestEncodingConfig()
+
 	type testCase struct {
 		description string
 		page, limit int
@@ -116,7 +107,8 @@ func TestGetPaginatedVotes(t *testing.T) {
 			},
 			votes: []types.Vote{
 				types.NewVote(0, acc1, types.NewNonSplitVoteOption(types.OptionYes)),
-				types.NewVote(0, acc2, types.NewNonSplitVoteOption(types.OptionYes))},
+				types.NewVote(0, acc2, types.NewNonSplitVoteOption(types.OptionYes)),
+			},
 		},
 		{
 			description: "2MsgPerTx1Chunk",
@@ -129,6 +121,7 @@ func TestGetPaginatedVotes(t *testing.T) {
 			votes: []types.Vote{
 				types.NewVote(0, acc1, types.NewNonSplitVoteOption(types.OptionYes)),
 				types.NewVote(0, acc1, types.NewNonSplitVoteOption(types.OptionYes)),
+			},
 		},
 		{
 			description: "2MsgPerTx2Chunk",
@@ -141,6 +134,7 @@ func TestGetPaginatedVotes(t *testing.T) {
 			votes: []types.Vote{
 				types.NewVote(0, acc2, types.NewNonSplitVoteOption(types.OptionYes)),
 				types.NewVote(0, acc2, types.NewNonSplitVoteOption(types.OptionYes)),
+			},
 		},
 		{
 			description: "IncompleteSearchTx",
@@ -149,7 +143,7 @@ func TestGetPaginatedVotes(t *testing.T) {
 			msgs: [][]sdk.Msg{
 				acc1Msgs[:1],
 			},
-			votes: []types.Vote{types.NewVote(0, acc1, types.NewNonSplitVoteOption(types.OptionYes)),
+			votes: []types.Vote{types.NewVote(0, acc1, types.NewNonSplitVoteOption(types.OptionYes))},
 		},
 		{
 			description: "InvalidPage",
@@ -170,17 +164,12 @@ func TestGetPaginatedVotes(t *testing.T) {
 		tc := tc
 
 		t.Run(tc.description, func(t *testing.T) {
-			var (
-				marshalled = make([]octypes.Tx, len(tc.msgs))
-				cdc        = newTestCodec()
-			)
-
-			encodingConfig := simapp.MakeTestEncodingConfig()
-			cli := TxSearchMock{txs: marshalled, txConfig: encodingConfig.TxConfig}
+			var marshalled = make([]octypes.Tx, len(tc.msgs))
+			cli := TxSearchMock{txs: marshalled, txConfig: encCfg.TxConfig}
 			clientCtx := client.Context{}.
-				WithLegacyAmino(cdc).
+				WithLegacyAmino(encCfg.Amino).
 				WithClient(cli).
-				WithTxConfig(encodingConfig.TxConfig)
+				WithTxConfig(encCfg.TxConfig)
 
 			for i := range tc.msgs {
 				txBuilder := clientCtx.TxConfig.NewTxBuilder()

x/ibc/applications/transfer/client/cli/tx.go
@@ -10,6 +10,7 @@ import (
 	"github.com/line/lbm-sdk/client/flags"
 	"github.com/line/lbm-sdk/client/tx"
 	sdk "github.com/line/lbm-sdk/types"
+	"github.com/line/lbm-sdk/types/msgservice"
 	"github.com/line/lbm-sdk/version"
 	"github.com/line/lbm-sdk/x/ibc/applications/transfer/types"
 	clienttypes "github.com/line/lbm-sdk/x/ibc/core/02-client/types"
@@ -96,11 +97,14 @@ to the counterparty channel. Any timeout set to 0 is disabled.`),
 			msg := types.NewMsgTransfer(
 				srcPort, srcChannel, coin, sender, receiver, timeoutHeight, timeoutTimestamp,
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.Transfer(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 

x/ibc/core/04-channel/client/cli/tx.go
@@ -9,6 +9,7 @@ import (
 	"github.com/line/lbm-sdk/client"
 	"github.com/line/lbm-sdk/client/flags"
 	"github.com/line/lbm-sdk/client/tx"
+	"github.com/line/lbm-sdk/types/msgservice"
 	ibctransfertypes "github.com/line/lbm-sdk/x/ibc/applications/transfer/types"
 	clienttypes "github.com/line/lbm-sdk/x/ibc/core/02-client/types"
 	connectionutils "github.com/line/lbm-sdk/x/ibc/core/03-connection/client/utils"
@@ -42,11 +43,14 @@ func NewChannelOpenInitCmd() *cobra.Command {
 				portID, version, order, hops,
 				counterpartyPortID, clientCtx.GetFromAddress(),
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelOpenInit(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 
@@ -93,11 +97,14 @@ func NewChannelOpenTryCmd() *cobra.Command {
 				counterpartyPortID, counterpartyChannelID, version,
 				proofInit, proofHeight, clientCtx.GetFromAddress(),
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelOpenTry(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 
@@ -139,11 +146,14 @@ func NewChannelOpenAckCmd() *cobra.Command {
 			msg := types.NewMsgChannelOpenAck(
 				portID, channelID, counterpartyChannelID, version, proofTry, proofHeight, clientCtx.GetFromAddress(),
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelOpenAck(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 	cmd.Flags().String(FlagIBCVersion, ibctransfertypes.Version, "IBC application version")
@@ -179,11 +189,14 @@ func NewChannelOpenConfirmCmd() *cobra.Command {
 			msg := types.NewMsgChannelOpenConfirm(
 				portID, channelID, proofAck, proofHeight, clientCtx.GetFromAddress(),
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelOpenConfirm(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 
@@ -207,11 +220,14 @@ func NewChannelCloseInitCmd() *cobra.Command {
 			channelID := args[1]
 
 			msg := types.NewMsgChannelCloseInit(portID, channelID, clientCtx.GetFromAddress())
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelCloseInit(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 
@@ -247,11 +263,14 @@ func NewChannelCloseConfirmCmd() *cobra.Command {
 			msg := types.NewMsgChannelCloseConfirm(
 				portID, channelID, proofInit, proofHeight, clientCtx.GetFromAddress(),
 			)
-			if err := msg.ValidateBasic(); err != nil {
+			svcMsgClientConn := &msgservice.ServiceMsgClientConn{}
+			msgClient := types.NewMsgClient(svcMsgClientConn)
+			_, err = msgClient.ChannelCloseConfirm(cmd.Context(), msg)
+			if err != nil {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), svcMsgClientConn.GetMsgs()...)
 		},
 	}
 