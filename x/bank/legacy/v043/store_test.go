cosmos/cosmos-sdk

Move all migration scripts to v043 (#8814)
* Move all migration scripts to v043

* Fix permaling

* Fix test

* Fix test again

Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

x/bank/legacy/v043/store_test.go
@@ -1,4 +1,4 @@
-package v042_test
+package v043_test
 
 import (
 	"testing"
@@ -11,7 +11,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v040bank "github.com/cosmos/cosmos-sdk/x/bank/legacy/v040"
-	v042bank "github.com/cosmos/cosmos-sdk/x/bank/legacy/v042"
+	v043bank "github.com/cosmos/cosmos-sdk/x/bank/legacy/v043"
 	"github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
@@ -29,7 +29,7 @@ func TestSupplyMigration(t *testing.T) {
 	store.Set(v040bank.SupplyKey, encCfg.Marshaler.MustMarshalBinaryBare(&oldSupply))
 
 	// Run migration.
-	err := v042bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
+	err := v043bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
 	require.NoError(t, err)
 
 	// New supply is indexed by denom.
@@ -55,7 +55,7 @@ func TestBalanceKeysMigration(t *testing.T) {
 	oldKey := append(append(v040bank.BalancesPrefix, addr...), denom...)
 	store.Set(oldKey, value)
 
-	err := v042bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
+	err := v043bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
 	require.NoError(t, err)
 
 	newKey := append(types.CreateAccountBalancesPrefix(addr), denom...)

x/distribution/legacy/v043/store_test.go
@@ -1,4 +1,4 @@
-package v042_test
+package v043_test
 
 import (
 	"bytes"
@@ -10,7 +10,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v040distribution "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v040"
-	v042distribution "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v042"
+	v043distribution "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v043"
 	"github.com/cosmos/cosmos-sdk/x/distribution/types"
 )
 
@@ -83,7 +83,7 @@ func TestStoreMigration(t *testing.T) {
 	}
 
 	// Run migrations.
-	err := v042distribution.MigrateStore(ctx, distributionKey)
+	err := v043distribution.MigrateStore(ctx, distributionKey)
 	require.NoError(t, err)
 
 	// Make sure the new keys are set and old keys are deleted.

x/slashing/legacy/v043/store.go
@@ -1,8 +1,8 @@
-package v042
+package v043
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	v042distribution "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v042"
+	v043distribution "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v043"
 	v040slashing "github.com/cosmos/cosmos-sdk/x/slashing/legacy/v040"
 )
 
@@ -12,9 +12,9 @@ import (
 // - Change addresses to be length-prefixed.
 func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
 	store := ctx.KVStore(storeKey)
-	v042distribution.MigratePrefixAddress(store, v040slashing.ValidatorSigningInfoKeyPrefix)
-	v042distribution.MigratePrefixAddressBytes(store, v040slashing.ValidatorMissedBlockBitArrayKeyPrefix)
-	v042distribution.MigratePrefixAddress(store, v040slashing.AddrPubkeyRelationKeyPrefix)
+	v043distribution.MigratePrefixAddress(store, v040slashing.ValidatorSigningInfoKeyPrefix)
+	v043distribution.MigratePrefixAddressBytes(store, v040slashing.ValidatorMissedBlockBitArrayKeyPrefix)
+	v043distribution.MigratePrefixAddress(store, v040slashing.AddrPubkeyRelationKeyPrefix)
 
 	return nil
 }

x/slashing/legacy/v043/store_test.go
@@ -1,4 +1,4 @@
-package v042_test
+package v043_test
 
 import (
 	"bytes"
@@ -10,7 +10,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v040slashing "github.com/cosmos/cosmos-sdk/x/slashing/legacy/v040"
-	v042slashing "github.com/cosmos/cosmos-sdk/x/slashing/legacy/v042"
+	v043slashing "github.com/cosmos/cosmos-sdk/x/slashing/legacy/v043"
 	"github.com/cosmos/cosmos-sdk/x/slashing/types"
 )
 
@@ -52,7 +52,7 @@ func TestStoreMigration(t *testing.T) {
 	}
 
 	// Run migrations.
-	err := v042slashing.MigrateStore(ctx, slashingKey)
+	err := v043slashing.MigrateStore(ctx, slashingKey)
 	require.NoError(t, err)
 
 	// Make sure the new keys are set and old keys are deleted.

x/staking/legacy/v043/store.go
@@ -1,11 +1,11 @@
-package v042
+package v043
 
 import (
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/address"
 	v040auth "github.com/cosmos/cosmos-sdk/x/auth/legacy/v040"
-	v042distribution "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v042"
+	v043distribution "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v043"
 	v040staking "github.com/cosmos/cosmos-sdk/x/staking/legacy/v040"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
 )
@@ -61,15 +61,15 @@ func migrateValidatorsByPowerIndexKey(store sdk.KVStore) {
 func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
 	store := ctx.KVStore(storeKey)
 
-	v042distribution.MigratePrefixAddress(store, v040staking.LastValidatorPowerKey)
+	v043distribution.MigratePrefixAddress(store, v040staking.LastValidatorPowerKey)
 
-	v042distribution.MigratePrefixAddress(store, v040staking.ValidatorsKey)
-	v042distribution.MigratePrefixAddress(store, v040staking.ValidatorsByConsAddrKey)
+	v043distribution.MigratePrefixAddress(store, v040staking.ValidatorsKey)
+	v043distribution.MigratePrefixAddress(store, v040staking.ValidatorsByConsAddrKey)
 	migrateValidatorsByPowerIndexKey(store)
 
-	v042distribution.MigratePrefixAddressAddress(store, v040staking.DelegationKey)
-	v042distribution.MigratePrefixAddressAddress(store, v040staking.UnbondingDelegationKey)
-	v042distribution.MigratePrefixAddressAddress(store, v040staking.UnbondingDelegationByValIndexKey)
+	v043distribution.MigratePrefixAddressAddress(store, v040staking.DelegationKey)
+	v043distribution.MigratePrefixAddressAddress(store, v040staking.UnbondingDelegationKey)
+	v043distribution.MigratePrefixAddressAddress(store, v040staking.UnbondingDelegationByValIndexKey)
 	migratePrefixAddressAddressAddress(store, v040staking.RedelegationKey)
 	migratePrefixAddressAddressAddress(store, v040staking.RedelegationByValSrcIndexKey)
 	migratePrefixAddressAddressAddress(store, v040staking.RedelegationByValDstIndexKey)

x/staking/legacy/v043/store_test.go
@@ -1,4 +1,4 @@
-package v042_test
+package v043_test
 
 import (
 	"bytes"
@@ -11,7 +11,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v040staking "github.com/cosmos/cosmos-sdk/x/staking/legacy/v040"
-	v042staking "github.com/cosmos/cosmos-sdk/x/staking/legacy/v042"
+	v043staking "github.com/cosmos/cosmos-sdk/x/staking/legacy/v043"
 	"github.com/cosmos/cosmos-sdk/x/staking/teststaking"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
 )
@@ -121,7 +121,7 @@ func TestStoreMigration(t *testing.T) {
 	}
 
 	// Run migrations.
-	err := v042staking.MigrateStore(ctx, stakingKey)
+	err := v043staking.MigrateStore(ctx, stakingKey)
 	require.NoError(t, err)
 
 	// Make sure the new keys are set and old keys are deleted.

Finschia/finschia-sdk

x/bank/legacy/v043/store_test.go
@@ -1,4 +1,4 @@
-package v042_test
+package v043_test
 
 import (
 	"testing"
@@ -11,7 +11,7 @@ import (
 	"github.com/line/lbm-sdk/testutil/testdata"
 	sdk "github.com/line/lbm-sdk/types"
 	v040bank "github.com/line/lbm-sdk/x/bank/legacy/v040"
-	v042bank "github.com/line/lbm-sdk/x/bank/legacy/v042"
+	v043bank "github.com/line/lbm-sdk/x/bank/legacy/v043"
 	"github.com/line/lbm-sdk/x/bank/types"
 )
 
@@ -29,7 +29,7 @@ func TestSupplyMigration(t *testing.T) {
 	store.Set(v040bank.SupplyKey, encCfg.Marshaler.MustMarshalBinaryBare(&oldSupply))
 
 	// Run migration.
-	err := v042bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
+	err := v043bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
 	require.NoError(t, err)
 
 	// New supply is indexed by denom.
@@ -55,7 +55,7 @@ func TestBalanceKeysMigration(t *testing.T) {
 	oldKey := append(append(v040bank.BalancesPrefix, addr...), denom...)
 	store.Set(oldKey, value)
 
-	err := v042bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
+	err := v043bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
 	require.NoError(t, err)
 
 	newKey := append(types.CreateAccountBalancesPrefix(addr.Bytes()), denom...)

x/distribution/legacy/v043/store_test.go
@@ -1,4 +1,4 @@
-package v042_test
+package v043_test
 
 import (
 	"bytes"
@@ -10,7 +10,7 @@ import (
 	"github.com/line/lbm-sdk/testutil/testdata"
 	sdk "github.com/line/lbm-sdk/types"
 	v040distribution "github.com/line/lbm-sdk/x/distribution/legacy/v040"
-	v042distribution "github.com/line/lbm-sdk/x/distribution/legacy/v042"
+	v043distribution "github.com/line/lbm-sdk/x/distribution/legacy/v043"
 	"github.com/line/lbm-sdk/x/distribution/types"
 )
 
@@ -83,7 +83,7 @@ func TestStoreMigration(t *testing.T) {
 	}
 
 	// Run migrations.
-	err := v042distribution.MigrateStore(ctx, distributionKey)
+	err := v043distribution.MigrateStore(ctx, distributionKey)
 	require.NoError(t, err)
 
 	// Make sure the new keys are set and old keys are deleted.

x/slashing/legacy/v043/store.go
@@ -1,8 +1,8 @@
-package v042
+package v043
 
 import (
 	sdk "github.com/line/lbm-sdk/types"
-	v042distribution "github.com/line/lbm-sdk/x/distribution/legacy/v042"
+	v043distribution "github.com/line/lbm-sdk/x/distribution/legacy/v043"
 	v040slashing "github.com/line/lbm-sdk/x/slashing/legacy/v040"
 )
 
@@ -12,9 +12,9 @@ import (
 // - Change addresses to be length-prefixed.
 func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
 	store := ctx.KVStore(storeKey)
-	v042distribution.MigratePrefixAddress(store, v040slashing.ValidatorSigningInfoKeyPrefix)
-	v042distribution.MigratePrefixAddressBytes(store, v040slashing.ValidatorMissedBlockBitArrayKeyPrefix)
-	v042distribution.MigratePrefixAddress(store, v040slashing.AddrPubkeyRelationKeyPrefix)
+	v043distribution.MigratePrefixAddress(store, v040slashing.ValidatorSigningInfoKeyPrefix)
+	v043distribution.MigratePrefixAddressBytes(store, v040slashing.ValidatorMissedBlockBitArrayKeyPrefix)
+	v043distribution.MigratePrefixAddress(store, v040slashing.AddrPubkeyRelationKeyPrefix)
 
 	return nil
 }

x/slashing/legacy/v043/store_test.go
@@ -1,4 +1,4 @@
-package v042_test
+package v043_test
 
 import (
 	"bytes"
@@ -10,7 +10,7 @@ import (
 	"github.com/line/lbm-sdk/testutil/testdata"
 	sdk "github.com/line/lbm-sdk/types"
 	v040slashing "github.com/line/lbm-sdk/x/slashing/legacy/v040"
-	v042slashing "github.com/line/lbm-sdk/x/slashing/legacy/v042"
+	v043slashing "github.com/line/lbm-sdk/x/slashing/legacy/v043"
 	"github.com/line/lbm-sdk/x/slashing/types"
 )
 
@@ -52,7 +52,7 @@ func TestStoreMigration(t *testing.T) {
 	}
 
 	// Run migrations.
-	err := v042slashing.MigrateStore(ctx, slashingKey)
+	err := v043slashing.MigrateStore(ctx, slashingKey)
 	require.NoError(t, err)
 
 	// Make sure the new keys are set and old keys are deleted.

x/staking/legacy/v043/store.go
@@ -1,11 +1,11 @@
-package v042
+package v043
 
 import (
 	"github.com/line/lbm-sdk/store/prefix"
 	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/types/address"
 	v040auth "github.com/line/lbm-sdk/x/auth/legacy/v040"
-	v042distribution "github.com/line/lbm-sdk/x/distribution/legacy/v042"
+	v043distribution "github.com/line/lbm-sdk/x/distribution/legacy/v043"
 	v040staking "github.com/line/lbm-sdk/x/staking/legacy/v040"
 	"github.com/line/lbm-sdk/x/staking/types"
 )
@@ -111,15 +111,15 @@ func migrateValidatorsByPowerIndexKey(store sdk.KVStore) {
 func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
 	store := ctx.KVStore(storeKey)
 
-	v042distribution.MigratePrefixAddress(store, v040staking.LastValidatorPowerKey)
+	v043distribution.MigratePrefixAddress(store, v040staking.LastValidatorPowerKey)
 
-	v042distribution.MigratePrefixAddress(store, v040staking.ValidatorsKey)
-	v042distribution.MigratePrefixAddress(store, v040staking.ValidatorsByConsAddrKey)
+	v043distribution.MigratePrefixAddress(store, v040staking.ValidatorsKey)
+	v043distribution.MigratePrefixAddress(store, v040staking.ValidatorsByConsAddrKey)
 	migrateValidatorsByPowerIndexKey(store)
 
-	v042distribution.MigratePrefixAddressAddress(store, v040staking.DelegationKey)
-	v042distribution.MigratePrefixAddressAddress(store, v040staking.UnbondingDelegationKey)
-	v042distribution.MigratePrefixValAddressAddress(store, v040staking.UnbondingDelegationByValIndexKey)
+	v043distribution.MigratePrefixAddressAddress(store, v040staking.DelegationKey)
+	v043distribution.MigratePrefixAddressAddress(store, v040staking.UnbondingDelegationKey)
+	v043distribution.MigratePrefixValAddressAddress(store, v040staking.UnbondingDelegationByValIndexKey)
 	migratePrefixAddressValAddressValAddress(store, v040staking.RedelegationKey)
 	migratePrefixValAddressAddressValAddress(store, v040staking.RedelegationByValSrcIndexKey)
 	migratePrefixValAddressAddressValAddress(store, v040staking.RedelegationByValDstIndexKey)

x/staking/legacy/v043/store_test.go
@@ -1,4 +1,4 @@
-package v042_test
+package v043_test
 
 import (
 	"bytes"
@@ -11,7 +11,7 @@ import (
 	"github.com/line/lbm-sdk/testutil/testdata"
 	sdk "github.com/line/lbm-sdk/types"
 	v040staking "github.com/line/lbm-sdk/x/staking/legacy/v040"
-	v042staking "github.com/line/lbm-sdk/x/staking/legacy/v042"
+	v043staking "github.com/line/lbm-sdk/x/staking/legacy/v043"
 	"github.com/line/lbm-sdk/x/staking/teststaking"
 	"github.com/line/lbm-sdk/x/staking/types"
 )
@@ -121,7 +121,7 @@ func TestStoreMigration(t *testing.T) {
 	}
 
 	// Run migrations.
-	err := v042staking.MigrateStore(ctx, stakingKey)
+	err := v043staking.MigrateStore(ctx, stakingKey)
 	require.NoError(t, err)
 
 	// Make sure the new keys are set and old keys are deleted.