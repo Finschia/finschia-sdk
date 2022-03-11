cosmos/cosmos-sdk

Weighted votes migrate in-place migrations (#8663)
* Weighted votes migrate in-place migrations

* Rename to pb.go

* Fix some lint

* Fix lint

Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

x/gov/legacy/v042/store.go
@@ -1,10 +1,12 @@
 package v042
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/address"
 	v040gov "github.com/cosmos/cosmos-sdk/x/gov/legacy/v040"
+	"github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
 const proposalIDLen = 8
@@ -30,14 +32,41 @@ func migratePrefixProposalAddress(store sdk.KVStore, prefixBz []byte) {
 	}
 }
 
+// migrateWeightedVotes migrates the ADR-037 weighted votes.
+func migrateWeightedVotes(store sdk.KVStore, cdc codec.BinaryMarshaler) error {
+	iterator := sdk.KVStorePrefixIterator(store, v040gov.VotesKeyPrefix)
+
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		var oldVote v040gov.Vote
+		err := cdc.UnmarshalBinaryBare(iterator.Value(), &oldVote)
+		if err != nil {
+			return err
+		}
+
+		newVote := &types.Vote{
+			ProposalId: oldVote.ProposalId,
+			Voter:      oldVote.Voter,
+			Options:    []types.WeightedVoteOption{{Option: oldVote.Option, Weight: sdk.NewDec(1)}},
+		}
+		bz, err := cdc.MarshalBinaryBare(newVote)
+		if err != nil {
+			return err
+		}
+
+		store.Set(iterator.Key(), bz)
+	}
+
+	return nil
+}
+
 // MigrateStore performs in-place store migrations from v0.40 to v0.42. The
 // migration includes:
 //
 // - Change addresses to be length-prefixed.
-func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey, cdc codec.BinaryMarshaler) error {
 	store := ctx.KVStore(storeKey)
 	migratePrefixProposalAddress(store, v040gov.DepositsKeyPrefix)
 	migratePrefixProposalAddress(store, v040gov.VotesKeyPrefix)
-
-	return nil
+	return migrateWeightedVotes(store, cdc)
 }

x/gov/legacy/v042/store_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/cosmos/cosmos-sdk/testutil"
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -16,6 +17,7 @@ import (
 )
 
 func TestStoreMigration(t *testing.T) {
+	cdc := simapp.MakeTestEncodingConfig().Marshaler
 	govKey := sdk.NewKVStoreKey("gov")
 	ctx := testutil.DefaultContext(govKey, sdk.NewTransientStoreKey("transient_test"))
 	store := ctx.KVStore(govKey)
@@ -23,53 +25,57 @@ func TestStoreMigration(t *testing.T) {
 	_, _, addr1 := testdata.KeyTestPubAddr()
 	proposalID := uint64(6)
 	now := time.Now()
-	// Use dummy value for all keys.
-	value := []byte("foo")
+	// Use dummy value for keys where we don't test values.
+	dummyValue := []byte("foo")
+	// Use real values for votes, as we're testing weighted votes.
+	oldVote := v040gov.Vote{ProposalId: 1, Voter: "foobar", Option: types.OptionNoWithVeto}
+	oldVoteValue := cdc.MustMarshalBinaryBare(&oldVote)
+	newVote := types.Vote{ProposalId: 1, Voter: "foobar", Options: types.WeightedVoteOptions{{Option: types.OptionNoWithVeto, Weight: sdk.NewDec(1)}}}
+	newVoteValue := cdc.MustMarshalBinaryBare(&newVote)
 
 	testCases := []struct {
-		name   string
-		oldKey []byte
-		newKey []byte
+		name                               string
+		oldKey, oldValue, newKey, newValue []byte
 	}{
 		{
 			"ProposalKey",
-			v040gov.ProposalKey(proposalID),
-			types.ProposalKey(proposalID),
+			v040gov.ProposalKey(proposalID), dummyValue,
+			types.ProposalKey(proposalID), dummyValue,
 		},
 		{
 			"ActiveProposalQueue",
-			v040gov.ActiveProposalQueueKey(proposalID, now),
-			types.ActiveProposalQueueKey(proposalID, now),
+			v040gov.ActiveProposalQueueKey(proposalID, now), dummyValue,
+			types.ActiveProposalQueueKey(proposalID, now), dummyValue,
 		},
 		{
 			"InactiveProposalQueue",
-			v040gov.InactiveProposalQueueKey(proposalID, now),
-			types.InactiveProposalQueueKey(proposalID, now),
+			v040gov.InactiveProposalQueueKey(proposalID, now), dummyValue,
+			types.InactiveProposalQueueKey(proposalID, now), dummyValue,
 		},
 		{
 			"ProposalIDKey",
-			v040gov.ProposalIDKey,
-			types.ProposalIDKey,
+			v040gov.ProposalIDKey, dummyValue,
+			types.ProposalIDKey, dummyValue,
 		},
 		{
 			"DepositKey",
-			v040gov.DepositKey(proposalID, addr1),
-			types.DepositKey(proposalID, addr1),
+			v040gov.DepositKey(proposalID, addr1), dummyValue,
+			types.DepositKey(proposalID, addr1), dummyValue,
 		},
 		{
 			"VotesKeyPrefix",
-			v040gov.VoteKey(proposalID, addr1),
-			types.VoteKey(proposalID, addr1),
+			v040gov.VoteKey(proposalID, addr1), oldVoteValue,
+			types.VoteKey(proposalID, addr1), newVoteValue,
 		},
 	}
 
 	// Set all the old keys to the store
 	for _, tc := range testCases {
-		store.Set(tc.oldKey, value)
+		store.Set(tc.oldKey, tc.oldValue)
 	}
 
 	// Run migrations.
-	err := v042gov.MigrateStore(ctx, govKey)
+	err := v042gov.MigrateStore(ctx, govKey, cdc)
 	require.NoError(t, err)
 
 	// Make sure the new keys are set and old keys are deleted.
@@ -79,7 +85,7 @@ func TestStoreMigration(t *testing.T) {
 			if bytes.Compare(tc.oldKey, tc.newKey) != 0 {
 				require.Nil(t, store.Get(tc.oldKey))
 			}
-			require.Equal(t, value, store.Get(tc.newKey))
+			require.Equal(t, tc.newValue, store.Get(tc.newKey))
 		})
 	}
 }

Finschia/finschia-sdk

x/gov/legacy/v042/store.go
@@ -1,10 +1,12 @@
 package v042
 
 import (
+	"github.com/line/lbm-sdk/codec"
 	"github.com/line/lbm-sdk/store/prefix"
 	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/types/address"
 	v040gov "github.com/line/lbm-sdk/x/gov/legacy/v040"
+	"github.com/line/lbm-sdk/x/gov/types"
 )
 
 const proposalIDLen = 8
@@ -30,14 +32,41 @@ func migratePrefixProposalAddress(store sdk.KVStore, prefixBz []byte) {
 	}
 }
 
+// migrateWeightedVotes migrates the ADR-037 weighted votes.
+func migrateWeightedVotes(store sdk.KVStore, cdc codec.BinaryMarshaler) error {
+	iterator := sdk.KVStorePrefixIterator(store, v040gov.VotesKeyPrefix)
+
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		var oldVote v040gov.Vote
+		err := cdc.UnmarshalBinaryBare(iterator.Value(), &oldVote)
+		if err != nil {
+			return err
+		}
+
+		newVote := &types.Vote{
+			ProposalId: oldVote.ProposalId,
+			Voter:      oldVote.Voter,
+			Options:    []types.WeightedVoteOption{{Option: oldVote.Option, Weight: sdk.NewDec(1)}},
+		}
+		bz, err := cdc.MarshalBinaryBare(newVote)
+		if err != nil {
+			return err
+		}
+
+		store.Set(iterator.Key(), bz)
+	}
+
+	return nil
+}
+
 // MigrateStore performs in-place store migrations from v0.40 to v0.42. The
 // migration includes:
 //
 // - Change addresses to be length-prefixed.
-func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey, cdc codec.BinaryMarshaler) error {
 	store := ctx.KVStore(storeKey)
 	migratePrefixProposalAddress(store, v040gov.DepositsKeyPrefix)
 	migratePrefixProposalAddress(store, v040gov.VotesKeyPrefix)
-
-	return nil
+	return migrateWeightedVotes(store, cdc)
 }

x/gov/legacy/v042/store_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/line/lbm-sdk/simapp"
 	"github.com/line/lbm-sdk/testutil"
 	"github.com/line/lbm-sdk/testutil/testdata"
 	sdk "github.com/line/lbm-sdk/types"
@@ -16,6 +17,7 @@ import (
 )
 
 func TestStoreMigration(t *testing.T) {
+	cdc := simapp.MakeTestEncodingConfig().Marshaler
 	govKey := sdk.NewKVStoreKey("gov")
 	ctx := testutil.DefaultContext(govKey, sdk.NewTransientStoreKey("transient_test"))
 	store := ctx.KVStore(govKey)
@@ -23,53 +25,57 @@ func TestStoreMigration(t *testing.T) {
 	_, _, addr1 := testdata.KeyTestPubAddr()
 	proposalID := uint64(6)
 	now := time.Now()
-	// Use dummy value for all keys.
-	value := []byte("foo")
+	// Use dummy value for keys where we don't test values.
+	dummyValue := []byte("foo")
+	// Use real values for votes, as we're testing weighted votes.
+	oldVote := v040gov.Vote{ProposalId: 1, Voter: "foobar", Option: types.OptionNoWithVeto}
+	oldVoteValue := cdc.MustMarshalBinaryBare(&oldVote)
+	newVote := types.Vote{ProposalId: 1, Voter: "foobar", Options: types.WeightedVoteOptions{{Option: types.OptionNoWithVeto, Weight: sdk.NewDec(1)}}}
+	newVoteValue := cdc.MustMarshalBinaryBare(&newVote)
 
 	testCases := []struct {
-		name   string
-		oldKey []byte
-		newKey []byte
+		name                               string
+		oldKey, oldValue, newKey, newValue []byte
 	}{
 		{
 			"ProposalKey",
-			v040gov.ProposalKey(proposalID),
-			types.ProposalKey(proposalID),
+			v040gov.ProposalKey(proposalID), dummyValue,
+			types.ProposalKey(proposalID), dummyValue,
 		},
 		{
 			"ActiveProposalQueue",
-			v040gov.ActiveProposalQueueKey(proposalID, now),
-			types.ActiveProposalQueueKey(proposalID, now),
+			v040gov.ActiveProposalQueueKey(proposalID, now), dummyValue,
+			types.ActiveProposalQueueKey(proposalID, now), dummyValue,
 		},
 		{
 			"InactiveProposalQueue",
-			v040gov.InactiveProposalQueueKey(proposalID, now),
-			types.InactiveProposalQueueKey(proposalID, now),
+			v040gov.InactiveProposalQueueKey(proposalID, now), dummyValue,
+			types.InactiveProposalQueueKey(proposalID, now), dummyValue,
 		},
 		{
 			"ProposalIDKey",
-			v040gov.ProposalIDKey,
-			types.ProposalIDKey,
+			v040gov.ProposalIDKey, dummyValue,
+			types.ProposalIDKey, dummyValue,
 		},
 		{
 			"DepositKey",
-			v040gov.DepositKey(proposalID, addr1),
-			types.DepositKey(proposalID, addr1),
+			v040gov.DepositKey(proposalID, addr1), dummyValue,
+			types.DepositKey(proposalID, addr1), dummyValue,
 		},
 		{
 			"VotesKeyPrefix",
-			v040gov.VoteKey(proposalID, addr1),
-			types.VoteKey(proposalID, addr1),
+			v040gov.VoteKey(proposalID, addr1), oldVoteValue,
+			types.VoteKey(proposalID, addr1), newVoteValue,
 		},
 	}
 
 	// Set all the old keys to the store
 	for _, tc := range testCases {
-		store.Set(tc.oldKey, value)
+		store.Set(tc.oldKey, tc.oldValue)
 	}
 
 	// Run migrations.
-	err := v042gov.MigrateStore(ctx, govKey)
+	err := v042gov.MigrateStore(ctx, govKey, cdc)
 	require.NoError(t, err)
 
 	// Make sure the new keys are set and old keys are deleted.
@@ -79,7 +85,7 @@ func TestStoreMigration(t *testing.T) {
 			if bytes.Compare(tc.oldKey, tc.newKey) != 0 {
 				require.Nil(t, store.Get(tc.oldKey))
 			}
-			require.Equal(t, value, store.Get(tc.newKey))
+			require.Equal(t, tc.newValue, store.Get(tc.newKey))
 		})
 	}
 }