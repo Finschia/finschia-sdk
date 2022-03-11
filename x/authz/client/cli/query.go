cosmos/cosmos-sdk

x/authz: Add DelegateAuthorization, UndelegateAuthorization (#8472)
* add proto msgs

* add delegate authorization

* add delegate authorization tests

* add delegate authorization to cli

* fix lint

* add cli tests

* made max_tokens optional

* add tests

* add table tests

* resolve conflicts

* add undelegate authorization

* proto-gen

* fix errors

* fix lint

* resolve conflicts

* fix imports

* add allow & deny lists to delegate authorization

* refactor authorizations

* proto-docs

* fix lint

* review changes

* golint

* proto lint

* review changes

* add staking authorization

* review changes

* fix protos

* review changes

* review changes

* clean docs

* proto-docs

* proto-gen

* proto-docs

x/authz/client/cli/query.go
@@ -12,6 +12,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/cosmos/cosmos-sdk/x/authz/types"
+	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
 // GetQueryCmd returns the cli query commands for this module
@@ -97,7 +98,7 @@ func GetCmdQueryAuthorization() *cobra.Command {
 			fmt.Sprintf(`Query authorization for a granter-grantee pair that matches the given msg-type:
 Example:
 $ %s query %s authorization cosmos1skjw.. cosmos1skjwj.. %s
-`, version.AppName, types.ModuleName, types.SendAuthorization{}.MethodName()),
+`, version.AppName, types.ModuleName, bank.SendAuthorization{}.MethodName()),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)

x/authz/client/cli/query_test.go
@@ -1,0 +1,186 @@
+// +build norace
+
+package cli_test
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	tmcli "github.com/tendermint/tendermint/libs/cli"
+
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/authz/client/cli"
+
+	authztestutil "github.com/cosmos/cosmos-sdk/x/authz/client/testutil"
+	"github.com/cosmos/cosmos-sdk/x/authz/types"
+)
+
+func (s *IntegrationTestSuite) TestQueryAuthorizations() {
+	val := s.network.Validators[0]
+
+	grantee := s.grantee
+	twoHours := time.Now().Add(time.Minute * time.Duration(120)).Unix()
+
+	_, err := authztestutil.ExecGrantAuthorization(
+		val,
+		[]string{
+			grantee.String(),
+			"send",
+			fmt.Sprintf("--%s=100steak", cli.FlagSpendLimit),
+			fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+			fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address),
+			fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+			fmt.Sprintf("--%s=%d", cli.FlagExpiration, twoHours),
+			fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+		},
+	)
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		name      string
+		args      []string
+		expectErr bool
+		expErrMsg string
+	}{
+		{
+			"Error: Invalid grantee",
+			[]string{
+				val.Address.String(),
+				"invalid grantee",
+				fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+			},
+			true,
+			"decoding bech32 failed: invalid character in string: ' '",
+		},
+		{
+			"Error: Invalid granter",
+			[]string{
+				"invalid granter",
+				grantee.String(),
+				fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+			},
+			true,
+			"decoding bech32 failed: invalid character in string: ' '",
+		},
+		{
+			"Valid txn (json)",
+			[]string{
+				val.Address.String(),
+				grantee.String(),
+				fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+			},
+			false,
+			``,
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+
+		s.Run(tc.name, func() {
+			cmd := cli.GetCmdQueryAuthorizations()
+			clientCtx := val.ClientCtx
+			resp, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, tc.args)
+			if tc.expectErr {
+				s.Require().Error(err)
+				s.Require().Contains(string(resp.Bytes()), tc.expErrMsg)
+			} else {
+				s.Require().NoError(err)
+				var grants types.QueryAuthorizationsResponse
+				err = val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp.Bytes(), &grants)
+				s.Require().NoError(err)
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestQueryAuthorization() {
+	val := s.network.Validators[0]
+
+	grantee := s.grantee
+	twoHours := time.Now().Add(time.Minute * time.Duration(120)).Unix()
+
+	_, err := authztestutil.ExecGrantAuthorization(
+		val,
+		[]string{
+			grantee.String(),
+			"send",
+			fmt.Sprintf("--%s=100steak", cli.FlagSpendLimit),
+			fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+			fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address),
+			fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+			fmt.Sprintf("--%s=%d", cli.FlagExpiration, twoHours),
+			fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+		},
+	)
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		name           string
+		args           []string
+		expectErr      bool
+		expectedOutput string
+	}{
+		{
+			"Error: Invalid grantee",
+			[]string{
+				val.Address.String(),
+				"invalid grantee",
+				typeMsgSend,
+				fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+			},
+			true,
+			"",
+		},
+		{
+			"Error: Invalid granter",
+			[]string{
+				"invalid granter",
+				grantee.String(),
+				typeMsgSend,
+				fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+			},
+			true,
+			"",
+		},
+		{
+			"no authorization found",
+			[]string{
+				val.Address.String(),
+				grantee.String(),
+				"typeMsgSend",
+				fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+			},
+			true,
+			"",
+		},
+		{
+			"Valid txn (json)",
+			[]string{
+				val.Address.String(),
+				grantee.String(),
+				typeMsgSend,
+				fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+			},
+			false,
+			`{"@type":"/cosmos.bank.v1beta1.SendAuthorization","spend_limit":[{"denom":"steak","amount":"100"}]}`,
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+
+		s.Run(tc.name, func() {
+			cmd := cli.GetCmdQueryAuthorization()
+			clientCtx := val.ClientCtx
+			out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, tc.args)
+			if tc.expectErr {
+				s.Require().Error(err)
+			} else {
+				s.Require().NoError(err)
+				s.Require().Contains(strings.TrimSpace(out.String()), tc.expectedOutput)
+			}
+		})
+	}
+}

x/authz/exported/authorizations.go
@@ -1,0 +1,21 @@
+package exported
+
+import (
+	"github.com/gogo/protobuf/proto"
+
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// Authorization represents the interface of various Authorization types.
+type Authorization interface {
+	proto.Message
+
+	// MethodName returns the fully-qualified Msg service method name as described in ADR 031.
+	MethodName() string
+
+	// Accept determines whether this grant permits the provided sdk.ServiceMsg to be performed, and if
+	// so provides an upgraded authorization instance.
+	Accept(msg sdk.ServiceMsg, block tmproto.Header) (updated Authorization, delete bool, err error)
+}

x/authz/keeper/grpc_query_test.go
@@ -6,14 +6,16 @@ import (
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/authz/exported"
 	"github.com/cosmos/cosmos-sdk/x/authz/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
 func (suite *TestSuite) TestGRPCQueryAuthorization() {
 	app, ctx, queryClient, addrs := suite.app, suite.ctx, suite.queryClient, suite.addrs
 	var (
 		req              *types.QueryAuthorizationRequest
-		expAuthorization types.Authorization
+		expAuthorization exported.Authorization
 	)
 	testCases := []struct {
 		msg      string
@@ -55,7 +57,7 @@ func (suite *TestSuite) TestGRPCQueryAuthorization() {
 			func() {
 				now := ctx.BlockHeader().Time
 				newCoins := sdk.NewCoins(sdk.NewInt64Coin("steak", 100))
-				expAuthorization = &types.SendAuthorization{SpendLimit: newCoins}
+				expAuthorization = &banktypes.SendAuthorization{SpendLimit: newCoins}
 				err := app.AuthzKeeper.Grant(ctx, addrs[0], addrs[1], expAuthorization, now.Add(time.Hour))
 				suite.Require().NoError(err)
 				req = &types.QueryAuthorizationRequest{
@@ -66,7 +68,7 @@ func (suite *TestSuite) TestGRPCQueryAuthorization() {
 			},
 			true,
 			func(res *types.QueryAuthorizationResponse) {
-				var auth types.Authorization
+				var auth exported.Authorization
 				err := suite.app.InterfaceRegistry().UnpackAny(res.Authorization.Authorization, &auth)
 				suite.Require().NoError(err)
 				suite.Require().NotNil(auth)
@@ -92,7 +94,7 @@ func (suite *TestSuite) TestGRPCQueryAuthorizations() {
 	app, ctx, queryClient, addrs := suite.app, suite.ctx, suite.queryClient, suite.addrs
 	var (
 		req              *types.QueryAuthorizationsRequest
-		expAuthorization types.Authorization
+		expAuthorization exported.Authorization
 	)
 	testCases := []struct {
 		msg      string
@@ -123,7 +125,7 @@ func (suite *TestSuite) TestGRPCQueryAuthorizations() {
 			func() {
 				now := ctx.BlockHeader().Time
 				newCoins := sdk.NewCoins(sdk.NewInt64Coin("steak", 100))
-				expAuthorization = &types.SendAuthorization{SpendLimit: newCoins}
+				expAuthorization = &banktypes.SendAuthorization{SpendLimit: newCoins}
 				err := app.AuthzKeeper.Grant(ctx, addrs[0], addrs[1], expAuthorization, now.Add(time.Hour))
 				suite.Require().NoError(err)
 				req = &types.QueryAuthorizationsRequest{
@@ -133,7 +135,7 @@ func (suite *TestSuite) TestGRPCQueryAuthorizations() {
 			},
 			true,
 			func(res *types.QueryAuthorizationsResponse) {
-				var auth types.Authorization
+				var auth exported.Authorization
 				suite.Require().Equal(1, len(res.Authorizations))
 				err := suite.app.InterfaceRegistry().UnpackAny(res.Authorizations[0].Authorization, &auth)
 				suite.Require().NoError(err)

x/authz/types/codec.go
@@ -4,6 +4,9 @@ import (
 	types "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
+	"github.com/cosmos/cosmos-sdk/x/authz/exported"
+	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
+	staking "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
 // RegisterInterfaces registers the interfaces types with the interface registry
@@ -16,9 +19,10 @@ func RegisterInterfaces(registry types.InterfaceRegistry) {
 
 	registry.RegisterInterface(
 		"cosmos.authz.v1beta1.Authorization",
-		(*Authorization)(nil),
-		&SendAuthorization{},
+		(*exported.Authorization)(nil),
+		&bank.SendAuthorization{},
 		&GenericAuthorization{},
+		&staking.StakeAuthorization{},
 	)
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)

Finschia/finschia-sdk

x/authz/client/cli/query.go
@@ -12,6 +12,7 @@ import (
 	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/version"
 	"github.com/line/lbm-sdk/x/authz/types"
+	bank "github.com/line/lbm-sdk/x/bank/types"
 )
 
 // GetQueryCmd returns the cli query commands for this module
@@ -99,7 +100,7 @@ func GetCmdQueryAuthorization() *cobra.Command {
 			fmt.Sprintf(`Query authorization for a granter-grantee pair that matches the given msg-type:
 Example:
 $ %s query %s authorization cosmos1skjw.. cosmos1skjwj.. %s
-`, version.AppName, types.ModuleName, types.SendAuthorization{}.MethodName()),
+`, version.AppName, types.ModuleName, bank.SendAuthorization{}.MethodName()),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)

x/authz/client/cli/query_test.go
@@ -1,0 +1,187 @@
+//go:build norace
+// +build norace
+
+package cli_test
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	ostcli "github.com/line/ostracon/libs/cli"
+
+	"github.com/line/lbm-sdk/client/flags"
+	clitestutil "github.com/line/lbm-sdk/testutil/cli"
+	sdk "github.com/line/lbm-sdk/types"
+	"github.com/line/lbm-sdk/x/authz/client/cli"
+
+	authztestutil "github.com/line/lbm-sdk/x/authz/client/testutil"
+	"github.com/line/lbm-sdk/x/authz/types"
+)
+
+func (s *IntegrationTestSuite) TestQueryAuthorizations() {
+	val := s.network.Validators[0]
+
+	grantee := s.grantee
+	twoHours := time.Now().Add(time.Minute * time.Duration(120)).Unix()
+
+	_, err := authztestutil.ExecGrantAuthorization(
+		val,
+		[]string{
+			grantee.String(),
+			"send",
+			fmt.Sprintf("--%s=100steak", cli.FlagSpendLimit),
+			fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+			fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address),
+			fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+			fmt.Sprintf("--%s=%d", cli.FlagExpiration, twoHours),
+			fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+		},
+	)
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		name      string
+		args      []string
+		expectErr bool
+		expErrMsg string
+	}{
+		{
+			"Error: Invalid grantee",
+			[]string{
+				val.Address.String(),
+				"invalid grantee",
+				fmt.Sprintf("--%s=json", ostcli.OutputFlag),
+			},
+			true,
+			"decoding bech32 failed: invalid character in string: ' '",
+		},
+		{
+			"Error: Invalid granter",
+			[]string{
+				"invalid granter",
+				grantee.String(),
+				fmt.Sprintf("--%s=json", ostcli.OutputFlag),
+			},
+			true,
+			"decoding bech32 failed: invalid character in string: ' '",
+		},
+		{
+			"Valid txn (json)",
+			[]string{
+				val.Address.String(),
+				grantee.String(),
+				fmt.Sprintf("--%s=json", ostcli.OutputFlag),
+			},
+			false,
+			``,
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+
+		s.Run(tc.name, func() {
+			cmd := cli.GetCmdQueryAuthorizations()
+			clientCtx := val.ClientCtx
+			resp, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, tc.args)
+			if tc.expectErr {
+				s.Require().Error(err)
+				s.Require().Contains(string(resp.Bytes()), tc.expErrMsg)
+			} else {
+				s.Require().NoError(err)
+				var grants types.QueryAuthorizationsResponse
+				err = val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp.Bytes(), &grants)
+				s.Require().NoError(err)
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestQueryAuthorization() {
+	val := s.network.Validators[0]
+
+	grantee := s.grantee
+	twoHours := time.Now().Add(time.Minute * time.Duration(120)).Unix()
+
+	_, err := authztestutil.ExecGrantAuthorization(
+		val,
+		[]string{
+			grantee.String(),
+			"send",
+			fmt.Sprintf("--%s=100steak", cli.FlagSpendLimit),
+			fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+			fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address),
+			fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+			fmt.Sprintf("--%s=%d", cli.FlagExpiration, twoHours),
+			fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+		},
+	)
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		name           string
+		args           []string
+		expectErr      bool
+		expectedOutput string
+	}{
+		{
+			"Error: Invalid grantee",
+			[]string{
+				val.Address.String(),
+				"invalid grantee",
+				typeMsgSend,
+				fmt.Sprintf("--%s=json", ostcli.OutputFlag),
+			},
+			true,
+			"",
+		},
+		{
+			"Error: Invalid granter",
+			[]string{
+				"invalid granter",
+				grantee.String(),
+				typeMsgSend,
+				fmt.Sprintf("--%s=json", ostcli.OutputFlag),
+			},
+			true,
+			"",
+		},
+		{
+			"no authorization found",
+			[]string{
+				val.Address.String(),
+				grantee.String(),
+				"typeMsgSend",
+				fmt.Sprintf("--%s=json", ostcli.OutputFlag),
+			},
+			true,
+			"",
+		},
+		{
+			"Valid txn (json)",
+			[]string{
+				val.Address.String(),
+				grantee.String(),
+				typeMsgSend,
+				fmt.Sprintf("--%s=json", ostcli.OutputFlag),
+			},
+			false,
+			`{"@type":"/cosmos.bank.v1beta1.SendAuthorization","spend_limit":[{"denom":"steak","amount":"100"}]}`,
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+
+		s.Run(tc.name, func() {
+			cmd := cli.GetCmdQueryAuthorization()
+			clientCtx := val.ClientCtx
+			out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, tc.args)
+			if tc.expectErr {
+				s.Require().Error(err)
+			} else {
+				s.Require().NoError(err)
+				s.Require().Contains(strings.TrimSpace(out.String()), tc.expectedOutput)
+			}
+		})
+	}
+}

x/authz/exported/authorizations.go
@@ -1,0 +1,21 @@
+package exported
+
+import (
+	"github.com/gogo/protobuf/proto"
+
+	ocproto "github.com/line/ostracon/proto/ostracon/types"
+
+	sdk "github.com/line/lbm-sdk/types"
+)
+
+// Authorization represents the interface of various Authorization types.
+type Authorization interface {
+	proto.Message
+
+	// MethodName returns the fully-qualified Msg service method name as described in ADR 031.
+	MethodName() string
+
+	// Accept determines whether this grant permits the provided sdk.ServiceMsg to be performed, and if
+	// so provides an upgraded authorization instance.
+	Accept(msg sdk.ServiceMsg, block ocproto.Header) (updated Authorization, delete bool, err error)
+}

x/authz/keeper/grpc_query_test.go
@@ -6,14 +6,16 @@ import (
 	"time"
 
 	sdk "github.com/line/lbm-sdk/types"
+	"github.com/line/lbm-sdk/x/authz/exported"
 	"github.com/line/lbm-sdk/x/authz/types"
+	banktypes "github.com/line/lbm-sdk/x/bank/types"
 )
 
 func (suite *TestSuite) TestGRPCQueryAuthorization() {
 	app, ctx, queryClient, addrs := suite.app, suite.ctx, suite.queryClient, suite.addrs
 	var (
 		req              *types.QueryAuthorizationRequest
-		expAuthorization types.Authorization
+		expAuthorization exported.Authorization
 	)
 	testCases := []struct {
 		msg      string
@@ -55,7 +57,7 @@ func (suite *TestSuite) TestGRPCQueryAuthorization() {
 			func() {
 				now := ctx.BlockHeader().Time
 				newCoins := sdk.NewCoins(sdk.NewInt64Coin("steak", 100))
-				expAuthorization = &types.SendAuthorization{SpendLimit: newCoins}
+				expAuthorization = &banktypes.SendAuthorization{SpendLimit: newCoins}
 				err := app.AuthzKeeper.Grant(ctx, addrs[0], addrs[1], expAuthorization, now.Add(time.Hour))
 				suite.Require().NoError(err)
 				req = &types.QueryAuthorizationRequest{
@@ -66,7 +68,7 @@ func (suite *TestSuite) TestGRPCQueryAuthorization() {
 			},
 			true,
 			func(res *types.QueryAuthorizationResponse) {
-				var auth types.Authorization
+				var auth exported.Authorization
 				err := suite.app.InterfaceRegistry().UnpackAny(res.Authorization.Authorization, &auth)
 				suite.Require().NoError(err)
 				suite.Require().NotNil(auth)
@@ -92,7 +94,7 @@ func (suite *TestSuite) TestGRPCQueryAuthorizations() {
 	app, ctx, queryClient, addrs := suite.app, suite.ctx, suite.queryClient, suite.addrs
 	var (
 		req              *types.QueryAuthorizationsRequest
-		expAuthorization types.Authorization
+		expAuthorization exported.Authorization
 	)
 	testCases := []struct {
 		msg      string
@@ -123,7 +125,7 @@ func (suite *TestSuite) TestGRPCQueryAuthorizations() {
 			func() {
 				now := ctx.BlockHeader().Time
 				newCoins := sdk.NewCoins(sdk.NewInt64Coin("steak", 100))
-				expAuthorization = &types.SendAuthorization{SpendLimit: newCoins}
+				expAuthorization = &banktypes.SendAuthorization{SpendLimit: newCoins}
 				err := app.AuthzKeeper.Grant(ctx, addrs[0], addrs[1], expAuthorization, now.Add(time.Hour))
 				suite.Require().NoError(err)
 				req = &types.QueryAuthorizationsRequest{
@@ -133,7 +135,7 @@ func (suite *TestSuite) TestGRPCQueryAuthorizations() {
 			},
 			true,
 			func(res *types.QueryAuthorizationsResponse) {
-				var auth types.Authorization
+				var auth exported.Authorization
 				suite.Require().Equal(1, len(res.Authorizations))
 				err := suite.app.InterfaceRegistry().UnpackAny(res.Authorizations[0].Authorization, &auth)
 				suite.Require().NoError(err)

x/authz/types/codec.go
@@ -4,6 +4,9 @@ import (
 	types "github.com/line/lbm-sdk/codec/types"
 	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/types/msgservice"
+	"github.com/line/lbm-sdk/x/authz/exported"
+	bank "github.com/line/lbm-sdk/x/bank/types"
+	staking "github.com/line/lbm-sdk/x/staking/types"
 )
 
 // RegisterInterfaces registers the interfaces types with the interface registry
@@ -16,9 +19,10 @@ func RegisterInterfaces(registry types.InterfaceRegistry) {
 
 	registry.RegisterInterface(
 		"lbm.authz.v1.Authorization",
-		(*Authorization)(nil),
-		&SendAuthorization{},
+		(*exported.Authorization)(nil),
+		&bank.SendAuthorization{},
 		&GenericAuthorization{},
+		&staking.StakeAuthorization{},
 	)
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)