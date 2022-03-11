cosmos/cosmos-sdk

[Bank] Remove the unsafe balance changing API (#8473)
* temp commit

* setbalance now is internal

* remove set balances in genesis

* feedback test commit

* update tests

* fix: genesis panic message

* fix not bonded pool

* fix(staking): genesis test

* fix(simapp): rollback state fix change

* fix(staking): genesis large val set test

* [Bank Refactor] Frojdi jonathan/remove setsupply (#8491)

* init supply in a different way

* remove external usage of set supply

* change(staking): replace SetSupply with MintCoins in tests

* change(evidence): replace SetSupply with MintCoins in tests

* change(crisis): remove SetSupply in tests

* change(bank): remove set supply from genesis tests

* change(bank): remove set supply from keeper tests

* change(bank): remove remaining set supply usage from keeper tests

* change(bank): remove set supply usage from grpc query and querier tests

* change(bank): remove SetSupply from keeper interface

Co-authored-by: Frojdi Dymylja <frojdi.dymylja@gmail.com>

* remove setbalances from genesis in gov

* remove keyring

* add init genesis state

* change(staking): make genesis checks coherent and add tests

* remove setbalances on distribution

* fix(staking): genesis tests

* [Bank Refactor]: Remove SetBalances usage from the code and tests (#8509)

* change(distribution): remove SetBalances usage from keeper tests

* add(simapp): FundAccount utility function

* chore(staking): use FundAccount in keeper tests

* change(staking): remove usage of SetBalance in allocation tests

* change(staking): remove usage of SetBalance in delegation tests

* change(staking): remove usage of SetBalance in proposal handler tests

* change(staking): remove usage of SetBalances in grpc query tests

* change(staking): remove usage of SetBalances in operations tests

* change(distribution): remove usage of SetBalances in genesis

* change(authz): remove usage of SetBalances keeper and operations test

* fix(auth

types/query/filtered_pagination_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/address"
@@ -27,10 +28,11 @@ func (s *paginationTestSuite) TestFilteredPaginations() {
 		balances = append(balances, sdk.NewInt64Coin(denom, 250))
 	}
 
+	balances = balances.Sort()
 	addr1 := sdk.AccAddress([]byte("addr1"))
 	acc1 := app.AccountKeeper.NewAccountWithAddress(ctx, addr1)
 	app.AccountKeeper.SetAccount(ctx, acc1)
-	s.Require().NoError(app.BankKeeper.SetBalances(ctx, addr1, balances))
+	s.Require().NoError(simapp.FundAccount(app, ctx, addr1, balances))
 	store := ctx.KVStore(app.GetKey(types.StoreKey))
 
 	// verify pagination with limit > total values
@@ -100,10 +102,12 @@ func ExampleFilteredPaginate() {
 		denom := fmt.Sprintf("test%ddenom", i)
 		balances = append(balances, sdk.NewInt64Coin(denom, 250))
 	}
+
+	balances = balances.Sort()
 	addr1 := sdk.AccAddress([]byte("addr1"))
 	acc1 := app.AccountKeeper.NewAccountWithAddress(ctx, addr1)
 	app.AccountKeeper.SetAccount(ctx, acc1)
-	err := app.BankKeeper.SetBalances(ctx, addr1, balances)
+	err := simapp.FundAccount(app, ctx, addr1, balances)
 	if err != nil { // should return no error
 		fmt.Println(err)
 	}

x/bank/keeper/genesis_test.go
@@ -3,43 +3,53 @@ package keeper_test
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/bank/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 )
 
 func (suite *IntegrationTestSuite) TestExportGenesis() {
 	app, ctx := suite.app, suite.ctx
 
 	expectedMetadata := suite.getTestMetadata()
-	expectedBalances := suite.getTestBalances()
+	expectedBalances, totalSupply := suite.getTestBalancesAndSupply()
 	for i := range []int{1, 2} {
 		app.BankKeeper.SetDenomMetaData(ctx, expectedMetadata[i])
 		accAddr, err1 := sdk.AccAddressFromBech32(expectedBalances[i].Address)
 		if err1 != nil {
 			panic(err1)
 		}
-		err := app.BankKeeper.SetBalances(ctx, accAddr, expectedBalances[i].Coins)
-		suite.Require().NoError(err)
+		// set balances via mint and send
+		suite.
+			Require().
+			NoError(app.BankKeeper.MintCoins(ctx, minttypes.ModuleName, expectedBalances[i].Coins))
+		suite.
+			Require().
+			NoError(app.BankKeeper.SendCoinsFromModuleToAccount(ctx, minttypes.ModuleName, accAddr, expectedBalances[i].Coins))
 	}
-
-	totalSupply := types.NewSupply(sdk.NewCoins(sdk.NewInt64Coin("test", 400000000)))
-	app.BankKeeper.SetSupply(ctx, totalSupply)
+	// add mint module balance as nil
+	expectedBalances = append(expectedBalances, types.Balance{Address: "cosmos1m3h30wlvsf8llruxtpukdvsy0km2kum8g38c8q", Coins: nil})
 	app.BankKeeper.SetParams(ctx, types.DefaultParams())
 
 	exportGenesis := app.BankKeeper.ExportGenesis(ctx)
 
 	suite.Require().Len(exportGenesis.Params.SendEnabled, 0)
 	suite.Require().Equal(types.DefaultParams().DefaultSendEnabled, exportGenesis.Params.DefaultSendEnabled)
-	suite.Require().Equal(totalSupply.GetTotal(), exportGenesis.Supply)
+	suite.Require().Equal(totalSupply.Total, exportGenesis.Supply)
 	suite.Require().Equal(expectedBalances, exportGenesis.Balances)
 	suite.Require().Equal(expectedMetadata, exportGenesis.DenomMetadata)
 }
 
-func (suite *IntegrationTestSuite) getTestBalances() []types.Balance {
+func (suite *IntegrationTestSuite) getTestBalancesAndSupply() ([]types.Balance, *types.Supply) {
 	addr2, _ := sdk.AccAddressFromBech32("cosmos1f9xjhxm0plzrh9cskf4qee4pc2xwp0n0556gh0")
 	addr1, _ := sdk.AccAddressFromBech32("cosmos1fl48vsnmsdzcv85q5d2q4z5ajdha8yu34mf0eh")
+	addr1Balance := sdk.Coins{sdk.NewInt64Coin("testcoin3", 10)}
+	addr2Balance := sdk.Coins{sdk.NewInt64Coin("testcoin1", 32), sdk.NewInt64Coin("testcoin2", 34)}
+
+	totalSupply := types.NewSupply(addr1Balance)
+	totalSupply.Inflate(addr2Balance)
 	return []types.Balance{
-		{Address: addr2.String(), Coins: sdk.Coins{sdk.NewInt64Coin("testcoin1", 32), sdk.NewInt64Coin("testcoin2", 34)}},
-		{Address: addr1.String(), Coins: sdk.Coins{sdk.NewInt64Coin("testcoin3", 10)}},
-	}
+		{Address: addr2.String(), Coins: addr2Balance},
+		{Address: addr1.String(), Coins: addr1Balance},
+	}, totalSupply
 
 }
 

x/gov/genesis_test.go
@@ -11,6 +11,7 @@ import (
 	dbm "github.com/tendermint/tm-db"
 
 	"github.com/cosmos/cosmos-sdk/simapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
@@ -109,6 +110,27 @@ func TestImportExportQueues(t *testing.T) {
 	proposal2, ok = app2.GovKeeper.GetProposal(ctx2, proposalID2)
 	require.True(t, ok)
 	require.True(t, proposal2.Status == types.StatusRejected)
+}
+
+func TestImportExportQueues_ErrorUnconsistentState(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+	require.Panics(t, func() {
+		gov.InitGenesis(ctx, app.AccountKeeper, app.BankKeeper, app.GovKeeper, &types.GenesisState{
+			Deposits: types.Deposits{
+				{
+					1234,
+					"me",
+					sdk.Coins{
+						{
+							"stake",
+							sdk.NewInt(1234),
+						},
+					},
+				},
+			},
+		})
+	})
 }
 
 func TestEqualProposals(t *testing.T) {

x/staking/genesis_test.go
@@ -2,47 +2,39 @@ package staking_test
 
 import (
 	"fmt"
+	"log"
 	"testing"
+
+	auth "github.com/cosmos/cosmos-sdk/x/auth/types"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 	"github.com/cosmos/cosmos-sdk/x/staking/teststaking"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
-func bootstrapGenesisTest(t *testing.T, power int64, numAddrs int) (*simapp.SimApp, sdk.Context, []sdk.AccAddress) {
+func bootstrapGenesisTest(numAddrs int) (*simapp.SimApp, sdk.Context, []sdk.AccAddress) {
 	_, app, ctx := getBaseSimappWithCustomKeeper()
 
 	addrDels, _ := generateAddresses(app, ctx, numAddrs, sdk.NewInt(10000))
-
-	amt := sdk.TokensFromConsensusPower(power)
-	totalSupply := sdk.NewCoins(sdk.NewCoin(app.StakingKeeper.BondDenom(ctx), amt.MulRaw(int64(len(addrDels)))))
-
-	notBondedPool := app.StakingKeeper.GetNotBondedPool(ctx)
-	err := app.BankKeeper.SetBalances(ctx, notBondedPool.GetAddress(), totalSupply)
-	require.NoError(t, err)
-
-	app.AccountKeeper.SetModuleAccount(ctx, notBondedPool)
-	app.BankKeeper.SetSupply(ctx, banktypes.NewSupply(totalSupply))
-
 	return app, ctx, addrDels
 }
 
 func TestInitGenesis(t *testing.T) {
-	app, ctx, addrs := bootstrapGenesisTest(t, 1000, 10)
+	app, ctx, addrs := bootstrapGenesisTest(10)
 
 	valTokens := sdk.TokensFromConsensusPower(1)
 
 	params := app.StakingKeeper.GetParams(ctx)
-	validators := make([]types.Validator, 2)
+	validators := app.StakingKeeper.GetAllValidators(ctx)
 	var delegations []types.Delegation
 
 	pk0, err := codectypes.NewAnyWithValue(PKs[0])
@@ -52,19 +44,40 @@ func TestInitGenesis(t *testing.T) {
 	require.NoError(t, err)
 
 	// initialize the validators
-	validators[0].OperatorAddress = sdk.ValAddress(addrs[0]).String()
-	validators[0].ConsensusPubkey = pk0
-	validators[0].Description = types.NewDescription("hoop", "", "", "", "")
-	validators[0].Status = types.Bonded
-	validators[0].Tokens = valTokens
-	validators[0].DelegatorShares = valTokens.ToDec()
-	validators[1].OperatorAddress = sdk.ValAddress(addrs[1]).String()
-	validators[1].ConsensusPubkey = pk1
-	validators[1].Description = types.NewDescription("bloop", "", "", "", "")
-	validators[1].Status = types.Bonded
-	validators[1].Tokens = valTokens
-	validators[1].DelegatorShares = valTokens.ToDec()
+	bondedVal1 := types.Validator{
+		OperatorAddress: sdk.ValAddress(addrs[0]).String(),
+		ConsensusPubkey: pk0,
+		Status:          types.Bonded,
+		Tokens:          valTokens,
+		DelegatorShares: valTokens.ToDec(),
+		Description:     types.NewDescription("hoop", "", "", "", ""),
+	}
+	bondedVal2 := types.Validator{
+		OperatorAddress: sdk.ValAddress(addrs[1]).String(),
+		ConsensusPubkey: pk1,
+		Status:          types.Bonded,
+		Tokens:          valTokens,
+		DelegatorShares: valTokens.ToDec(),
+		Description:     types.NewDescription("bloop", "", "", "", ""),
+	}
 
+	// append new bonded validators to the list
+	validators = append(validators, bondedVal1, bondedVal2)
+	log.Printf("%#v", len(validators))
+	// mint coins in the bonded pool representing the validators coins
+	require.NoError(t,
+		simapp.FundAccount(
+			app,
+			ctx,
+			auth.NewModuleAddress(types.BondedPoolName),
+			sdk.NewCoins(
+				sdk.NewCoin(
+					params.BondDenom,
+					valTokens.MulRaw((int64)(len(validators))),
+				),
+			),
+		),
+	)
 	genesisState := types.NewGenesisState(params, validators, delegations)
 	vals := staking.InitGenesis(ctx, app.StakingKeeper, app.AccountKeeper, app.BankKeeper, genesisState)
 
@@ -97,16 +110,63 @@ func TestInitGenesis(t *testing.T) {
 	require.Equal(t, abcivals, vals)
 }
 
+func TestInitGenesis_PoolsBalanceMismatch(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.NewContext(false, tmproto.Header{})
+
+	consPub, err := codectypes.NewAnyWithValue(PKs[0])
+	require.NoError(t, err)
+
+	// create mock validator
+	validator := types.Validator{
+		OperatorAddress: sdk.ValAddress("12345678901234567890").String(),
+		ConsensusPubkey: consPub,
+		Jailed:          false,
+		Tokens:          sdk.NewInt(10),
+		DelegatorShares: sdk.NewInt(10).ToDec(),
+		Description:     types.NewDescription("bloop", "", "", "", ""),
+	}
+	// valid params
+	params := types.Params{
+		UnbondingTime: 10000,
+		MaxValidators: 1,
+		MaxEntries:    10,
+		BondDenom:     "stake",
+	}
+
+	// test
+
+	require.Panics(t, func() {
+		// setting validator status to bonded so the balance counts towards bonded pool
+		validator.Status = types.Bonded
+		staking.InitGenesis(ctx, app.StakingKeeper, app.AccountKeeper, app.BankKeeper, &types.GenesisState{
+			Params:     params,
+			Validators: []types.Validator{validator},
+		})
+	}, "should panic because bonded pool balance is different from bonded pool coins")
+
+	require.Panics(t, func() {
+		// setting validator status to unbonded so the balance counts towards not bonded pool
+		validator.Status = types.Unbonded
+		staking.InitGenesis(ctx, app.StakingKeeper, app.AccountKeeper, app.BankKeeper, &types.GenesisState{
+			Params:     params,
+			Validators: []types.Validator{validator},
+		})
+	}, "should panic because not bonded pool balance is different from not bonded pool coins")
+}
+
 func TestInitGenesisLargeValidatorSet(t *testing.T) {
 	size := 200
 	require.True(t, size > 100)
 
-	app, ctx, addrs := bootstrapGenesisTest(t, 1000, 200)
+	app, ctx, addrs := bootstrapGenesisTest(200)
 
 	params := app.StakingKeeper.GetParams(ctx)
 	delegations := []types.Delegation{}
 	validators := make([]types.Validator, size)
 	var err error
+
+	bondedPoolAmt := sdk.ZeroInt()
 	for i := range validators {
 		validators[i], err = types.NewValidator(sdk.ValAddress(addrs[i]),
 			PKs[i], types.NewDescription(fmt.Sprintf("#%d", i), "", "", "", ""))
@@ -119,9 +179,22 @@ func TestInitGenesisLargeValidatorSet(t *testing.T) {
 		}
 		validators[i].Tokens = tokens
 		validators[i].DelegatorShares = tokens.ToDec()
+		// add bonded coins
+		bondedPoolAmt = bondedPoolAmt.Add(tokens)
 	}
-
 	genesisState := types.NewGenesisState(params, validators, delegations)
+	// mint coins in the bonded pool representing the validators coins
+	require.NoError(t,
+		simapp.FundAccount(
+			app,
+			ctx,
+			auth.NewModuleAddress(types.BondedPoolName),
+			sdk.NewCoins(
+				sdk.NewCoin(params.BondDenom, bondedPoolAmt),
+			),
+		),
+	)
+
 	vals := staking.InitGenesis(ctx, app.StakingKeeper, app.AccountKeeper, app.BankKeeper, genesisState)
 
 	abcivals := make([]abci.ValidatorUpdate, 100)

Finschia/finschia-sdk

types/query/filtered_pagination_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/line/lbm-sdk/codec"
+	"github.com/line/lbm-sdk/simapp"
 	"github.com/line/lbm-sdk/store/prefix"
 	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/types/address"
@@ -27,10 +28,11 @@ func (s *paginationTestSuite) TestFilteredPaginations() {
 		balances = append(balances, sdk.NewInt64Coin(denom, 250))
 	}
 
+	balances = balances.Sort()
 	addr1 := sdk.BytesToAccAddress([]byte("addr1"))
 	acc1 := app.AccountKeeper.NewAccountWithAddress(ctx, addr1)
 	app.AccountKeeper.SetAccount(ctx, acc1)
-	s.Require().NoError(app.BankKeeper.SetBalances(ctx, addr1, balances))
+	s.Require().NoError(simapp.FundAccount(app, ctx, addr1, balances))
 	store := ctx.KVStore(app.GetKey(types.StoreKey))
 
 	// verify pagination with limit > total values
@@ -100,10 +102,12 @@ func ExampleFilteredPaginate() {
 		denom := fmt.Sprintf("test%ddenom", i)
 		balances = append(balances, sdk.NewInt64Coin(denom, 250))
 	}
+
+	balances = balances.Sort()
 	addr1 := sdk.AccAddress([]byte("addr1"))
 	acc1 := app.AccountKeeper.NewAccountWithAddress(ctx, addr1)
 	app.AccountKeeper.SetAccount(ctx, acc1)
-	err := app.BankKeeper.SetBalances(ctx, addr1, balances)
+	err := simapp.FundAccount(app, ctx, addr1, balances)
 	if err != nil { // should return no error
 		fmt.Println(err)
 	}

x/bank/keeper/genesis_test.go
@@ -3,43 +3,53 @@ package keeper_test
 import (
 	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/x/bank/types"
+	minttypes "github.com/line/lbm-sdk/x/mint/types"
 )
 
 func (suite *IntegrationTestSuite) TestExportGenesis() {
 	app, ctx := suite.app, suite.ctx
 
 	expectedMetadata := suite.getTestMetadata()
-	expectedBalances := suite.getTestBalances()
+	expectedBalances, totalSupply := suite.getTestBalancesAndSupply()
 	for i := range []int{1, 2} {
 		app.BankKeeper.SetDenomMetaData(ctx, expectedMetadata[i])
 		err1 := sdk.ValidateAccAddress(expectedBalances[i].Address)
 		if err1 != nil {
 			panic(err1)
 		}
-		err := app.BankKeeper.SetBalances(ctx, sdk.AccAddress(expectedBalances[i].Address), expectedBalances[i].Coins)
-		suite.Require().NoError(err)
+		// set balances via mint and send
+		suite.
+			Require().
+			NoError(app.BankKeeper.MintCoins(ctx, minttypes.ModuleName, expectedBalances[i].Coins))
+		suite.
+			Require().
+			NoError(app.BankKeeper.SendCoinsFromModuleToAccount(ctx, minttypes.ModuleName, sdk.AccAddress(expectedBalances[i].Address), expectedBalances[i].Coins))
 	}
-
-	totalSupply := types.NewSupply(sdk.NewCoins(sdk.NewInt64Coin("test", 400000000)))
-	app.BankKeeper.SetSupply(ctx, totalSupply)
+	// add mint module balance as nil
+	expectedBalances = append(expectedBalances, types.Balance{Address: "cosmos1m3h30wlvsf8llruxtpukdvsy0km2kum8g38c8q", Coins: nil})
 	app.BankKeeper.SetParams(ctx, types.DefaultParams())
 
 	exportGenesis := app.BankKeeper.ExportGenesis(ctx)
 
 	suite.Require().Len(exportGenesis.Params.SendEnabled, 0)
 	suite.Require().Equal(types.DefaultParams().DefaultSendEnabled, exportGenesis.Params.DefaultSendEnabled)
-	suite.Require().Equal(totalSupply.GetTotal(), exportGenesis.Supply)
+	suite.Require().Equal(totalSupply.Total, exportGenesis.Supply)
 	suite.Require().Equal(expectedBalances, exportGenesis.Balances)
 	suite.Require().Equal(expectedMetadata, exportGenesis.DenomMetadata)
 }
 
-func (suite *IntegrationTestSuite) getTestBalances() []types.Balance {
-	addr2 := sdk.AccAddress("link15klks9yty6rwvnqk47q4cg92r38qj4gsvxdfj8")
-	addr1 := sdk.AccAddress("link14uxsrqf2cakphyw9ywwy9a9fv7yjspku4lkrny")
+func (suite *IntegrationTestSuite) getTestBalancesAndSupply() ([]types.Balance, *types.Supply) {
+	addr2 := sdk.AccAddress("line1f9xjhxm0plzrh9cskf4qee4pc2xwp0n0556gh0")
+	addr1 := sdk.AccAddress("line1fl48vsnmsdzcv85q5d2q4z5ajdha8yu34mf0eh")
+	addr1Balance := sdk.Coins{sdk.NewInt64Coin("testcoin3", 10)}
+	addr2Balance := sdk.Coins{sdk.NewInt64Coin("testcoin1", 32), sdk.NewInt64Coin("testcoin2", 34)}
+
+	totalSupply := types.NewSupply(addr1Balance)
+	totalSupply.Inflate(addr2Balance)
 	return []types.Balance{
-		{Address: addr1.String(), Coins: sdk.Coins{sdk.NewInt64Coin("testcoin3", 10)}},
-		{Address: addr2.String(), Coins: sdk.Coins{sdk.NewInt64Coin("testcoin1", 32), sdk.NewInt64Coin("testcoin2", 34)}},
-	}
+		{Address: addr2.String(), Coins: addr2Balance},
+		{Address: addr1.String(), Coins: addr1Balance},
+	}, totalSupply
 
 }
 

x/gov/genesis_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/line/lbm-sdk/simapp"
+	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/x/auth"
 	authtypes "github.com/line/lbm-sdk/x/auth/types"
 	banktypes "github.com/line/lbm-sdk/x/bank/types"
@@ -109,6 +110,27 @@ func TestImportExportQueues(t *testing.T) {
 	proposal2, ok = app2.GovKeeper.GetProposal(ctx2, proposalID2)
 	require.True(t, ok)
 	require.True(t, proposal2.Status == types.StatusRejected)
+}
+
+func TestImportExportQueues_ErrorUnconsistentState(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, ocproto.Header{})
+	require.Panics(t, func() {
+		gov.InitGenesis(ctx, app.AccountKeeper, app.BankKeeper, app.GovKeeper, &types.GenesisState{
+			Deposits: types.Deposits{
+				{
+					ProposalId: 1234,
+					Depositor:  "me",
+					Amount: sdk.Coins{
+						sdk.NewCoin(
+							"stake",
+							sdk.NewInt(1234),
+						),
+					},
+				},
+			},
+		})
+	})
 }
 
 func TestEqualProposals(t *testing.T) {

x/staking/genesis_test.go
@@ -2,9 +2,11 @@ package staking_test
 
 import (
 	"fmt"
+	"log"
 	"testing"
 
 	abci "github.com/line/ostracon/abci/types"
+	tmproto "github.com/line/ostracon/proto/ostracon/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -12,37 +14,26 @@ import (
 	"github.com/line/lbm-sdk/crypto/keys/ed25519"
 	"github.com/line/lbm-sdk/simapp"
 	sdk "github.com/line/lbm-sdk/types"
-	banktypes "github.com/line/lbm-sdk/x/bank/types"
+	auth "github.com/line/lbm-sdk/x/auth/types"
 	"github.com/line/lbm-sdk/x/staking"
 	"github.com/line/lbm-sdk/x/staking/teststaking"
 	"github.com/line/lbm-sdk/x/staking/types"
 )
 
-func bootstrapGenesisTest(t *testing.T, power int64, numAddrs int) (*simapp.SimApp, sdk.Context, []sdk.AccAddress) {
+func bootstrapGenesisTest(numAddrs int) (*simapp.SimApp, sdk.Context, []sdk.AccAddress) {
 	_, app, ctx := getBaseSimappWithCustomKeeper()
 
 	addrDels, _ := generateAddresses(app, ctx, numAddrs, sdk.NewInt(10000))
-
-	amt := sdk.TokensFromConsensusPower(power)
-	totalSupply := sdk.NewCoins(sdk.NewCoin(app.StakingKeeper.BondDenom(ctx), amt.MulRaw(int64(len(addrDels)))))
-
-	notBondedPool := app.StakingKeeper.GetNotBondedPool(ctx)
-	err := app.BankKeeper.SetBalances(ctx, notBondedPool.GetAddress(), totalSupply)
-	require.NoError(t, err)
-
-	app.AccountKeeper.SetModuleAccount(ctx, notBondedPool)
-	app.BankKeeper.SetSupply(ctx, banktypes.NewSupply(totalSupply))
-
 	return app, ctx, addrDels
 }
 
 func TestInitGenesis(t *testing.T) {
-	app, ctx, addrs := bootstrapGenesisTest(t, 1000, 10)
+	app, ctx, addrs := bootstrapGenesisTest(10)
 
 	valTokens := sdk.TokensFromConsensusPower(1)
 
 	params := app.StakingKeeper.GetParams(ctx)
-	validators := make([]types.Validator, 2)
+	validators := app.StakingKeeper.GetAllValidators(ctx)
 	var delegations []types.Delegation
 
 	pk0, err := codectypes.NewAnyWithValue(PKs[0])
@@ -52,19 +43,40 @@ func TestInitGenesis(t *testing.T) {
 	require.NoError(t, err)
 
 	// initialize the validators
-	validators[0].OperatorAddress = addrs[0].ToValAddress().String()
-	validators[0].ConsensusPubkey = pk0
-	validators[0].Description = types.NewDescription("hoop", "", "", "", "")
-	validators[0].Status = types.Bonded
-	validators[0].Tokens = valTokens
-	validators[0].DelegatorShares = valTokens.ToDec()
-	validators[1].OperatorAddress = addrs[1].ToValAddress().String()
-	validators[1].ConsensusPubkey = pk1
-	validators[1].Description = types.NewDescription("bloop", "", "", "", "")
-	validators[1].Status = types.Bonded
-	validators[1].Tokens = valTokens
-	validators[1].DelegatorShares = valTokens.ToDec()
+	bondedVal1 := types.Validator{
+		OperatorAddress: addrs[0].ToValAddress().String(),
+		ConsensusPubkey: pk0,
+		Status:          types.Bonded,
+		Tokens:          valTokens,
+		DelegatorShares: valTokens.ToDec(),
+		Description:     types.NewDescription("hoop", "", "", "", ""),
+	}
+	bondedVal2 := types.Validator{
+		OperatorAddress: addrs[1].ToValAddress().String(),
+		ConsensusPubkey: pk1,
+		Status:          types.Bonded,
+		Tokens:          valTokens,
+		DelegatorShares: valTokens.ToDec(),
+		Description:     types.NewDescription("bloop", "", "", "", ""),
+	}
 
+	// append new bonded validators to the list
+	validators = append(validators, bondedVal1, bondedVal2)
+	log.Printf("%#v", len(validators))
+	// mint coins in the bonded pool representing the validators coins
+	require.NoError(t,
+		simapp.FundAccount(
+			app,
+			ctx,
+			auth.NewModuleAddress(types.BondedPoolName),
+			sdk.NewCoins(
+				sdk.NewCoin(
+					params.BondDenom,
+					valTokens.MulRaw((int64)(len(validators))),
+				),
+			),
+		),
+	)
 	genesisState := types.NewGenesisState(params, validators, delegations)
 	vals := staking.InitGenesis(ctx, app.StakingKeeper, app.AccountKeeper, app.BankKeeper, genesisState)
 
@@ -97,16 +109,63 @@ func TestInitGenesis(t *testing.T) {
 	require.Equal(t, abcivals, vals)
 }
 
+func TestInitGenesis_PoolsBalanceMismatch(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.NewContext(false, tmproto.Header{})
+
+	consPub, err := codectypes.NewAnyWithValue(PKs[0])
+	require.NoError(t, err)
+
+	// create mock validator
+	validator := types.Validator{
+		OperatorAddress: sdk.ValAddress("12345678901234567890").String(),
+		ConsensusPubkey: consPub,
+		Jailed:          false,
+		Tokens:          sdk.NewInt(10),
+		DelegatorShares: sdk.NewInt(10).ToDec(),
+		Description:     types.NewDescription("bloop", "", "", "", ""),
+	}
+	// valid params
+	params := types.Params{
+		UnbondingTime: 10000,
+		MaxValidators: 1,
+		MaxEntries:    10,
+		BondDenom:     "stake",
+	}
+
+	// test
+
+	require.Panics(t, func() {
+		// setting validator status to bonded so the balance counts towards bonded pool
+		validator.Status = types.Bonded
+		staking.InitGenesis(ctx, app.StakingKeeper, app.AccountKeeper, app.BankKeeper, &types.GenesisState{
+			Params:     params,
+			Validators: []types.Validator{validator},
+		})
+	}, "should panic because bonded pool balance is different from bonded pool coins")
+
+	require.Panics(t, func() {
+		// setting validator status to unbonded so the balance counts towards not bonded pool
+		validator.Status = types.Unbonded
+		staking.InitGenesis(ctx, app.StakingKeeper, app.AccountKeeper, app.BankKeeper, &types.GenesisState{
+			Params:     params,
+			Validators: []types.Validator{validator},
+		})
+	}, "should panic because not bonded pool balance is different from not bonded pool coins")
+}
+
 func TestInitGenesisLargeValidatorSet(t *testing.T) {
 	size := 200
 	require.True(t, size > 100)
 
-	app, ctx, addrs := bootstrapGenesisTest(t, 1000, 200)
+	app, ctx, addrs := bootstrapGenesisTest(200)
 
 	params := app.StakingKeeper.GetParams(ctx)
 	delegations := []types.Delegation{}
 	validators := make([]types.Validator, size)
 	var err error
+
+	bondedPoolAmt := sdk.ZeroInt()
 	for i := range validators {
 		validators[i], err = types.NewValidator(addrs[i].ToValAddress(),
 			PKs[i], types.NewDescription(fmt.Sprintf("#%d", i), "", "", "", ""))
@@ -119,9 +178,22 @@ func TestInitGenesisLargeValidatorSet(t *testing.T) {
 		}
 		validators[i].Tokens = tokens
 		validators[i].DelegatorShares = tokens.ToDec()
+		// add bonded coins
+		bondedPoolAmt = bondedPoolAmt.Add(tokens)
 	}
-
 	genesisState := types.NewGenesisState(params, validators, delegations)
+	// mint coins in the bonded pool representing the validators coins
+	require.NoError(t,
+		simapp.FundAccount(
+			app,
+			ctx,
+			auth.NewModuleAddress(types.BondedPoolName),
+			sdk.NewCoins(
+				sdk.NewCoin(params.BondDenom, bondedPoolAmt),
+			),
+		),
+	)
+
 	vals := staking.InitGenesis(ctx, app.StakingKeeper, app.AccountKeeper, app.BankKeeper, genesisState)
 
 	abcivals := make([]abci.ValidatorUpdate, 100)