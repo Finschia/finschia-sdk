cosmos/cosmos-sdk

add reverse iteration to pagination (#8875)
* WIP

* add tests

* add tests

* fix lint

* fix pagination

* add proto message doc

* fix filtered_pagination

* fix test

* cleanup

* add reverse flag to pagination

* changelog

* Update client/flags/flags.go

* Update CHANGELOG.md

Co-authored-by: Alessio Treglia <alessio@tendermint.com>
Co-authored-by: Federico Kunze <31522760+fedekunze@users.noreply.github.com>

types/query/pagination.go
@@ -7,6 +7,7 @@ import (
 	"google.golang.org/grpc/status"
 
 	"github.com/cosmos/cosmos-sdk/store/types"
+	db "github.com/tendermint/tm-db"
 )
 
 // DefaultLimit is the default `limit` for queries
@@ -54,6 +55,7 @@ func Paginate(
 	key := pageRequest.Key
 	limit := pageRequest.Limit
 	countTotal := pageRequest.CountTotal
+	reverse := pageRequest.Reverse
 
 	if offset > 0 && key != nil {
 		return nil, fmt.Errorf("invalid request, either offset or key is expected, got both")
@@ -67,13 +69,14 @@ func Paginate(
 	}
 
 	if len(key) != 0 {
-		iterator := prefixStore.Iterator(key, nil)
+		iterator := getIterator(prefixStore, key, reverse)
 		defer iterator.Close()
 
 		var count uint64
 		var nextKey []byte
 
 		for ; iterator.Valid(); iterator.Next() {
+
 			if count == limit {
 				nextKey = iterator.Key()
 				break
@@ -94,7 +97,7 @@ func Paginate(
 		}, nil
 	}
 
-	iterator := prefixStore.Iterator(nil, nil)
+	iterator := getIterator(prefixStore, nil, reverse)
 	defer iterator.Close()
 
 	end := offset + limit
@@ -131,4 +134,20 @@ func Paginate(
 	}
 
 	return res, nil
+}
+
+func getIterator(prefixStore types.KVStore, start []byte, reverse bool) db.Iterator {
+	if reverse {
+		var end []byte
+		if start != nil {
+			itr := prefixStore.Iterator(start, nil)
+			defer itr.Close()
+			if itr.Valid() {
+				itr.Next()
+				end = itr.Key()
+			}
+		}
+		return prefixStore.ReverseIterator(nil, end)
+	}
+	return prefixStore.Iterator(start, nil)
 }

Finschia/finschia-sdk

types/query/pagination.go
@@ -3,6 +3,7 @@ package query
 import (
 	"fmt"
 
+	tmdb "github.com/line/tm-db/v2"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -54,6 +55,7 @@ func Paginate(
 	key := pageRequest.Key
 	limit := pageRequest.Limit
 	countTotal := pageRequest.CountTotal
+	reverse := pageRequest.Reverse
 
 	if offset > 0 && key != nil {
 		return nil, fmt.Errorf("invalid request, either offset or key is expected, got both")
@@ -67,13 +69,14 @@ func Paginate(
 	}
 
 	if len(key) != 0 {
-		iterator := prefixStore.Iterator(key, nil)
+		iterator := getIterator(prefixStore, key, reverse)
 		defer iterator.Close()
 
 		var count uint64
 		var nextKey []byte
 
 		for ; iterator.Valid(); iterator.Next() {
+
 			if count == limit {
 				nextKey = iterator.Key()
 				break
@@ -94,7 +97,7 @@ func Paginate(
 		}, nil
 	}
 
-	iterator := prefixStore.Iterator(nil, nil)
+	iterator := getIterator(prefixStore, nil, reverse)
 	defer iterator.Close()
 
 	end := offset + limit
@@ -131,4 +134,20 @@ func Paginate(
 	}
 
 	return res, nil
+}
+
+func getIterator(prefixStore types.KVStore, start []byte, reverse bool) tmdb.Iterator {
+	if reverse {
+		var end []byte
+		if start != nil {
+			itr := prefixStore.Iterator(start, nil)
+			defer itr.Close()
+			if itr.Valid() {
+				itr.Next()
+				end = itr.Key()
+			}
+		}
+		return prefixStore.ReverseIterator(nil, end)
+	}
+	return prefixStore.Iterator(start, nil)
 }