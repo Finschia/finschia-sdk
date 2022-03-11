cosmos/cosmos-sdk

move feegrant ante to auth ante (#8682)
* move feegrant ante to auth ante

* update ante builder

* remove commented code

* review changes

* fix lint

* review changes

* review changes

* review changes

* review changes

* fix ante builder

* review changes

* update ante builder with options struct

* review changes

* review changes

* add changelog

Co-authored-by: Amaury <1293565+amaurym@users.noreply.github.com>
Co-authored-by: Aaron Craelius <aaron@regen.network>

x/auth/ante/ante.go
@@ -2,32 +2,57 @@ package ante
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/auth/signing"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 )
+
+// HandlerOptions are the options required for constructing a default SDK AnteHandler.
+type HandlerOptions struct {
+	AccountKeeper   AccountKeeper
+	BankKeeper      types.BankKeeper
+	FeegrantKeeper  FeegrantKeeper
+	SignModeHandler authsigning.SignModeHandler
+	SigGasConsumer  func(meter sdk.GasMeter, sig signing.SignatureV2, params types.Params) error
+}
 
 // NewAnteHandler returns an AnteHandler that checks and increments sequence
 // numbers, checks signatures & account numbers, and deducts fees from the first
 // signer.
-func NewAnteHandler(
-	ak AccountKeeper, bankKeeper types.BankKeeper,
-	sigGasConsumer SignatureVerificationGasConsumer,
-	signModeHandler signing.SignModeHandler,
-) sdk.AnteHandler {
-	return sdk.ChainAnteDecorators(
+func NewAnteHandler(options HandlerOptions) (sdk.AnteHandler, error) {
+	if options.AccountKeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "account keeper is required for ante builder")
+	}
+
+	if options.BankKeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "bank keeper is required for ante builder")
+	}
+
+	if options.SignModeHandler == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "sign mode handler is required for ante builder")
+	}
+
+	var sigGasConsumer = options.SigGasConsumer
+	if sigGasConsumer == nil {
+		sigGasConsumer = DefaultSigVerificationGasConsumer
+	}
+
+	anteDecorators := []sdk.AnteDecorator{
 		NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
 		NewRejectExtensionOptionsDecorator(),
 		NewMempoolFeeDecorator(),
 		NewValidateBasicDecorator(),
 		TxTimeoutHeightDecorator{},
-		NewValidateMemoDecorator(ak),
-		NewConsumeGasForTxSizeDecorator(ak),
-		NewRejectFeeGranterDecorator(),
-		NewSetPubKeyDecorator(ak), // SetPubKeyDecorator must be called before all signature verification decorators
-		NewValidateSigCountDecorator(ak),
-		NewDeductFeeDecorator(ak, bankKeeper),
-		NewSigGasConsumeDecorator(ak, sigGasConsumer),
-		NewSigVerificationDecorator(ak, signModeHandler),
-		NewIncrementSequenceDecorator(ak),
-	)
+		NewValidateMemoDecorator(options.AccountKeeper),
+		NewConsumeGasForTxSizeDecorator(options.AccountKeeper),
+		NewDeductFeeDecorator(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper),
+		NewSetPubKeyDecorator(options.AccountKeeper), // SetPubKeyDecorator must be called before all signature verification decorators
+		NewValidateSigCountDecorator(options.AccountKeeper),
+		NewSigGasConsumeDecorator(options.AccountKeeper, sigGasConsumer),
+		NewSigVerificationDecorator(options.AccountKeeper, options.SignModeHandler),
+		NewIncrementSequenceDecorator(options.AccountKeeper),
+	}
+
+	return sdk.ChainAnteDecorators(anteDecorators...), nil
 }

Finschia/finschia-sdk

x/auth/ante/ante.go
@@ -2,37 +2,57 @@ package ante
 
 import (
 	sdk "github.com/line/lbm-sdk/types"
-	keeper2 "github.com/line/lbm-sdk/x/auth/keeper"
-	"github.com/line/lbm-sdk/x/auth/signing"
+	sdkerrors "github.com/line/lbm-sdk/types/errors"
+	"github.com/line/lbm-sdk/types/tx/signing"
+	authsigning "github.com/line/lbm-sdk/x/auth/signing"
 	"github.com/line/lbm-sdk/x/auth/types"
-	"github.com/line/lbm-sdk/x/feegrant/keeper"
-	types2 "github.com/line/lbm-sdk/x/feegrant/types"
 )
+
+// HandlerOptions are the options required for constructing a default SDK AnteHandler.
+type HandlerOptions struct {
+	AccountKeeper   AccountKeeper
+	BankKeeper      types.BankKeeper
+	FeegrantKeeper  FeegrantKeeper
+	SignModeHandler authsigning.SignModeHandler
+	SigGasConsumer  func(meter sdk.GasMeter, sig signing.SignatureV2, params types.Params) error
+}
 
 // NewAnteHandler returns an AnteHandler that checks and increments sequence
 // numbers, checks signatures & account numbers, and deducts fees from the first
 // signer.
-func NewAnteHandler(
-	ak AccountKeeper, bankKeeper types.BankKeeper, feegrantKeeper keeper.Keeper,
-	sigGasConsumer SignatureVerificationGasConsumer,
-	signModeHandler signing.SignModeHandler,
-) sdk.AnteHandler {
-	return sdk.ChainAnteDecorators(
+func NewAnteHandler(options HandlerOptions) (sdk.AnteHandler, error) {
+	if options.AccountKeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "account keeper is required for ante builder")
+	}
+
+	if options.BankKeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "bank keeper is required for ante builder")
+	}
+
+	if options.SignModeHandler == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "sign mode handler is required for ante builder")
+	}
+
+	var sigGasConsumer = options.SigGasConsumer
+	if sigGasConsumer == nil {
+		sigGasConsumer = DefaultSigVerificationGasConsumer
+	}
+
+	anteDecorators := []sdk.AnteDecorator{
 		NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
 		NewRejectExtensionOptionsDecorator(),
 		NewMempoolFeeDecorator(),
 		NewValidateBasicDecorator(),
 		TxTimeoutHeightDecorator{},
-		NewValidateMemoDecorator(ak),
-		NewConsumeGasForTxSizeDecorator(ak),
-		NewDeductGrantedFeeDecorator(ak.(keeper2.AccountKeeper), bankKeeper.(types2.BankKeeper), feegrantKeeper),
-		// NewRejectFeeGranterDecorator(),	// todo: Please check it when upgrade `feegrant`
-		// The above handlers should not call `GetAccount` or `GetSignerAcc` for signer
-		NewSetPubKeyDecorator(ak), // SetPubKeyDecorator must be called before all signature verification decorators
-		// The handlers below may call `GetAccount` or `GetSignerAcc` for signer
-		NewValidateSigCountDecorator(ak),
-		NewSigGasConsumeDecorator(ak, sigGasConsumer),
-		NewSigVerificationDecorator(ak, signModeHandler),
-		NewIncrementSequenceDecorator(ak, bankKeeper),
-	)
+		NewValidateMemoDecorator(options.AccountKeeper),
+		NewConsumeGasForTxSizeDecorator(options.AccountKeeper),
+		NewDeductFeeDecorator(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper),
+		NewSetPubKeyDecorator(options.AccountKeeper), // SetPubKeyDecorator must be called before all signature verification decorators
+		NewValidateSigCountDecorator(options.AccountKeeper),
+		NewSigGasConsumeDecorator(options.AccountKeeper, sigGasConsumer),
+		NewSigVerificationDecorator(options.AccountKeeper, options.SignModeHandler),
+		NewIncrementSequenceDecorator(options.AccountKeeper, options.BankKeeper),
+	}
+
+	return sdk.ChainAnteDecorators(anteDecorators...), nil
 }