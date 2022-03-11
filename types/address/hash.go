cosmos/cosmos-sdk

internal: create package for unsafe bytes convertion (#8733)
Co-authored-by: Alessio Treglia <alessio@tendermint.com>

types/address/hash.go
@@ -4,10 +4,9 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
-	"reflect"
 	"sort"
-	"unsafe"
 
+	"github.com/cosmos/cosmos-sdk/internal/conv"
 	"github.com/cosmos/cosmos-sdk/types/errors"
 )
 
@@ -21,7 +20,7 @@ type Addressable interface {
 // Hash creates a new address from address type and key
 func Hash(typ string, key []byte) []byte {
 	hasher := sha256.New()
-	hasher.Write(unsafeStrToByteArray(typ))
+	hasher.Write(conv.UnsafeStrToBytes(typ))
 	th := hasher.Sum(nil)
 
 	hasher.Reset()
@@ -62,12 +61,4 @@ func NewComposed(typ string, subAddresses []Addressable) ([]byte, error) {
 func Module(moduleName string, key []byte) []byte {
 	mKey := append([]byte(moduleName), 0)
 	return Hash("module", append(mKey, key...))
-}
-
-// unsafeStrToByteArray uses unsafe to convert string into byte array. Returned bytes
-// must not be altered after this function is called as it will cause a segmentation fault.
-func unsafeStrToByteArray(s string) []byte {
-	var buf = *(*[]byte)(unsafe.Pointer(&s))
-	(*reflect.SliceHeader)(unsafe.Pointer(&buf)).Cap = len(s)
-	return buf
 }

Finschia/finschia-sdk

types/address/hash.go
@@ -4,10 +4,9 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
-	"reflect"
 	"sort"
-	"unsafe"
 
+	"github.com/line/lbm-sdk/internal/conv"
 	"github.com/line/lbm-sdk/types/errors"
 )
 
@@ -21,7 +20,7 @@ type Addressable interface {
 // Hash creates a new address from address type and key
 func Hash(typ string, key []byte) []byte {
 	hasher := sha256.New()
-	hasher.Write(unsafeStrToByteArray(typ))
+	hasher.Write(conv.UnsafeStrToBytes(typ))
 	th := hasher.Sum(nil)
 
 	hasher.Reset()
@@ -62,12 +61,4 @@ func NewComposed(typ string, subAddresses []Addressable) ([]byte, error) {
 func Module(moduleName string, key []byte) []byte {
 	mKey := append([]byte(moduleName), 0)
 	return Hash("module", append(mKey, key...))
-}
-
-// unsafeStrToByteArray uses unsafe to convert string into byte array. Returned bytes
-// must not be altered after this function is called as it will cause a segmentation fault.
-func unsafeStrToByteArray(s string) []byte {
-	var buf = *(*[]byte)(unsafe.Pointer(&s))
-	(*reflect.SliceHeader)(unsafe.Pointer(&buf)).Cap = len(s)
-	return buf
 }