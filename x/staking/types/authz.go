cosmos/cosmos-sdk

Deprecate SetFullFundraiserPath in favor of SetPurpose and SetCoinType (#8629)
Co-authored-by: Alessio Treglia <alessio@tendermint.com>

x/staking/types/authz.go
@@ -1,10 +1,11 @@
 package types
 
 import (
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	authz "github.com/cosmos/cosmos-sdk/x/authz/exported"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
 var (

Finschia/finschia-sdk

x/staking/types/authz.go
@@ -1,10 +1,11 @@
 package types
 
 import (
+	ocproto "github.com/line/ostracon/proto/ostracon/types"
+
 	sdk "github.com/line/lbm-sdk/types"
 	sdkerrors "github.com/line/lbm-sdk/types/errors"
 	authz "github.com/line/lbm-sdk/x/authz/exported"
-	ocproto "github.com/line/ostracon/proto/ostracon/types"
 )
 
 var (