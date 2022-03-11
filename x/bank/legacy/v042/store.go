cosmos/cosmos-sdk

Supply by denom Migrations (#8780)
* Add back supply proto

* Add migration for supply

* Fix lint

* Update x/bank/spec/01_state.md

* Fix test

* Proto gen

* Update x/bank/spec/01_state.md

* Make proto gen

Co-authored-by: Jonathan Gimeno <jgimeno@gmail.com>

x/bank/legacy/v042/store.go
@@ -1,6 +1,7 @@
 package v042
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v040auth "github.com/cosmos/cosmos-sdk/x/auth/legacy/v040"
@@ -8,14 +9,38 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
-// MigrateStore performs in-place store migrations from v0.40 to v0.42. The
-// migration includes:
-//
-// - Change addresses to be length-prefixed.
-// - Change balances prefix to 1 byte
-func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
-	store := ctx.KVStore(storeKey)
+// migrateSupply migrates the supply to be stored by denom key instead in a
+// single blob.
+// ref: https://github.com/cosmos/cosmos-sdk/issues/7092
+func migrateSupply(store sdk.KVStore, cdc codec.BinaryMarshaler) error {
+	// Old supply was stored as a single blob under the SupplyKey.
+	var oldSupply types.Supply // nolint:staticcheck
+	err := cdc.UnmarshalBinaryBare(store.Get(v040bank.SupplyKey), &oldSupply)
+	if err != nil {
+		return err
+	}
 
+	// We delete the single key holding the whole blob.
+	store.Delete(v040bank.SupplyKey)
+
+	// We add a new key for each denom
+	supplyStore := prefix.NewStore(store, types.SupplyKey)
+	for i := range oldSupply.Total {
+		coin := oldSupply.Total[i]
+		coinBz, err := cdc.MarshalBinaryBare(&coin)
+		if err != nil {
+			return err
+		}
+
+		supplyStore.Set([]byte(coin.Denom), coinBz)
+	}
+
+	return nil
+}
+
+// migrateBalanceKeys migrate the balances keys to cater for variable-length
+// addresses.
+func migrateBalanceKeys(store sdk.KVStore) {
 	// old key is of format:
 	// prefix ("balances") || addrBytes (20 bytes) || denomBytes
 	// new key is of format
@@ -34,6 +59,17 @@ func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
 		store.Set(newStoreKey, oldStoreIter.Value())
 		oldStore.Delete(oldStoreIter.Key())
 	}
+}
 
-	return nil
+// MigrateStore performs in-place store migrations from v0.40 to v0.42. The
+// migration includes:
+//
+// - Change addresses to be length-prefixed.
+// - Change balances prefix to 1 byte
+// - Change supply to be indexed by denom
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey, cdc codec.BinaryMarshaler) error {
+	store := ctx.KVStore(storeKey)
+
+	migrateBalanceKeys(store)
+	return migrateSupply(store, cdc)
 }

x/bank/legacy/v042/store_test.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	"github.com/cosmos/cosmos-sdk/testutil"
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -13,7 +15,35 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
-func TestStoreMigration(t *testing.T) {
+func TestSupplyMigration(t *testing.T) {
+	encCfg := simapp.MakeTestEncodingConfig()
+	bankKey := sdk.NewKVStoreKey("bank")
+	ctx := testutil.DefaultContext(bankKey, sdk.NewTransientStoreKey("transient_test"))
+	store := ctx.KVStore(bankKey)
+
+	oldFooCoin := sdk.NewCoin("foo", sdk.NewInt(100))
+	oldBarCoin := sdk.NewCoin("bar", sdk.NewInt(200))
+
+	// Old supply was stored as a single blob under the `SupplyKey`.
+	oldSupply := types.Supply{Total: sdk.NewCoins(oldFooCoin, oldBarCoin)}
+	store.Set(v040bank.SupplyKey, encCfg.Marshaler.MustMarshalBinaryBare(&oldSupply))
+
+	// Run migration.
+	err := v042bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
+	require.NoError(t, err)
+
+	// New supply is indexed by denom.
+	var newFooCoin, newBarCoin sdk.Coin
+	supplyStore := prefix.NewStore(store, types.SupplyKey)
+	encCfg.Marshaler.MustUnmarshalBinaryBare(supplyStore.Get([]byte("foo")), &newFooCoin)
+	encCfg.Marshaler.MustUnmarshalBinaryBare(supplyStore.Get([]byte("bar")), &newBarCoin)
+
+	require.Equal(t, oldFooCoin, newFooCoin)
+	require.Equal(t, oldBarCoin, newBarCoin)
+}
+
+func TestBalanceKeysMigration(t *testing.T) {
+	encCfg := simapp.MakeTestEncodingConfig()
 	bankKey := sdk.NewKVStoreKey("bank")
 	ctx := testutil.DefaultContext(bankKey, sdk.NewTransientStoreKey("transient_test"))
 	store := ctx.KVStore(bankKey)
@@ -25,7 +55,7 @@ func TestStoreMigration(t *testing.T) {
 	oldKey := append(append(v040bank.BalancesPrefix, addr...), denom...)
 	store.Set(oldKey, value)
 
-	err := v042bank.MigrateStore(ctx, bankKey)
+	err := v042bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
 	require.NoError(t, err)
 
 	newKey := append(types.CreateAccountBalancesPrefix(addr), denom...)

Finschia/finschia-sdk

x/bank/legacy/v042/store.go
@@ -1,6 +1,7 @@
 package v042
 
 import (
+	"github.com/line/lbm-sdk/codec"
 	"github.com/line/lbm-sdk/store/prefix"
 	sdk "github.com/line/lbm-sdk/types"
 	v040auth "github.com/line/lbm-sdk/x/auth/legacy/v040"
@@ -8,14 +9,38 @@ import (
 	"github.com/line/lbm-sdk/x/bank/types"
 )
 
-// MigrateStore performs in-place store migrations from v0.40 to v0.42. The
-// migration includes:
-//
-// - Change addresses to be length-prefixed.
-// - Change balances prefix to 1 byte
-func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
-	store := ctx.KVStore(storeKey)
+// migrateSupply migrates the supply to be stored by denom key instead in a
+// single blob.
+// ref: https://github.com/line/lbm-sdk/issues/7092
+func migrateSupply(store sdk.KVStore, cdc codec.BinaryMarshaler) error {
+	// Old supply was stored as a single blob under the SupplyKey.
+	var oldSupply types.Supply // nolint:staticcheck
+	err := cdc.UnmarshalBinaryBare(store.Get(v040bank.SupplyKey), &oldSupply)
+	if err != nil {
+		return err
+	}
 
+	// We delete the single key holding the whole blob.
+	store.Delete(v040bank.SupplyKey)
+
+	// We add a new key for each denom
+	supplyStore := prefix.NewStore(store, types.SupplyKey)
+	for i := range oldSupply.Total {
+		coin := oldSupply.Total[i]
+		coinBz, err := cdc.MarshalBinaryBare(&coin)
+		if err != nil {
+			return err
+		}
+
+		supplyStore.Set([]byte(coin.Denom), coinBz)
+	}
+
+	return nil
+}
+
+// migrateBalanceKeys migrate the balances keys to cater for variable-length
+// addresses.
+func migrateBalanceKeys(store sdk.KVStore) {
 	// old key is of format:
 	// prefix ("balances") || addrBytes (20 bytes) || denomBytes
 	// new key is of format
@@ -34,6 +59,17 @@ func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey) error {
 		store.Set(newStoreKey, oldStoreIter.Value())
 		oldStore.Delete(oldStoreIter.Key())
 	}
+}
 
-	return nil
+// MigrateStore performs in-place store migrations from v0.40 to v0.42. The
+// migration includes:
+//
+// - Change addresses to be length-prefixed.
+// - Change balances prefix to 1 byte
+// - Change supply to be indexed by denom
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey, cdc codec.BinaryMarshaler) error {
+	store := ctx.KVStore(storeKey)
+
+	migrateBalanceKeys(store)
+	return migrateSupply(store, cdc)
 }

x/bank/legacy/v042/store_test.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/line/lbm-sdk/simapp"
+	"github.com/line/lbm-sdk/store/prefix"
 	"github.com/line/lbm-sdk/testutil"
 	"github.com/line/lbm-sdk/testutil/testdata"
 	sdk "github.com/line/lbm-sdk/types"
@@ -13,7 +15,35 @@ import (
 	"github.com/line/lbm-sdk/x/bank/types"
 )
 
-func TestStoreMigration(t *testing.T) {
+func TestSupplyMigration(t *testing.T) {
+	encCfg := simapp.MakeTestEncodingConfig()
+	bankKey := sdk.NewKVStoreKey("bank")
+	ctx := testutil.DefaultContext(bankKey, sdk.NewTransientStoreKey("transient_test"))
+	store := ctx.KVStore(bankKey)
+
+	oldFooCoin := sdk.NewCoin("foo", sdk.NewInt(100))
+	oldBarCoin := sdk.NewCoin("bar", sdk.NewInt(200))
+
+	// Old supply was stored as a single blob under the `SupplyKey`.
+	oldSupply := types.Supply{Total: sdk.NewCoins(oldFooCoin, oldBarCoin)}
+	store.Set(v040bank.SupplyKey, encCfg.Marshaler.MustMarshalBinaryBare(&oldSupply))
+
+	// Run migration.
+	err := v042bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
+	require.NoError(t, err)
+
+	// New supply is indexed by denom.
+	var newFooCoin, newBarCoin sdk.Coin
+	supplyStore := prefix.NewStore(store, types.SupplyKey)
+	encCfg.Marshaler.MustUnmarshalBinaryBare(supplyStore.Get([]byte("foo")), &newFooCoin)
+	encCfg.Marshaler.MustUnmarshalBinaryBare(supplyStore.Get([]byte("bar")), &newBarCoin)
+
+	require.Equal(t, oldFooCoin, newFooCoin)
+	require.Equal(t, oldBarCoin, newBarCoin)
+}
+
+func TestBalanceKeysMigration(t *testing.T) {
+	encCfg := simapp.MakeTestEncodingConfig()
 	bankKey := sdk.NewKVStoreKey("bank")
 	ctx := testutil.DefaultContext(bankKey, sdk.NewTransientStoreKey("transient_test"))
 	store := ctx.KVStore(bankKey)
@@ -25,7 +55,7 @@ func TestStoreMigration(t *testing.T) {
 	oldKey := append(append(v040bank.BalancesPrefix, addr...), denom...)
 	store.Set(oldKey, value)
 
-	err := v042bank.MigrateStore(ctx, bankKey)
+	err := v042bank.MigrateStore(ctx, bankKey, encCfg.Marshaler)
 	require.NoError(t, err)
 
 	newKey := append(types.CreateAccountBalancesPrefix(addr.Bytes()), denom...)