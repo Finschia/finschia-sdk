cosmos/cosmos-sdk

gRPC: fix server reflection (#8945)
* add: gogoreflection

* fix: server reflection

* fix: make tests resolve imports in a transitive way

* chore: cleanup getMessageType

* chore: lint

* add: extensions reflection

* chore: update interact-node.md docs

* chore: update CHANGELOG.md

* Update server/grpc/gogoreflection/fix_registration.go

Co-authored-by: Robert Zaremba <robert@zaremba.ch>

Co-authored-by: Jonathan Gimeno <jgimeno@gmail.com>
Co-authored-by: Alessio Treglia <alessio@tendermint.com>
Co-authored-by: Robert Zaremba <robert@zaremba.ch>

server/grpc/server.go
@@ -6,9 +6,9 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/server/grpc/gogoreflection"
 	"github.com/cosmos/cosmos-sdk/server/types"
 )
 
@@ -19,7 +19,7 @@ func StartGRPCServer(clientCtx client.Context, app types.Application, address st
 
 	// Reflection allows external clients to see what services and methods
 	// the gRPC server exposes.
-	reflection.Register(grpcSrv)
+	gogoreflection.Register(grpcSrv)
 
 	listener, err := net.Listen("tcp", address)
 	if err != nil {

Finschia/finschia-sdk

server/grpc/server.go
@@ -6,9 +6,9 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 
 	"github.com/line/lbm-sdk/client"
+	"github.com/line/lbm-sdk/server/grpc/gogoreflection"
 	"github.com/line/lbm-sdk/server/types"
 )
 
@@ -19,7 +19,7 @@ func StartGRPCServer(clientCtx client.Context, app types.Application, address st
 
 	// Reflection allows external clients to see what services and methods
 	// the gRPC server exposes.
-	reflection.Register(grpcSrv)
+	gogoreflection.Register(grpcSrv)
 
 	listener, err := net.Listen("tcp", address)
 	if err != nil {