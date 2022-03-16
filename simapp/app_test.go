cosmos/cosmos-sdk

x/authz: audit updates (#9042)
* x/authz: audit updates

* audit with Aaron

* authz: Update Authorization.Accept method

* authz: add event proto definitions

* update query service

* authz: use typed events

* refactore and rename query authorizations

* remve Authorization infix from proto services

* renames wip

* refactoring

* update tests

* fix compilation

* fixing gRPC query tests

* fix simulation tests

* few renames

* more refactore

* add missing file

* moving export genesis to keeper

* Update docs

* update tests

* rename event Msg attribute to MsgTypeURL

* Upate Authorization interface

* rollback Makefile changes

* fix tests

* Apply suggestions from code review

Co-authored-by: Aaron Craelius <aaron@regen.network>

* renames

* refactore authz/exported

* lint fix

* authz/types refactore

* comment update

* conflict updates

* Apply suggestions from code review

Co-authored-by: Amaury <1293565+amaurym@users.noreply.github.com>

* authz: move storage keys to keeper

* review updates

* docs update

* Update x/authz/client/cli/query.go

Co-authored-by: Aaron Craelius <aaron@regen.network>

* move codec to the root package

* authz CMD info update

* comment update

* update imports and build flags

* fix functional tests

* update proto comment

* fix tests

* fix test

Co-authored-by: Aaron Craelius <aaron@regen.network>
Co-authored-by: Amaury <1293565+amaurym@users.noreply.github.com>

simapp/app_test.go
@@ -18,7 +18,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/auth/vesting"
-	"github.com/cosmos/cosmos-sdk/x/authz"
+	authz_m "github.com/cosmos/cosmos-sdk/x/authz/module"
 	"github.com/cosmos/cosmos-sdk/x/bank"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/capability"
@@ -168,7 +168,7 @@ func TestRunMigrations(t *testing.T) {
 				module.VersionMap{
 					"bank":         1,
 					"auth":         auth.AppModule{}.ConsensusVersion(),
-					"authz":        authz.AppModule{}.ConsensusVersion(),
+					"authz":        authz_m.AppModule{}.ConsensusVersion(),
 					"staking":      staking.AppModule{}.ConsensusVersion(),
 					"mint":         mint.AppModule{}.ConsensusVersion(),
 					"distribution": distribution.AppModule{}.ConsensusVersion(),
@@ -219,7 +219,7 @@ func TestInitGenesisOnMigration(t *testing.T) {
 		module.VersionMap{
 			"bank":         bank.AppModule{}.ConsensusVersion(),
 			"auth":         auth.AppModule{}.ConsensusVersion(),
-			"authz":        authz.AppModule{}.ConsensusVersion(),
+			"authz":        authz_m.AppModule{}.ConsensusVersion(),
 			"staking":      staking.AppModule{}.ConsensusVersion(),
 			"mint":         mint.AppModule{}.ConsensusVersion(),
 			"distribution": distribution.AppModule{}.ConsensusVersion(),

x/authz/client/cli/query.go
@@ -1,7 +1,6 @@
 package cli
 
 import (
-	"context"
 	"fmt"
 	"strings"
 
@@ -11,14 +10,14 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
-	"github.com/cosmos/cosmos-sdk/x/authz/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
 	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
 // GetQueryCmd returns the cli query commands for this module
 func GetQueryCmd() *cobra.Command {
 	authorizationQueryCmd := &cobra.Command{
-		Use:                        types.ModuleName,
+		Use:                        authz.ModuleName,
 		Short:                      "Querying commands for the authz module",
 		Long:                       "",
 		DisableFlagParsing:         true,
@@ -27,54 +26,59 @@ func GetQueryCmd() *cobra.Command {
 	}
 
 	authorizationQueryCmd.AddCommand(
-		GetCmdQueryAuthorization(),
-		GetCmdQueryAuthorizations(),
+		GetCmdQueryGrants(),
 	)
 
 	return authorizationQueryCmd
 }
 
-// GetCmdQueryAuthorizations implements the query authorizations command.
-func GetCmdQueryAuthorizations() *cobra.Command {
+// GetCmdQueryGrants implements the query authorization command.
+func GetCmdQueryGrants() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "authorizations [granter-addr] [grantee-addr]",
-		Args:  cobra.ExactArgs(2),
-		Short: "query list of authorizations for a granter-grantee pair",
+		Use:   "grants [granter-addr] [grantee-addr] [msg-type-url]?",
+		Args:  cobra.RangeArgs(2, 3),
+		Short: "query grants for a granter-grantee pair and optionally a msg-type-url",
 		Long: strings.TrimSpace(
-			fmt.Sprintf(`Query list of authorizations for a granter-grantee pair:
-Example:
-$ %s query %s authorizations cosmos1skj.. cosmos1skjwj..
-`, version.AppName, types.ModuleName),
+			fmt.Sprintf(`Query authorization grants for a granter-grantee pair. If msg-type-url
+is set, it will select grants only for that msg type.
+Examples:
+$ %s query %s grants cosmos1skj.. cosmos1skjwj..
+$ %s query %s grants cosmos1skjw.. cosmos1skjwj.. %s
+`,
+				version.AppName, authz.ModuleName,
+				version.AppName, authz.ModuleName, bank.SendAuthorization{}.MsgTypeURL()),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
 				return err
 			}
-			queryClient := types.NewQueryClient(clientCtx)
+			queryClient := authz.NewQueryClient(clientCtx)
 
-			granterAddr, err := sdk.AccAddressFromBech32(args[0])
+			granter, err := sdk.AccAddressFromBech32(args[0])
 			if err != nil {
 				return err
 			}
-
-			granteeAddr, err := sdk.AccAddressFromBech32(args[1])
+			grantee, err := sdk.AccAddressFromBech32(args[1])
 			if err != nil {
 				return err
 			}
-
+			var msgAuthorized = ""
+			if len(args) >= 3 {
+				msgAuthorized = args[2]
+			}
 			pageReq, err := client.ReadPageRequest(cmd.Flags())
 			if err != nil {
 				return err
 			}
 
-			res, err := queryClient.Authorizations(
-				context.Background(),
-				&types.QueryAuthorizationsRequest{
-					Granter:    granterAddr.String(),
-					Grantee:    granteeAddr.String(),
-					Pagination: pageReq,
-				},
+			res, err := queryClient.Grants(
+				cmd.Context(),
+				&authz.QueryGrantsRequest{
+					Granter:    granter.String(),
+					Grantee:    grantee.String(),
+					MsgTypeUrl: msgAuthorized,
+					Pagination: pageReq},
 			)
 			if err != nil {
 				return err
@@ -84,56 +88,6 @@ $ %s query %s authorizations cosmos1skj.. cosmos1skjwj..
 		},
 	}
 	flags.AddQueryFlagsToCmd(cmd)
-	flags.AddPaginationFlagsToCmd(cmd, "authorizations")
-	return cmd
-}
-
-// GetCmdQueryAuthorization implements the query authorization command.
-func GetCmdQueryAuthorization() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "authorization [granter-addr] [grantee-addr] [msg-type]",
-		Args:  cobra.ExactArgs(3),
-		Short: "query authorization for a granter-grantee pair",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`Query authorization for a granter-grantee pair that matches the given msg-type:
-Example:
-$ %s query %s authorization cosmos1skjw.. cosmos1skjwj.. %s
-`, version.AppName, types.ModuleName, bank.SendAuthorization{}.MethodName()),
-		),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientQueryContext(cmd)
-			if err != nil {
-				return err
-			}
-			queryClient := types.NewQueryClient(clientCtx)
-
-			granter, err := sdk.AccAddressFromBech32(args[0])
-			if err != nil {
-				return err
-			}
-
-			grantee, err := sdk.AccAddressFromBech32(args[1])
-			if err != nil {
-				return err
-			}
-
-			msgAuthorized := args[2]
-
-			res, err := queryClient.Authorization(
-				context.Background(),
-				&types.QueryAuthorizationRequest{
-					Granter:    granter.String(),
-					Grantee:    grantee.String(),
-					MethodName: msgAuthorized,
-				},
-			)
-			if err != nil {
-				return err
-			}
-
-			return clientCtx.PrintProto(res.Authorization)
-		},
-	}
-	flags.AddQueryFlagsToCmd(cmd)
+	flags.AddPaginationFlagsToCmd(cmd, "grants")
 	return cmd
 }

x/authz/module/module.go
@@ -16,12 +16,10 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	"github.com/cosmos/cosmos-sdk/x/authz/keeper"
-	"github.com/cosmos/cosmos-sdk/x/authz/types"
-
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-
+	"github.com/cosmos/cosmos-sdk/x/authz"
 	"github.com/cosmos/cosmos-sdk/x/authz/client/cli"
+	"github.com/cosmos/cosmos-sdk/x/authz/keeper"
 	"github.com/cosmos/cosmos-sdk/x/authz/simulation"
 )
 
@@ -38,14 +36,14 @@ type AppModuleBasic struct {
 
 // Name returns the authz module's name.
 func (AppModuleBasic) Name() string {
-	return types.ModuleName
+	return authz.ModuleName
 }
 
 // RegisterServices registers a gRPC query service to respond to the
 // module-specific gRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
-	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
-	types.RegisterMsgServer(cfg.MsgServer(), am.keeper)
+	authz.RegisterQueryServer(cfg.QueryServer(), am.keeper)
+	authz.RegisterMsgServer(cfg.MsgServer(), am.keeper)
 }
 
 // RegisterLegacyAminoCodec registers the authz module's types for the given codec.
@@ -53,23 +51,23 @@ func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {}
 
 // RegisterInterfaces registers the authz module's interface types
 func (AppModuleBasic) RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	types.RegisterInterfaces(registry)
+	authz.RegisterInterfaces(registry)
 }
 
 // DefaultGenesis returns default genesis state as raw bytes for the authz
 // module.
 func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
-	return cdc.MustMarshalJSON(types.DefaultGenesisState())
+	return cdc.MustMarshalJSON(authz.DefaultGenesisState())
 }
 
 // ValidateGenesis performs genesis state validation for the authz module.
 func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config sdkclient.TxEncodingConfig, bz json.RawMessage) error {
-	var data types.GenesisState
+	var data authz.GenesisState
 	if err := cdc.UnmarshalJSON(bz, &data); err != nil {
-		return sdkerrors.Wrapf(err, "failed to unmarshal %s genesis state", types.ModuleName)
+		return sdkerrors.Wrapf(err, "failed to unmarshal %s genesis state", authz.ModuleName)
 	}
 
-	return types.ValidateGenesis(data)
+	return authz.ValidateGenesis(data)
 }
 
 // RegisterRESTRoutes registers the REST routes for the authz module.
@@ -78,7 +76,7 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx sdkclient.Context, r *mux.Rou
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the authz module.
 func (a AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx sdkclient.Context, mux *runtime.ServeMux) {
-	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
+	authz.RegisterQueryHandlerClient(context.Background(), mux, authz.NewQueryClient(clientCtx))
 }
 
 // GetQueryCmd returns the cli query commands for the authz module
@@ -95,13 +93,13 @@ func (AppModuleBasic) GetTxCmd() *cobra.Command {
 type AppModule struct {
 	AppModuleBasic
 	keeper        keeper.Keeper
-	accountKeeper types.AccountKeeper
-	bankKeeper    types.BankKeeper
+	accountKeeper authz.AccountKeeper
+	bankKeeper    authz.BankKeeper
 	registry      cdctypes.InterfaceRegistry
 }
 
 // NewAppModule creates a new AppModule object
-func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak types.AccountKeeper, bk types.BankKeeper, registry cdctypes.InterfaceRegistry) AppModule {
+func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak authz.AccountKeeper, bk authz.BankKeeper, registry cdctypes.InterfaceRegistry) AppModule {
 	return AppModule{
 		AppModuleBasic: AppModuleBasic{cdc: cdc},
 		keeper:         keeper,
@@ -113,7 +111,7 @@ func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak types.AccountKeeper,
 
 // Name returns the authz module's name.
 func (AppModule) Name() string {
-	return types.ModuleName
+	return authz.ModuleName
 }
 
 // RegisterInvariants does nothing, there are no invariants to enforce
@@ -121,7 +119,7 @@ func (AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
 
 // Route returns the message routing key for the staking module.
 func (am AppModule) Route() sdk.Route {
-	return sdk.NewRoute(types.RouterKey, nil)
+	return sdk.NewRoute(authz.RouterKey, nil)
 }
 
 func (am AppModule) NewHandler() sdk.Handler {
@@ -139,16 +137,16 @@ func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sd
 // InitGenesis performs genesis initialization for the authz module. It returns
 // no validator updates.
 func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) []abci.ValidatorUpdate {
-	var genesisState types.GenesisState
+	var genesisState authz.GenesisState
 	cdc.MustUnmarshalJSON(data, &genesisState)
-	InitGenesis(ctx, am.keeper, &genesisState)
+	am.keeper.InitGenesis(ctx, &genesisState)
 	return []abci.ValidatorUpdate{}
 }
 
 // ExportGenesis returns the exported genesis state as raw bytes for the authz
 // module.
 func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
-	gs := ExportGenesis(ctx, am.keeper)
+	gs := am.keeper.ExportGenesis(ctx)
 	return cdc.MustMarshalJSON(gs)
 }
 
@@ -184,7 +182,7 @@ func (AppModule) RandomizedParams(r *rand.Rand) []simtypes.ParamChange {
 
 // RegisterStoreDecoder registers a decoder for authz module's types
 func (am AppModule) RegisterStoreDecoder(sdr sdk.StoreDecoderRegistry) {
-	sdr[types.StoreKey] = simulation.NewDecodeStore(am.cdc)
+	sdr[keeper.StoreKey] = simulation.NewDecodeStore(am.cdc)
 }
 
 // WeightedOperations returns the all the gov module operations with their respective weights.

Finschia/finschia-sdk

simapp/app_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/line/lbm-sdk/types/module"
 	"github.com/line/lbm-sdk/x/auth"
 	"github.com/line/lbm-sdk/x/auth/vesting"
-	"github.com/line/lbm-sdk/x/authz"
+	authz_m "github.com/line/lbm-sdk/x/authz/module"
 	"github.com/line/lbm-sdk/x/bank"
 	banktypes "github.com/line/lbm-sdk/x/bank/types"
 	"github.com/line/lbm-sdk/x/capability"
@@ -27,7 +27,6 @@ import (
 	"github.com/line/lbm-sdk/x/crisis"
 	"github.com/line/lbm-sdk/x/distribution"
 	"github.com/line/lbm-sdk/x/evidence"
-	"github.com/line/lbm-sdk/x/feegrant"
 	"github.com/line/lbm-sdk/x/genutil"
 	"github.com/line/lbm-sdk/x/gov"
 	transfer "github.com/line/lbm-sdk/x/ibc/applications/transfer"
@@ -171,7 +170,7 @@ func TestRunMigrations(t *testing.T) {
 				module.VersionMap{
 					"bank":         1,
 					"auth":         auth.AppModule{}.ConsensusVersion(),
-					"authz":        authz.AppModule{}.ConsensusVersion(),
+					"authz":        authz_m.AppModule{}.ConsensusVersion(),
 					"staking":      staking.AppModule{}.ConsensusVersion(),
 					"mint":         mint.AppModule{}.ConsensusVersion(),
 					"distribution": distribution.AppModule{}.ConsensusVersion(),
@@ -227,7 +226,7 @@ func TestInitGenesisOnMigration(t *testing.T) {
 		module.VersionMap{
 			"bank":         bank.AppModule{}.ConsensusVersion(),
 			"auth":         auth.AppModule{}.ConsensusVersion(),
-			"authz":        authz.AppModule{}.ConsensusVersion(),
+			"authz":        authz_m.AppModule{}.ConsensusVersion(),
 			"staking":      staking.AppModule{}.ConsensusVersion(),
 			"mint":         mint.AppModule{}.ConsensusVersion(),
 			"distribution": distribution.AppModule{}.ConsensusVersion(),

x/authz/client/cli/query.go
@@ -1,7 +1,6 @@
 package cli
 
 import (
-	"context"
 	"fmt"
 	"strings"
 
@@ -11,14 +10,14 @@ import (
 	"github.com/line/lbm-sdk/client/flags"
 	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/version"
-	"github.com/line/lbm-sdk/x/authz/types"
+	"github.com/line/lbm-sdk/x/authz"
 	bank "github.com/line/lbm-sdk/x/bank/types"
 )
 
 // GetQueryCmd returns the cli query commands for this module
 func GetQueryCmd() *cobra.Command {
 	authorizationQueryCmd := &cobra.Command{
-		Use:                        types.ModuleName,
+		Use:                        authz.ModuleName,
 		Short:                      "Querying commands for the authz module",
 		Long:                       "",
 		DisableFlagParsing:         true,
@@ -27,31 +26,34 @@ func GetQueryCmd() *cobra.Command {
 	}
 
 	authorizationQueryCmd.AddCommand(
-		GetCmdQueryAuthorization(),
-		GetCmdQueryAuthorizations(),
+		GetCmdQueryGrants(),
 	)
 
 	return authorizationQueryCmd
 }
 
-// GetCmdQueryAuthorizations implements the query authorizations command.
-func GetCmdQueryAuthorizations() *cobra.Command {
+// GetCmdQueryGrants implements the query authorization command.
+func GetCmdQueryGrants() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "authorizations [granter-addr] [grantee-addr]",
-		Args:  cobra.ExactArgs(2),
-		Short: "query list of authorizations for a granter-grantee pair",
+		Use:   "grants [granter-addr] [grantee-addr] [msg-type-url]?",
+		Args:  cobra.RangeArgs(2, 3),
+		Short: "query grants for a granter-grantee pair and optionally a msg-type-url",
 		Long: strings.TrimSpace(
-			fmt.Sprintf(`Query list of authorizations for a granter-grantee pair:
-Example:
-$ %s query %s authorizations link1skj.. link1skjwj..
-`, version.AppName, types.ModuleName),
+			fmt.Sprintf(`Query authorization grants for a granter-grantee pair. If msg-type-url
+is set, it will select grants only for that msg type.
+Examples:
+$ %s query %s grants link1skj.. link1skjwj..
+$ %s query %s grants link1skjw.. link1skjwj.. %s
+`,
+				version.AppName, authz.ModuleName,
+				version.AppName, authz.ModuleName, bank.SendAuthorization{}.MsgTypeURL()),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
 				return err
 			}
-			queryClient := types.NewQueryClient(clientCtx)
+			queryClient := authz.NewQueryClient(clientCtx)
 
 			err = sdk.ValidateAccAddress(args[0])
 			if err != nil {
@@ -65,18 +67,23 @@ $ %s query %s authorizations link1skj.. link1skjwj..
 			}
 			granteeAddr := sdk.AccAddress(args[1])
 
+			var msgAuthorized = ""
+			if len(args) >= 3 {
+				msgAuthorized = args[2]
+			}
+
 			pageReq, err := client.ReadPageRequest(cmd.Flags())
 			if err != nil {
 				return err
 			}
 
-			res, err := queryClient.Authorizations(
-				context.Background(),
-				&types.QueryAuthorizationsRequest{
-					Granter:    granterAddr.String(),
-					Grantee:    granteeAddr.String(),
-					Pagination: pageReq,
-				},
+			res, err := queryClient.Grants(
+				cmd.Context(),
+				&authz.QueryGrantsRequest{
+					Granter:    granter.String(),
+					Grantee:    grantee.String(),
+					MsgTypeUrl: msgAuthorized,
+					Pagination: pageReq},
 			)
 			if err != nil {
 				return err
@@ -87,57 +94,5 @@ $ %s query %s authorizations link1skj.. link1skjwj..
 	}
 	flags.AddQueryFlagsToCmd(cmd)
 	flags.AddPaginationFlagsToCmd(cmd, "authorizations")
-	return cmd
-}
-
-// GetCmdQueryAuthorization implements the query authorization command.
-func GetCmdQueryAuthorization() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "authorization [granter-addr] [grantee-addr] [msg-type]",
-		Args:  cobra.ExactArgs(3),
-		Short: "query authorization for a granter-grantee pair",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`Query authorization for a granter-grantee pair that matches the given msg-type:
-Example:
-$ %s query %s authorization link1skjw.. link1skjwj.. %s
-`, version.AppName, types.ModuleName, bank.SendAuthorization{}.MethodName()),
-		),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientQueryContext(cmd)
-			if err != nil {
-				return err
-			}
-			queryClient := types.NewQueryClient(clientCtx)
-
-			err = sdk.ValidateAccAddress(args[0])
-			if err != nil {
-				return err
-			}
-			granter := sdk.AccAddress(args[0])
-
-			err = sdk.ValidateAccAddress(args[1])
-			if err != nil {
-				return err
-			}
-			grantee := sdk.AccAddress(args[1])
-
-			msgAuthorized := args[2]
-
-			res, err := queryClient.Authorization(
-				context.Background(),
-				&types.QueryAuthorizationRequest{
-					Granter:    granter.String(),
-					Grantee:    grantee.String(),
-					MethodName: msgAuthorized,
-				},
-			)
-			if err != nil {
-				return err
-			}
-
-			return clientCtx.PrintProto(res.Authorization)
-		},
-	}
-	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }

x/authz/module/module.go
@@ -7,8 +7,9 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	abci "github.com/line/ostracon/abci/types"
 	"github.com/spf13/cobra"
+
+	abci "github.com/line/ostracon/abci/types"
 
 	sdkclient "github.com/line/lbm-sdk/client"
 	"github.com/line/lbm-sdk/codec"
@@ -16,13 +17,10 @@ import (
 	sdk "github.com/line/lbm-sdk/types"
 	sdkerrors "github.com/line/lbm-sdk/types/errors"
 	"github.com/line/lbm-sdk/types/module"
-	"github.com/line/lbm-sdk/x/authz/keeper"
-	"github.com/line/lbm-sdk/x/authz/types"
-
 	simtypes "github.com/line/lbm-sdk/types/simulation"
-
 	"github.com/line/lbm-sdk/x/authz/client/cli"
 	"github.com/line/lbm-sdk/x/authz/simulation"
+	"github.com/line/lbm-sdk/x/authz"
 )
 
 var (
@@ -38,14 +36,14 @@ type AppModuleBasic struct {
 
 // Name returns the authz module's name.
 func (AppModuleBasic) Name() string {
-	return types.ModuleName
+	return authz.ModuleName
 }
 
 // RegisterServices registers a gRPC query service to respond to the
 // module-specific gRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
-	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
-	types.RegisterMsgServer(cfg.MsgServer(), am.keeper)
+	authz.RegisterQueryServer(cfg.QueryServer(), am.keeper)
+	authz.RegisterMsgServer(cfg.MsgServer(), am.keeper)
 }
 
 // RegisterLegacyAminoCodec registers the authz module's types for the given codec.
@@ -53,23 +51,23 @@ func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {}
 
 // RegisterInterfaces registers the authz module's interface types
 func (AppModuleBasic) RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	types.RegisterInterfaces(registry)
+	authz.RegisterInterfaces(registry)
 }
 
 // DefaultGenesis returns default genesis state as raw bytes for the authz
 // module.
 func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
-	return cdc.MustMarshalJSON(types.DefaultGenesisState())
+	return cdc.MustMarshalJSON(authz.DefaultGenesisState())
 }
 
 // ValidateGenesis performs genesis state validation for the authz module.
 func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config sdkclient.TxEncodingConfig, bz json.RawMessage) error {
-	var data types.GenesisState
+	var data authz.GenesisState
 	if err := cdc.UnmarshalJSON(bz, &data); err != nil {
-		return sdkerrors.Wrapf(err, "failed to unmarshal %s genesis state", types.ModuleName)
+		return sdkerrors.Wrapf(err, "failed to unmarshal %s genesis state", authz.ModuleName)
 	}
 
-	return types.ValidateGenesis(data)
+	return authz.ValidateGenesis(data)
 }
 
 // RegisterRESTRoutes registers the REST routes for the authz module.
@@ -78,7 +76,7 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx sdkclient.Context, r *mux.Rou
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the authz module.
 func (a AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx sdkclient.Context, mux *runtime.ServeMux) {
-	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
+	authz.RegisterQueryHandlerClient(context.Background(), mux, authz.NewQueryClient(clientCtx))
 }
 
 // GetQueryCmd returns the cli query commands for the authz module
@@ -95,13 +93,13 @@ func (AppModuleBasic) GetTxCmd() *cobra.Command {
 type AppModule struct {
 	AppModuleBasic
 	keeper        keeper.Keeper
-	accountKeeper types.AccountKeeper
-	bankKeeper    types.BankKeeper
+	accountKeeper authz.AccountKeeper
+	bankKeeper    authz.BankKeeper
 	registry      cdctypes.InterfaceRegistry
 }
 
 // NewAppModule creates a new AppModule object
-func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak types.AccountKeeper, bk types.BankKeeper, registry cdctypes.InterfaceRegistry) AppModule {
+func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak authz.AccountKeeper, bk authz.BankKeeper, registry cdctypes.InterfaceRegistry) AppModule {
 	return AppModule{
 		AppModuleBasic: AppModuleBasic{cdc: cdc},
 		keeper:         keeper,
@@ -113,7 +111,7 @@ func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak types.AccountKeeper,
 
 // Name returns the authz module's name.
 func (AppModule) Name() string {
-	return types.ModuleName
+	return authz.ModuleName
 }
 
 // RegisterInvariants does nothing, there are no invariants to enforce
@@ -121,7 +119,7 @@ func (AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
 
 // Route returns the message routing key for the staking module.
 func (am AppModule) Route() sdk.Route {
-	return sdk.NewRoute(types.RouterKey, nil)
+	return sdk.NewRoute(authz.RouterKey, nil)
 }
 
 func (am AppModule) NewHandler() sdk.Handler {
@@ -139,16 +137,16 @@ func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sd
 // InitGenesis performs genesis initialization for the authz module. It returns
 // no validator updates.
 func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) []abci.ValidatorUpdate {
-	var genesisState types.GenesisState
+	var genesisState authz.GenesisState
 	cdc.MustUnmarshalJSON(data, &genesisState)
-	InitGenesis(ctx, am.keeper, &genesisState)
+	am.keeper.InitGenesis(ctx, &genesisState)
 	return []abci.ValidatorUpdate{}
 }
 
 // ExportGenesis returns the exported genesis state as raw bytes for the authz
 // module.
 func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
-	gs := ExportGenesis(ctx, am.keeper)
+	gs := am.keeper.ExportGenesis(ctx)
 	return cdc.MustMarshalJSON(gs)
 }
 
@@ -184,7 +182,7 @@ func (AppModule) RandomizedParams(r *rand.Rand) []simtypes.ParamChange {
 
 // RegisterStoreDecoder registers a decoder for authz module's types
 func (am AppModule) RegisterStoreDecoder(sdr sdk.StoreDecoderRegistry) {
-	sdr[types.StoreKey] = simulation.NewDecodeStore(am.cdc)
+	sdr[keeper.StoreKey] = simulation.NewDecodeStore(am.cdc)
 }
 
 // WeightedOperations returns the all the gov module operations with their respective weights.