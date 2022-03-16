cosmos/cosmos-sdk

x/upgrade gRPC methods for VersionMap (#9073)
* setup version map query

* query methods

* grpc methods

* cleanup

* grpc for VersionMap

* swagger update

* swagger sync

* cleanup

* reset docs

* clean

* grpc with field

* daily

* -add grpc methods
-add optional field to get specific module from version map
-added cli test for version map query from cli

* -consolidate grpc test to one test function
-return better errors in grpc methods

* consolidate tests

* swagger update

* this breaks

* Try stringer on individual msgs

* change map to slice in proto

* cleanup

* add comments to proto fields

* cleanup

* regen proto files

* jsoncodec

* rename gRPC methods

* * add fetch method for module version slice
* add method to return version given a module name
* remove sorting functions

* lint

* Update proto/cosmos/upgrade/v1beta1/query.proto

Co-authored-by: Aaron Craelius <aaron@regen.network>

* Update proto/cosmos/upgrade/v1beta1/upgrade.proto

Co-authored-by: Aaron Craelius <aaron@regen.network>

* fix up comments and regen proto/swagger

* Update x/upgrade/client/cli/query.go

Co-authored-by: Amaury <1293565+amaurym@users.noreply.github.com>

* conform to pr 6711

* lint

Co-authored-by: technicallyty <48813565+tytech3@users.noreply.github.com>
Co-authored-by: Amaury M <1293565+amaurym@users.noreply.github.com>
Co-authored-by: Aaron Craelius <aaron@regen.network>

x/upgrade/client/testutil/cli_test.go
@@ -1,0 +1,13 @@
+package testutil
+
+import (
+	"github.com/cosmos/cosmos-sdk/testutil/network"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+func TestIntegrationTestSuite(t *testing.T) {
+	cfg := network.DefaultConfig()
+	cfg.NumValidators = 1
+	suite.Run(t, NewIntegrationTestSuite(cfg))
+}

x/upgrade/client/testutil/suite.go
@@ -1,0 +1,113 @@
+package testutil
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/simapp"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	"github.com/cosmos/cosmos-sdk/testutil/network"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/upgrade/client/cli"
+	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	"github.com/stretchr/testify/suite"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+)
+
+func NewIntegrationTestSuite(cfg network.Config) *IntegrationTestSuite {
+	return &IntegrationTestSuite{cfg: cfg}
+}
+
+type IntegrationTestSuite struct {
+	suite.Suite
+
+	app     *simapp.SimApp
+	cfg     network.Config
+	network *network.Network
+	ctx     sdk.Context
+}
+
+func (s *IntegrationTestSuite) SetupSuite() {
+	s.T().Log("setting up integration test suite")
+	app := simapp.Setup(false)
+	s.app = app
+	s.ctx = app.BaseApp.NewContext(false, tmproto.Header{})
+
+	cfg := network.DefaultConfig()
+	cfg.NumValidators = 1
+
+	s.cfg = cfg
+	s.network = network.New(s.T(), cfg)
+}
+
+func (s *IntegrationTestSuite) TearDownSuite() {
+	s.T().Log("tearing down integration test suite")
+	s.network.Cleanup()
+}
+
+func (s *IntegrationTestSuite) TestModuleVersionsCLI() {
+	testCases := []struct {
+		msg     string
+		req     types.QueryModuleVersionsRequest
+		single  bool
+		expPass bool
+	}{
+		{
+			msg:     "test full query",
+			req:     types.QueryModuleVersionsRequest{ModuleName: ""},
+			single:  false,
+			expPass: true,
+		},
+		{
+			msg:     "test single module",
+			req:     types.QueryModuleVersionsRequest{ModuleName: "bank"},
+			single:  true,
+			expPass: true,
+		},
+		{
+			msg:     "test non-existent module",
+			req:     types.QueryModuleVersionsRequest{ModuleName: "abcdefg"},
+			single:  true,
+			expPass: false,
+		},
+	}
+
+	val := s.network.Validators[0]
+	clientCtx := val.ClientCtx
+	// avoid printing as yaml from CLI command
+	clientCtx.OutputFormat = "JSON"
+
+	vm := s.app.UpgradeKeeper.GetModuleVersionMap(s.ctx)
+	mv := s.app.UpgradeKeeper.GetModuleVersions(s.ctx)
+	s.Require().NotEmpty(vm)
+
+	for _, tc := range testCases {
+		s.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+
+			expect := mv
+			if tc.expPass {
+				if tc.single {
+					expect = []*types.ModuleVersion{{Name: tc.req.ModuleName, Version: vm[tc.req.ModuleName]}}
+				}
+				// setup expected response
+				pm := types.QueryModuleVersionsResponse{
+					ModuleVersions: expect,
+				}
+				jsonVM, _ := clientCtx.JSONCodec.MarshalJSON(&pm)
+				expectedRes := string(jsonVM)
+				// append new line to match behaviour of PrintProto
+				expectedRes += "\n"
+
+				// get actual module versions list response from cli
+				cmd := cli.GetModuleVersionsCmd()
+				outVM, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, []string{tc.req.ModuleName})
+				s.Require().NoError(err)
+
+				s.Require().Equal(expectedRes, outVM.String())
+			} else {
+				cmd := cli.GetModuleVersionsCmd()
+				_, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, []string{tc.req.ModuleName})
+				s.Require().Error(err)
+			}
+		})
+	}
+}

Finschia/finschia-sdk

x/upgrade/client/testutil/cli_test.go
@@ -1,0 +1,15 @@
+package testutil
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/line/lbm-sdk/testutil/network"
+)
+
+func TestIntegrationTestSuite(t *testing.T) {
+	cfg := network.DefaultConfig()
+	cfg.NumValidators = 1
+	suite.Run(t, NewIntegrationTestSuite(cfg))
+}

x/upgrade/client/testutil/suite.go
@@ -1,0 +1,115 @@
+package testutil
+
+import (
+	"fmt"
+
+	"github.com/stretchr/testify/suite"
+
+	ocproto "github.com/line/ostracon/proto/ostracon/types"
+
+	"github.com/line/link-sdk/simapp"
+	clitestutil "github.com/line/link-sdk/testutil/cli"
+	"github.com/line/link-sdk/testutil/network"
+	sdk "github.com/line/link-sdk/types"
+	"github.com/line/link-sdk/x/upgrade/client/cli"
+	"github.com/line/link-sdk/x/upgrade/types"
+)
+
+func NewIntegrationTestSuite(cfg network.Config) *IntegrationTestSuite {
+	return &IntegrationTestSuite{cfg: cfg}
+}
+
+type IntegrationTestSuite struct {
+	suite.Suite
+
+	app     *simapp.SimApp
+	cfg     network.Config
+	network *network.Network
+	ctx     sdk.Context
+}
+
+func (s *IntegrationTestSuite) SetupSuite() {
+	s.T().Log("setting up integration test suite")
+	app := simapp.Setup(false)
+	s.app = app
+	s.ctx = app.BaseApp.NewContext(false, tmproto.Header{})
+
+	cfg := network.DefaultConfig()
+	cfg.NumValidators = 1
+
+	s.cfg = cfg
+	s.network = network.New(s.T(), cfg)
+}
+
+func (s *IntegrationTestSuite) TearDownSuite() {
+	s.T().Log("tearing down integration test suite")
+	s.network.Cleanup()
+}
+
+func (s *IntegrationTestSuite) TestModuleVersionsCLI() {
+	testCases := []struct {
+		msg     string
+		req     types.QueryModuleVersionsRequest
+		single  bool
+		expPass bool
+	}{
+		{
+			msg:     "test full query",
+			req:     types.QueryModuleVersionsRequest{ModuleName: ""},
+			single:  false,
+			expPass: true,
+		},
+		{
+			msg:     "test single module",
+			req:     types.QueryModuleVersionsRequest{ModuleName: "bank"},
+			single:  true,
+			expPass: true,
+		},
+		{
+			msg:     "test non-existent module",
+			req:     types.QueryModuleVersionsRequest{ModuleName: "abcdefg"},
+			single:  true,
+			expPass: false,
+		},
+	}
+
+	val := s.network.Validators[0]
+	clientCtx := val.ClientCtx
+	// avoid printing as yaml from CLI command
+	clientCtx.OutputFormat = "JSON"
+
+	vm := s.app.UpgradeKeeper.GetModuleVersionMap(s.ctx)
+	mv := s.app.UpgradeKeeper.GetModuleVersions(s.ctx)
+	s.Require().NotEmpty(vm)
+
+	for _, tc := range testCases {
+		s.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+
+			expect := mv
+			if tc.expPass {
+				if tc.single {
+					expect = []*types.ModuleVersion{{Name: tc.req.ModuleName, Version: vm[tc.req.ModuleName]}}
+				}
+				// setup expected response
+				pm := types.QueryModuleVersionsResponse{
+					ModuleVersions: expect,
+				}
+				jsonVM, _ := clientCtx.JSONCodec.MarshalJSON(&pm)
+				expectedRes := string(jsonVM)
+				// append new line to match behaviour of PrintProto
+				expectedRes += "\n"
+
+				// get actual module versions list response from cli
+				cmd := cli.GetModuleVersionsCmd()
+				outVM, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, []string{tc.req.ModuleName})
+				s.Require().NoError(err)
+
+				s.Require().Equal(expectedRes, outVM.String())
+			} else {
+				cmd := cli.GetModuleVersionsCmd()
+				_, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, []string{tc.req.ModuleName})
+				s.Require().Error(err)
+			}
+		})
+	}
+}