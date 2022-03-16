cosmos/cosmos-sdk

fix: Staking delegations should return empty list instead of rpc error when no records found (#9423)
* return empty list instead of rpc error when no records found for staking delegations

* fix grpc query DelegatorDelegations tests

* remove response code tests for staking delegations

* fix failing tests

* change staking delegations response code to 200 in grpc test

* add staking delegations response code tests to TestQueryDelegatorDelegationsGRPC

* add address without delegations testcase

* add changes to grpc query tests of delegatorDelegations

* remove getRequest unused function from x/staking/client/rest/grpc_query_test.go

* minor fixes

* add testcases for request with no delegations

* address review comments

* add changelog

Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

x/staking/client/rest/grpc_query_test.go
@@ -4,8 +4,6 @@ package rest_test
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"testing"
 
 	"github.com/gogo/protobuf/proto"
@@ -414,38 +412,14 @@ func (s *IntegrationTestSuite) TestQueryUnbondingDelegationGRPC() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestQueryDelegationsResponseCode() {
-	val := s.network.Validators[0]
-
-	// Create new account in the keyring.
-	info, _, err := val.ClientCtx.Keyring.NewMnemonic("test", keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
-	s.Require().NoError(err)
-	newAddr := sdk.AccAddress(info.GetPubKey().Address())
-
-	s.T().Log("expect 404 error for address without delegations")
-	res, statusCode, err := getRequest(fmt.Sprintf("%s/cosmos/staking/v1beta1/delegations/%s", val.APIAddress, newAddr.String()))
-	s.Require().NoError(err)
-	s.Require().Contains(string(res), "\"code\": 5")
-	s.Require().Equal(404, statusCode)
-}
-
-func getRequest(url string) ([]byte, int, error) {
-	res, err := http.Get(url) // nolint:gosec
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, res.StatusCode, err
-	}
-
-	if err = res.Body.Close(); err != nil {
-		return nil, res.StatusCode, err
-	}
-
-	return body, res.StatusCode, nil
-}
-
 func (s *IntegrationTestSuite) TestQueryDelegatorDelegationsGRPC() {
 	val := s.network.Validators[0]
 	baseURL := val.APIAddress
+
+	// Create new account in the keyring for address without delegations.
+	info, _, err := val.ClientCtx.Keyring.NewMnemonic("test", keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
+	s.Require().NoError(err)
+	newAddr := sdk.AccAddress(info.GetPubKey().Address())
 
 	testCases := []struct {
 		name         string
@@ -484,6 +458,19 @@ func (s *IntegrationTestSuite) TestQueryDelegatorDelegationsGRPC() {
 					types.NewDelegationResp(val.Address, val.ValAddress, sdk.NewDecFromInt(cli.DefaultTokens), sdk.NewCoin(sdk.DefaultBondDenom, cli.DefaultTokens)),
 				},
 				Pagination: &query.PageResponse{Total: 1},
+			},
+		},
+		{
+			"address without delegations",
+			fmt.Sprintf("%s/cosmos/staking/v1beta1/delegations/%s", baseURL, newAddr.String()),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "1",
+			},
+			false,
+			&types.QueryDelegatorDelegationsResponse{},
+			&types.QueryDelegatorDelegationsResponse{
+				DelegationResponses: types.DelegationResponses{},
+				Pagination:          &query.PageResponse{Total: 0},
 			},
 		},
 	}

Finschia/finschia-sdk

x/staking/client/rest/grpc_query_test.go
@@ -5,8 +5,6 @@ package rest_test
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"testing"
 
 	"github.com/gogo/protobuf/proto"
@@ -415,38 +413,14 @@ func (s *IntegrationTestSuite) TestQueryUnbondingDelegationGRPC() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestQueryDelegationsResponseCode() {
-	val := s.network.Validators[0]
-
-	// Create new account in the keyring.
-	info, _, err := val.ClientCtx.Keyring.NewMnemonic("test", keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
-	s.Require().NoError(err)
-	newAddr := sdk.BytesToAccAddress(info.GetPubKey().Address())
-
-	s.T().Log("expect 404 error for address without delegations")
-	res, statusCode, err := getRequest(fmt.Sprintf("%s/lbm/staking/v1/delegations/%s", val.APIAddress, newAddr.String()))
-	s.Require().NoError(err)
-	s.Require().Contains(string(res), "\"code\": 5")
-	s.Require().Equal(404, statusCode)
-}
-
-func getRequest(url string) ([]byte, int, error) {
-	res, err := http.Get(url) // nolint:gosec
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, res.StatusCode, err
-	}
-
-	if err = res.Body.Close(); err != nil {
-		return nil, res.StatusCode, err
-	}
-
-	return body, res.StatusCode, nil
-}
-
 func (s *IntegrationTestSuite) TestQueryDelegatorDelegationsGRPC() {
 	val := s.network.Validators[0]
 	baseURL := val.APIAddress
+
+	// Create new account in the keyring for address without delegations.
+	info, _, err := val.ClientCtx.Keyring.NewMnemonic("test", keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
+	s.Require().NoError(err)
+	newAddr := sdk.BytesToAccAddress(info.GetPubKey().Address())
 
 	testCases := []struct {
 		name         string
@@ -485,6 +459,19 @@ func (s *IntegrationTestSuite) TestQueryDelegatorDelegationsGRPC() {
 					types.NewDelegationResp(val.Address, val.ValAddress, sdk.NewDecFromInt(cli.DefaultTokens), sdk.NewCoin(sdk.DefaultBondDenom, cli.DefaultTokens)),
 				},
 				Pagination: &query.PageResponse{Total: 1},
+			},
+		},
+		{
+			"address without delegations",
+			fmt.Sprintf("%s/cosmos/staking/v1beta1/delegations/%s", baseURL, newAddr.String()),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "1",
+			},
+			false,
+			&types.QueryDelegatorDelegationsResponse{},
+			&types.QueryDelegatorDelegationsResponse{
+				DelegationResponses: types.DelegationResponses{},
+				Pagination:          &query.PageResponse{Total: 0},
 			},
 		},
 	}