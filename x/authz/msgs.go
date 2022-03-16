cosmos/cosmos-sdk

feat: Add amino support for x/authz and x/feegrant (#9457)
* add amino for authz

* Add amion for feegrant

* add cl

* Remove protoCdc from simulations

* Update x/authz/client/testutil/tx.go

Co-authored-by: likhita-809 <78951027+likhita-809@users.noreply.github.com>

* Address reviews

Co-authored-by: likhita-809 <78951027+likhita-809@users.noreply.github.com>

x/authz/msgs.go
@@ -5,15 +5,22 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
 )
 
 var (
 	_ sdk.Msg = &MsgGrant{}
 	_ sdk.Msg = &MsgRevoke{}
 	_ sdk.Msg = &MsgExec{}
+
+	// For amino support.
+	_ legacytx.LegacyMsg = &MsgGrant{}
+	_ legacytx.LegacyMsg = &MsgRevoke{}
+	_ legacytx.LegacyMsg = &MsgExec{}
 
 	_ cdctypes.UnpackInterfacesMessage = &MsgGrant{}
 	_ cdctypes.UnpackInterfacesMessage = &MsgExec{}
@@ -58,6 +65,21 @@ func (msg MsgGrant) ValidateBasic() error {
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "granter and grantee cannot be same")
 	}
 	return msg.Grant.ValidateBasic()
+}
+
+// Type implements the LegacyMsg.Type method.
+func (msg MsgGrant) Type() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// Route implements the LegacyMsg.Route method.
+func (msg MsgGrant) Route() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// GetSignBytes implements the LegacyMsg.GetSignBytes method.
+func (msg MsgGrant) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&msg))
 }
 
 // GetAuthorization returns the cache value from the MsgGrant.Authorization if present.
@@ -138,6 +160,21 @@ func (msg MsgRevoke) ValidateBasic() error {
 	return nil
 }
 
+// Type implements the LegacyMsg.Type method.
+func (msg MsgRevoke) Type() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// Route implements the LegacyMsg.Route method.
+func (msg MsgRevoke) Route() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// GetSignBytes implements the LegacyMsg.GetSignBytes method.
+func (msg MsgRevoke) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&msg))
+}
+
 // NewMsgExec creates a new MsgExecAuthorized
 //nolint:interfacer
 func NewMsgExec(grantee sdk.AccAddress, msgs []sdk.Msg) MsgExec {
@@ -192,4 +229,19 @@ func (msg MsgExec) ValidateBasic() error {
 	}
 
 	return nil
+}
+
+// Type implements the LegacyMsg.Type method.
+func (msg MsgExec) Type() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// Route implements the LegacyMsg.Route method.
+func (msg MsgExec) Route() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// GetSignBytes implements the LegacyMsg.GetSignBytes method.
+func (msg MsgExec) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&msg))
 }

Finschia/finschia-sdk

x/authz/msgs.go
@@ -5,15 +5,22 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 
+	"github.com/line/lbm-sdk/codec/legacy"
 	cdctypes "github.com/line/lbm-sdk/codec/types"
 	sdk "github.com/line/lbm-sdk/types"
 	sdkerrors "github.com/line/lbm-sdk/types/errors"
+	"github.com/line/lbm-sdk/x/auth/legacy/legacytx"
 )
 
 var (
 	_ sdk.Msg = &MsgGrant{}
 	_ sdk.Msg = &MsgRevoke{}
 	_ sdk.Msg = &MsgExec{}
+
+	// For amino support.
+	_ legacytx.LegacyMsg = &MsgGrant{}
+	_ legacytx.LegacyMsg = &MsgRevoke{}
+	_ legacytx.LegacyMsg = &MsgExec{}
 
 	_ cdctypes.UnpackInterfacesMessage = &MsgGrant{}
 	_ cdctypes.UnpackInterfacesMessage = &MsgExec{}
@@ -62,6 +69,21 @@ func (msg MsgGrant) ValidateBasic() error {
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "granter and grantee cannot be same")
 	}
 	return msg.Grant.ValidateBasic()
+}
+
+// Type implements the LegacyMsg.Type method.
+func (msg MsgGrant) Type() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// Route implements the LegacyMsg.Route method.
+func (msg MsgGrant) Route() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// GetSignBytes implements the LegacyMsg.GetSignBytes method.
+func (msg MsgGrant) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&msg))
 }
 
 // GetAuthorization returns the cache value from the MsgGrant.Authorization if present.
@@ -146,6 +168,21 @@ func (msg MsgRevoke) ValidateBasic() error {
 	return nil
 }
 
+// Type implements the LegacyMsg.Type method.
+func (msg MsgRevoke) Type() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// Route implements the LegacyMsg.Route method.
+func (msg MsgRevoke) Route() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// GetSignBytes implements the LegacyMsg.GetSignBytes method.
+func (msg MsgRevoke) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&msg))
+}
+
 // NewMsgExec creates a new MsgExecAuthorized
 //nolint:interfacer
 func NewMsgExec(grantee sdk.AccAddress, msgs []sdk.Msg) MsgExec {
@@ -201,4 +238,19 @@ func (msg MsgExec) ValidateBasic() error {
 	}
 
 	return nil
+}
+
+// Type implements the LegacyMsg.Type method.
+func (msg MsgExec) Type() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// Route implements the LegacyMsg.Route method.
+func (msg MsgExec) Route() string {
+	return sdk.MsgTypeURL(&msg)
+}
+
+// GetSignBytes implements the LegacyMsg.GetSignBytes method.
+func (msg MsgExec) GetSignBytes() []byte {
+	return sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(&msg))
 }