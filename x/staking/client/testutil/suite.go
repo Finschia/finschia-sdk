cosmos/cosmos-sdk

x/staking v0.43 Audit updates  (#9267)
* update staking specs

* use gosimple code in x/staking/client/testutil/suite.go

* small fixes

* add godoc to methods in msg_server and grpc_query

* changes to godoc and small fixes

* remove unnecessary lines

* remove mentions of serviceMsg and avoid having slash after Msg

x/staking/client/testutil/suite.go
@@ -230,7 +230,7 @@ func (s *IntegrationTestSuite) TestGetCmdQueryValidator() {
 		},
 		{
 			"happy case",
-			[]string{fmt.Sprintf("%s", val.ValAddress), fmt.Sprintf("--%s=json", tmcli.OutputFlag)},
+			[]string{val.ValAddress.String(), fmt.Sprintf("--%s=json", tmcli.OutputFlag)},
 			false,
 		},
 	}
@@ -415,7 +415,7 @@ func (s *IntegrationTestSuite) TestGetCmdQueryDelegations() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestGetCmdQueryDelegationsTo() {
+func (s *IntegrationTestSuite) TestGetCmdQueryValidatorDelegations() {
 	val := s.network.Validators[0]
 
 	testCases := []struct {
@@ -755,7 +755,7 @@ func (s *IntegrationTestSuite) TestGetCmdQueryRedelegation() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestGetCmdQueryRedelegationsFrom() {
+func (s *IntegrationTestSuite) TestGetCmdQueryValidatorRedelegations() {
 	val := s.network.Validators[0]
 	val2 := s.network.Validators[1]
 
@@ -925,7 +925,7 @@ not_bonded_tokens: "0"`, cli.DefaultTokens.Mul(sdk.NewInt(2)).String()),
 	}
 }
 
-func (s *IntegrationTestSuite) TestNewCmdEditValidator() {
+func (s *IntegrationTestSuite) TestNewEditValidatorCmd() {
 	val := s.network.Validators[0]
 
 	details := "bio"
@@ -1041,7 +1041,7 @@ func (s *IntegrationTestSuite) TestNewCmdEditValidator() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestNewCmdDelegate() {
+func (s *IntegrationTestSuite) TestNewDelegateCmd() {
 	val := s.network.Validators[0]
 
 	info, _, err := val.ClientCtx.Keyring.NewMnemonic("NewAccount", keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
@@ -1123,7 +1123,7 @@ func (s *IntegrationTestSuite) TestNewCmdDelegate() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestNewCmdRedelegate() {
+func (s *IntegrationTestSuite) TestNewRedelegateCmd() {
 	val := s.network.Validators[0]
 	val2 := s.network.Validators[1]
 
@@ -1209,7 +1209,7 @@ func (s *IntegrationTestSuite) TestNewCmdRedelegate() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestNewCmdUnbond() {
+func (s *IntegrationTestSuite) TestNewUnbondCmd() {
 	val := s.network.Validators[0]
 
 	testCases := []struct {

x/staking/simulation/params.go
@@ -6,9 +6,8 @@ import (
 	"fmt"
 	"math/rand"
 
-	"github.com/cosmos/cosmos-sdk/x/simulation"
-
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
@@ -18,17 +17,17 @@ func ParamChanges(r *rand.Rand) []simtypes.ParamChange {
 	return []simtypes.ParamChange{
 		simulation.NewSimParamChange(types.ModuleName, string(types.KeyMaxValidators),
 			func(r *rand.Rand) string {
-				return fmt.Sprintf("%d", GenMaxValidators(r))
+				return fmt.Sprintf("%d", genMaxValidators(r))
 			},
 		),
 		simulation.NewSimParamChange(types.ModuleName, string(types.KeyUnbondingTime),
 			func(r *rand.Rand) string {
-				return fmt.Sprintf("\"%d\"", GenUnbondingTime(r))
+				return fmt.Sprintf("\"%d\"", genUnbondingTime(r))
 			},
 		),
 		simulation.NewSimParamChange(types.ModuleName, string(types.KeyHistoricalEntries),
 			func(r *rand.Rand) string {
-				return fmt.Sprintf("%d", GetHistEntries(r))
+				return fmt.Sprintf("%d", getHistEntries(r))
 			},
 		),
 	}

Finschia/finschia-sdk

x/staking/client/testutil/suite.go
@@ -231,7 +231,7 @@ func (s *IntegrationTestSuite) TestGetCmdQueryValidator() {
 		},
 		{
 			"happy case",
-			[]string{fmt.Sprintf("%s", val.ValAddress), fmt.Sprintf("--%s=json", ostcli.OutputFlag)},
+			[]string{val.ValAddress.String(), fmt.Sprintf("--%s=json", ostcli.OutputFlag)},
 			false,
 		},
 	}
@@ -416,7 +416,7 @@ func (s *IntegrationTestSuite) TestGetCmdQueryDelegations() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestGetCmdQueryDelegationsTo() {
+func (s *IntegrationTestSuite) TestGetCmdQueryValidatorDelegations() {
 	val := s.network.Validators[0]
 
 	testCases := []struct {
@@ -756,7 +756,7 @@ func (s *IntegrationTestSuite) TestGetCmdQueryRedelegation() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestGetCmdQueryRedelegationsFrom() {
+func (s *IntegrationTestSuite) TestGetCmdQueryValidatorRedelegations() {
 	val := s.network.Validators[0]
 	val2 := s.network.Validators[1]
 
@@ -926,7 +926,7 @@ not_bonded_tokens: "0"`, cli.DefaultTokens.Mul(sdk.NewInt(2)).String()),
 	}
 }
 
-func (s *IntegrationTestSuite) TestNewCmdEditValidator() {
+func (s *IntegrationTestSuite) TestNewEditValidatorCmd() {
 	val := s.network.Validators[0]
 
 	details := "bio"
@@ -1042,7 +1042,7 @@ func (s *IntegrationTestSuite) TestNewCmdEditValidator() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestNewCmdDelegate() {
+func (s *IntegrationTestSuite) TestNewDelegateCmd() {
 	val := s.network.Validators[0]
 
 	info, _, err := val.ClientCtx.Keyring.NewMnemonic("NewAccount", keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
@@ -1124,7 +1124,7 @@ func (s *IntegrationTestSuite) TestNewCmdDelegate() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestNewCmdRedelegate() {
+func (s *IntegrationTestSuite) TestNewRedelegateCmd() {
 	val := s.network.Validators[0]
 	val2 := s.network.Validators[1]
 
@@ -1210,7 +1210,7 @@ func (s *IntegrationTestSuite) TestNewCmdRedelegate() {
 	}
 }
 
-func (s *IntegrationTestSuite) TestNewCmdUnbond() {
+func (s *IntegrationTestSuite) TestNewUnbondCmd() {
 	val := s.network.Validators[0]
 
 	testCases := []struct {

x/staking/simulation/params.go
@@ -6,9 +6,8 @@ import (
 	"fmt"
 	"math/rand"
 
-	"github.com/line/lbm-sdk/x/simulation"
-
 	simtypes "github.com/line/lbm-sdk/types/simulation"
+	"github.com/line/lbm-sdk/x/simulation"
 	"github.com/line/lbm-sdk/x/staking/types"
 )
 
@@ -18,17 +17,17 @@ func ParamChanges(r *rand.Rand) []simtypes.ParamChange {
 	return []simtypes.ParamChange{
 		simulation.NewSimParamChange(types.ModuleName, string(types.KeyMaxValidators),
 			func(r *rand.Rand) string {
-				return fmt.Sprintf("%d", GenMaxValidators(r))
+				return fmt.Sprintf("%d", genMaxValidators(r))
 			},
 		),
 		simulation.NewSimParamChange(types.ModuleName, string(types.KeyUnbondingTime),
 			func(r *rand.Rand) string {
-				return fmt.Sprintf("\"%d\"", GenUnbondingTime(r))
+				return fmt.Sprintf("\"%d\"", genUnbondingTime(r))
 			},
 		),
 		simulation.NewSimParamChange(types.ModuleName, string(types.KeyHistoricalEntries),
 			func(r *rand.Rand) string {
-				return fmt.Sprintf("%d", GetHistEntries(r))
+				return fmt.Sprintf("%d", getHistEntries(r))
 			},
 		),
 	}