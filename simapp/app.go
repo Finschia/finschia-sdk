cosmos/cosmos-sdk

Move x/feegrant/types to x/feegrant (#9273)
* Move x/feegrant/types to x/feegrant

* Update spec

* Use feegrantmodule

simapp/app.go
@@ -58,9 +58,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/evidence"
 	evidencekeeper "github.com/cosmos/cosmos-sdk/x/evidence/keeper"
 	evidencetypes "github.com/cosmos/cosmos-sdk/x/evidence/types"
-	feegrant "github.com/cosmos/cosmos-sdk/x/feegrant"
+	"github.com/cosmos/cosmos-sdk/x/feegrant"
 	feegrantkeeper "github.com/cosmos/cosmos-sdk/x/feegrant/keeper"
-	feegranttypes "github.com/cosmos/cosmos-sdk/x/feegrant/types"
+	feegrantmodule "github.com/cosmos/cosmos-sdk/x/feegrant/module"
 	"github.com/cosmos/cosmos-sdk/x/genutil"
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	"github.com/cosmos/cosmos-sdk/x/gov"
@@ -112,7 +112,7 @@ var (
 		params.AppModuleBasic{},
 		crisis.AppModuleBasic{},
 		slashing.AppModuleBasic{},
-		feegrant.AppModuleBasic{},
+		feegrantmodule.AppModuleBasic{},
 		upgrade.AppModuleBasic{},
 		evidence.AppModuleBasic{},
 		authz_m.AppModuleBasic{},
@@ -205,7 +205,7 @@ func NewSimApp(
 	keys := sdk.NewKVStoreKeys(
 		authtypes.StoreKey, banktypes.StoreKey, stakingtypes.StoreKey,
 		minttypes.StoreKey, distrtypes.StoreKey, slashingtypes.StoreKey,
-		govtypes.StoreKey, paramstypes.StoreKey, upgradetypes.StoreKey, feegranttypes.StoreKey,
+		govtypes.StoreKey, paramstypes.StoreKey, upgradetypes.StoreKey, feegrant.StoreKey,
 		evidencetypes.StoreKey, capabilitytypes.StoreKey,
 		authzkeeper.StoreKey,
 	)
@@ -255,7 +255,7 @@ func NewSimApp(
 		app.GetSubspace(crisistypes.ModuleName), invCheckPeriod, app.BankKeeper, authtypes.FeeCollectorName,
 	)
 
-	app.FeeGrantKeeper = feegrantkeeper.NewKeeper(appCodec, keys[feegranttypes.StoreKey], app.AccountKeeper)
+	app.FeeGrantKeeper = feegrantkeeper.NewKeeper(appCodec, keys[feegrant.StoreKey], app.AccountKeeper)
 	app.UpgradeKeeper = upgradekeeper.NewKeeper(skipUpgradeHeights, keys[upgradetypes.StoreKey], appCodec, homePath, app.BaseApp)
 
 	// register the staking hooks
@@ -308,7 +308,7 @@ func NewSimApp(
 		bank.NewAppModule(appCodec, app.BankKeeper, app.AccountKeeper),
 		capability.NewAppModule(appCodec, *app.CapabilityKeeper),
 		crisis.NewAppModule(&app.CrisisKeeper, skipGenesisInvariants),
-		feegrant.NewAppModule(appCodec, app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, app.interfaceRegistry),
+		feegrantmodule.NewAppModule(appCodec, app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, app.interfaceRegistry),
 		gov.NewAppModule(appCodec, app.GovKeeper, app.AccountKeeper, app.BankKeeper),
 		mint.NewAppModule(appCodec, app.MintKeeper, app.AccountKeeper),
 		slashing.NewAppModule(appCodec, app.SlashingKeeper, app.AccountKeeper, app.BankKeeper, app.StakingKeeper),
@@ -339,7 +339,7 @@ func NewSimApp(
 		capabilitytypes.ModuleName, authtypes.ModuleName, banktypes.ModuleName, distrtypes.ModuleName, stakingtypes.ModuleName,
 		slashingtypes.ModuleName, govtypes.ModuleName, minttypes.ModuleName, crisistypes.ModuleName,
 		genutiltypes.ModuleName, evidencetypes.ModuleName, authz.ModuleName,
-		feegranttypes.ModuleName,
+		feegrant.ModuleName,
 	)
 
 	app.mm.RegisterInvariants(&app.CrisisKeeper)
@@ -358,7 +358,7 @@ func NewSimApp(
 		auth.NewAppModule(appCodec, app.AccountKeeper, authsims.RandomGenesisAccounts),
 		bank.NewAppModule(appCodec, app.BankKeeper, app.AccountKeeper),
 		capability.NewAppModule(appCodec, *app.CapabilityKeeper),
-		feegrant.NewAppModule(appCodec, app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, app.interfaceRegistry),
+		feegrantmodule.NewAppModule(appCodec, app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, app.interfaceRegistry),
 		gov.NewAppModule(appCodec, app.GovKeeper, app.AccountKeeper, app.BankKeeper),
 		mint.NewAppModule(appCodec, app.MintKeeper, app.AccountKeeper),
 		staking.NewAppModule(appCodec, app.StakingKeeper, app.AccountKeeper, app.BankKeeper),

x/feegrant/msgs_test.go
@@ -1,4 +1,4 @@
-package types_test
+package feegrant_test
 
 import (
 	"testing"
@@ -7,17 +7,17 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/feegrant/types"
+	"github.com/cosmos/cosmos-sdk/x/feegrant"
 	"github.com/stretchr/testify/require"
 )
 
-func TestMsgGrantFeeAllowance(t *testing.T) {
+func TestMsgGrantAllowance(t *testing.T) {
 	cdc := codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
 	addr, _ := sdk.AccAddressFromBech32("cosmos1aeuqja06474dfrj7uqsvukm6rael982kk89mqr")
 	addr2, _ := sdk.AccAddressFromBech32("cosmos1nph3cfzk6trsmfxkeu943nvach5qw4vwstnvkl")
 	atom := sdk.NewCoins(sdk.NewInt64Coin("atom", 555))
 	threeHours := time.Now().Add(3 * time.Hour)
-	basic := &types.BasicAllowance{
+	basic := &feegrant.BasicAllowance{
 		SpendLimit: atom,
 		Expiration: &threeHours,
 	}
@@ -25,7 +25,7 @@ func TestMsgGrantFeeAllowance(t *testing.T) {
 	cases := map[string]struct {
 		grantee sdk.AccAddress
 		granter sdk.AccAddress
-		grant   *types.BasicAllowance
+		grant   *feegrant.BasicAllowance
 		valid   bool
 	}{
 		"valid": {
@@ -55,7 +55,7 @@ func TestMsgGrantFeeAllowance(t *testing.T) {
 	}
 
 	for _, tc := range cases {
-		msg, err := types.NewMsgGrantAllowance(tc.grant, tc.granter, tc.grantee)
+		msg, err := feegrant.NewMsgGrantAllowance(tc.grant, tc.granter, tc.grantee)
 		require.NoError(t, err)
 		err = msg.ValidateBasic()
 
@@ -77,20 +77,20 @@ func TestMsgGrantFeeAllowance(t *testing.T) {
 	}
 }
 
-func TestMsgRevokeFeeAllowance(t *testing.T) {
+func TestMsgRevokeAllowance(t *testing.T) {
 	addr, _ := sdk.AccAddressFromBech32("cosmos1aeuqja06474dfrj7uqsvukm6rael982kk89mqr")
 	addr2, _ := sdk.AccAddressFromBech32("cosmos1nph3cfzk6trsmfxkeu943nvach5qw4vwstnvkl")
 	atom := sdk.NewCoins(sdk.NewInt64Coin("atom", 555))
 	threeHours := time.Now().Add(3 * time.Hour)
 
-	basic := &types.BasicAllowance{
+	basic := &feegrant.BasicAllowance{
 		SpendLimit: atom,
 		Expiration: &threeHours,
 	}
 	cases := map[string]struct {
 		grantee sdk.AccAddress
 		granter sdk.AccAddress
-		grant   *types.BasicAllowance
+		grant   *feegrant.BasicAllowance
 		valid   bool
 	}{
 		"valid": {
@@ -120,7 +120,7 @@ func TestMsgRevokeFeeAllowance(t *testing.T) {
 	}
 
 	for _, tc := range cases {
-		msg := types.NewMsgRevokeAllowance(tc.granter, tc.grantee)
+		msg := feegrant.NewMsgRevokeAllowance(tc.granter, tc.grantee)
 		err := msg.ValidateBasic()
 		if tc.valid {
 			require.NoError(t, err)

x/feegrant/simulation/genesis.go
@@ -8,15 +8,12 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	"github.com/cosmos/cosmos-sdk/x/feegrant/types"
+	"github.com/cosmos/cosmos-sdk/x/feegrant"
 )
 
-// Simulation parameter constants
-const feegrant = "feegrant"
-
 // genFeeGrants returns a slice of randomly generated allowances.
-func genFeeGrants(r *rand.Rand, accounts []simtypes.Account) []types.Grant {
-	allowances := make([]types.Grant, len(accounts)-1)
+func genFeeGrants(r *rand.Rand, accounts []simtypes.Account) []feegrant.Grant {
+	allowances := make([]feegrant.Grant, len(accounts)-1)
 	for i := 0; i < len(accounts)-1; i++ {
 		granter := accounts[i].Address
 		grantee := accounts[i+1].Address
@@ -25,22 +22,22 @@ func genFeeGrants(r *rand.Rand, accounts []simtypes.Account) []types.Grant {
 	return allowances
 }
 
-func generateRandomAllowances(granter, grantee sdk.AccAddress, r *rand.Rand) types.Grant {
-	allowances := make([]types.Grant, 3)
+func generateRandomAllowances(granter, grantee sdk.AccAddress, r *rand.Rand) feegrant.Grant {
+	allowances := make([]feegrant.Grant, 3)
 	spendLimit := sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(100)))
 	periodSpendLimit := sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(10)))
 
-	basic := types.BasicAllowance{
+	basic := feegrant.BasicAllowance{
 		SpendLimit: spendLimit,
 	}
 
-	basicAllowance, err := types.NewGrant(granter, grantee, &basic)
+	basicAllowance, err := feegrant.NewGrant(granter, grantee, &basic)
 	if err != nil {
 		panic(err)
 	}
 	allowances[0] = basicAllowance
 
-	periodicAllowance, err := types.NewGrant(granter, grantee, &types.PeriodicAllowance{
+	periodicAllowance, err := feegrant.NewGrant(granter, grantee, &feegrant.PeriodicAllowance{
 		Basic:            basic,
 		PeriodSpendLimit: periodSpendLimit,
 		Period:           time.Hour,
@@ -50,7 +47,7 @@ func generateRandomAllowances(granter, grantee sdk.AccAddress, r *rand.Rand) typ
 	}
 	allowances[1] = periodicAllowance
 
-	filteredAllowance, err := types.NewGrant(granter, grantee, &types.AllowedMsgAllowance{
+	filteredAllowance, err := feegrant.NewGrant(granter, grantee, &feegrant.AllowedMsgAllowance{
 		Allowance:       basicAllowance.GetAllowance(),
 		AllowedMessages: []string{"/cosmos.gov.v1beta1.MsgSubmitProposal"},
 	})
@@ -64,19 +61,19 @@ func generateRandomAllowances(granter, grantee sdk.AccAddress, r *rand.Rand) typ
 
 // RandomizedGenState generates a random GenesisState for feegrant
 func RandomizedGenState(simState *module.SimulationState) {
-	var feegrants []types.Grant
+	var feegrants []feegrant.Grant
 
 	simState.AppParams.GetOrGenerate(
-		simState.Cdc, feegrant, &feegrants, simState.Rand,
+		simState.Cdc, "feegrant", &feegrants, simState.Rand,
 		func(r *rand.Rand) { feegrants = genFeeGrants(r, simState.Accounts) },
 	)
 
-	feegrantGenesis := types.NewGenesisState(feegrants)
+	feegrantGenesis := feegrant.NewGenesisState(feegrants)
 	bz, err := simState.Cdc.MarshalJSON(feegrantGenesis)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Selected randomly generated %s parameters:\n%s\n", types.ModuleName, bz)
-	simState.GenState[types.ModuleName] = bz
+	fmt.Printf("Selected randomly generated %s parameters:\n%s\n", feegrant.ModuleName, bz)
+	simState.GenState[feegrant.ModuleName] = bz
 }

x/feegrant/simulation/operations_test.go
@@ -15,8 +15,8 @@ import (
 	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/feegrant"
 	"github.com/cosmos/cosmos-sdk/x/feegrant/simulation"
-	"github.com/cosmos/cosmos-sdk/x/feegrant/types"
 )
 
 type SimTestSuite struct {
@@ -78,14 +78,14 @@ func (suite *SimTestSuite) TestWeightedOperations() {
 		opMsgName  string
 	}{
 		{
-			simappparams.DefaultWeightGrantFeeAllowance,
-			types.ModuleName,
-			simulation.TypeMsgGrantFeeAllowance,
+			simappparams.DefaultWeightGrantAllowance,
+			feegrant.ModuleName,
+			simulation.TypeMsgGrantAllowance,
 		},
 		{
-			simappparams.DefaultWeightRevokeFeeAllowance,
-			types.ModuleName,
-			simulation.TypeMsgRevokeFeeAllowance,
+			simappparams.DefaultWeightRevokeAllowance,
+			feegrant.ModuleName,
+			simulation.TypeMsgRevokeAllowance,
 		},
 	}
 
@@ -100,7 +100,7 @@ func (suite *SimTestSuite) TestWeightedOperations() {
 	}
 }
 
-func (suite *SimTestSuite) TestSimulateMsgGrantFeeAllowance() {
+func (suite *SimTestSuite) TestSimulateMsgGrantAllowance() {
 	app, ctx := suite.app, suite.ctx
 	require := suite.Require()
 
@@ -112,11 +112,11 @@ func (suite *SimTestSuite) TestSimulateMsgGrantFeeAllowance() {
 	app.BeginBlock(abci.RequestBeginBlock{Header: tmproto.Header{Height: app.LastBlockHeight() + 1, AppHash: app.LastCommitID().Hash}})
 
 	// execute operation
-	op := simulation.SimulateMsgGrantFeeAllowance(app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, suite.protoCdc)
+	op := simulation.SimulateMsgGrantAllowance(app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, suite.protoCdc)
 	operationMsg, futureOperations, err := op(r, app.BaseApp, ctx, accounts, "")
 	require.NoError(err)
 
-	var msg types.MsgGrantAllowance
+	var msg feegrant.MsgGrantAllowance
 	suite.app.AppCodec().UnmarshalJSON(operationMsg.Msg, &msg)
 
 	require.True(operationMsg.OK)
@@ -125,7 +125,7 @@ func (suite *SimTestSuite) TestSimulateMsgGrantFeeAllowance() {
 	require.Len(futureOperations, 0)
 }
 
-func (suite *SimTestSuite) TestSimulateMsgRevokeFeeAllowance() {
+func (suite *SimTestSuite) TestSimulateMsgRevokeAllowance() {
 	app, ctx := suite.app, suite.ctx
 	require := suite.Require()
 
@@ -146,7 +146,7 @@ func (suite *SimTestSuite) TestSimulateMsgRevokeFeeAllowance() {
 		ctx,
 		granter.Address,
 		grantee.Address,
-		&types.BasicAllowance{
+		&feegrant.BasicAllowance{
 			SpendLimit: feeCoins,
 			Expiration: &oneYear,
 		},
@@ -154,11 +154,11 @@ func (suite *SimTestSuite) TestSimulateMsgRevokeFeeAllowance() {
 	require.NoError(err)
 
 	// execute operation
-	op := simulation.SimulateMsgRevokeFeeAllowance(app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, suite.protoCdc)
+	op := simulation.SimulateMsgRevokeAllowance(app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, suite.protoCdc)
 	operationMsg, futureOperations, err := op(r, app.BaseApp, ctx, accounts, "")
 	require.NoError(err)
 
-	var msg types.MsgRevokeAllowance
+	var msg feegrant.MsgRevokeAllowance
 	suite.app.AppCodec().UnmarshalJSON(operationMsg.Msg, &msg)
 
 	require.True(operationMsg.OK)

Finschia/finschia-sdk

simapp/app.go
@@ -39,8 +39,9 @@ import (
 	authtx "github.com/line/lbm-sdk/x/auth/tx"
 	authtypes "github.com/line/lbm-sdk/x/auth/types"
 	"github.com/line/lbm-sdk/x/auth/vesting"
-	authz "github.com/line/lbm-sdk/x/authz"
+	"github.com/line/lbm-sdk/x/authz"
 	authzkeeper "github.com/line/lbm-sdk/x/authz/keeper"
+	authzmodule "github.com/line/lbm-sdk/x/authz/module"
 	authztypes "github.com/line/lbm-sdk/x/authz/types"
 	"github.com/line/lbm-sdk/x/bank"
 	bankkeeper "github.com/line/lbm-sdk/x/bank/keeper"
@@ -66,6 +67,7 @@ import (
 	evidencetypes "github.com/line/lbm-sdk/x/evidence/types"
 	"github.com/line/lbm-sdk/x/feegrant"
 	feegrantkeeper "github.com/line/lbm-sdk/x/feegrant/keeper"
+	feegrantmodule "github.com/line/lbm-sdk/x/feegrant/module"
 	feegranttypes "github.com/line/lbm-sdk/x/feegrant/types"
 	"github.com/line/lbm-sdk/x/genutil"
 	genutiltypes "github.com/line/lbm-sdk/x/genutil/types"
@@ -144,11 +146,11 @@ var (
 		crisis.AppModuleBasic{},
 		slashing.AppModuleBasic{},
 		ibc.AppModuleBasic{},
-		feegrant.AppModuleBasic{},
+		feegrantmodule.AppModuleBasic{},
 		upgrade.AppModuleBasic{},
 		evidence.AppModuleBasic{},
 		transfer.AppModuleBasic{},
-		authz.AppModuleBasic{},
+		authzmodule.AppModuleBasic{},
 		vesting.AppModuleBasic{},
 		tokenmodule.AppModuleBasic{},
 	)
@@ -322,7 +324,7 @@ func NewSimApp(
 		app.GetSubspace(crisistypes.ModuleName), invCheckPeriod, app.BankKeeper, authtypes.FeeCollectorName,
 	)
 
-	app.FeeGrantKeeper = feegrantkeeper.NewKeeper(appCodec, keys[feegranttypes.StoreKey], app.AccountKeeper)
+	app.FeeGrantKeeper = feegrantkeeper.NewKeeper(appCodec, keys[feegrant.StoreKey], app.AccountKeeper)
 	app.UpgradeKeeper = upgradekeeper.NewKeeper(skipUpgradeHeights, keys[upgradetypes.StoreKey], appCodec, homePath, app.BaseApp)
 	app.ConsortiumKeeper = consortiumkeeper.NewKeeper(appCodec, keys[consortiumtypes.StoreKey], stakingKeeper)
 
@@ -405,7 +407,7 @@ func NewSimApp(
 		bankplus.NewAppModule(appCodec, app.BankKeeper, app.AccountKeeper),
 		capability.NewAppModule(appCodec, *app.CapabilityKeeper),
 		crisis.NewAppModule(&app.CrisisKeeper, skipGenesisInvariants),
-		feegrant.NewAppModule(appCodec, app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, app.interfaceRegistry),
+		feegrantmodule.NewAppModule(appCodec, app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, app.interfaceRegistry),
 		consortium.NewAppModule(appCodec, app.ConsortiumKeeper, app.StakingKeeper),
 		gov.NewAppModule(appCodec, app.GovKeeper, app.AccountKeeper, app.BankKeeper),
 		mint.NewAppModule(appCodec, app.MintKeeper, app.AccountKeeper),
@@ -417,7 +419,7 @@ func NewSimApp(
 		ibc.NewAppModule(app.IBCKeeper),
 		params.NewAppModule(app.ParamsKeeper),
 		tokenmodule.NewAppModule(appCodec, app.TokenKeeper),
-		authz.NewAppModule(appCodec, app.AuthzKeeper, app.AccountKeeper, app.BankKeeper, app.interfaceRegistry),
+		authzmodule.NewAppModule(appCodec, app.AuthzKeeper, app.AccountKeeper, app.BankKeeper, app.interfaceRegistry),
 		transferModule,
 	)
 
@@ -472,7 +474,7 @@ func NewSimApp(
 		auth.NewAppModule(appCodec, app.AccountKeeper, authsims.RandomGenesisAccounts),
 		bankplus.NewAppModule(appCodec, app.BankKeeper, app.AccountKeeper),
 		capability.NewAppModule(appCodec, *app.CapabilityKeeper),
-		feegrant.NewAppModule(appCodec, app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, app.interfaceRegistry),
+		feegrantmodule.NewAppModule(appCodec, app.AccountKeeper, app.BankKeeper, app.FeeGrantKeeper, app.interfaceRegistry),
 		gov.NewAppModule(appCodec, app.GovKeeper, app.AccountKeeper, app.BankKeeper),
 		mint.NewAppModule(appCodec, app.MintKeeper, app.AccountKeeper),
 		staking.NewAppModule(appCodec, app.StakingKeeper, app.AccountKeeper, app.BankKeeper),
@@ -480,7 +482,7 @@ func NewSimApp(
 		slashing.NewAppModule(appCodec, app.SlashingKeeper, app.AccountKeeper, app.BankKeeper, app.StakingKeeper),
 		params.NewAppModule(app.ParamsKeeper),
 		evidence.NewAppModule(app.EvidenceKeeper),
-		authz.NewAppModule(appCodec, app.AuthzKeeper, app.AccountKeeper, app.BankKeeper, app.interfaceRegistry),
+		authzmodule.NewAppModule(appCodec, app.AuthzKeeper, app.AccountKeeper, app.BankKeeper, app.interfaceRegistry),
 		ibc.NewAppModule(app.IBCKeeper),
 		transferModule,
 	)

x/feegrant/msgs_test.go
@@ -1,4 +1,4 @@
-package types_test
+package feegrant_test
 
 import (
 	"testing"
@@ -11,7 +11,7 @@ import (
 	"github.com/line/lbm-sdk/codec"
 	codectypes "github.com/line/lbm-sdk/codec/types"
 	sdk "github.com/line/lbm-sdk/types"
-	"github.com/line/lbm-sdk/x/feegrant/types"
+	"github.com/line/lbm-sdk/x/feegrant"
 )
 
 func TestMsgGrantAllowance(t *testing.T) {
@@ -20,7 +20,7 @@ func TestMsgGrantAllowance(t *testing.T) {
 	addr2 := sdk.AccAddress("link18vd8fpwxzck93qlwghaj6arh4p7c5n89fvcmzu")
 	atom := sdk.NewCoins(sdk.NewInt64Coin("atom", 555))
 	threeHours := time.Now().Add(3 * time.Hour)
-	basic := &types.BasicAllowance{
+	basic := &feegrant.BasicAllowance{
 		SpendLimit: atom,
 		Expiration: &threeHours,
 	}
@@ -28,7 +28,7 @@ func TestMsgGrantAllowance(t *testing.T) {
 	cases := map[string]struct {
 		grantee sdk.AccAddress
 		granter sdk.AccAddress
-		grant   *types.BasicAllowance
+		grant   *feegrant.BasicAllowance
 		valid   bool
 	}{
 		"valid": {
@@ -58,7 +58,7 @@ func TestMsgGrantAllowance(t *testing.T) {
 	}
 
 	for _, tc := range cases {
-		msg, err := types.NewMsgGrantAllowance(tc.grant, tc.granter, tc.grantee)
+		msg, err := feegrant.NewMsgGrantAllowance(tc.grant, tc.granter, tc.grantee)
 		require.NoError(t, err)
 		err = msg.ValidateBasic()
 
@@ -86,14 +86,14 @@ func TestMsgRevokeAllowance(t *testing.T) {
 	atom := sdk.NewCoins(sdk.NewInt64Coin("atom", 555))
 	threeHours := time.Now().Add(3 * time.Hour)
 
-	basic := &types.BasicAllowance{
+	basic := &feegrant.BasicAllowance{
 		SpendLimit: atom,
 		Expiration: &threeHours,
 	}
 	cases := map[string]struct {
 		grantee sdk.AccAddress
 		granter sdk.AccAddress
-		grant   *types.BasicAllowance
+		grant   *feegrant.BasicAllowance
 		valid   bool
 	}{
 		"valid": {
@@ -123,7 +123,7 @@ func TestMsgRevokeAllowance(t *testing.T) {
 	}
 
 	for _, tc := range cases {
-		msg := types.NewMsgRevokeAllowance(tc.granter, tc.grantee)
+		msg := feegrant.NewMsgRevokeAllowance(tc.granter, tc.grantee)
 		err := msg.ValidateBasic()
 		if tc.valid {
 			require.NoError(t, err)

x/feegrant/simulation/genesis.go
@@ -8,15 +8,12 @@ import (
 	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/types/module"
 	simtypes "github.com/line/lbm-sdk/types/simulation"
-	"github.com/line/lbm-sdk/x/feegrant/types"
+	"github.com/line/lbm-sdk/x/feegrant"
 )
 
-// Simulation parameter constants
-const feegrant = "feegrant"
-
 // genFeeGrants returns a slice of randomly generated allowances.
-func genFeeGrants(r *rand.Rand, accounts []simtypes.Account) []types.Grant {
-	allowances := make([]types.Grant, len(accounts)-1)
+func genFeeGrants(r *rand.Rand, accounts []simtypes.Account) []feegrant.Grant {
+	allowances := make([]feegrant.Grant, len(accounts)-1)
 	for i := 0; i < len(accounts)-1; i++ {
 		granter := accounts[i].Address
 		grantee := accounts[i+1].Address
@@ -25,22 +22,22 @@ func genFeeGrants(r *rand.Rand, accounts []simtypes.Account) []types.Grant {
 	return allowances
 }
 
-func generateRandomAllowances(granter, grantee sdk.AccAddress, r *rand.Rand) types.Grant {
-	allowances := make([]types.Grant, 3)
+func generateRandomAllowances(granter, grantee sdk.AccAddress, r *rand.Rand) feegrant.Grant {
+	allowances := make([]feegrant.Grant, 3)
 	spendLimit := sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(100)))
 	periodSpendLimit := sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(10)))
 
-	basic := types.BasicAllowance{
+	basic := feegrant.BasicAllowance{
 		SpendLimit: spendLimit,
 	}
 
-	basicAllowance, err := types.NewGrant(granter, grantee, &basic)
+	basicAllowance, err := feegrant.NewGrant(granter, grantee, &basic)
 	if err != nil {
 		panic(err)
 	}
 	allowances[0] = basicAllowance
 
-	periodicAllowance, err := types.NewGrant(granter, grantee, &types.PeriodicAllowance{
+	periodicAllowance, err := feegrant.NewGrant(granter, grantee, &feegrant.PeriodicAllowance{
 		Basic:            basic,
 		PeriodSpendLimit: periodSpendLimit,
 		Period:           time.Hour,
@@ -50,7 +47,7 @@ func generateRandomAllowances(granter, grantee sdk.AccAddress, r *rand.Rand) typ
 	}
 	allowances[1] = periodicAllowance
 
-	filteredAllowance, err := types.NewGrant(granter, grantee, &types.AllowedMsgAllowance{
+	filteredAllowance, err := feegrant.NewGrant(granter, grantee, &feegrant.AllowedMsgAllowance{
 		Allowance:       basicAllowance.GetAllowance(),
 		AllowedMessages: []string{"/lbm.gov.v1.MsgSubmitProposal"},
 	})
@@ -64,19 +61,19 @@ func generateRandomAllowances(granter, grantee sdk.AccAddress, r *rand.Rand) typ
 
 // RandomizedGenState generates a random GenesisState for feegrant
 func RandomizedGenState(simState *module.SimulationState) {
-	var feegrants []types.Grant
+	var feegrants []feegrant.Grant
 
 	simState.AppParams.GetOrGenerate(
-		simState.Cdc, feegrant, &feegrants, simState.Rand,
+		simState.Cdc, "feegrant", &feegrants, simState.Rand,
 		func(r *rand.Rand) { feegrants = genFeeGrants(r, simState.Accounts) },
 	)
 
-	feegrantGenesis := types.NewGenesisState(feegrants)
+	feegrantGenesis := feegrant.NewGenesisState(feegrants)
 	bz, err := simState.Cdc.MarshalJSON(feegrantGenesis)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Selected randomly generated %s parameters:\n%s\n", types.ModuleName, bz)
-	simState.GenState[types.ModuleName] = bz
+	fmt.Printf("Selected randomly generated %s parameters:\n%s\n", feegrant.ModuleName, bz)
+	simState.GenState[feegrant.ModuleName] = bz
 }

x/feegrant/simulation/operations_test.go
@@ -9,12 +9,12 @@ import (
 	ocproto "github.com/line/ostracon/proto/ostracon/types"
 	"github.com/stretchr/testify/suite"
 
-	"github.com/line/lbm-sdk/x/feegrant/types"
-
+	"github.com/line/lbm-sdk/codec"
 	"github.com/line/lbm-sdk/simapp"
 	simappparams "github.com/line/lbm-sdk/simapp/params"
 	sdk "github.com/line/lbm-sdk/types"
 	simtypes "github.com/line/lbm-sdk/types/simulation"
+	"github.com/line/lbm-sdk/x/feegrant"
 	"github.com/line/lbm-sdk/x/feegrant/simulation"
 )
 
@@ -75,12 +75,12 @@ func (suite *SimTestSuite) TestWeightedOperations() {
 	}{
 		{
 			simappparams.DefaultWeightGrantAllowance,
-			types.MsgGrantAllowance{}.Route(),
+			feegrant.MsgGrantAllowance{}.Route(),
 			simulation.TypeMsgGrantAllowance,
 		},
 		{
 			simappparams.DefaultWeightRevokeAllowance,
-			types.MsgRevokeAllowance{}.Route(),
+			feegrant.MsgRevokeAllowance{}.Route(),
 			simulation.TypeMsgRevokeAllowance,
 		},
 	}
@@ -112,7 +112,7 @@ func (suite *SimTestSuite) TestSimulateMsgGrantAllowance() {
 	operationMsg, futureOperations, err := op(r, app.BaseApp, ctx, accounts, "")
 	require.NoError(err)
 
-	var msg types.MsgGrantAllowance
+	var msg feegrant.MsgGrantAllowance
 	suite.app.AppCodec().UnmarshalJSON(operationMsg.Msg, &msg)
 
 	require.True(operationMsg.OK)
@@ -142,7 +142,7 @@ func (suite *SimTestSuite) TestSimulateMsgRevokeAllowance() {
 		ctx,
 		granter.Address,
 		grantee.Address,
-		&types.BasicAllowance{
+		&feegrant.BasicAllowance{
 			SpendLimit: feeCoins,
 			Expiration: &oneYear,
 		},
@@ -154,7 +154,7 @@ func (suite *SimTestSuite) TestSimulateMsgRevokeAllowance() {
 	operationMsg, futureOperations, err := op(r, app.BaseApp, ctx, accounts, "")
 	require.NoError(err)
 
-	var msg types.MsgRevokeAllowance
+	var msg feegrant.MsgRevokeAllowance
 	suite.app.AppCodec().UnmarshalJSON(operationMsg.Msg, &msg)
 
 	require.True(operationMsg.OK)