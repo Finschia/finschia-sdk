cosmos/cosmos-sdk

chore: revert TxFactory changes (#10053)
* Revert "refactor: Move some methods inside TX Factory (backport #9421) (#10039)"

This reverts commit 0155244d2a104af8722e04320bf3972eecc7e9b1.

* remove conflict marks from changelog

* update changelog

client/tx/tx.go
@@ -14,6 +14,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/input"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -44,17 +45,49 @@ func GenerateOrBroadcastTxWithFactory(clientCtx client.Context, txf Factory, msg
 	}
 
 	if clientCtx.GenerateOnly {
-		return txf.PrintUnsignedTx(clientCtx, msgs...)
+		return GenerateTx(clientCtx, txf, msgs...)
 	}
 
 	return BroadcastTx(clientCtx, txf, msgs...)
+}
+
+// GenerateTx will generate an unsigned transaction and print it to the writer
+// specified by ctx.Output. If simulation was requested, the gas will be
+// simulated and also printed to the same writer before the transaction is
+// printed.
+func GenerateTx(clientCtx client.Context, txf Factory, msgs ...sdk.Msg) error {
+	if txf.SimulateAndExecute() {
+		if clientCtx.Offline {
+			return errors.New("cannot estimate gas in offline mode")
+		}
+
+		_, adjusted, err := CalculateGas(clientCtx, txf, msgs...)
+		if err != nil {
+			return err
+		}
+
+		txf = txf.WithGas(adjusted)
+		_, _ = fmt.Fprintf(os.Stderr, "%s\n", GasEstimateResponse{GasEstimate: txf.Gas()})
+	}
+
+	tx, err := BuildUnsignedTx(txf, msgs...)
+	if err != nil {
+		return err
+	}
+
+	json, err := clientCtx.TxConfig.TxJSONEncoder()(tx.GetTx())
+	if err != nil {
+		return err
+	}
+
+	return clientCtx.PrintString(fmt.Sprintf("%s\n", json))
 }
 
 // BroadcastTx attempts to generate, sign and broadcast a transaction with the
 // given set of messages. It will also simulate gas requirements if necessary.
 // It will return an error upon failure.
 func BroadcastTx(clientCtx client.Context, txf Factory, msgs ...sdk.Msg) error {
-	txf, err := txf.Prepare(clientCtx)
+	txf, err := prepareFactory(clientCtx, txf)
 	if err != nil {
 		return err
 	}
@@ -73,7 +106,7 @@ func BroadcastTx(clientCtx client.Context, txf Factory, msgs ...sdk.Msg) error {
 		return nil
 	}
 
-	tx, err := txf.BuildUnsignedTx(msgs...)
+	tx, err := BuildUnsignedTx(txf, msgs...)
 	if err != nil {
 		return err
 	}
@@ -164,7 +197,7 @@ func WriteGeneratedTxResponse(
 		}
 	}
 
-	tx, err := txf.BuildUnsignedTx(msgs...)
+	tx, err := BuildUnsignedTx(txf, msgs...)
 	if rest.CheckBadRequestError(w, err) {
 		return
 	}
@@ -184,12 +217,78 @@ func WriteGeneratedTxResponse(
 	_, _ = w.Write(output)
 }
 
+// BuildUnsignedTx builds a transaction to be signed given a set of messages. The
+// transaction is initially created via the provided factory's generator. Once
+// created, the fee, memo, and messages are set.
+func BuildUnsignedTx(txf Factory, msgs ...sdk.Msg) (client.TxBuilder, error) {
+	if txf.chainID == "" {
+		return nil, fmt.Errorf("chain ID required but not specified")
+	}
+
+	fees := txf.fees
+
+	if !txf.gasPrices.IsZero() {
+		if !fees.IsZero() {
+			return nil, errors.New("cannot provide both fees and gas prices")
+		}
+
+		glDec := sdk.NewDec(int64(txf.gas))
+
+		// Derive the fees based on the provided gas prices, where
+		// fee = ceil(gasPrice * gasLimit).
+		fees = make(sdk.Coins, len(txf.gasPrices))
+
+		for i, gp := range txf.gasPrices {
+			fee := gp.Amount.Mul(glDec)
+			fees[i] = sdk.NewCoin(gp.Denom, fee.Ceil().RoundInt())
+		}
+	}
+
+	tx := txf.txConfig.NewTxBuilder()
+
+	if err := tx.SetMsgs(msgs...); err != nil {
+		return nil, err
+	}
+
+	tx.SetMemo(txf.memo)
+	tx.SetFeeAmount(fees)
+	tx.SetGasLimit(txf.gas)
+	tx.SetTimeoutHeight(txf.TimeoutHeight())
+
+	return tx, nil
+}
+
+// BuildSimTx creates an unsigned tx with an empty single signature and returns
+// the encoded transaction or an error if the unsigned transaction cannot be
+// built.
+func BuildSimTx(txf Factory, msgs ...sdk.Msg) ([]byte, error) {
+	txb, err := BuildUnsignedTx(txf, msgs...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create an empty signature literal as the ante handler will populate with a
+	// sentinel pubkey.
+	sig := signing.SignatureV2{
+		PubKey: &secp256k1.PubKey{},
+		Data: &signing.SingleSignatureData{
+			SignMode: txf.signMode,
+		},
+		Sequence: txf.Sequence(),
+	}
+	if err := txb.SetSignatures(sig); err != nil {
+		return nil, err
+	}
+
+	return txf.txConfig.TxEncoder()(txb.GetTx())
+}
+
 // CalculateGas simulates the execution of a transaction and returns the
 // simulation response obtained by the query and the adjusted gas amount.
 func CalculateGas(
 	clientCtx gogogrpc.ClientConn, txf Factory, msgs ...sdk.Msg,
 ) (*tx.SimulateResponse, uint64, error) {
-	txBytes, err := txf.BuildSimTx(msgs...)
+	txBytes, err := BuildSimTx(txf, msgs...)
 	if err != nil {
 		return nil, 0, err
 	}
@@ -203,6 +302,36 @@ func CalculateGas(
 	}
 
 	return simRes, uint64(txf.GasAdjustment() * float64(simRes.GasInfo.GasUsed)), nil
+}
+
+// prepareFactory ensures the account defined by ctx.GetFromAddress() exists and
+// if the account number and/or the account sequence number are zero (not set),
+// they will be queried for and set on the provided Factory. A new Factory with
+// the updated fields will be returned.
+func prepareFactory(clientCtx client.Context, txf Factory) (Factory, error) {
+	from := clientCtx.GetFromAddress()
+
+	if err := txf.accountRetriever.EnsureExists(clientCtx, from); err != nil {
+		return txf, err
+	}
+
+	initNum, initSeq := txf.accountNumber, txf.sequence
+	if initNum == 0 || initSeq == 0 {
+		num, seq, err := txf.accountRetriever.GetAccountNumberSequence(clientCtx, from)
+		if err != nil {
+			return txf, err
+		}
+
+		if initNum == 0 {
+			txf = txf.WithAccountNumber(num)
+		}
+
+		if initSeq == 0 {
+			txf = txf.WithSequence(seq)
+		}
+	}
+
+	return txf, nil
 }
 
 // SignWithPrivKey signs a given tx with the given private key, and returns the

Finschia/finschia-sdk

client/tx/tx.go
@@ -14,6 +14,7 @@ import (
 	"github.com/line/lbm-sdk/client"
 	"github.com/line/lbm-sdk/client/flags"
 	"github.com/line/lbm-sdk/client/input"
+	"github.com/line/lbm-sdk/crypto/keys/secp256k1"
 	cryptotypes "github.com/line/lbm-sdk/crypto/types"
 	sdk "github.com/line/lbm-sdk/types"
 	sdkerrors "github.com/line/lbm-sdk/types/errors"
@@ -44,17 +45,49 @@ func GenerateOrBroadcastTxWithFactory(clientCtx client.Context, txf Factory, msg
 	}
 
 	if clientCtx.GenerateOnly {
-		return txf.PrintUnsignedTx(clientCtx, msgs...)
+		return GenerateTx(clientCtx, txf, msgs...)
 	}
 
 	return BroadcastTx(clientCtx, txf, msgs...)
+}
+
+// GenerateTx will generate an unsigned transaction and print it to the writer
+// specified by ctx.Output. If simulation was requested, the gas will be
+// simulated and also printed to the same writer before the transaction is
+// printed.
+func GenerateTx(clientCtx client.Context, txf Factory, msgs ...sdk.Msg) error {
+	if txf.SimulateAndExecute() {
+		if clientCtx.Offline {
+			return errors.New("cannot estimate gas in offline mode")
+		}
+
+		_, adjusted, err := CalculateGas(clientCtx, txf, msgs...)
+		if err != nil {
+			return err
+		}
+
+		txf = txf.WithGas(adjusted)
+		_, _ = fmt.Fprintf(os.Stderr, "%s\n", GasEstimateResponse{GasEstimate: txf.Gas()})
+	}
+
+	tx, err := BuildUnsignedTx(txf, msgs...)
+	if err != nil {
+		return err
+	}
+
+	json, err := clientCtx.TxConfig.TxJSONEncoder()(tx.GetTx())
+	if err != nil {
+		return err
+	}
+
+	return clientCtx.PrintString(fmt.Sprintf("%s\n", json))
 }
 
 // BroadcastTx attempts to generate, sign and broadcast a transaction with the
 // given set of messages. It will also simulate gas requirements if necessary.
 // It will return an error upon failure.
 func BroadcastTx(clientCtx client.Context, txf Factory, msgs ...sdk.Msg) error {
-	txf, err := txf.Prepare(clientCtx)
+	txf, err := prepareFactory(clientCtx, txf)
 	if err != nil {
 		return err
 	}
@@ -73,7 +106,7 @@ func BroadcastTx(clientCtx client.Context, txf Factory, msgs ...sdk.Msg) error {
 		return nil
 	}
 
-	tx, err := txf.BuildUnsignedTx(msgs...)
+	tx, err := BuildUnsignedTx(txf, msgs...)
 	if err != nil {
 		return err
 	}
@@ -164,7 +197,7 @@ func WriteGeneratedTxResponse(
 		}
 	}
 
-	tx, err := txf.BuildUnsignedTx(msgs...)
+	tx, err := BuildUnsignedTx(txf, msgs...)
 	if rest.CheckBadRequestError(w, err) {
 		return
 	}
@@ -184,12 +217,78 @@ func WriteGeneratedTxResponse(
 	_, _ = w.Write(output)
 }
 
+// BuildUnsignedTx builds a transaction to be signed given a set of messages. The
+// transaction is initially created via the provided factory's generator. Once
+// created, the fee, memo, and messages are set.
+func BuildUnsignedTx(txf Factory, msgs ...sdk.Msg) (client.TxBuilder, error) {
+	if txf.chainID == "" {
+		return nil, fmt.Errorf("chain ID required but not specified")
+	}
+
+	fees := txf.fees
+
+	if !txf.gasPrices.IsZero() {
+		if !fees.IsZero() {
+			return nil, errors.New("cannot provide both fees and gas prices")
+		}
+
+		glDec := sdk.NewDec(int64(txf.gas))
+
+		// Derive the fees based on the provided gas prices, where
+		// fee = ceil(gasPrice * gasLimit).
+		fees = make(sdk.Coins, len(txf.gasPrices))
+
+		for i, gp := range txf.gasPrices {
+			fee := gp.Amount.Mul(glDec)
+			fees[i] = sdk.NewCoin(gp.Denom, fee.Ceil().RoundInt())
+		}
+	}
+
+	tx := txf.txConfig.NewTxBuilder()
+
+	if err := tx.SetMsgs(msgs...); err != nil {
+		return nil, err
+	}
+
+	tx.SetMemo(txf.memo)
+	tx.SetFeeAmount(fees)
+	tx.SetGasLimit(txf.gas)
+	tx.SetTimeoutHeight(txf.TimeoutHeight())
+
+	return tx, nil
+}
+
+// BuildSimTx creates an unsigned tx with an empty single signature and returns
+// the encoded transaction or an error if the unsigned transaction cannot be
+// built.
+func BuildSimTx(txf Factory, msgs ...sdk.Msg) ([]byte, error) {
+	txb, err := BuildUnsignedTx(txf, msgs...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create an empty signature literal as the ante handler will populate with a
+	// sentinel pubkey.
+	sig := signing.SignatureV2{
+		PubKey: &secp256k1.PubKey{},
+		Data: &signing.SingleSignatureData{
+			SignMode: txf.signMode,
+		},
+		Sequence: txf.Sequence(),
+	}
+	if err := txb.SetSignatures(sig); err != nil {
+		return nil, err
+	}
+
+	return txf.txConfig.TxEncoder()(txb.GetTx())
+}
+
 // CalculateGas simulates the execution of a transaction and returns the
 // simulation response obtained by the query and the adjusted gas amount.
 func CalculateGas(
 	clientCtx gogogrpc.ClientConn, txf Factory, msgs ...sdk.Msg,
 ) (*tx.SimulateResponse, uint64, error) {
-	txBytes, err := txf.BuildSimTx(msgs...)
+	txBytes, err := BuildSimTx(txf, msgs...)
 	if err != nil {
 		return nil, 0, err
 	}
@@ -203,6 +302,36 @@ func CalculateGas(
 	}
 
 	return simRes, uint64(txf.GasAdjustment() * float64(simRes.GasInfo.GasUsed)), nil
+}
+
+// prepareFactory ensures the account defined by ctx.GetFromAddress() exists and
+// if the account number and/or the account sequence number are zero (not set),
+// they will be queried for and set on the provided Factory. A new Factory with
+// the updated fields will be returned.
+func prepareFactory(clientCtx client.Context, txf Factory) (Factory, error) {
+	from := clientCtx.GetFromAddress()
+
+	if err := txf.accountRetriever.EnsureExists(clientCtx, from); err != nil {
+		return txf, err
+	}
+
+	initNum, initSeq := txf.accountNumber, txf.sequence
+	if initNum == 0 || initSeq == 0 {
+		num, seq, err := txf.accountRetriever.GetAccountNumberSequence(clientCtx, from)
+		if err != nil {
+			return txf, err
+		}
+
+		if initNum == 0 {
+			txf = txf.WithAccountNumber(num)
+		}
+
+		if initSeq == 0 {
+			txf = txf.WithSequence(seq)
+		}
+	}
+
+	return txf, nil
 }
 
 // SignWithPrivKey signs a given tx with the given private key, and returns the