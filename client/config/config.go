0c7dd80a71a472a5b82be0b17db0182a8bb8feec
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2022-03-24 08:17:24 +0000

    Revert "fix: change the default output format into """
    This reverts commit 77a069619a5c964f6bc717b4a0f6c850be34cbec.

--- client/config/config.go
+++ client/config/config.go
@@ -12,7 +12,7 @@ import (
 const (
 	chainID        = ""
 	keyringBackend = "os"
-	output         = ""
+	output         = "text"
 	node           = "tcp://localhost:26657"
 	broadcastMode  = "sync"
 )
