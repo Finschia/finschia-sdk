fd8ada31f0d41d7f6e5b69a90e1bb8d4df9cfb64
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2022-04-18 07:34:25 +0000

    feat: register msgs

--- x/foundation/codec.go
+++ x/foundation/codec.go
@@ -1,14 +1,29 @@
 package foundation
 
 import (
+	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/codec/types"
+	"github.com/line/lbm-sdk/types/msgservice"
 	govtypes "github.com/line/lbm-sdk/x/gov/types"
 )
 
 func RegisterInterfaces(registry types.InterfaceRegistry) {
-	registry.RegisterImplementations(
-		(*govtypes.Content)(nil),
+	registry.RegisterImplementations((*govtypes.Content)(nil),
 		&UpdateFoundationParamsProposal{},
 		&UpdateValidatorAuthsProposal{},
 	)
+
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgFundTreasury{},
+		&MsgWithdrawFromTreasury{},
+		&MsgUpdateMembers{},
+		&MsgUpdateDecisionPolicy{},
+		&MsgSubmitProposal{},
+		&MsgWithdrawProposal{},
+		&MsgVote{},
+		&MsgExec{},
+		&MsgLeaveFoundation{},
+	)
+
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
