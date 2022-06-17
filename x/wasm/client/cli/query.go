CosmWasm/wasmd

Use LibwasmvmVersion from wasmvm root

x/wasm/client/cli/query.go
@@ -10,13 +10,12 @@ import (
 	"io/ioutil"
 	"strconv"
 
+	wasmvm "github.com/CosmWasm/wasmvm"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/spf13/cobra"
 	flag "github.com/spf13/pflag"
-
-	wasmvmapi "github.com/CosmWasm/wasmvm/api"
 
 	"github.com/CosmWasm/wasmd/x/wasm/types"
 )
@@ -52,7 +51,7 @@ func GetCmdLibVersion() *cobra.Command {
 		Aliases: []string{"lib-version"},
 		Args:    cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			version, err := wasmvmapi.LibwasmvmVersion()
+			version, err := wasmvm.LibwasmvmVersion()
 			if err != nil {
 				return fmt.Errorf("error retrieving libwasmvm version: %w", err)
 			}

Finschia/finschia-sdk

x/wasm/client/cli/query.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"strconv"
 
+	wasmvm "github.com/line/wasmvm"
 	"github.com/spf13/cobra"
 	flag "github.com/spf13/pflag"
 
@@ -17,7 +18,6 @@ import (
 	"github.com/line/lbm-sdk/client/flags"
 	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/x/wasm/types"
-	wasmvmapi "github.com/line/wasmvm/api"
 )
 
 func GetQueryCmd() *cobra.Command {
@@ -51,7 +51,7 @@ func GetCmdLibVersion() *cobra.Command {
 		Aliases: []string{"lib-version"},
 		Args:    cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			version, err := wasmvmapi.LibwasmvmVersion()
+			version, err := wasmvm.LibwasmvmVersion()
 			if err != nil {
 				return fmt.Errorf("error retrieving libwasmvm version: %w", err)
 			}