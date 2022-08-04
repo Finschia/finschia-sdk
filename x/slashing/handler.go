b22a9024c198e2329d9e8b12db51281b870a5ef8
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2022-08-04 07:14:10 +0000

    Revert "Remove legacy handler"
    This reverts commit 9e003b32e6e59f4ae262019dd9ade67202409d05.

---
+++ x/slashing/handler.go
@@ -0,0 +1,26 @@
+package slashing
+
+import (
+	sdk "github.com/line/lbm-sdk/types"
+	sdkerrors "github.com/line/lbm-sdk/types/errors"
+	"github.com/line/lbm-sdk/x/slashing/keeper"
+	"github.com/line/lbm-sdk/x/slashing/types"
+)
+
+// NewHandler creates an sdk.Handler for all the slashing type messages
+func NewHandler(k keeper.Keeper) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+
+		msgServer := keeper.NewMsgServerImpl(k)
+
+		switch msg := msg.(type) {
+		case *types.MsgUnjail:
+			res, err := msgServer.Unjail(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+
+		default:
+			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized %s message type: %T", types.ModuleName, msg)
+		}
+	}
+}
