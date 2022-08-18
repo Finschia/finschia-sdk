3ee14ef667fe020b862ea21eda64dfdfb9b9731f
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2022-08-18 10:57:22 +0000

    fix: add omitted cli commands on x/collection (#644)
    * Add omitted commands
    
    * Update the cli descriptions
    
    * Update CHANGELOG.md
    
    * Fix the description of cli

--- x/collection/client/cli/tx.go
+++ x/collection/client/cli/tx.go
@@ -49,10 +49,15 @@ func NewTxCmd() *cobra.Command {
 		NewTxCmdTransferNFT(),
 		NewTxCmdTransferNFTFrom(),
 		NewTxCmdCreateContract(),
+		NewTxCmdModify(),
 		NewTxCmdIssueFT(),
 		NewTxCmdIssueNFT(),
 		NewTxCmdMintFT(),
 		NewTxCmdMintNFT(),
+		NewTxCmdBurnFT(),
+		NewTxCmdBurnFTFrom(),
+		NewTxCmdBurnNFT(),
+		NewTxCmdBurnNFTFrom(),
 		NewTxCmdAttach(),
 		NewTxCmdDetach(),
 		NewTxCmdAttachFrom(),
@@ -112,7 +117,7 @@ func NewTxCmdTransferFTFrom() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "transfer-ft-from [contract-id] [operator] [from] [to] [amount]",
 		Args:  cobra.ExactArgs(5),
-		Short: "send tokens by operator",
+		Short: "send fungible tokens by operator",
 		Long: strings.TrimSpace(fmt.Sprintf(`
 			$ %s tx %s operator-send [contract-id] [operator] [from] [to] [amount]`, version.AppName, collection.ModuleName),
 		),
@@ -191,7 +196,7 @@ func NewTxCmdTransferNFTFrom() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "transfer-nft-from [contract-id] [operator] [from] [to] [amount]",
 		Args:  cobra.ExactArgs(5),
-		Short: "send tokens by operator",
+		Short: "send non-fungible tokens by operator",
 		Long: strings.TrimSpace(fmt.Sprintf(`
 			$ %s tx %s transfer-nft-from [contract-id] [operator] [from] [to] [amount]`, version.AppName, collection.ModuleName),
 		),
@@ -281,11 +286,11 @@ func NewTxCmdCreateContract() *cobra.Command {
 
 func NewTxCmdIssueFT() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "issue-ft [contract-id] [operator]",
+		Use:   fmt.Sprintf("issue-ft [contract-id] [operator] --%s [%s] --%s [%s]", FlagName, FlagName, FlagTo, FlagTo),
 		Args:  cobra.ExactArgs(2),
 		Short: "create a fungible token class",
 		Long: strings.TrimSpace(fmt.Sprintf(`
-			$ %s tx %s issue-ft [contract-id] [operator]`, version.AppName, collection.ModuleName),
+			$ %s tx %s issue-ft [contract-id] [operator] --%s [%s] --%s [%s]`, version.AppName, collection.ModuleName, FlagName, FlagName, FlagTo, FlagTo),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			operator := args[1]
@@ -513,7 +518,7 @@ func NewTxCmdBurnFT() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "burn-ft [contract-id] [from] [amount]",
 		Args:  cobra.ExactArgs(3),
-		Short: "burn tokens",
+		Short: "burn fungible tokens",
 		Long: strings.TrimSpace(fmt.Sprintf(`
 			$ %s tx %s burn-ft [contract-id] [from] [amount]`, version.AppName, collection.ModuleName),
 		),
@@ -554,7 +559,7 @@ func NewTxCmdBurnFTFrom() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "burn-ft-from [contract-id] [operator] [from] [amount]",
 		Args:  cobra.ExactArgs(4),
-		Short: "burn tokens by a given operator",
+		Short: "burn fungible tokens by a given operator",
 		Long: strings.TrimSpace(fmt.Sprintf(`
 			$ %s tx %s burn-ft-from [contract-id] [operator] [from] [amount]`, version.AppName, collection.ModuleName),
 		),
@@ -596,7 +601,7 @@ func NewTxCmdBurnNFT() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "burn-nft [contract-id] [from] [token-id]",
 		Args:  cobra.ExactArgs(3),
-		Short: "burn tokens",
+		Short: "burn non-fungible tokens",
 		Long: strings.TrimSpace(fmt.Sprintf(`
 			$ %s tx %s burn-nft [contract-id] [from] [token-id]`, version.AppName, collection.ModuleName),
 		),
@@ -631,7 +636,7 @@ func NewTxCmdBurnNFTFrom() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "burn-nft-from [contract-id] [operator] [from] [token-id]",
 		Args:  cobra.ExactArgs(4),
-		Short: "burn tokens by a given operator",
+		Short: "burn non-fungible tokens by a given operator",
 		Long: strings.TrimSpace(fmt.Sprintf(`
 			$ %s tx %s burn-nft-from [contract-id] [operator] [from] [token-id]`, version.AppName, collection.ModuleName),
 		),
@@ -667,7 +672,7 @@ func NewTxCmdModify() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "modify [contract-id] [operator] [token-type] [token-index] [key] [value]",
 		Args:  cobra.ExactArgs(6),
-		Short: "modify",
+		Short: "modify a contract or a token-class or a token",
 		Long: strings.TrimSpace(fmt.Sprintf(`
 			$ %s tx %s modify [contract-id] [operator] [token-type] [token-index] [key] [value]`, version.AppName, collection.ModuleName),
 		),
@@ -708,7 +713,7 @@ func NewTxCmdAttach() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "attach [contract-id] [holder] [subject] [target]",
 		Args:  cobra.ExactArgs(4),
-		Short: "attach a token to another",
+		Short: "attach a (non-fungible) token to another",
 		Long: strings.TrimSpace(fmt.Sprintf(`
 			$ %s tx %s attach [contract-id] [holder] [subject] [target]`, version.AppName, collection.ModuleName),
 		),
@@ -744,7 +749,7 @@ func NewTxCmdDetach() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "detach [contract-id] [holder] [subject]",
 		Args:  cobra.ExactArgs(3),
-		Short: "detach a token from its parent",
+		Short: "detach a (non-fungible) token from its parent",
 		Long: strings.TrimSpace(fmt.Sprintf(`
 			$ %s tx %s detach [contract-id] [holder] [subject]`, version.AppName, collection.ModuleName),
 		),
@@ -779,7 +784,7 @@ func NewTxCmdAttachFrom() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "attach-from [contract-id] [operator] [holder] [subject] [target]",
 		Args:  cobra.ExactArgs(5),
-		Short: "attach a token to another by the operator",
+		Short: "attach a (non-fungible) token to another by the operator",
 		Long: strings.TrimSpace(fmt.Sprintf(`
 			$ %s tx %s attach-from [contract-id] [operator] [holder] [subject] [target]`, version.AppName, collection.ModuleName),
 		),
@@ -816,7 +821,7 @@ func NewTxCmdDetachFrom() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "detach-from [contract-id] [operator] [holder] [subject]",
 		Args:  cobra.ExactArgs(4),
-		Short: "detach a token from its parent by the operator",
+		Short: "detach a (non-fungible) token from its parent by the operator",
 		Long: strings.TrimSpace(fmt.Sprintf(`
 			$ %s tx %s detach-from [contract-id] [operator] [holder] [subject]`, version.AppName, collection.ModuleName),
 		),
