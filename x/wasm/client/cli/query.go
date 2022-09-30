f3c69cf25964e85767510c25184c91e5ab2d2ce2
Daisuke Iuchi <42408108+da1suk8@users.noreply.github.com>
2022-09-30 10:32:38 +0900

    fix: fix to prevent accepting file name

--- x/wasm/client/cli/query.go
+++ x/wasm/client/cli/query.go
@@ -146,11 +146,11 @@ func GetCmdListContractByCode() *cobra.Command {
 // GetCmdQueryCode returns the bytecode for a given contract
 func GetCmdQueryCode() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "code [code_id] [output filename]",
+		Use:     "code [code_id]",
 		Short:   "Downloads wasm bytecode for given code id",
 		Long:    "Downloads wasm bytecode for given code id",
 		Aliases: []string{"source-code", "source"},
-		Args:    cobra.ExactArgs(2),
+		Args:    cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
@@ -176,8 +176,9 @@ func GetCmdQueryCode() *cobra.Command {
 				return fmt.Errorf("contract not found")
 			}
 
-			fmt.Printf("Downloading wasm code to %s\n", args[1])
-			return ioutil.WriteFile(args[1], res.Data, 0600)
+			fileName := "wasm_bytecode-" + strconv.FormatUint(codeID, 10)
+			fmt.Printf("Downloading wasm code to %s\n", fileName)
+			return ioutil.WriteFile(fileName, res.Data, 0600)
 		},
 	}
 	flags.AddQueryFlagsToCmd(cmd)
