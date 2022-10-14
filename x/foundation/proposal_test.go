317c8c24c26599b461dcd5794d5aa5df2e0102b2
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2022-10-14 08:09:09 +0000

    Update unit test on UpdateFoundationParamsProposal

--- x/foundation/proposal_test.go
+++ x/foundation/proposal_test.go
@@ -1,6 +1,7 @@
 package foundation_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,13 +11,29 @@ import (
 )
 
 func TestUpdateFoundationParamsProposal(t *testing.T) {
+	title := "hello"
+	description := "world"
+
 	testCases := map[string]struct {
-		params foundation.Params
-		valid  bool
+		params      foundation.Params
+		valid       bool
+		stringified string
 	}{
 		"valid proposal": {
-			params: foundation.DefaultParams(),
-			valid:  true,
+			params: foundation.Params{
+				FoundationTax: sdk.OneDec(),
+				CensoredMsgTypeUrls: []string{
+					sdk.MsgTypeURL((*foundation.MsgWithdrawFromTreasury)(nil)),
+				},
+			},
+			valid: true,
+			stringified: fmt.Sprintf(`title: %s
+description: %s
+params:
+  foundationtax: "%s"
+  censoredmsgtypeurls:
+  - %s
+`, title, description, sdk.OneDec(), sdk.MsgTypeURL((*foundation.MsgWithdrawFromTreasury)(nil))),
 		},
 		"invalid tax rate": {
 			params: foundation.Params{
@@ -26,18 +43,20 @@ func TestUpdateFoundationParamsProposal(t *testing.T) {
 	}
 
 	for name, tc := range testCases {
-		proposal := foundation.NewUpdateFoundationParamsProposal("", "", tc.params)
+		proposal := foundation.NewUpdateFoundationParamsProposal(title, description, tc.params)
 
-		require.Empty(t, proposal.GetTitle(), name)
-		require.Empty(t, proposal.GetDescription(), name)
+		require.Equal(t, title, proposal.GetTitle(), name)
+		require.Equal(t, description, proposal.GetDescription(), name)
 		require.Equal(t, foundation.RouterKey, proposal.ProposalRoute(), name)
 		require.Equal(t, foundation.ProposalTypeUpdateFoundationParams, proposal.ProposalType(), name)
 
 		err := proposal.ValidateBasic()
-		if tc.valid {
-			require.NoError(t, err, name)
-		} else {
+		if !tc.valid {
 			require.Error(t, err, name)
+			continue
 		}
+		require.NoError(t, err, name)
+
+		require.Equal(t, tc.stringified, proposal.String(), name)
 	}
 }
