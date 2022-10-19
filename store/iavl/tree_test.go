5f1ea9216e17a17802c6f728d409789785bcef81
dudong2 <leesj9476lsj@gmail.com>
2022-10-19 20:00:13 +0900

    test: cover iavl tree test

--- store/iavl/tree_test.go
+++ store/iavl/tree_test.go
@@ -15,8 +15,13 @@ func TestImmutableTreePanics(t *testing.T) {
 	it := &immutableTree{immTree}
 	require.Panics(t, func() { it.Set([]byte{}, []byte{}) })
 	require.Panics(t, func() { it.Remove([]byte{}) })
-	require.Panics(t, func() { it.SaveVersion() })           // nolint:errcheck
-	require.Panics(t, func() { it.DeleteVersion(int64(1)) }) // nolint:errcheck
+	require.Panics(t, func() { it.SaveVersion() })                       // nolint:errcheck
+	require.Panics(t, func() { it.DeleteVersion(int64(1)) })             // nolint:errcheck
+	require.Panics(t, func() { it.LoadVersionForOverwriting(int64(1)) }) // nolint:errcheck
+
+	val, err := it.GetVersioned(nil, 1)
+	require.Error(t, err)
+	require.Nil(t, val)
 
 	val, proof, err := it.GetVersionedWithProof(nil, 1)
 	require.Error(t, err)
