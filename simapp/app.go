cosmos/cosmos-sdk

feat: ADR-038 Part 2: StreamingService interface, file writing implementation, and configuration (#8664) (#13326)
* feat: ADR-038 Part 2: StreamingService interface, file writing implementation, and configuration (#8664)

<!-- < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < ☺
v                               ✰  Thanks for creating a PR! ✰
v    Before smashing the submit button please review the checkboxes.
v    If a checkbox is n/a - please still include it but + a little note why
☺ > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >  -->

<!-- Add a description of the changes that this PR introduces and the files that
are the most critical to review.
-->

Hello 👋 this PR introduces the second stage of changes to support [ADR-038](https://github.com/cosmos/cosmos-sdk/pull/8012) state listening. This is rebased on top of the [first segment](https://github.com/cosmos/cosmos-sdk/pull/8551), which introduces the low level changes to the MultiStore and KVStore interfaces and implementations, the new WriteListener types, and the new listen.KVStore type.

In this segment we introduce the StreamingService interface, an implementation that writes out to files, and it's integration and configuration at the BaseApp level.

The idea was to have the first segment reviewed independently first but if people think it is easier/more time efficient to review both at the same time then we could start here.

Thanks!

This work is towards satisfying [ADR-038](https://github.com/cosmos/cosmos-sdk/blob/master/docs/architecture/adr-038-state-listening.md)

---

Before we can merge this PR, please make sure that all the following items have been
checked off. If any of the checklist items are not applicable, please leave them but
write a little note why.

- [x] Targeted PR against correct branch (see [CONTRIBUTING.md](https://github.com/cosmos/cosmos-sdk/blob/master/CONTRIBUTING.md#pr-targeting))
- [x] Linked to Github issue with di

simapp/app.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/server/config"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
+	"github.com/cosmos/cosmos-sdk/store/streaming"
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -211,6 +212,12 @@ func NewSimApp(
 	// NOTE: The testingkey is just mounted for testing purposes. Actual applications should
 	// not include this key.
 	memKeys := sdk.NewMemoryStoreKeys(capabilitytypes.MemStoreKey, "testingkey")
+
+	// configure state listening capabilities using AppOptions
+	// we are doing nothing with the returned streamingServices and waitGroup in this case
+	if _, _, err := streaming.LoadStreamingServices(bApp, appOpts, appCodec, keys); err != nil {
+		tmos.Exit(err.Error())
+	}
 
 	app := &SimApp{
 		BaseApp:           bApp,

store/cachemulti/store.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/store/cachekv"
 	"github.com/cosmos/cosmos-sdk/store/dbadapter"
+	"github.com/cosmos/cosmos-sdk/store/listenkv"
+	"github.com/cosmos/cosmos-sdk/store/tracekv"
 	"github.com/cosmos/cosmos-sdk/store/types"
 )
 
@@ -49,17 +51,13 @@ func NewFromKVStore(
 	}
 
 	for key, store := range stores {
-		var cacheWrapped types.CacheWrap
 		if cms.TracingEnabled() {
-			cacheWrapped = store.CacheWrapWithTrace(cms.traceWriter, cms.traceContext)
-		} else {
-			cacheWrapped = store.CacheWrap()
+			store = tracekv.NewStore(store.(types.KVStore), cms.traceWriter, cms.traceContext)
 		}
 		if cms.ListeningEnabled(key) {
-			cms.stores[key] = cacheWrapped.CacheWrapWithListeners(key, cms.listeners[key])
-		} else {
-			cms.stores[key] = cacheWrapped
+			store = listenkv.NewStore(store.(types.KVStore), key, listeners[key])
 		}
+		cms.stores[key] = cachekv.NewStore(store.(types.KVStore))
 	}
 
 	return cms

store/streaming/constructor.go
@@ -1,0 +1,137 @@
+package streaming
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/codec"
+	serverTypes "github.com/cosmos/cosmos-sdk/server/types"
+	"github.com/cosmos/cosmos-sdk/store/streaming/file"
+	"github.com/cosmos/cosmos-sdk/store/types"
+
+	"github.com/spf13/cast"
+)
+
+// ServiceConstructor is used to construct a streaming service
+type ServiceConstructor func(opts serverTypes.AppOptions, keys []types.StoreKey, marshaller codec.BinaryCodec) (baseapp.StreamingService, error)
+
+// ServiceType enum for specifying the type of StreamingService
+type ServiceType int
+
+const (
+	Unknown ServiceType = iota
+	File
+	// add more in the future
+)
+
+// ServiceTypeFromString returns the streaming.ServiceType corresponding to the provided name
+func ServiceTypeFromString(name string) ServiceType {
+	switch strings.ToLower(name) {
+	case "file", "f":
+		return File
+	default:
+		return Unknown
+	}
+}
+
+// String returns the string name of a streaming.ServiceType
+func (sst ServiceType) String() string {
+	switch sst {
+	case File:
+		return "file"
+	default:
+		return "unknown"
+	}
+}
+
+// ServiceConstructorLookupTable is a mapping of streaming.ServiceTypes to streaming.ServiceConstructors
+var ServiceConstructorLookupTable = map[ServiceType]ServiceConstructor{
+	File: NewFileStreamingService,
+}
+
+// NewServiceConstructor returns the streaming.ServiceConstructor corresponding to the provided name
+func NewServiceConstructor(name string) (ServiceConstructor, error) {
+	ssType := ServiceTypeFromString(name)
+	if ssType == Unknown {
+		return nil, fmt.Errorf("unrecognized streaming service name %s", name)
+	}
+	if constructor, ok := ServiceConstructorLookupTable[ssType]; ok && constructor != nil {
+		return constructor, nil
+	}
+	return nil, fmt.Errorf("streaming service constructor of type %s not found", ssType.String())
+}
+
+// NewFileStreamingService is the streaming.ServiceConstructor function for creating a FileStreamingService
+func NewFileStreamingService(opts serverTypes.AppOptions, keys []types.StoreKey, marshaller codec.BinaryCodec) (baseapp.StreamingService, error) {
+	filePrefix := cast.ToString(opts.Get("streamers.file.prefix"))
+	fileDir := cast.ToString(opts.Get("streamers.file.write_dir"))
+	return file.NewStreamingService(fileDir, filePrefix, keys, marshaller)
+}
+
+// LoadStreamingServices is a function for loading StreamingServices onto the BaseApp using the provided AppOptions, codec, and keys
+// It returns the WaitGroup and quit channel used to synchronize with the streaming services and any error that occurs during the setup
+func LoadStreamingServices(bApp *baseapp.BaseApp, appOpts serverTypes.AppOptions, appCodec codec.BinaryCodec, keys map[string]*types.KVStoreKey) ([]baseapp.StreamingService, *sync.WaitGroup, error) {
+	// waitgroup and quit channel for optional shutdown coordination of the streaming service(s)
+	wg := new(sync.WaitGroup)
+	// configure state listening capabilities using AppOptions
+	streamers := cast.ToStringSlice(appOpts.Get("store.streamers"))
+	activeStreamers := make([]baseapp.StreamingService, 0, len(streamers))
+	for _, streamerName := range streamers {
+		// get the store keys allowed to be exposed for this streaming service
+		exposeKeyStrs := cast.ToStringSlice(appOpts.Get(fmt.Sprintf("streamers.%s.keys", streamerName)))
+		var exposeStoreKeys []types.StoreKey
+		if exposeAll(exposeKeyStrs) { // if list contains `*`, expose all StoreKeys
+			exposeStoreKeys = make([]types.StoreKey, 0, len(keys))
+			for _, storeKey := range keys {
+				exposeStoreKeys = append(exposeStoreKeys, storeKey)
+			}
+		} else {
+			exposeStoreKeys = make([]types.StoreKey, 0, len(exposeKeyStrs))
+			for _, keyStr := range exposeKeyStrs {
+				if storeKey, ok := keys[keyStr]; ok {
+					exposeStoreKeys = append(exposeStoreKeys, storeKey)
+				}
+			}
+		}
+		if len(exposeStoreKeys) == 0 { // short circuit if we are not exposing anything
+			continue
+		}
+		// get the constructor for this streamer name
+		constructor, err := NewServiceConstructor(streamerName)
+		if err != nil {
+			// close any services we may have already spun up before hitting the error on this one
+			for _, activeStreamer := range activeStreamers {
+				activeStreamer.Close()
+			}
+			return nil, nil, err
+		}
+		// generate the streaming service using the constructor, appOptions, and the StoreKeys we want to expose
+		streamingService, err := constructor(appOpts, exposeStoreKeys, appCodec)
+		if err != nil {
+			// close any services we may have already spun up before hitting the error on this one
+			for _, activeStreamer := range activeStreamers {
+				activeStreamer.Close()
+			}
+			return nil, nil, err
+		}
+		// register the streaming service with the BaseApp
+		bApp.SetStreamingService(streamingService)
+		// kick off the background streaming service loop
+		streamingService.Stream(wg)
+		// add to the list of active streamers
+		activeStreamers = append(activeStreamers, streamingService)
+	}
+	// if there are no active streamers, activeStreamers is empty (len == 0) and the waitGroup is not waiting on anything
+	return activeStreamers, wg, nil
+}
+
+func exposeAll(list []string) bool {
+	for _, ele := range list {
+		if ele == "*" {
+			return true
+		}
+	}
+	return false
+}

store/streaming/constructor_test.go
@@ -1,0 +1,43 @@
+package streaming
+
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	codecTypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/store/streaming/file"
+	"github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/stretchr/testify/require"
+)
+
+type fakeOptions struct{}
+
+func (f *fakeOptions) Get(string) interface{} { return nil }
+
+var (
+	mockOptions       = new(fakeOptions)
+	mockKeys          = []types.StoreKey{sdk.NewKVStoreKey("mockKey1"), sdk.NewKVStoreKey("mockKey2")}
+	interfaceRegistry = codecTypes.NewInterfaceRegistry()
+	testMarshaller    = codec.NewProtoCodec(interfaceRegistry)
+)
+
+func TestStreamingServiceConstructor(t *testing.T) {
+	_, err := NewServiceConstructor("unexpectedName")
+	require.NotNil(t, err)
+
+	constructor, err := NewServiceConstructor("file")
+	require.Nil(t, err)
+	var expectedType ServiceConstructor
+	require.IsType(t, expectedType, constructor)
+
+	serv, err := constructor(mockOptions, mockKeys, testMarshaller)
+	require.Nil(t, err)
+	require.IsType(t, &file.StreamingService{}, serv)
+	listeners := serv.Listeners()
+	for _, key := range mockKeys {
+		_, ok := listeners[key]
+		require.True(t, ok)
+	}
+}

Finschia/finschia-sdk

simapp/app.go
@@ -28,6 +28,7 @@ import (
 	"github.com/line/lbm-sdk/server/config"
 	servertypes "github.com/line/lbm-sdk/server/types"
 	simappparams "github.com/line/lbm-sdk/simapp/params"
+	"github.com/line/lbm-sdk/store/streaming"
 	"github.com/line/lbm-sdk/testutil/testdata"
 	sdk "github.com/line/lbm-sdk/types"
 	"github.com/line/lbm-sdk/types/module"
@@ -314,6 +315,12 @@ func NewSimApp(
 	// NOTE: The testingkey is just mounted for testing purposes. Actual applications should
 	// not include this key.
 	memKeys := sdk.NewMemoryStoreKeys(capabilitytypes.MemStoreKey, "testingkey")
+
+	// configure state listening capabilities using AppOptions
+	// we are doing nothing with the returned streamingServices and waitGroup in this case
+	if _, _, err := streaming.LoadStreamingServices(bApp, appOpts, appCodec, keys); err != nil {
+		ostos.Exit(err.Error())
+	}
 
 	app := &SimApp{
 		BaseApp:           bApp,

store/cachemulti/store.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/line/lbm-sdk/store/cachekv"
 	"github.com/line/lbm-sdk/store/dbadapter"
+	"github.com/line/lbm-sdk/store/listenkv"
+	"github.com/line/lbm-sdk/store/tracekv"
 	"github.com/line/lbm-sdk/store/types"
 )
 
@@ -49,17 +51,13 @@ func NewFromKVStore(
 	}
 
 	for key, store := range stores {
-		var cacheWrapped types.CacheWrap
 		if cms.TracingEnabled() {
-			cacheWrapped = store.CacheWrapWithTrace(cms.traceWriter, cms.traceContext)
-		} else {
-			cacheWrapped = store.CacheWrap()
+			store = tracekv.NewStore(store.(types.KVStore), cms.traceWriter, cms.traceContext)
 		}
 		if cms.ListeningEnabled(key) {
-			cms.stores[key] = cacheWrapped.CacheWrapWithListeners(key, cms.listeners[key])
-		} else {
-			cms.stores[key] = cacheWrapped
+			store = listenkv.NewStore(store.(types.KVStore), key, listeners[key])
 		}
+		cms.stores[key] = cachekv.NewStore(store.(types.KVStore))
 	}
 
 	return cms

store/streaming/constructor.go
@@ -1,0 +1,137 @@
+package streaming
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+
+	"github.com/spf13/cast"
+
+	"github.com/line/lbm-sdk/baseapp"
+	"github.com/line/lbm-sdk/codec"
+	serverTypes "github.com/line/lbm-sdk/server/types"
+	"github.com/line/lbm-sdk/store/streaming/file"
+	"github.com/line/lbm-sdk/store/types"
+)
+
+// ServiceConstructor is used to construct a streaming service
+type ServiceConstructor func(opts serverTypes.AppOptions, keys []types.StoreKey, marshaller codec.BinaryCodec) (baseapp.StreamingService, error)
+
+// ServiceType enum for specifying the type of StreamingService
+type ServiceType int
+
+const (
+	Unknown ServiceType = iota
+	File
+	// add more in the future
+)
+
+// ServiceTypeFromString returns the streaming.ServiceType corresponding to the provided name
+func ServiceTypeFromString(name string) ServiceType {
+	switch strings.ToLower(name) {
+	case "file", "f":
+		return File
+	default:
+		return Unknown
+	}
+}
+
+// String returns the string name of a streaming.ServiceType
+func (sst ServiceType) String() string {
+	switch sst {
+	case File:
+		return "file"
+	default:
+		return "unknown"
+	}
+}
+
+// ServiceConstructorLookupTable is a mapping of streaming.ServiceTypes to streaming.ServiceConstructors
+var ServiceConstructorLookupTable = map[ServiceType]ServiceConstructor{
+	File: NewFileStreamingService,
+}
+
+// NewServiceConstructor returns the streaming.ServiceConstructor corresponding to the provided name
+func NewServiceConstructor(name string) (ServiceConstructor, error) {
+	ssType := ServiceTypeFromString(name)
+	if ssType == Unknown {
+		return nil, fmt.Errorf("unrecognized streaming service name %s", name)
+	}
+	if constructor, ok := ServiceConstructorLookupTable[ssType]; ok && constructor != nil {
+		return constructor, nil
+	}
+	return nil, fmt.Errorf("streaming service constructor of type %s not found", ssType.String())
+}
+
+// NewFileStreamingService is the streaming.ServiceConstructor function for creating a FileStreamingService
+func NewFileStreamingService(opts serverTypes.AppOptions, keys []types.StoreKey, marshaller codec.BinaryCodec) (baseapp.StreamingService, error) {
+	filePrefix := cast.ToString(opts.Get("streamers.file.prefix"))
+	fileDir := cast.ToString(opts.Get("streamers.file.write_dir"))
+	return file.NewStreamingService(fileDir, filePrefix, keys, marshaller)
+}
+
+// LoadStreamingServices is a function for loading StreamingServices onto the BaseApp using the provided AppOptions, codec, and keys
+// It returns the WaitGroup and quit channel used to synchronize with the streaming services and any error that occurs during the setup
+func LoadStreamingServices(bApp *baseapp.BaseApp, appOpts serverTypes.AppOptions, appCodec codec.BinaryCodec, keys map[string]*types.KVStoreKey) ([]baseapp.StreamingService, *sync.WaitGroup, error) {
+	// waitgroup and quit channel for optional shutdown coordination of the streaming service(s)
+	wg := new(sync.WaitGroup)
+	// configure state listening capabilities using AppOptions
+	streamers := cast.ToStringSlice(appOpts.Get("store.streamers"))
+	activeStreamers := make([]baseapp.StreamingService, 0, len(streamers))
+	for _, streamerName := range streamers {
+		// get the store keys allowed to be exposed for this streaming service
+		exposeKeyStrs := cast.ToStringSlice(appOpts.Get(fmt.Sprintf("streamers.%s.keys", streamerName)))
+		var exposeStoreKeys []types.StoreKey
+		if exposeAll(exposeKeyStrs) { // if list contains `*`, expose all StoreKeys
+			exposeStoreKeys = make([]types.StoreKey, 0, len(keys))
+			for _, storeKey := range keys {
+				exposeStoreKeys = append(exposeStoreKeys, storeKey)
+			}
+		} else {
+			exposeStoreKeys = make([]types.StoreKey, 0, len(exposeKeyStrs))
+			for _, keyStr := range exposeKeyStrs {
+				if storeKey, ok := keys[keyStr]; ok {
+					exposeStoreKeys = append(exposeStoreKeys, storeKey)
+				}
+			}
+		}
+		if len(exposeStoreKeys) == 0 { // short circuit if we are not exposing anything
+			continue
+		}
+		// get the constructor for this streamer name
+		constructor, err := NewServiceConstructor(streamerName)
+		if err != nil {
+			// close any services we may have already spun up before hitting the error on this one
+			for _, activeStreamer := range activeStreamers {
+				activeStreamer.Close()
+			}
+			return nil, nil, err
+		}
+		// generate the streaming service using the constructor, appOptions, and the StoreKeys we want to expose
+		streamingService, err := constructor(appOpts, exposeStoreKeys, appCodec)
+		if err != nil {
+			// close any services we may have already spun up before hitting the error on this one
+			for _, activeStreamer := range activeStreamers {
+				activeStreamer.Close()
+			}
+			return nil, nil, err
+		}
+		// register the streaming service with the BaseApp
+		bApp.SetStreamingService(streamingService)
+		// kick off the background streaming service loop
+		streamingService.Stream(wg)
+		// add to the list of active streamers
+		activeStreamers = append(activeStreamers, streamingService)
+	}
+	// if there are no active streamers, activeStreamers is empty (len == 0) and the waitGroup is not waiting on anything
+	return activeStreamers, wg, nil
+}
+
+func exposeAll(list []string) bool {
+	for _, ele := range list {
+		if ele == "*" {
+			return true
+		}
+	}
+	return false
+}

store/streaming/constructor_test.go
@@ -1,0 +1,43 @@
+package streaming
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/line/lbm-sdk/codec"
+	codecTypes "github.com/line/lbm-sdk/codec/types"
+	"github.com/line/lbm-sdk/store/streaming/file"
+	"github.com/line/lbm-sdk/store/types"
+	sdk "github.com/line/lbm-sdk/types"
+)
+
+type fakeOptions struct{}
+
+func (f *fakeOptions) Get(string) interface{} { return nil }
+
+var (
+	mockOptions       = new(fakeOptions)
+	mockKeys          = []types.StoreKey{sdk.NewKVStoreKey("mockKey1"), sdk.NewKVStoreKey("mockKey2")}
+	interfaceRegistry = codecTypes.NewInterfaceRegistry()
+	testMarshaller    = codec.NewProtoCodec(interfaceRegistry)
+)
+
+func TestStreamingServiceConstructor(t *testing.T) {
+	_, err := NewServiceConstructor("unexpectedName")
+	require.NotNil(t, err)
+
+	constructor, err := NewServiceConstructor("file")
+	require.Nil(t, err)
+	var expectedType ServiceConstructor
+	require.IsType(t, expectedType, constructor)
+
+	serv, err := constructor(mockOptions, mockKeys, testMarshaller)
+	require.Nil(t, err)
+	require.IsType(t, &file.StreamingService{}, serv)
+	listeners := serv.Listeners()
+	for _, key := range mockKeys {
+		_, ok := listeners[key]
+		require.True(t, ok)
+	}
+}