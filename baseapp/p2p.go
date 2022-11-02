cosmos/cosmos-sdk

Merge pull request from GHSA-3qp7-gj37-g9rx
Dragonberry 🐉 🍓

Co-authored-by: Facundo Medica <14063057+facundomedica@users.noreply.github.com>
Co-authored-by: Marko <marbar3778@yahoo.com>
Co-authored-by: Ethan Buchman <ethan@coinculture.info>
Co-authored-by: Nicolas Lara <nicolaslara@gmail.com>
Co-authored-by: Roman <34196718+p0mvn@users.noreply.github.com>
Co-authored-by: Roman <roman@osmosis.team>
Co-authored-by: Julien Robert <julien@rbrt.fr>
Co-authored-by: emidev98 <49301655+emidev98@users.noreply.github.com>

Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>
Co-authored-by: Facundo Medica <14063057+facundomedica@users.noreply.github.com>
Co-authored-by: Marko <marbar3778@yahoo.com>
Co-authored-by: Nicolas Lara <nicolaslara@gmail.com>
Co-authored-by: Roman <34196718+p0mvn@users.noreply.github.com>
Co-authored-by: Roman <roman@osmosis.team>
Co-authored-by: Julien Robert <julien@rbrt.fr>
Co-authored-by: emidev98 <49301655+emidev98@users.noreply.github.com>

baseapp/p2p.go
@@ -1,0 +1,66 @@
+package baseapp
+
+// This file exists because Tendermint allows the application to control which peers it connects to.
+// This is for an interesting idea -- allow the application to control the peer layer/ topology!
+// It would be really exciting to mix web of trust and expander-graph style primitives
+// for how information gets disseminated.
+// However the API surface for this to make sense isn't really well exposed / thought through,
+// so this file mostly acts as confusing boilerplate.
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+type peerFilters struct {
+	addrPeerFilter sdk.PeerFilter // filter peers by address and port
+	idPeerFilter   sdk.PeerFilter // filter peers by node ID
+}
+
+// FilterPeerByAddrPort filters peers by address/port.
+func (app *BaseApp) FilterPeerByAddrPort(info string) abci.ResponseQuery {
+	if app.addrPeerFilter != nil {
+		return app.addrPeerFilter(info)
+	}
+
+	return abci.ResponseQuery{}
+}
+
+// FilterPeerByID filters peers by node ID.
+func (app *BaseApp) FilterPeerByID(info string) abci.ResponseQuery {
+	if app.idPeerFilter != nil {
+		return app.idPeerFilter(info)
+	}
+
+	return abci.ResponseQuery{}
+}
+
+func handleQueryP2P(app *BaseApp, path []string) abci.ResponseQuery {
+	// "/p2p" prefix for p2p queries
+	if len(path) < 4 {
+		return sdkerrors.QueryResultWithDebug(
+			sdkerrors.Wrap(
+				sdkerrors.ErrUnknownRequest, "path should be p2p filter <addr|id> <parameter>",
+			), app.trace)
+	}
+
+	var resp abci.ResponseQuery
+
+	cmd, typ, arg := path[1], path[2], path[3]
+	switch cmd {
+	case "filter":
+		switch typ {
+		case "addr":
+			resp = app.FilterPeerByAddrPort(arg)
+
+		case "id":
+			resp = app.FilterPeerByID(arg)
+		}
+
+	default:
+		resp = sdkerrors.QueryResultWithDebug(sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "expected second parameter to be 'filter'"), app.trace)
+	}
+
+	return resp
+}

baseapp/testutil/messages.go
@@ -1,0 +1,60 @@
+package testutil
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
+)
+
+func RegisterInterfaces(registry types.InterfaceRegistry) {
+	registry.RegisterImplementations(
+		(*sdk.Msg)(nil),
+		&MsgCounter{},
+		&MsgCounter2{},
+		&MsgKeyValue{},
+	)
+	msgservice.RegisterMsgServiceDesc(registry, &_Counter_serviceDesc)
+	msgservice.RegisterMsgServiceDesc(registry, &_Counter2_serviceDesc)
+	msgservice.RegisterMsgServiceDesc(registry, &_KeyValue_serviceDesc)
+}
+
+var _ sdk.Msg = &MsgCounter{}
+
+func (msg *MsgCounter) GetSigners() []sdk.AccAddress { return []sdk.AccAddress{} }
+func (msg *MsgCounter) ValidateBasic() error {
+	if msg.Counter >= 0 {
+		return nil
+	}
+	return sdkerrors.Wrap(sdkerrors.ErrInvalidSequence, "counter should be a non-negative integer")
+}
+
+var _ sdk.Msg = &MsgCounter2{}
+
+func (msg *MsgCounter2) GetSigners() []sdk.AccAddress { return []sdk.AccAddress{} }
+func (msg *MsgCounter2) ValidateBasic() error {
+	if msg.Counter >= 0 {
+		return nil
+	}
+	return sdkerrors.Wrap(sdkerrors.ErrInvalidSequence, "counter should be a non-negative integer")
+}
+
+var _ sdk.Msg = &MsgKeyValue{}
+
+func (msg *MsgKeyValue) GetSigners() []sdk.AccAddress {
+	if msg.Signer == "" {
+		return []sdk.AccAddress{}
+	}
+
+	return []sdk.AccAddress{sdk.MustAccAddressFromBech32(msg.Signer)}
+}
+
+func (msg *MsgKeyValue) ValidateBasic() error {
+	if msg.Key == nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "key cannot be nil")
+	}
+	if msg.Value == nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "value cannot be nil")
+	}
+	return nil
+}

store/rootmulti/store.go
@@ -15,6 +15,7 @@ import (
 	"github.com/pkg/errors"
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/log"
+	"github.com/tendermint/tendermint/proto/tendermint/crypto"
 	dbm "github.com/tendermint/tm-db"
 
 	snapshottypes "github.com/cosmos/cosmos-sdk/snapshots/types"
@@ -33,6 +34,8 @@ const (
 	latestVersionKey = "s/latest"
 	pruneHeightsKey  = "s/pruneheights"
 	commitInfoKeyFmt = "s/%d" // s/<version>
+
+	proofsPath = "proofs"
 )
 
 const iavlDisablefastNodeDefault = true
@@ -238,9 +241,7 @@ func (rs *Store) loadVersion(ver int64, upgrades *types.StoreUpgrades) error {
 
 		// If it was deleted, remove all data
 		if upgrades.IsDeleted(key.Name()) {
-			if err := deleteKVStore(store.(types.KVStore)); err != nil {
-				return errors.Wrapf(err, "failed to delete store %s", key.Name())
-			}
+			deleteKVStore(store.(types.KVStore))
 		} else if oldName := upgrades.RenamedFrom(key.Name()); oldName != "" {
 			// handle renames specially
 			// make an unregistered key to satify loadCommitStore params
@@ -255,9 +256,7 @@ func (rs *Store) loadVersion(ver int64, upgrades *types.StoreUpgrades) error {
 			}
 
 			// move all data
-			if err := moveKVStoreData(oldStore.(types.KVStore), store.(types.KVStore)); err != nil {
-				return errors.Wrapf(err, "failed to move store %s -> %s", oldName, key.Name())
-			}
+			moveKVStoreData(oldStore.(types.KVStore), store.(types.KVStore))
 		}
 	}
 
@@ -282,7 +281,7 @@ func (rs *Store) getCommitID(infos map[string]types.StoreInfo, name string) type
 	return info.CommitId
 }
 
-func deleteKVStore(kv types.KVStore) error {
+func deleteKVStore(kv types.KVStore) {
 	// Note that we cannot write while iterating, so load all keys here, delete below
 	var keys [][]byte
 	itr := kv.Iterator(nil, nil)
@@ -295,11 +294,10 @@ func deleteKVStore(kv types.KVStore) error {
 	for _, k := range keys {
 		kv.Delete(k)
 	}
-	return nil
 }
 
 // we simulate move by a copy and delete
-func moveKVStoreData(oldDB types.KVStore, newDB types.KVStore) error {
+func moveKVStoreData(oldDB types.KVStore, newDB types.KVStore) {
 	// we read from one and write to another
 	itr := oldDB.Iterator(nil, nil)
 	for itr.Valid() {
@@ -309,7 +307,7 @@ func moveKVStoreData(oldDB types.KVStore, newDB types.KVStore) error {
 	itr.Close()
 
 	// then delete the old store
-	return deleteKVStore(oldDB)
+	deleteKVStore(oldDB)
 }
 
 // SetInterBlockCache sets the Store's internal inter-block (persistent) cache.
@@ -579,22 +577,28 @@ func (rs *Store) GetStoreByName(name string) types.Store {
 // Query calls substore.Query with the same `req` where `req.Path` is
 // modified to remove the substore prefix.
 // Ie. `req.Path` here is `/<substore>/<path>`, and trimmed to `/<path>` for the substore.
-// TODO: add proof for `multistore -> substore`.
+// Special case: if `req.Path` is `/proofs`, the commit hash is included
+// as response value. In addition, proofs of every store are appended to the response for
+// the requested height
 func (rs *Store) Query(req abci.RequestQuery) abci.ResponseQuery {
 	path := req.Path
-	storeName, subpath, err := parsePath(path)
+	firstPath, subpath, err := parsePath(path)
 	if err != nil {
 		return sdkerrors.QueryResult(err)
 	}
 
-	store := rs.GetStoreByName(storeName)
+	if firstPath == proofsPath {
+		return rs.doProofsQuery(req)
+	}
+
+	store := rs.GetStoreByName(firstPath)
 	if store == nil {
-		return sdkerrors.QueryResult(sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "no such store: %s", storeName))
+		return sdkerrors.QueryResult(sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "no such store: %s", firstPath))
 	}
 
 	queryable, ok := store.(types.Queryable)
 	if !ok {
-		return sdkerrors.QueryResult(sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "store %s (type %T) doesn't support queries", storeName, store))
+		return sdkerrors.QueryResult(sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "store %s (type %T) doesn't support queries", firstPath, store))
 	}
 
 	// trim the path and make the query
@@ -624,13 +628,14 @@ func (rs *Store) Query(req abci.RequestQuery) abci.ResponseQuery {
 	}
 
 	// Restore origin path and append proof op.
-	res.ProofOps.Ops = append(res.ProofOps.Ops, commitInfo.ProofOp(storeName))
+	res.ProofOps.Ops = append(res.ProofOps.Ops, commitInfo.ProofOp(firstPath))
 
 	return res
 }
 
 // SetInitialVersion sets the initial version of the IAVL tree. It is used when
 // starting a new chain at an arbitrary height.
+// NOTE: this never errors. Can we fix the function signature ?
 func (rs *Store) SetInitialVersion(version int64) error {
 	rs.initialVersion = version
 
@@ -978,6 +983,24 @@ func commitStores(version int64, storeMap map[types.StoreKey]types.CommitKVStore
 		Version:    version,
 		StoreInfos: storeInfos,
 	}
+}
+
+func (rs *Store) doProofsQuery(req abci.RequestQuery) abci.ResponseQuery {
+	commitInfo, err := getCommitInfo(rs.db, req.Height)
+	if err != nil {
+		return sdkerrors.QueryResult(err)
+	}
+	res := abci.ResponseQuery{
+		Height:   req.Height,
+		Key:      []byte(proofsPath),
+		Value:    commitInfo.CommitID().Hash,
+		ProofOps: &crypto.ProofOps{Ops: make([]crypto.ProofOp, 0, len(commitInfo.StoreInfos))},
+	}
+
+	for _, storeInfo := range commitInfo.StoreInfos {
+		res.ProofOps.Ops = append(res.ProofOps.Ops, commitInfo.ProofOp(storeInfo.Name))
+	}
+	return res
 }
 
 // Gets commitInfo from disk.

Finschia/finschia-sdk

baseapp/p2p.go
@@ -1,0 +1,66 @@
+package baseapp
+
+// This file exists because Tendermint allows the application to control which peers it connects to.
+// This is for an interesting idea -- allow the application to control the peer layer/ topology!
+// It would be really exciting to mix web of trust and expander-graph style primitives
+// for how information gets disseminated.
+// However the API surface for this to make sense isn't really well exposed / thought through,
+// so this file mostly acts as confusing boilerplate.
+
+import (
+	sdk "github.com/line/lbm-sdk/types"
+	sdkerrors "github.com/line/lbm-sdk/types/errors"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+type peerFilters struct {
+	addrPeerFilter sdk.PeerFilter // filter peers by address and port
+	idPeerFilter   sdk.PeerFilter // filter peers by node ID
+}
+
+// FilterPeerByAddrPort filters peers by address/port.
+func (app *BaseApp) FilterPeerByAddrPort(info string) abci.ResponseQuery {
+	if app.addrPeerFilter != nil {
+		return app.addrPeerFilter(info)
+	}
+
+	return abci.ResponseQuery{}
+}
+
+// FilterPeerByID filters peers by node ID.
+func (app *BaseApp) FilterPeerByID(info string) abci.ResponseQuery {
+	if app.idPeerFilter != nil {
+		return app.idPeerFilter(info)
+	}
+
+	return abci.ResponseQuery{}
+}
+
+func handleQueryP2P(app *BaseApp, path []string) abci.ResponseQuery {
+	// "/p2p" prefix for p2p queries
+	if len(path) < 4 {
+		return sdkerrors.QueryResultWithDebug(
+			sdkerrors.Wrap(
+				sdkerrors.ErrUnknownRequest, "path should be p2p filter <addr|id> <parameter>",
+			), app.trace)
+	}
+
+	var resp abci.ResponseQuery
+
+	cmd, typ, arg := path[1], path[2], path[3]
+	switch cmd {
+	case "filter":
+		switch typ {
+		case "addr":
+			resp = app.FilterPeerByAddrPort(arg)
+
+		case "id":
+			resp = app.FilterPeerByID(arg)
+		}
+
+	default:
+		resp = sdkerrors.QueryResultWithDebug(sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "expected second parameter to be 'filter'"), app.trace)
+	}
+
+	return resp
+}

baseapp/testutil/messages.go
@@ -1,0 +1,60 @@
+package testutil
+
+import (
+	"github.com/line/lbm-sdk/codec/types"
+	sdk "github.com/line/lbm-sdk/types"
+	sdkerrors "github.com/line/lbm-sdk/types/errors"
+	"github.com/line/lbm-sdk/types/msgservice"
+)
+
+func RegisterInterfaces(registry types.InterfaceRegistry) {
+	registry.RegisterImplementations(
+		(*sdk.Msg)(nil),
+		&MsgCounter{},
+		&MsgCounter2{},
+		&MsgKeyValue{},
+	)
+	msgservice.RegisterMsgServiceDesc(registry, &_Counter_serviceDesc)
+	msgservice.RegisterMsgServiceDesc(registry, &_Counter2_serviceDesc)
+	msgservice.RegisterMsgServiceDesc(registry, &_KeyValue_serviceDesc)
+}
+
+var _ sdk.Msg = &MsgCounter{}
+
+func (msg *MsgCounter) GetSigners() []sdk.AccAddress { return []sdk.AccAddress{} }
+func (msg *MsgCounter) ValidateBasic() error {
+	if msg.Counter >= 0 {
+		return nil
+	}
+	return sdkerrors.Wrap(sdkerrors.ErrInvalidSequence, "counter should be a non-negative integer")
+}
+
+var _ sdk.Msg = &MsgCounter2{}
+
+func (msg *MsgCounter2) GetSigners() []sdk.AccAddress { return []sdk.AccAddress{} }
+func (msg *MsgCounter2) ValidateBasic() error {
+	if msg.Counter >= 0 {
+		return nil
+	}
+	return sdkerrors.Wrap(sdkerrors.ErrInvalidSequence, "counter should be a non-negative integer")
+}
+
+var _ sdk.Msg = &MsgKeyValue{}
+
+func (msg *MsgKeyValue) GetSigners() []sdk.AccAddress {
+	if msg.Signer == "" {
+		return []sdk.AccAddress{}
+	}
+
+	return []sdk.AccAddress{sdk.MustAccAddressFromBech32(msg.Signer)}
+}
+
+func (msg *MsgKeyValue) ValidateBasic() error {
+	if msg.Key == nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "key cannot be nil")
+	}
+	if msg.Value == nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "value cannot be nil")
+	}
+	return nil
+}

store/rootmulti/store.go
@@ -17,6 +17,7 @@ import (
 
 	abci "github.com/line/ostracon/abci/types"
 	"github.com/line/ostracon/libs/log"
+	"github.com/line/ostracon/proto/ostracon/crypto"
 
 	snapshottypes "github.com/line/lbm-sdk/snapshots/types"
 	"github.com/line/lbm-sdk/store/cachemulti"
@@ -34,6 +35,8 @@ const (
 	latestVersionKey = "s/latest"
 	pruneHeightsKey  = "s/pruneheights"
 	commitInfoKeyFmt = "s/%d" // s/<version>
+
+	proofsPath = "proofs"
 )
 
 const iavlDisablefastNodeDefault = true
@@ -239,9 +242,7 @@ func (rs *Store) loadVersion(ver int64, upgrades *types.StoreUpgrades) error {
 
 		// If it was deleted, remove all data
 		if upgrades.IsDeleted(key.Name()) {
-			if err := deleteKVStore(store.(types.KVStore)); err != nil {
-				return errors.Wrapf(err, "failed to delete store %s", key.Name())
-			}
+			deleteKVStore(store.(types.KVStore))
 		} else if oldName := upgrades.RenamedFrom(key.Name()); oldName != "" {
 			// handle renames specially
 			// make an unregistered key to satify loadCommitStore params
@@ -256,9 +257,7 @@ func (rs *Store) loadVersion(ver int64, upgrades *types.StoreUpgrades) error {
 			}
 
 			// move all data
-			if err := moveKVStoreData(oldStore.(types.KVStore), store.(types.KVStore)); err != nil {
-				return errors.Wrapf(err, "failed to move store %s -> %s", oldName, key.Name())
-			}
+			moveKVStoreData(oldStore.(types.KVStore), store.(types.KVStore))
 		}
 	}
 
@@ -283,7 +282,7 @@ func (rs *Store) getCommitID(infos map[string]types.StoreInfo, name string) type
 	return info.CommitId
 }
 
-func deleteKVStore(kv types.KVStore) error {
+func deleteKVStore(kv types.KVStore) {
 	// Note that we cannot write while iterating, so load all keys here, delete below
 	var keys [][]byte
 	itr := kv.Iterator(nil, nil)
@@ -296,11 +295,10 @@ func deleteKVStore(kv types.KVStore) error {
 	for _, k := range keys {
 		kv.Delete(k)
 	}
-	return nil
 }
 
 // we simulate move by a copy and delete
-func moveKVStoreData(oldDB types.KVStore, newDB types.KVStore) error {
+func moveKVStoreData(oldDB types.KVStore, newDB types.KVStore) {
 	// we read from one and write to another
 	itr := oldDB.Iterator(nil, nil)
 	for itr.Valid() {
@@ -310,7 +308,7 @@ func moveKVStoreData(oldDB types.KVStore, newDB types.KVStore) error {
 	itr.Close()
 
 	// then delete the old store
-	return deleteKVStore(oldDB)
+	deleteKVStore(oldDB)
 }
 
 // SetInterBlockCache sets the Store's internal inter-block (persistent) cache.
@@ -580,22 +578,28 @@ func (rs *Store) GetStoreByName(name string) types.Store {
 // Query calls substore.Query with the same `req` where `req.Path` is
 // modified to remove the substore prefix.
 // Ie. `req.Path` here is `/<substore>/<path>`, and trimmed to `/<path>` for the substore.
-// TODO: add proof for `multistore -> substore`.
+// Special case: if `req.Path` is `/proofs`, the commit hash is included
+// as response value. In addition, proofs of every store are appended to the response for
+// the requested height
 func (rs *Store) Query(req abci.RequestQuery) abci.ResponseQuery {
 	path := req.Path
-	storeName, subpath, err := parsePath(path)
+	firstPath, subpath, err := parsePath(path)
 	if err != nil {
 		return sdkerrors.QueryResult(err)
 	}
 
-	store := rs.GetStoreByName(storeName)
+	if firstPath == proofsPath {
+		return rs.doProofsQuery(req)
+	}
+
+	store := rs.GetStoreByName(firstPath)
 	if store == nil {
-		return sdkerrors.QueryResult(sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "no such store: %s", storeName))
+		return sdkerrors.QueryResult(sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "no such store: %s", firstPath))
 	}
 
 	queryable, ok := store.(types.Queryable)
 	if !ok {
-		return sdkerrors.QueryResult(sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "store %s (type %T) doesn't support queries", storeName, store))
+		return sdkerrors.QueryResult(sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "store %s (type %T) doesn't support queries", firstPath, store))
 	}
 
 	// trim the path and make the query
@@ -625,13 +629,14 @@ func (rs *Store) Query(req abci.RequestQuery) abci.ResponseQuery {
 	}
 
 	// Restore origin path and append proof op.
-	res.ProofOps.Ops = append(res.ProofOps.Ops, commitInfo.ProofOp(storeName))
+	res.ProofOps.Ops = append(res.ProofOps.Ops, commitInfo.ProofOp(firstPath))
 
 	return res
 }
 
 // SetInitialVersion sets the initial version of the IAVL tree. It is used when
 // starting a new chain at an arbitrary height.
+// NOTE: this never errors. Can we fix the function signature ?
 func (rs *Store) SetInitialVersion(version int64) error {
 	rs.initialVersion = version
 
@@ -979,6 +984,24 @@ func commitStores(version int64, storeMap map[types.StoreKey]types.CommitKVStore
 		Version:    version,
 		StoreInfos: storeInfos,
 	}
+}
+
+func (rs *Store) doProofsQuery(req abci.RequestQuery) abci.ResponseQuery {
+	commitInfo, err := getCommitInfo(rs.db, req.Height)
+	if err != nil {
+		return sdkerrors.QueryResult(err)
+	}
+	res := abci.ResponseQuery{
+		Height:   req.Height,
+		Key:      []byte(proofsPath),
+		Value:    commitInfo.CommitID().Hash,
+		ProofOps: &crypto.ProofOps{Ops: make([]crypto.ProofOp, 0, len(commitInfo.StoreInfos))},
+	}
+
+	for _, storeInfo := range commitInfo.StoreInfos {
+		res.ProofOps.Ops = append(res.ProofOps.Ops, commitInfo.ProofOp(storeInfo.Name))
+	}
+	return res
 }
 
 // Gets commitInfo from disk.