7b6d731d5a2df27d8c6d232a8e0d35666e7fb941
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2022-12-14 05:56:25 +0000

    Revert "Update code number"
    This reverts commit c9e9c66f45be11ae9c8eb3e3efac0a61755d5258.

--- x/collection/errors.go
+++ x/collection/errors.go
@@ -10,63 +10,63 @@ import (
 const collectionCodespace = ModuleName
 
 var (
-	ErrInvalidContractID          = sdkerrors.Register(collectionCodespace, 2, "invalid contract id")
-	ErrContractNotFound           = sdkerrors.Register(collectionCodespace, 3, "contract not found")
-	ErrInvalidClassID             = sdkerrors.Register(collectionCodespace, 4, "invalid class id")
-	ErrWrongClass                 = sdkerrors.Register(collectionCodespace, 5, "class not supports this feature")
-	ErrClassNotFound              = sdkerrors.Register(collectionCodespace, 6, "class not found")
-	ErrInvalidTokenID             = sdkerrors.Register(collectionCodespace, 7, "invalid token id")
-	ErrTokenNotFound              = sdkerrors.Register(collectionCodespace, 8, "token not found")
-	ErrInvalidPermission          = sdkerrors.Register(collectionCodespace, 9, "invalid permission")
-	ErrGrantNotFound              = sdkerrors.Register(collectionCodespace, 10, "grant not found")
-	ErrGrantAlreadyExists         = sdkerrors.Register(collectionCodespace, 11, "grant already exists")
-	ErrOperatorIsHolder           = sdkerrors.Register(collectionCodespace, 12, "operator and holder should be different")
-	ErrAuthorizationNotFound      = sdkerrors.Register(collectionCodespace, 13, "authorization not found")
-	ErrAuthorizationAlreadyExists = sdkerrors.Register(collectionCodespace, 14, "authorization already exists")
-	ErrInvalidCoins               = sdkerrors.Register(collectionCodespace, 15, "invalid coins")
-	ErrInsufficientTokens         = sdkerrors.Register(collectionCodespace, 16, "insufficient tokens")
-	ErrInvalidName                = sdkerrors.Register(collectionCodespace, 17, "invalid name")
-	ErrInvalidBaseImgURI          = sdkerrors.Register(collectionCodespace, 18, "invalid base_img_uri")
-	ErrInvalidMeta                = sdkerrors.Register(collectionCodespace, 19, "invalid meta")
-	ErrInvalidDecimals            = sdkerrors.Register(collectionCodespace, 20, "invalid decimals")
-	ErrNotMintable                = sdkerrors.Register(collectionCodespace, 21, "not mintable")
-	ErrBadUseCase                 = sdkerrors.Register(collectionCodespace, 22, "bad use case")
-	ErrInvalidChanges             = sdkerrors.Register(collectionCodespace, 23, "invalid changes")
-	ErrInvalidModificationTarget  = sdkerrors.Register(collectionCodespace, 24, "invalid modification target")
-	ErrParentNotFound             = sdkerrors.Register(collectionCodespace, 25, "parent not found")
-	ErrInvalidComposition         = sdkerrors.Register(collectionCodespace, 26, "invalid nft composition")
-	ErrInvalidMintNFTParams       = sdkerrors.Register(collectionCodespace, 27, "invalid mint nft params")
-	ErrEmptyTokenIDs              = sdkerrors.Register(collectionCodespace, 28, "empty token ids")
+	ErrTokenNotFound              = sdkerrors.Register(collectionCodespace, 3, "token not found")
+	ErrNotMintable                = sdkerrors.Register(collectionCodespace, 4, "not mintable")
+	ErrInvalidName                = sdkerrors.Register(collectionCodespace, 5, "invalid name")
+	ErrInvalidTokenID             = sdkerrors.Register(collectionCodespace, 6, "invalid token id")
+	ErrInvalidDecimals            = sdkerrors.Register(collectionCodespace, 7, "invalid decimals")
+	ErrBadUseCase                 = sdkerrors.Register(collectionCodespace, 8, "bad use case")
+	ErrInvalidBaseImgURI          = sdkerrors.Register(collectionCodespace, 10, "invalid base_img_uri")
+	ErrInvalidClassID             = sdkerrors.Register(collectionCodespace, 12, "invalid class id")
+	ErrContractNotFound           = sdkerrors.Register(collectionCodespace, 15, "contract not found")
+	ErrClassNotFound              = sdkerrors.Register(collectionCodespace, 17, "class not found")
+	ErrGrantNotFound              = sdkerrors.Register(collectionCodespace, 21, "grant not found")
+	ErrParentNotFound             = sdkerrors.Register(collectionCodespace, 23, "parent not found")
+	ErrWrongClass                 = sdkerrors.Register(collectionCodespace, 26, "class not supports this feature")
+	ErrInvalidComposition         = sdkerrors.Register(collectionCodespace, 27, "invalid nft composition")
+	ErrOperatorIsHolder           = sdkerrors.Register(collectionCodespace, 29, "operator and holder should be different")
+	ErrAuthorizationNotFound      = sdkerrors.Register(collectionCodespace, 30, "authorization not found")
+	ErrAuthorizationAlreadyExists = sdkerrors.Register(collectionCodespace, 31, "authorization already exists")
+	ErrInvalidCoins               = sdkerrors.Register(collectionCodespace, 35, "invalid coins")
+	ErrInvalidChanges             = sdkerrors.Register(collectionCodespace, 36, "invalid changes")
+	ErrInvalidModificationTarget  = sdkerrors.Register(collectionCodespace, 39, "invalid modification target")
+	ErrInsufficientTokens         = sdkerrors.Register(collectionCodespace, 41, "insufficient tokens")
+	ErrInvalidMeta                = sdkerrors.Register(collectionCodespace, 43, "invalid meta")
+	ErrInvalidPermission          = sdkerrors.Register(collectionCodespace, 49, "invalid permission")
+	ErrGrantAlreadyExists         = sdkerrors.Register(collectionCodespace, 50, "grant already exists")
+	ErrInvalidMintNFTParams       = sdkerrors.Register(collectionCodespace, 51, "invalid mint nft params")
+	ErrInvalidContractID          = sdkerrors.Register(collectionCodespace, 52, "invalid contract id")
+	ErrEmptyTokenIDs              = sdkerrors.Register(collectionCodespace, 53, "empty token ids")
 
 	sdkToGRPC = map[*sdkerrors.Error]codes.Code{
 		// this codespace
+		ErrTokenNotFound:              codes.NotFound,
+		ErrNotMintable:                codes.Unimplemented,
+		ErrInvalidName:                codes.InvalidArgument,
+		ErrInvalidTokenID:             codes.InvalidArgument,
+		ErrInvalidDecimals:            codes.InvalidArgument,
+		ErrBadUseCase:                 codes.InvalidArgument,
+		ErrInvalidBaseImgURI:          codes.InvalidArgument,
+		ErrInvalidClassID:             codes.InvalidArgument,
 		ErrContractNotFound:           codes.NotFound,
 		ErrClassNotFound:              codes.NotFound,
-		ErrTokenNotFound:              codes.NotFound,
 		ErrGrantNotFound:              codes.NotFound, // PermissionDenied
-		ErrAuthorizationNotFound:      codes.NotFound, // PermissionDenied
 		ErrParentNotFound:             codes.NotFound,
-		ErrInvalidContractID:          codes.InvalidArgument,
-		ErrInvalidClassID:             codes.InvalidArgument,
-		ErrInvalidTokenID:             codes.InvalidArgument,
-		ErrInvalidPermission:          codes.InvalidArgument,
-		ErrOperatorIsHolder:           codes.InvalidArgument,
-		ErrInvalidCoins:               codes.InvalidArgument,
-		ErrInvalidName:                codes.InvalidArgument,
-		ErrInvalidDecimals:            codes.InvalidArgument,
-		ErrInvalidBaseImgURI:          codes.InvalidArgument,
 		ErrWrongClass:                 codes.InvalidArgument,
-		ErrBadUseCase:                 codes.InvalidArgument,
+		ErrInvalidComposition:         codes.FailedPrecondition,
+		ErrOperatorIsHolder:           codes.InvalidArgument,
+		ErrAuthorizationNotFound:      codes.NotFound, // PermissionDenied
+		ErrAuthorizationAlreadyExists: codes.AlreadyExists,
+		ErrInvalidCoins:               codes.InvalidArgument,
 		ErrInvalidChanges:             codes.InvalidArgument,
 		ErrInvalidModificationTarget:  codes.InvalidArgument,
-		ErrInvalidMeta:                codes.InvalidArgument,
-		ErrInvalidMintNFTParams:       codes.InvalidArgument,
-		ErrEmptyTokenIDs:              codes.InvalidArgument,
 		ErrInsufficientTokens:         codes.FailedPrecondition,
-		ErrInvalidComposition:         codes.FailedPrecondition,
+		ErrInvalidMeta:                codes.InvalidArgument,
+		ErrInvalidPermission:          codes.InvalidArgument,
 		ErrGrantAlreadyExists:         codes.AlreadyExists,
-		ErrAuthorizationAlreadyExists: codes.AlreadyExists,
-		ErrNotMintable:                codes.Unimplemented,
+		ErrInvalidMintNFTParams:       codes.InvalidArgument,
+		ErrInvalidContractID:          codes.InvalidArgument,
+		ErrEmptyTokenIDs:              codes.InvalidArgument,
 
 		// sdk codespace
 		sdkerrors.ErrInvalidAddress: codes.InvalidArgument,
