c012aaefadf02f8122fe97504ff313b7d9630f77
zemyblue <zemyblue@gmail.com>
2023-01-04 15:17:21 +0900

    fix: fix coverage error

--- simapp/simd/cmd/root_test.go
+++ simapp/simd/cmd/root_test.go
@@ -1,12 +1,16 @@
 package cmd
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 
-	"github.com/line/ostracon/libs/log"
 	"github.com/stretchr/testify/require"
 	dbm "github.com/tendermint/tm-db"
+
+	abci "github.com/line/ostracon/abci/types"
+	"github.com/line/ostracon/libs/log"
+	octypes "github.com/line/ostracon/types"
 
 	"github.com/line/lbm-sdk/client/flags"
 	"github.com/line/lbm-sdk/server"
@@ -28,13 +32,60 @@ func TestNewApp(t *testing.T) {
 
 func TestAppExport(t *testing.T) {
 	encodingConfig := simapp.MakeTestEncodingConfig()
+	logger := log.NewOCLogger(log.NewSyncWriter(os.Stdout))
 	a := appCreator{encodingConfig}
 	db := dbm.NewMemDB()
 	tempDir := t.TempDir()
 	ctx := server.NewDefaultContext()
 	ctx.Viper.Set(flags.FlagHome, tempDir)
 	ctx.Viper.Set(server.FlagPruning, types.PruningOptionNothing)
-	exported, err := a.appExport(log.NewOCLogger(log.NewSyncWriter(os.Stdout)), db, nil, 3, false, []string{}, ctx.Viper)
-	require.Error(t, err)
-	require.NotNil(t, exported)
+
+	// create default genesis data and save to store
+	app := simapp.NewSimApp(logger, db, nil, true, map[int64]bool{}, tempDir, 0, encodingConfig, simapp.EmptyAppOptions{})
+	genesisState := simapp.NewDefaultGenesisState(encodingConfig.Marshaler)
+	stateBytes, err := json.MarshalIndent(genesisState, "", "  ")
+	require.NoError(t, err)
+	genDoc := &octypes.GenesisDoc{}
+	genDoc.ChainID = "theChainId"
+	genDoc.Validators = nil
+	genDoc.AppState = stateBytes
+	app.InitChain(
+		abci.RequestInitChain{
+			Validators:      []abci.ValidatorUpdate{},
+			ConsensusParams: simapp.DefaultConsensusParams,
+			AppStateBytes:   genDoc.AppState,
+		},
+	)
+	app.Commit()
+
+	tests := []struct {
+		name      string
+		height    int64
+		expectErr bool
+	}{
+		{
+			"height error",
+			3,
+			true,
+		},
+		{
+			"valid export",
+			-1,
+			false,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			exported, err := a.appExport(logger, db, nil, tc.height, false, []string{}, ctx.Viper)
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+			require.NotNil(t, exported)
+		})
+	}
+
 }
