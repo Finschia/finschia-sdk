6fb4762f81ebdcb1da4a5fde4f06ffe74497a4cb
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2023-01-13 10:37:03 +0900

    Add a warning comment

--- types/errors/link.go
+++ types/errors/link.go
@@ -4,6 +4,7 @@ const (
 	linkCodespace = "link"
 )
 
+// NO additional errors allowed into this codespace
 var (
 	ErrInvalidPermission = Register(linkCodespace, 2, "invalid permission")
 	ErrInvalidDenom      = Register(linkCodespace, 3, "invalid denom")
