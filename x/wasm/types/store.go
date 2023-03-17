5dc8c469a92a3a646b87b7c8e6c76e01015c5c44
Daisuke Iuchi <42408108+da1suk8@users.noreply.github.com>
2023-03-17 16:26:05 +0900

    fix: remove Iterator and reverseIterator
    Because the Iterator was removed in wasmvm

--- x/wasm/types/store.go
+++ x/wasm/types/store.go
@@ -15,16 +15,6 @@ type WasmStore struct {
 	storetypes.KVStore
 }
 
-// Iterator re-define for wasmvm's `Iterator`
-func (s WasmStore) Iterator(start, end []byte) wasmvm.Iterator {
-	return s.KVStore.Iterator(start, end)
-}
-
-// ReverseIterator re-define for wasmvm's `Iterator`
-func (s WasmStore) ReverseIterator(start, end []byte) wasmvm.Iterator {
-	return s.KVStore.ReverseIterator(start, end)
-}
-
 // NewWasmStore creates a instance of WasmStore
 func NewWasmStore(kvStore storetypes.KVStore) WasmStore {
 	return WasmStore{kvStore}
