5fda0260e864d175515cd1002b95c5465883983d
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2023-06-26 22:27:24 +0900

    impl spliting key

--- x/or/da/types/keys.go
+++ x/or/da/types/keys.go
@@ -46,6 +46,10 @@ func GetCCQueueTxKey(rollupName string, i uint64) []byte {
 	return genPrefixIndexKey(GenRollupPrefix(rollupName, CCQueueTxPrefix), i)
 }
 
+func GetCCL2HeightToBatchKey(rollupName string, l2height uint64) []byte {
+	return genPrefixIndexKey(GenRollupPrefix(rollupName, CCL2HeightToBatchPrefix), l2height)
+}
+
 func GetSCCBatchIndexKey(rollupName string, i uint64) []byte {
 	if i < 1 {
 		panic("batch index must be positive")
@@ -66,10 +70,6 @@ func GetSCCStateStoreKey(rollupName string) []byte {
 	return GenRollupPrefix(rollupName, SCCStateStoreKey)
 }
 
-func GetCCL2HeightToBatchKey(rollupName string, l2height uint64) []byte {
-	return genPrefixIndexKey(GenRollupPrefix(rollupName, CCL2HeightToBatchPrefix), l2height)
-}
-
 func genPrefixIndexKey(prefix []byte, i uint64) []byte {
 	l := len(prefix)
 	k := make([]byte, 8+l)
@@ -79,9 +79,27 @@ func genPrefixIndexKey(prefix []byte, i uint64) []byte {
 }
 
 func GenRollupPrefix(rollupName string, prefix byte) []byte {
+	if len(rollupName) == 0 || len(rollupName) > 255 {
+		panic("invalid rollup name")
+	}
+
 	key := make([]byte, 1+1+len(rollupName))
 	key[0] = prefix
 	key[1] = byte(len(rollupName))
 	copy(key[2:], []byte(rollupName))
 	return key
+}
+
+func SplitIndexKey(key []byte) (prefix []byte, rollupName string, index uint64) {
+	if len(key) < 10 {
+		panic("invalid index key")
+	}
+
+	prefix = key[:1]
+	begin := 2
+	end := begin + int(key[begin-1])
+	rollupName = string(key[begin:end])
+	index = binary.BigEndian.Uint64(key[end:])
+
+	return
 }
