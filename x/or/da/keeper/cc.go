a614eb4db8088849bdc87b5e8387252bb78529b5
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2023-08-30 09:34:42 +0900

    Update rollup module expected keeper

--- x/or/da/keeper/cc.go
+++ x/or/da/keeper/cc.go
@@ -160,18 +160,18 @@ func (k Keeper) SaveCCBatch(ctx sdktypes.Context, rollupName string, batch *type
 }
 
 func (k Keeper) UpdateQueueTxsStatus(ctx sdktypes.Context) error {
-	rollupList := k.rollupKeeper.GetRegisteredRollups(ctx)
+	rollupList := k.rollupKeeper.GetAllRollup(ctx)
 	if rollupList == nil {
 		return nil
 	}
 
-	for _, name := range rollupList {
-		state, err := k.GetQueueTxState(ctx, name)
+	for _, r := range rollupList {
+		state, err := k.GetQueueTxState(ctx, r.RollupName)
 		if err != nil {
 			continue
 		}
 
-		if err := k.processQueueTxs(ctx, state, name); err != nil {
+		if err := k.processQueueTxs(ctx, state, r.RollupName); err != nil {
 			return err
 		}
 	}
--- x/or/da/types/expected_keepers.go
+++ x/or/da/types/expected_keepers.go
@@ -3,6 +3,7 @@ package types
 import (
 	sdk "github.com/Finschia/finschia-sdk/types"
 	authtypes "github.com/Finschia/finschia-sdk/x/auth/types"
+	rutypes "github.com/Finschia/finschia-sdk/x/or/rollup/types"
 )
 
 type AccountKeeper interface {
@@ -10,14 +11,7 @@ type AccountKeeper interface {
 }
 
 type RollupKeeper interface {
-	GetRollupInfo(ctx sdk.Context, name string) (RollupInfo, error)
-	GetRegisteredRollups(ctx sdk.Context) []string
-}
-
-type RollupInfo struct {
-	ID string
-
-	// The ratio between the cost of gas on L1 and L2.
-	// This is a positive integer.
-	L1ToL2GasRatio uint64
+	GetRollup(ctx sdk.Context, rollupName string) (val rutypes.Rollup, found bool)
+	GetAllRollup(ctx sdk.Context) (list []rutypes.Rollup)
+	GetSequencersByRollupName(ctx sdk.Context, rollupName string) (val rutypes.SequencersByRollup, found bool)
 }
