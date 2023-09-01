621073d871f9acacec1a8122cd871516cd42f90c
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2023-09-01 17:21:13 +0900

    Update consensus version

--- x/collection/module/module.go
+++ x/collection/module/module.go
@@ -129,4 +129,4 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements AppModule/ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 1 }
+func (AppModule) ConsensusVersion() uint64 { return 2 }
