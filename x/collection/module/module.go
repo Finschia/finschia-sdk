fe6883fc79d723f13bc1d54de938edd7c5ae08a3
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2023-09-01 18:13:59 +0900

    Revert "Add total-supply invariants"
    This reverts commit 3a3e42368a3d74c756b6a368e27b727f52ee6380.

--- x/collection/module/module.go
+++ x/collection/module/module.go
@@ -89,9 +89,7 @@ func NewAppModule(cdc codec.Codec, keeper keeper.Keeper) AppModule {
 }
 
 // RegisterInvariants does nothing, there are no invariants to enforce
-func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {
-	keeper.RegisterInvariants(ir, am.keeper)
-}
+func (AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
 
 // Route returns the message routing key for the collection module.
 func (am AppModule) Route() sdk.Route { return sdk.Route{} }
