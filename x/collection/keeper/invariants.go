ee0af49f30696f530d253b1b859eda06882c8f0d
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2023-09-01 18:13:53 +0900

    Revert "Do not verify nfts"
    This reverts commit e85305f69508f21753368d57cbd61fa9b4ab745c.

--- x/collection/keeper/invariants.go
+++ x/collection/keeper/invariants.go
@@ -8,41 +8,32 @@ import (
 )
 
 const (
-	totalFTSupplyInvariant = "total-ft-supply"
+	totalSupplyInvariant = "total-supply"
 )
 
 func RegisterInvariants(ir sdk.InvariantRegistry, k Keeper) {
 	for name, invariant := range map[string]func(k Keeper) sdk.Invariant{
-		totalFTSupplyInvariant: TotalFTSupplyInvariant,
+		totalSupplyInvariant: TotalSupplyInvariant,
 	} {
 		ir.RegisterRoute(collection.ModuleName, name, invariant(k))
 	}
 }
 
-func TotalFTSupplyInvariant(k Keeper) sdk.Invariant {
+func TotalSupplyInvariant(k Keeper) sdk.Invariant {
 	return func(ctx sdk.Context) (string, bool) {
 		// cache, we don't want to write changes
 		ctx, _ = ctx.CacheContext()
 
-		invalidFTClassIDs := map[string][]string{}
+		invalidClassIDs := map[string][]string{}
 		k.iterateContracts(ctx, func(contract collection.Contract) (stop bool) {
 			supplies := map[string]sdk.Int{}
 			k.iterateContractSupplies(ctx, contract.Id, func(classID string, amount sdk.Int) (stop bool) {
-				if err := collection.ValidateLegacyFTClassID(classID); err != nil {
-					return false
-				}
-
 				supplies[classID] = amount
-
 				return false
 			})
 
 			k.iterateContractBalances(ctx, contract.Id, func(address sdk.AccAddress, balance collection.Coin) (stop bool) {
 				classID := collection.SplitTokenID(balance.TokenId)
-				if err := collection.ValidateLegacyFTClassID(classID); err != nil {
-					return false
-				}
-
 				amount, ok := supplies[classID]
 				if !ok {
 					amount = sdk.ZeroInt()
@@ -52,34 +43,34 @@ func TotalFTSupplyInvariant(k Keeper) sdk.Invariant {
 				return false
 			})
 
-			invalidFTClassIDsCandidate := []string{}
+			invalidClassIDsCandidate := []string{}
 			for classID, supply := range supplies {
 				if !supply.IsZero() {
-					invalidFTClassIDsCandidate = append(invalidFTClassIDsCandidate, classID)
+					invalidClassIDsCandidate = append(invalidClassIDsCandidate, classID)
 				}
 			}
 
-			if len(invalidFTClassIDsCandidate) != 0 {
-				invalidFTClassIDs[contract.Id] = invalidFTClassIDsCandidate
+			if len(invalidClassIDsCandidate) != 0 {
+				invalidClassIDs[contract.Id] = invalidClassIDsCandidate
 			}
 
 			return false
 		})
 
-		broken := len(invalidFTClassIDs) != 0
+		broken := len(invalidClassIDs) != 0
 		msg := "no violation found"
 		if broken {
 			concatenated := []string{}
 			delimiter := ":"
-			for contractID, classIDs := range invalidFTClassIDs {
+			for contractID, classIDs := range invalidClassIDs {
 				for _, classID := range classIDs {
 					concatenated = append(concatenated, contractID+delimiter+classID)
 				}
 			}
 
-			msg = "violation found on following ft classIDs: " + strings.Join(concatenated, ", ")
+			msg = "violation found on following classIDs: " + strings.Join(concatenated, ", ")
 		}
 
-		return sdk.FormatInvariant(collection.ModuleName, totalFTSupplyInvariant, msg), broken
+		return sdk.FormatInvariant(collection.ModuleName, totalSupplyInvariant, msg), broken
 	}
 }
