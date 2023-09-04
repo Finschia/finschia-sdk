a98b521df07971907a59c80bc59e6845e7d2a107
shiki-tak <shiki.tak@gmail.com>
2023-09-04 13:59:39 +0900

    chore: change L1 binary name

--- demo/prepare_rollup.sh
+++ demo/prepare_rollup.sh
@@ -9,36 +9,36 @@ WITHDRAW="10"
 BASE_DIR=~/.simapp
 
 # Get address which want to register for sequencer
-SEQUENCER=$(./L1/simd keys list --keyring-backend=test --home $BASE_DIR --output json | jq -r '.[1]'.name)
-SEQUENCERADDRESS=$(./L1/simd keys list --keyring-backend=test --home $BASE_DIR --output json | jq -r '.[1]'.address)
-SEQUENCERPUBKEY=$(./L1/simd keys list --keyring-backend=test --home $BASE_DIR --output json | jq -r '.[1]'.pubkey)
+SEQUENCER=$(./L1/simd-darwin-arm64 keys list --keyring-backend=test --home $BASE_DIR --output json | jq -r '.[1]'.name)
+SEQUENCERADDRESS=$(./L1/simd-darwin-arm64 keys list --keyring-backend=test --home $BASE_DIR --output json | jq -r '.[1]'.address)
+SEQUENCERPUBKEY=$(./L1/simd-darwin-arm64 keys list --keyring-backend=test --home $BASE_DIR --output json | jq -r '.[1]'.pubkey)
 echo "Sequencer Info"
 echo $SEQUENCER
 echo $SEQUENCERADDRESS
 echo $SEQUENCERPUBKEY
 
 echo "# Check init balance"
-INITBALANCE=$(./L1/simd query bank balances $SEQUENCERADDRESS --node "tcp://localhost:26659" --home $BASE_DIR --output json | jq -r '.balances[0].amount')
+INITBALANCE=$(./L1/simd-darwin-arm64 query bank balances $SEQUENCERADDRESS --node "tcp://localhost:26659" --home $BASE_DIR --output json | jq -r '.balances[0].amount')
 echo $INITBALANCE
 
 echo "# Create rollup"
-./L1/simd tx rollup create-rollup $ROLLUPNAME 5 --from $SEQUENCER --keyring-backend=test --node "tcp://localhost:26659" --home $BASE_DIR --chain-id sim -y
+./L1/simd-darwin-arm64 tx rollup create-rollup $ROLLUPNAME 5 --from $SEQUENCER --keyring-backend=test --node "tcp://localhost:26659" --home $BASE_DIR --chain-id sim -y
 
 sleep 5
 
 echo "# Check created rollup"
-./L1/simd query rollup show-rollup $ROLLUPNAME --node "tcp://localhost:26659"
+./L1/simd-darwin-arm64 query rollup show-rollup $ROLLUPNAME --node "tcp://localhost:26659"
 
 echo "# Check rollup list"
-./L1/simd query rollup list --node "tcp://localhost:26659"
+./L1/simd-darwin-arm64 query rollup list --node "tcp://localhost:26659"
 
 echo "# Register sequencer"
-./L1/simd tx rollup register-sequencer "test-rollup" ${SEQUENCERPUBKEY} $DEPOSIT$DENOM --from $SEQUENCER --keyring-backend test --node "tcp://localhost:26659" --home $BASE_DIR --chain-id sim -y
+./L1/simd-darwin-arm64 tx rollup register-sequencer "test-rollup" ${SEQUENCERPUBKEY} $DEPOSIT$DENOM --from $SEQUENCER --keyring-backend test --node "tcp://localhost:26659" --home $BASE_DIR --chain-id sim -y
 
 sleep 5
 
 echo "# Check balance after registered sequencer"
-BALANCEAFTERREGISTER=$(./L1/simd query bank balances $SEQUENCERADDRESS --node "tcp://localhost:26659" --home $BASE_DIR --output json | jq -r '.balances[0].amount')
+BALANCEAFTERREGISTER=$(./L1/simd-darwin-arm64 query bank balances $SEQUENCERADDRESS --node "tcp://localhost:26659" --home $BASE_DIR --output json | jq -r '.balances[0].amount')
 echo $BALANCEAFTERREGISTER
 
 if [ $((${INITBALANCE}-${DEPOSIT})) -ne ${BALANCEAFTERREGISTER} ]; then
@@ -49,18 +49,18 @@ else
 fi
 
 echo "# Check sequencer by rollup name"
-./L1/simd query rollup show-sequencers-by-rollup $ROLLUPNAME --node "tcp://localhost:26659" --output json
+./L1/simd-darwin-arm64 query rollup show-sequencers-by-rollup $ROLLUPNAME --node "tcp://localhost:26659" --output json
 
 echo "# Check sequencer"
-./L1/simd query rollup show-sequencer $SEQUENCERADDRESS --node "tcp://localhost:26659" --home $BASE_DIR --output json
+./L1/simd-darwin-arm64 query rollup show-sequencer $SEQUENCERADDRESS --node "tcp://localhost:26659" --home $BASE_DIR --output json
 
 echo "# Withdraw deposit"
-./L1/simd tx rollup withdraw-by-sequencer $ROLLUPNAME $WITHDRAW$DENOM --from $SEQUENCER --keyring-backend test --node "tcp://localhost:26659" --home $BASE_DIR --chain-id sim -y
+./L1/simd-darwin-arm64 tx rollup withdraw-by-sequencer $ROLLUPNAME $WITHDRAW$DENOM --from $SEQUENCER --keyring-backend test --node "tcp://localhost:26659" --home $BASE_DIR --chain-id sim -y
 
 sleep 5
 
 echo "# Check balance after withdraw"
-BALANCEAFTERWITHDRAW=$(./L1/simd query bank balances $SEQUENCERADDRESS --node "tcp://localhost:26659" --home $BASE_DIR --output json | jq -r '.balances[0].amount')
+BALANCEAFTERWITHDRAW=$(./L1/simd-darwin-arm64 query bank balances $SEQUENCERADDRESS --node "tcp://localhost:26659" --home $BASE_DIR --output json | jq -r '.balances[0].amount')
 
 if [ $((${INITBALANCE}-${DEPOSIT}+${WITHDRAW})) -ne ${BALANCEAFTERWITHDRAW} ]; then
     echo "The balance after withdraw does not match."
@@ -70,12 +70,12 @@ else
 fi
 
 echo "# Deposit by sequencer again"
-./L1/simd tx rollup deposit-by-sequencer "test-rollup" $DEPOSIT$DENOM --from $SEQUENCER --keyring-backend=test --node "tcp://localhost:26659" --home $BASE_DIR --chain-id sim -y
+./L1/simd-darwin-arm64 tx rollup deposit-by-sequencer "test-rollup" $DEPOSIT$DENOM --from $SEQUENCER --keyring-backend=test --node "tcp://localhost:26659" --home $BASE_DIR --chain-id sim -y
 
 sleep 5
 
 echo "# Check balance after deposit again"
-BALANCEAFTERDEPOSITAGAIN=$(./L1/simd query bank balances $SEQUENCERADDRESS --node "tcp://localhost:26659" --home $BASE_DIR --output json | jq -r '.balances[0].amount')
+BALANCEAFTERDEPOSITAGAIN=$(./L1/simd-darwin-arm64 query bank balances $SEQUENCERADDRESS --node "tcp://localhost:26659" --home $BASE_DIR --output json | jq -r '.balances[0].amount')
 
 if [ $((${INITBALANCE}-2*${DEPOSIT}+${WITHDRAW})) -ne ${BALANCEAFTERDEPOSITAGAIN} ]; then
     echo "The balance after deposit again does not match."
