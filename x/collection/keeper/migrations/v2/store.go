4f7b891da437ac876f92482b84935f3c80c7a339
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2023-09-04 11:05:04 +0000

    fix: support x/collection migration on old chains (#1106)
    * Support old chains
    
    * Update CHANGELOG.md
    
    * Add comment

--- x/collection/keeper/migrations/v2/store.go
+++ x/collection/keeper/migrations/v2/store.go
@@ -91,7 +91,9 @@ func updateContractFTStatistics(store storetypes.KVStore, contractID string, sup
 		return err
 	}
 
-	for intClassID := uint64(1); intClassID < nextClassIDs.Fungible.Uint64(); intClassID++ {
+	// In the old chains, classID of fungible tokens starts from zero
+	// In the new chains, it starts from one, but it does not hurts because amount of zero is not set to the store.
+	for intClassID := uint64(0); intClassID < nextClassIDs.Fungible.Uint64(); intClassID++ {
 		classID := fmt.Sprintf("%08x", intClassID)
 
 		// update supply
@@ -104,6 +106,7 @@ func updateContractFTStatistics(store storetypes.KVStore, contractID string, sup
 			}
 			store.Set(supplyKey, bz)
 		} else {
+			supply = sdk.ZeroInt()
 			store.Delete(supplyKey)
 		}
 
