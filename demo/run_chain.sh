dfab7d1022e7b54974e047279e92802ae7eb0513
murogari <55307968+Mdaiki0730@users.noreply.github.com>
2023-11-06 11:36:09 +0900

    chore: Add shell to run l2node (#1156)
    * Add shell to run l2node
    
    * Change the notation of variables
    
    * Outputs the standard output of the setup command and exits if an error occurs
    
    * Include l2fullnode startup in test sequencer

--- demo/run_chain.sh
+++ demo/run_chain.sh
@@ -5,15 +5,15 @@ TEST_MNEMONIC="mind flame tobacco sense move hammer drift crime ring globe art g
 BASE_DIR=~/.simapp
 CHAIN_ID=sim
 
-./L1/simd-darwin-arm64 init rollupdemo --home $BASE_DIR --chain-id $CHAIN_ID > /dev/null 2>&1
-./L1/simd-darwin-arm64 keys add validator --keyring-backend=test --home $BASE_DIR --recover --account=0 <<< ${TEST_MNEMONIC} > /dev/null 2>&1
-./L1/simd-darwin-arm64 keys add sequencer --keyring-backend=test --home $BASE_DIR --recover --account=1 <<< ${TEST_MNEMONIC} > /dev/null 2>&1
-./L1/simd-darwin-arm64 keys add challenger --keyring-backend=test --home $BASE_DIR --recover --account=2 <<< ${TEST_MNEMONIC} > /dev/null 2>&1
-./L1/simd-darwin-arm64 add-genesis-account $(./L1/simd-darwin-arm64 --home $BASE_DIR keys show validator -a --keyring-backend=test) 100000000000stake,100000000000tcony --home $BASE_DIR > /dev/null 2>&1
-./L1/simd-darwin-arm64 add-genesis-account $(./L1/simd-darwin-arm64 --home $BASE_DIR keys show sequencer -a --keyring-backend=test) 100000000000stake,100000000000tcony --home $BASE_DIR > /dev/null 2>&1
-./L1/simd-darwin-arm64 add-genesis-account $(./L1/simd-darwin-arm64 --home $BASE_DIR keys show challenger -a --keyring-backend=test) 100000000000stake,100000000000tcony --home $BASE_DIR > /dev/null 2>&1
-./L1/simd-darwin-arm64 gentx validator 10000000000stake --keyring-backend=test --home $BASE_DIR --chain-id=$CHAIN_ID > /dev/null 2>&1
-./L1/simd-darwin-arm64 collect-gentxs --home $BASE_DIR > /dev/null 2>&1
+./L1/simd-darwin-arm64 init rollupdemo --home $BASE_DIR --chain-id $CHAIN_ID || exit 1
+./L1/simd-darwin-arm64 keys add validator --keyring-backend=test --home $BASE_DIR --recover --account=0 <<< ${TEST_MNEMONIC} || exit 1
+./L1/simd-darwin-arm64 keys add sequencer --keyring-backend=test --home $BASE_DIR --recover --account=1 <<< ${TEST_MNEMONIC} || exit 1
+./L1/simd-darwin-arm64 keys add challenger --keyring-backend=test --home $BASE_DIR --recover --account=2 <<< ${TEST_MNEMONIC} || exit 1
+./L1/simd-darwin-arm64 add-genesis-account $(./L1/simd-darwin-arm64 --home $BASE_DIR keys show validator -a --keyring-backend=test) 100000000000stake,100000000000tcony --home $BASE_DIR || exit 1
+./L1/simd-darwin-arm64 add-genesis-account $(./L1/simd-darwin-arm64 --home $BASE_DIR keys show sequencer -a --keyring-backend=test) 100000000000stake,100000000000tcony --home $BASE_DIR || exit 1
+./L1/simd-darwin-arm64 add-genesis-account $(./L1/simd-darwin-arm64 --home $BASE_DIR keys show challenger -a --keyring-backend=test) 100000000000stake,100000000000tcony --home $BASE_DIR || exit 1
+./L1/simd-darwin-arm64 gentx validator 10000000000stake --keyring-backend=test --home $BASE_DIR --chain-id=$CHAIN_ID || exit 1
+./L1/simd-darwin-arm64 collect-gentxs --home $BASE_DIR || exit 1
 
 # Run chain
 ./L1/simd-darwin-arm64 start --rpc.laddr "tcp://127.0.0.1:26659" --home $BASE_DIR > $BASE_DIR/$CHAIN_ID.log 2>&1 &
--- demo/test_sequencer.sh
+++ demo/test_sequencer.sh
@@ -11,16 +11,52 @@ TEST_SEQUENCER_ADDRESS=link1twsfmuj28ndph54k4nw8crwu8h9c8mh3rtx705
 SEQUENCER_DIR=simapp0
 TEST_MNEMONIC="mind flame tobacco sense move hammer drift crime ring globe art gaze cinnamon helmet cruise special produce notable negative wait path scrap recall have"
 
+# Reset
+rm -rf $L2_KEYRING_DIR
+pkill $L2BINARYNAME
+rm $GOPATH/bin/$L2BINARYNAME
+
+# Run L1 chain
+./run_chain.sh
+
+sleep 10
+
+# Prepare rollup info
+./prepare_rollup.sh
+
+cd ..
+
+# Build & rename
+make build && cp -r build/simd $GOPATH/bin/$L2BINARYNAME
+$L2BINARYNAME version
+
+# Init sequencer
+
+$L2BINARYNAME init rollupdemo --home $L2_KEYRING_DIR/$SEQUENCER_DIR --chain-id $L2_CHAIN_ID || exit 1
+$L2BINARYNAME keys add validator --keyring-backend=test --home $L2_KEYRING_DIR/$SEQUENCER_DIR --recover --account=0 <<< ${TEST_MNEMONIC} || exit 1
+$L2BINARYNAME add-genesis-account $($L2BINARYNAME --home $L2_KEYRING_DIR/$SEQUENCER_DIR keys show validator -a --keyring-backend=test) 100000000000stake,100000000000tcony --home $L2_KEYRING_DIR/$SEQUENCER_DIR || exit 1
+$L2BINARYNAME gentx validator 10000000000stake --keyring-backend=test --home $L2_KEYRING_DIR/$SEQUENCER_DIR --chain-id=$L2_CHAIN_ID || exit 1
+$L2BINARYNAME collect-gentxs --home $L2_KEYRING_DIR/$SEQUENCER_DIR || exit 1
+
+# Run L2 sequencer
+$L2BINARYNAME start --home $L2_KEYRING_DIR/$SEQUENCER_DIR --p2p.laddr "tcp://0.0.0.0:26556" --grpc.address "0.0.0.0:9190" --grpc-web.address "0.0.0.0:9191" --rollkit.sequencer "true" --rollkit.da_layer finschia --rollkit.da_config='{"rpc_uri":"'$RPC_URI'","chain_id":"'$L1_CHAIN_ID'","keyring_dir":"'$L1_KEYRING_DIR'","from":"'$TEST_SEQUENCER_ADDRESS'", "rollup_name":"'$ROLLUP_NAME'"}' --rollkit.namespace_id $NAMESPACE_ID  --rollkit.da_start_height $DA_BLOCK_HEIGH > $L2_KEYRING_DIR/$L2_CHAIN_ID.log 2>&1 &
+sleep 10
+
+# Run L2 fullnode
+cd ./demo
+TEST_SEQUENCER_P2P_ID=$(cat $L2_KEYRING_DIR/$L2_CHAIN_ID.log | sed -n 's/.*127.0.0.1\/tcp\/26556\/p2p\/\([^ ]*\).*/\1/p')
+./run_l2node.sh $TEST_SEQUENCER_P2P_ID
+
 # Send test
-${L2BINARYNAME} keys add alice --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test
+$L2BINARYNAME keys add alice --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test
 sleep 1
-ALICE_ADDR=$(${L2BINARYNAME} keys show alice -a --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test)
+ALICE_ADDR=$($L2BINARYNAME keys show alice -a --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test)
 echo $ALICE_ADDR
-${L2BINARYNAME} tx bank send validator $ALICE_ADDR 100stake --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test --chain-id $L2_CHAIN_ID -y
+$L2BINARYNAME tx bank send validator $ALICE_ADDR 100stake --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test --chain-id $L2_CHAIN_ID -y
 sleep 30
 
 # Check alice's balance
-BALANCE=$(${L2BINARYNAME} query bank balances $ALICE_ADDR --home $L2_KEYRING_DIR/$SEQUENCER_DIR --output json | jq -r '.balances[0].amount')
+BALANCE=$($L2BINARYNAME query bank balances $ALICE_ADDR --home $L2_KEYRING_DIR/$SEQUENCER_DIR --output json | jq -r '.balances[0].amount')
 if [ 100 -eq ${BALANCE} ]; then
     echo "send success!"
 else
@@ -28,15 +64,15 @@ else
 fi
 
 # NOTE: There is a bug in the current Ramus that only validators can execute tx.
-${L2BINARYNAME} keys add bob --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test
+$L2BINARYNAME keys add bob --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test
 sleep 1
-BOB_ADDR=$(${L2BINARYNAME} keys show bob -a --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test)
+BOB_ADDR=$($L2BINARYNAME keys show bob -a --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test)
 echo $ALICE_ADDR
-${L2BINARYNAME} tx bank send $ALICE_ADDR $BOB_ADDR 10stake --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test --chain-id $L2_CHAIN_ID -y
+$L2BINARYNAME tx bank send $ALICE_ADDR $BOB_ADDR 10stake --home $L2_KEYRING_DIR/$SEQUENCER_DIR --keyring-backend=test --chain-id $L2_CHAIN_ID -y
 sleep 30
 
 # Check alice's balance
-BALANCE=$(${L2BINARYNAME} query bank balances $BOB_ADDR --home $L2_KEYRING_DIR/$SEQUENCER_DIR --output json | jq -r '.balances[0].amount')
+BALANCE=$($L2BINARYNAME query bank balances $BOB_ADDR --home $L2_KEYRING_DIR/$SEQUENCER_DIR --output json | jq -r '.balances[0].amount')
 if [ 10 -eq ${BALANCE} ]; then
     echo "send success!"
 else
