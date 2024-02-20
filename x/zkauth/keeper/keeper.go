9acaddc6786ab14b46aee794524988d4e5b5663e
shiki <shiki.takahashi@linecorp.com>
2024-02-20 14:40:36 +0900

    feat: implement fetch jwk (#1240)
    * feat: implement fetch jwk
    
    * fix: lint error
    
    * feat: change jwt save to local and rename params to config
    
    * Update sonar configs
    
    * fix: test error
    
    * fix: try sonar
    
    * fix: apply review
    
    * fix: written more simply
    
    * fix: remove unused variable
    
    ---------
    
    Co-authored-by: Youngtaek Yoon <noreply@yoon.mailer.me>

--- x/zkauth/keeper/keeper.go
+++ x/zkauth/keeper/keeper.go
@@ -1,11 +1,17 @@
 package keeper
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/Finschia/finschia-sdk/codec"
 	storetypes "github.com/Finschia/finschia-sdk/store/types"
-	sdktypes "github.com/Finschia/finschia-sdk/types"
+	sdk "github.com/Finschia/finschia-sdk/types"
 	"github.com/Finschia/finschia-sdk/x/zkauth/types"
 	"github.com/Finschia/ostracon/libs/log"
 )
@@ -13,22 +19,106 @@ import (
 type Keeper struct {
 	cdc      codec.BinaryCodec
 	storeKey storetypes.StoreKey
-	memKey   storetypes.StoreKey
 }
 
 func NewKeeper(
 	cdc codec.BinaryCodec,
-	storeKey,
-	memKey storetypes.StoreKey,
+	storeKey storetypes.StoreKey,
 ) *Keeper {
 
 	return &Keeper{
 		cdc:      cdc,
 		storeKey: storeKey,
-		memKey:   memKey,
 	}
 }
 
-func (k Keeper) Logger(ctx sdktypes.Context) log.Logger {
+func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
+}
+
+func (k Keeper) FetchJWK(ctx sdk.Context, nodeHome string) {
+	logger := k.Logger(ctx)
+	var defaultZKAuthOAuthProviders = [1]types.OidcProvider{types.Google}
+	var fetchIntervals uint64
+	go func() {
+		for {
+			select {
+			case <-ctx.Context().Done():
+				return
+			default:
+				for _, name := range defaultZKAuthOAuthProviders {
+					provider := types.GetConfig(name)
+					fetchIntervals = provider.FetchIntervals
+
+					resp, err := k.GetJWK(provider.JwkEndpoint)
+					if err != nil {
+						time.Sleep(time.Duration(fetchIntervals) * time.Second)
+						logger.Error(fmt.Sprintf("%s", err))
+						continue
+					}
+
+					file, err := os.Create(filepath.Join(nodeHome, k.CreateJWKFileName(name)))
+					if err != nil {
+						time.Sleep(time.Duration(fetchIntervals) * time.Second)
+						logger.Error(fmt.Sprintf("%s", err))
+						continue
+					}
+
+					_, err = io.Copy(file, resp.Body)
+					if err != nil {
+						time.Sleep(time.Duration(fetchIntervals) * time.Second)
+						logger.Error(fmt.Sprintf("%s", err))
+						continue
+					}
+
+					resp.Body.Close()
+					file.Close()
+				}
+				time.Sleep(time.Duration(fetchIntervals) * time.Second)
+			}
+		}
+	}()
+}
+
+func (k Keeper) ParseJWKs(byteArray []byte) (jwks []types.JWK, err error) {
+	var data map[string]interface{}
+	err = json.Unmarshal(byteArray, &data)
+	if err != nil {
+		return jwks, err
+	}
+
+	for _, v := range data["keys"].([]interface{}) {
+		var jwk types.JWK
+
+		b, err := json.Marshal(v)
+		if err != nil {
+			return jwks, err
+		}
+
+		if err := json.Unmarshal(b, &jwk); err != nil {
+			return jwks, err
+		}
+
+		jwks = append(jwks, jwk)
+	}
+
+	return jwks, nil
+}
+
+func (k Keeper) GetJWK(endpoint string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (k Keeper) CreateJWKFileName(name types.OidcProvider) string {
+	return fmt.Sprintf("jwk-%s.json", name)
 }
--- x/zkauth/types/codec.go
+++ x/zkauth/types/codec.go
@@ -2,14 +2,24 @@ package types
 
 import (
 	"github.com/Finschia/finschia-sdk/codec"
+	"github.com/Finschia/finschia-sdk/codec/legacy"
 	cdctypes "github.com/Finschia/finschia-sdk/codec/types"
+	cryptocodec "github.com/Finschia/finschia-sdk/crypto/codec"
+	sdk "github.com/Finschia/finschia-sdk/types"
 	"github.com/Finschia/finschia-sdk/types/msgservice"
+	authzcodec "github.com/Finschia/finschia-sdk/x/authz/codec"
+	fdncodec "github.com/Finschia/finschia-sdk/x/foundation/codec"
+	govcodec "github.com/Finschia/finschia-sdk/x/gov/codec"
 )
 
-func RegisterCodec(cdc *codec.LegacyAmino) {
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	legacy.RegisterAminoMsg(cdc, &MsgExecution{}, "finschia-sdk/MsgExecution")
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgExecution{},
+	)
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
 
@@ -17,3 +27,15 @@ var (
 	Amino     = codec.NewLegacyAmino()
 	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
 )
+
+func init() {
+	RegisterLegacyAminoCodec(Amino)
+	cryptocodec.RegisterCrypto(Amino)
+	sdk.RegisterLegacyAminoCodec(Amino)
+
+	// Register all Amino interfaces and concrete types on the authz and gov Amino codec so that this can later be
+	// used to properly serialize MsgGrant, MsgExec and MsgSubmitProposal instances
+	RegisterLegacyAminoCodec(authzcodec.Amino)
+	RegisterLegacyAminoCodec(govcodec.Amino)
+	RegisterLegacyAminoCodec(fdncodec.Amino)
+}
