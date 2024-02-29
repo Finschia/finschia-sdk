a13c9a3cc9f2f56129d1854cfb7f8b4efa40d07d
jaeseung.bae <jaeseung.bae@linecorp.com>
2024-02-29 16:47:45 +0900

    chore: fix lint

--- x/bankplus/migrations/v1/types.go
+++ x/bankplus/migrations/v1/types.go
@@ -2,7 +2,7 @@ package v1
 
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
-// Keys for bankplus store but this prefix must not be overlap with bank key prefix.
+// InactiveAddrsKeyPrefix Keys for bankplus store but this prefix must not be overlap with bank key prefix.
 var InactiveAddrsKeyPrefix = []byte{0xa0}
 
 // InactiveAddrKey key of a specific inactiveAddr from store
