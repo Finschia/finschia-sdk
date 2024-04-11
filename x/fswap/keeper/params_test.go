fbea8d78022ac746d4e6e21bc7273af9d93f9398
170210 <j170210@icloud.com>
2024-04-11 14:29:06 +0900

    chore: remove unuse files
    Signed-off-by: 170210 <j170210@icloud.com>

--- x/fswap/keeper/params_test.go
+++ x/fswap/keeper/params_test.go
@@ -1,19 +1,1 @@
 package keeper_test
-
-import (
-	"testing"
-
-	testkeeper "github.com/Finschia/finschia-sdk/testutil/keeper"
-
-	"github.com/Finschia/finschia-sdk/x/fswap/types"
-	"github.com/stretchr/testify/require"
-)
-
-func TestGetParams(t *testing.T) {
-	k, ctx := testkeeper.FswapKeeper(t)
-	params := types.DefaultParams()
-
-	k.SetParams(ctx, params)
-
-	require.EqualValues(t, params, k.GetParams(ctx))
-}
