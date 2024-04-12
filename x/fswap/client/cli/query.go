d26da8481e6bd06e8c95bdf315fc2eeb5da1a6e3
170210 <j170210@icloud.com>
2024-04-12 17:45:56 +0900

    feat: add query&tx commands of fswap module
    Signed-off-by: 170210 <j170210@icloud.com>

--- x/fswap/client/cli/query.go
+++ x/fswap/client/cli/query.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/Finschia/finschia-sdk/client"
+	"github.com/Finschia/finschia-sdk/client/flags"
 	"github.com/Finschia/finschia-sdk/x/fswap/types"
 )
 
@@ -20,8 +21,59 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	cmd.AddCommand()
-	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(
+		CmdQuerySwapped(),
+		CmdQueryTotalSwappableAmount(),
+	)
+	return cmd
+}
 
+func CmdQuerySwapped() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "fswap swapped",
+		Short: "shows the current swap status, including both old and new coin amount",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Swapped(cmd.Context(), &types.QuerySwappedRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func CmdQueryTotalSwappableAmount() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "fswap total-swappable-amount",
+		Short: "shows the current total amount of new coin that're swappable",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.TotalNewCurrencySwapLimit(cmd.Context(), &types.QueryTotalSwappableAmountRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }
--- x/fswap/client/cli/tx.go
+++ x/fswap/client/cli/tx.go
@@ -7,16 +7,13 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/Finschia/finschia-sdk/client"
-	// "github.com/Finschia/finschia-sdk/client/flags"
+	"github.com/Finschia/finschia-sdk/client/flags"
+	"github.com/Finschia/finschia-sdk/client/tx"
+	sdk "github.com/Finschia/finschia-sdk/types"
 	"github.com/Finschia/finschia-sdk/x/fswap/types"
 )
 
 var DefaultRelativePacketTimeoutTimestamp = uint64((time.Duration(10) * time.Minute).Nanoseconds())
-
-// const (
-// 	flagPacketTimeoutTimestamp = "packet-timeout-timestamp"
-// 	listSeparator              = ","
-// )
 
 // GetTxCmd returns the transaction commands for this module
 func GetTxCmd() *cobra.Command {
@@ -27,8 +24,72 @@ func GetTxCmd() *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
+	cmd.AddCommand(
+		CmdTxMsgSwap(),
+		CmdTxMsgSwapAll(),
+	)
 
-	// this line is used by starport scaffolding # 1
+	return cmd
+}
 
+func CmdTxMsgSwap() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "fswap swap [address] [fnsa_amount]",
+		Short: "swap amounts of old coin to new coin. Note, the'--from' flag is ignored as it is implied from [address].",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := cmd.Flags().Set(flags.FlagFrom, args[0]); err != nil {
+				return err
+			}
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			coin, err := sdk.ParseCoinNormalized(args[2])
+			if err != nil {
+				return err
+			}
+
+			msg := &types.MsgSwapRequest{
+				FromAddress: clientCtx.GetFromAddress().String(),
+				Amount:      coin,
+			}
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func CmdTxMsgSwapAll() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "fswap swap-all [address]",
+		Short: "swap all the old coins. Note, the'--from' flag is ignored as it is implied from [address].",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := cmd.Flags().Set(flags.FlagFrom, args[0]); err != nil {
+				return err
+			}
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := &types.MsgSwapAllRequest{
+				FromAddress: clientCtx.GetFromAddress().String(),
+			}
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
 	return cmd
 }
