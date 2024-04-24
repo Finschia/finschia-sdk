115e37a608a1e5039a91f5642014d6528f77a9a7
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2024-04-24 16:29:55 +0900

    lint

--- x/fbridge/module/module.go
+++ x/fbridge/module/module.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/spf13/cobra"
-
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	sdkclient "github.com/Finschia/finschia-sdk/client"
@@ -157,7 +156,7 @@ func (am AppModule) LegacyQuerierHandler(*codec.LegacyAmino) sdk.Querier { retur
 // }
 
 //// WeightedOperations returns the all the gov module operations with their respective weights.
-//func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
+// func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
 //	return simulation.WeightedOperations(
 //		simState.AppParams, simState.Cdc, simState.TxConfig,
 //		am.accountKeeper, am.bankKeeper, am.keeper,
