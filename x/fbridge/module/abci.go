d0d5dfbd4f59f14bdd9dce4c3223487589a9b672
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2024-05-03 22:55:07 +0900

    add guardian invariant

--- x/fbridge/module/abci.go
+++ x/fbridge/module/abci.go
@@ -1,6 +1,7 @@
 package module
 
 import (
+	"fmt"
 	sdk "github.com/Finschia/finschia-sdk/types"
 	"github.com/Finschia/finschia-sdk/x/fbridge/keeper"
 	"github.com/Finschia/finschia-sdk/x/fbridge/types"
@@ -37,5 +38,31 @@ func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
 
 			k.DeleteRoleProposal(ctx, proposal.Id)
 		}
+	}
+}
+
+// RegisterInvariants registers the fbridge module invariants
+func RegisterInvariants(ir sdk.InvariantRegistry, k keeper.Keeper) {
+	ir.RegisterRoute(types.ModuleName, "guardian-invariant", GuardianInvariant(k))
+}
+
+func GuardianInvariant(k keeper.Keeper) sdk.Invariant {
+	return func(ctx sdk.Context) (string, bool) {
+
+		numGuardian := 0
+		for _, p := range k.GetRolePairs(ctx) {
+			if p.Role == types.RoleGuardian {
+				numGuardian++
+			}
+		}
+
+		numBridgeSw := len(k.GetBridgeSwitches(ctx))
+
+		broken := numGuardian != numBridgeSw
+
+		return sdk.FormatInvariant(
+			types.ModuleName, "guardian-invariant",
+			fmt.Sprintf("number of guardians(%d) != number of bridge switches(%d)", numGuardian, numBridgeSw),
+		), broken
 	}
 }
