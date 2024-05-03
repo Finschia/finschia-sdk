b00074379181aaea847c0ad3b45205c12559449f
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2024-05-03 18:29:27 +0900

    feat: add SeqToBlocknum

--- x/fbridge/client/cli/query.go
+++ x/fbridge/client/cli/query.go
@@ -1,11 +1,17 @@
 package cli
 
 import (
+	"fmt"
+	"github.com/Finschia/finschia-sdk/version"
 	"github.com/spf13/cobra"
 
 	"github.com/Finschia/finschia-sdk/client"
 	"github.com/Finschia/finschia-sdk/client/flags"
 	"github.com/Finschia/finschia-sdk/x/fbridge/types"
+)
+
+const (
+	flagSequences = "sequences"
 )
 
 // NewQueryCmd returns the query commands for fbridge module
@@ -17,6 +23,7 @@ func NewQueryCmd() *cobra.Command {
 
 	cmd.AddCommand(
 		NewQueryNextSeqSendCmd(),
+		NewQuerySeqToBlocknumsCmd(),
 	)
 
 	return cmd
@@ -24,7 +31,7 @@ func NewQueryCmd() *cobra.Command {
 
 func NewQueryNextSeqSendCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "nextseq-send",
+		Use:   "sending-next-seq",
 		Short: "Query the next sequence number for sending",
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -42,6 +49,43 @@ func NewQueryNextSeqSendCmd() *cobra.Command {
 		},
 	}
 
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func NewQuerySeqToBlocknumsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "seq-to-blocknums",
+		Short:   "Query the block number for given sequence numbers",
+		Args:    cobra.NoArgs,
+		Example: fmt.Sprintf("%s query %s sending seq-to-blocknums --sequences=1,2,3", version.AppName, types.ModuleName),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			qc := types.NewQueryClient(clientCtx)
+
+			seqSlice, err := cmd.Flags().GetInt64Slice(flagSequences)
+			if err != nil {
+				return err
+			}
+
+			seqs := make([]uint64, len(seqSlice))
+			for i, seq := range seqSlice {
+				seqs[i] = uint64(seq)
+			}
+
+			res, err := qc.SeqToBlocknums(cmd.Context(), &types.QuerySeqToBlocknumsRequest{Seqs: seqs})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	cmd.Flags().Int64Slice(flagSequences, []int64{}, "comma separated list of bridge sequnece numbers")
 	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }
--- x/fbridge/types/keys.go
+++ x/fbridge/types/keys.go
@@ -1,5 +1,7 @@
 package types
 
+import "encoding/binary"
+
 const (
 	// ModuleName is the module name constant used in many places
 	ModuleName = "fbridge"
@@ -9,6 +11,13 @@ const (
 )
 
 var (
-	KeyParams      = []byte{0x01} // key for fbridge module params
-	KeyNextSeqSend = []byte{0x02} // key for the next bridge send sequence
+	KeyParams              = []byte{0x01} // key for fbridge module params
+	KeyNextSeqSend         = []byte{0x02} // key for the next bridge send sequence
+	KeySeqToBlocknumPrefix = []byte{0x03} // key prefix for the sequence to block number mapping
 )
+
+func SeqToBlocknumKey(seq uint64) []byte {
+	bz := make([]byte, 8)
+	binary.BigEndian.PutUint64(bz, seq)
+	return append(KeySeqToBlocknumPrefix, bz...)
+}
