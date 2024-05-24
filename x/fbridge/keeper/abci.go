402f0f34804da9680ea46d37916e0ed5bcd8ffed
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2024-05-24 15:08:41 +0900

    revert abci updateRole

--- x/fbridge/keeper/abci.go
+++ x/fbridge/keeper/abci.go
@@ -34,12 +34,8 @@ func (k Keeper) EndBlocker(ctx sdk.Context) {
 		}
 
 		if types.CheckTrustLevelThreshold(k.GetRoleMetadata(ctx).Guardian, voteYes, guardianTrustLevel) || proposal.Proposer == k.GetAuthority() {
-			target := sdk.MustAccAddressFromBech32(proposal.Target)
-			if proposal.Role != k.GetRole(ctx, target) {
-				err := k.updateRole(ctx, proposal.Role, target)
-				if err != nil {
-					panic(err)
-				}
+			if err := k.updateRole(ctx, proposal.Role, sdk.MustAccAddressFromBech32(proposal.Target)); err != nil {
+				panic(err)
 			}
 
 			if err := k.deleteRoleProposal(ctx, proposal.Id); err != nil {
